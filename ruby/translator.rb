a41fb95e373d7041d8ee8dce6b2c1ce7b61b7cb2
Callum Brezden <callum.brezden@clio.com>
2024-08-29 19:59:12 -0400

    Finished version

--- ruby/translator.rb
+++ ruby/translator.rb
@@ -22,7 +22,7 @@ BRAILLE_TO_ENGLISH = {
 
   '.....O' => 'capital_follows',
   '.O.OOO' => 'number_follows',
-  '......' => ' '
+  '......' => ' ',
 }
 
 ENGLISH_TO_BRAILLE = BRAILLE_TO_ENGLISH.invert
@@ -32,12 +32,51 @@ NUMBERS = {
   'f' => '6', 'g' => '7', 'h' => '8', 'i' => '9', 'j' => '0'
 }
 
+BRAILLE_TO_NUMBERS = {
+  'O.....' => '1', 'O.O...' => '2',
+  'OO....' => '3', 'OO.O..' => '4',
+  'O..O..' => '5', 'OOO...' => '6',
+  'OOOO..' => '7', 'O.OO..' => '8',
+  '.OO...' => '9', '.OOO..' => '10',
+}
+
 def language_output(input)
   braille_detection(input) ? braille_to_english(input) : english_to_braille(input)
 end
 
 def braille_detection(input)
   input.match?(/^[O.]+$/)
+end
+
+def braille_to_english(input)
+  result = ""
+  capitalize_next = false
+  number_follows = false
+
+  input.scan(/.{1,6}/).each do |char|
+    character = BRAILLE_TO_ENGLISH[char]
+
+    if character == 'capital_follows'
+      capitalize_next = true
+    elsif character == 'number_follows'
+      number_follows = true
+    elsif character == ' '
+      result << ' '
+    else
+      if number_follows
+        letter = BRAILLE_TO_NUMBERS[char]
+      else
+        letter = BRAILLE_TO_ENGLISH[char]
+        letter = letter.upcase if capitalize_next
+      end
+
+      result << letter
+      capitalize_next = false
+      number_follows = false
+    end
+  end
+
+  result
 end
 
 def english_to_braille(input)
