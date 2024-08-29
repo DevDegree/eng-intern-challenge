59de25c6473ebd41c6b034a59fb5627dde8fa2de
Callum Brezden <callum.brezden@clio.com>
2024-08-29 19:25:32 -0400

    English to braille working

--- ruby/translator.rb
+++ ruby/translator.rb
@@ -20,8 +20,8 @@ BRAILLE_TO_ENGLISH = {
   '.OOO.O' => 'w', 'OO..OO' => 'x',
   'OO.OOO' => 'y', 'O..OOO' => 'z',
 
-  '.....0' => 'capital_follows',
-  '.0.000' => 'number_follows',
+  '.....O' => 'capital_follows',
+  '.O.OOO' => 'number_follows',
   '......' => ' '
 }
 
@@ -32,7 +32,39 @@ NUMBERS = {
   'f' => '6', 'g' => '7', 'h' => '8', 'i' => '9', 'j' => '0'
 }
 
-def language_detector(input)
-  is_braille = input.match?(/^[O.]+$/)
-  is_braille ? braille_to_english(input) : english_to_braille(input)
+def language_output(input)
+  braille_detection(input) ? braille_to_english(input) : english_to_braille(input)
 end
+
+def braille_detection(input)
+  input.match?(/^[O.]+$/)
+end
+
+def english_to_braille(input)
+  result = ""
+  number_mode = false
+
+  input.each_char do |char|
+    if char.match?(/[A-Z]/)
+      result << ENGLISH_TO_BRAILLE['capital_follows']
+      char = char.downcase
+    elsif char.match?(/[0-9]/)
+      unless number_mode
+        result << ENGLISH_TO_BRAILLE['number_follows']
+        number_mode = true
+      end
+      char = NUMBERS.key(char)
+    elsif char == ' '
+      number_mode = false
+    end
+
+    result << ENGLISH_TO_BRAILLE[char]
+  end
+
+  result
+end
+
+input = ARGV.join(' ')
+output = language_output(input)
+puts output
+puts output == ".....OO.OO..O..O..O.O.O.O.O.O.O..OO........OOO.OO..OO.O.OOO.O.O.O.OO.O.."
