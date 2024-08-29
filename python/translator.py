7e2ef21df02305cf83fec1cc52f9a951943f4441
Saad Makrod <saad.makrod@gmail.com>
2024-08-29 18:25:24 -0400

    basic scaffold of constants and functions

---
+++ python/translator.py
@@ -0,0 +1,76 @@
+ALPHABET = {
+    'a': 'O.....',
+    'b': 'O.O...',
+    'c': 'OO....',
+    'd': 'OO.O..',
+    'e': 'O..O..',
+    'f': 'OOO...',
+    'g': 'OOOO..',
+    'h': 'O.OO..',
+    'i': '.OO...',
+    'j': '.OOO..',
+    'k': 'O...O.',
+    'l': 'O.O.O.',
+    'm': 'OO..O.',
+    'n': 'OO.OO.',
+    'o': 'O..OO.',
+    'p': 'OOO.O.',
+    'q': 'OOOOO.',
+    'r': 'O.OOO.',
+    's': '.OO.O.',
+    't': '.OOOO.',
+    'u': 'O...OO',
+    'v': 'O.O.OO',
+    'w': '.OOO.O',
+    'x': 'OO..OO',
+    'y': 'OO.OOO',
+    'z': 'O..OOO'
+}
+
+PUNCTUATION = {
+    '.': '..OO.O',
+    ',': '..O...',
+    '?': '..O.OO',
+    '!': '..OOO.',
+    ':': '..OO..',
+    ';': '..O.O.',
+    '-': '....OO',
+    '/': '.O..O.',
+    '<': '.OO..O',
+    '>': 'O..OO.',
+    '(': 'O.O..O',
+    ')': '.O.OO.',
+    ' ': '......',
+}
+
+NUMBERS = {
+    '0': '.OOO..',
+    '1': 'O.....',
+    '2': 'O.O...',
+    '3': 'OO....',
+    '4': 'OO.O..',
+    '5': 'O..O..',
+    '6': 'OOO...',
+    '7': 'OOOO..',
+    '8': 'O.OO..',
+    '9': '.OO...',
+}
+
+BRAILLE_ALPHABET = dict((reversed(item) for item in ALPHABET.items()))
+BRAILLE_NUMBERS = dict((reversed(item) for item in NUMBERS.items()))
+BRAILLE_PUNCTUATION = dict((reversed(item) for item in PUNCTUATION.items()))
+
+UPPERCASE = '.....O'
+DECIMAL = '.O...O'
+NUMBER = '.O.OOO'
+
+BRAILLE = {'O', '.'}
+
+def braille_to_english():
+    pass
+
+def english_to_braille():
+    pass
+
+if __name__ == 'main':
+    pass
