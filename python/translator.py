aa1be178631603ae9e48d2ca003f3e15b495d566
EliyaFarhat <eliyahfarhat@gmail.com>
2024-08-29 18:46:04 -0400

    Built dictionary for braille translation

---
+++ python/translator.py
@@ -0,0 +1,14 @@
+import sys
+
+# Remove first argument (filename) from the list of command line arguments
+argumentList = sys.argv[1:]
+
+# Indicator for a number
+isnumber = '.O.OOO'
+# Indicator for a capital letter
+iscapital = '.....O'
+
+# Contains the braille translation to letters and numbers
+braille_translations = {
+    'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..', 'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..', 'k': 'O...O.', 'l': 'O.O.O.', 'm': 'OO..O.', 'n': 'OO.OO.', 'o': 'O..OO.', 'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.', 'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO', 'y': 'OO.OOO', 'z': 'O..OOO', '1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..', '5': 'O..O..', '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...', ' ': '......'
+}   
