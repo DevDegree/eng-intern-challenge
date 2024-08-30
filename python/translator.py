1cdedc02cfd1d429a2ca163a5df66f1719dc9407
afahimi <amin.f1000@gmail.com>
2024-08-29 20:07:28 -0700

    small fix

--- python/translator.py
+++ python/translator.py
@@ -1,16 +1,16 @@
 import sys
 
-numbers = { '0': '.OOO..', '1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..', '5': 'O..O..', '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...' }
+numbers = { "1": "O.....", "2": "O.O...", "3": "OO....", "4": "OO.O..", "5": "O..O..", "6": "OOO...", "7": "OOOO..", "8": "O.OO..", "9": ".OO...", "0": ".OOO..", }
 
 '''
 NOTE:
     The unit tests use the following mappings for the numbers, so some of the tests may fail:
     
-    { '0': 'O.....', '1': 'O.O...', '2': 'OO....', '3': 'OO.O..', '4': 'O..O..', '5': 'OOO...', '6': 'OOOO..', '7': 'O.OO..', '8': '.OO...', '9': '.OOO..' }
+    { "0": "O.....", "1": "O.O...", "2": "OO....", "3": "OO.O..", "4": "O..O..", "5": "OOO...", "6": "OOOO..", "7": "O.OO..", "8": ".OO...", "9": ".OOO..", }
 
     I have based my implementation on the Wikipedia page and the image provided in the task description, which uses the following mappings:
     
-    { '0': '.OOO..', '1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..', '5': 'O..O..', '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...' }
+    { "0": ".OOO..", "1": "O.....", "2": "O.O...", "3": "OO....", "4": "OO.O..", "5": "O..O..", "6": "OOO...", "7": "OOOO..", "8": "O.OO..", "9": ".OO...", }
 '''
 
 letters = {
