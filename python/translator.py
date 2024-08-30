da01644525121d65be4ef14f45a116eeea78dd00
MridulMehta51 <mridulmehta51@gmail.com>
2024-08-30 12:45:24 -0400

    Added basic project structure and Braille to English mapping.

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,13 @@
+# Basic Braille to English dictionary mapping
+braille_to_text = {
+    "O.....": "a", "O.O...": "b", "OO....": "c", "OO.O..": "d", "O..O..": "e",
+    "OOO...": "f", "OOOO..": "g", "O.OO..": "h", ".OO...": "i", ".OOO..": "j",
+    "O...O.": "k", "O.O.O.": "l", "OO..O.": "m", "OO.OO.": "n", "O..OO.": "o",
+    "OOO.O.": "p", "OOOOO.": "q", "O.OOO.": "r", ".OO.O.": "s", ".OOOO.": "t",
+    "O...OO": "u", "O.O.OO": "v", ".OOO.O": "w", "OO..OO": "x", "OO.OOO": "y",
+    "O..OOO": "z", ".....O": "capital", ".....O.O.....O.OOOO.O.O.....": " ",
+    ".O.OO.": "1", "..O.OO": "2", ".OO.O.": "3", ".OOOO.": "4", "O..O.O": "5",
+    "OOO.O.": "6", "OOOOO.": "7", "O.OOO.": "8", ".OO.O.": "9", ".OOOO.": "0"
+}
+
 
