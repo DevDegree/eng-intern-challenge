e73842ef8c3ca1bec5ff14b9895f469daf76b886
radha-k13 <rkotra@uwaterloo.ca>
2024-08-30 19:26:14 -0400

    Add Braille-English character constants for 'to_english' function

--- python/translator.py
+++ python/translator.py
@@ -32,7 +32,63 @@ def is_braille(string):
     #           -> add corresponding value to 'english-str'
 #       -> return 'english-str'
 
-
+def to_english(string):
+    BRAILLE_ENGLISH_LETTER_MAP = {
+        "O.....": 'a',
+        "O.O...": 'b',
+        "OO....": 'c',   
+        "OO.O..": 'd',   
+        "O..O..": 'e',  
+        "OOO...": 'f',  
+        "OOOO..": 'g',  
+        "O.OO..": 'h',  
+        ".OO...": 'i',  
+        ".OOO..": 'j',   
+        "O...O.": 'k',   
+        "O.O.O.": 'l',   
+        "OO..O.": 'm',   
+        "OO.OO.": 'n',   
+        "O..OO.": 'o',  
+        "OOO.O.": 'p',  
+        "OOOOO.": 'q',   
+        "O.OOO.": 'r',  
+        ".OO.O.": 's',  
+        ".OOOO.": 't',   
+        "O...OO": 'u', 
+        "O.O.OO": 'v',  
+        ".OOO.O": 'w',  
+        "OO..OO": 'x', 
+        "OO.OOO": 'y',  
+        "O..OOO": 'z',
+        "..OO.O": '.',
+        "..O...": ',',
+        "..O.OO": '?',
+        "..OO..": ':',
+        "..O.O.": ';',
+        "....OO": '-',
+        ".O..O.": '/',
+        ".OO..O": '<',
+        "O..OO.": '>',
+        "O.O..O": '(',
+        ".O.OO.": ')',
+        "......": ' '
+}
+    BRAILLE_ENGLISH_NUMBER_MAP = {
+        "O.....": '1',
+        "O.O...": '2',
+        "OO....": '3',
+        "OO.O..": '4',
+        "O..O..": '5',
+        "OOO...": '6',
+        "OOOO..": '7',
+        "O.OO..": '8',
+        ".OO...": '9',
+        ".OOO..": '0',
+    }
+    braille_characters = []
+    english_str = ''
+    number = False
+    
 
 # 3. If string is in english
 #       -> create dict variable 'ENGLISH-BRAILLE-MAP' that has the english characters as the keys, and braille characters as the values
