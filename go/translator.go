7fcda49c1417ec623469a9ef57670149e557ed5f
gastraderr <gaslimits@gmail.com>
2024-08-30 01:49:30 -0400

    fixing main func logic

--- go/translator.go
+++ go/translator.go
@@ -21,6 +21,9 @@ var brailleMap = map[string]string{
 	"k": "O...O.", "l": "O.O.O.", "m": "OO..O.", "n": "OO.OO.", "o": "O..OO.",
 	"p": "OOO.O.", "q": "OOOOO.", "r": "O.OOO.", "s": ".OO.O.", "t": ".OOOO.",
 	"u": "O...OO", "v": "O.O.OO", "w": ".OOO.O", "x": "OO..OO", "y": "OO.OOO", "z": "O..OOO",
+	".": "..OO.O", ",": "..O...", "?": "..O.OO", "!": "..OOO.", ":": "..OO..",
+	";": "..O.O.", "-": "....OO", "/": ".O..O.", "<": ".OO..O", ">": "O..OO.",
+	"(": "O.O..O", ")": ".O.OO.",
 }
 
 // Braille map for numbers
@@ -135,9 +138,20 @@ func main() {
 	}
 
 	input := strings.Join(os.Args[1:], " ")
-	if strings.ContainsAny(input, ".O") {
+	if isBraille(input) {
 		fmt.Println(toEnglish(input))
 	} else {
 		fmt.Println(toBraille(input))
 	}
+}
+
+// Helper function to check if the input is valid Braille
+func isBraille(input string) bool {
+	validChars := "O."
+	for _, char := range input {
+		if !strings.ContainsRune(validChars, char) {
+			return false
+		}
+	}
+	return len(input)%6 == 0
 }
