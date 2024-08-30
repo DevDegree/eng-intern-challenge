f289b5de5f083ff11370c4c40a0c3662733f77fa
nick-z-h <n.zhanghoule@gmail.com>
2024-08-30 17:54:00 -0400

    feat: handle args

--- go/translator.go
+++ go/translator.go
@@ -1,10 +1,30 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"log"
 	"os"
+	"strings"
 )
 
+var ErrMissingArguments = errors.New("missing required cli arguments")
+
+// processes arguments, joins them together, and removes leading/trailing whitespace
+func handleArguments() (string, error) {
+	if len(os.Args) < 2 {
+		return "", ErrMissingArguments
+	}
+	args := os.Args[1:]
+	return strings.TrimSpace(strings.Join(args, " ")), nil
+}
+
 func main() {
-	fmt.Println(os.Args)
+	text, err := handleArguments()
+
+	if err != nil {
+		log.Fatalf("invalid args: %s\n", err)
+	}
+
+	fmt.Println(text)
 }
