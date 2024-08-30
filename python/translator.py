174df6ea2dd4ba9697cefe465e0d2c908cc313de
ramjayakumar21 <ramjayakumar21@gmail.com>
2024-08-30 10:28:48 -0700

    inital commit with generic classes

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,31 @@
+import sys
 
+class BrailleTranslator:
+    """
+    Class for automatically detecting and applying English <-> Braille translation.
+    """
+
+    def __init__(self):
+        """
+        Initialize the MainClass with a name.
+        :param name: The name for this instance.
+        """
+
+    def greet(self):
+        """
+        Greet with the name provided.
+        """
+        print(f"Hello, {self.name}!")
+
+def main():
+
+    if len(sys.argv) != 2:
+        print("Usage: python main.py <english/braille>")
+        sys.exit(1)
+    
+    translator = BrailleTranslator()
+    
+    
+
+if __name__ == "__main__":
+    main()
