4059a035cb6e53574048e9af8d91b0b95a8937c6
Azaria <askelman5@gmail.com>
2024-08-29 23:08:39 -0400

    Filled in dictionary values, updated File Description

--- python/translator.py
+++ python/translator.py
@@ -1,26 +1,51 @@
-"""Shopify Eng Intern Challenge Fall - Winter 2025
+'''Shopify Eng Intern Challenge Fall - Winter 2025
 
 Author: Azaria Kelman
 
 File Description:
-This file allows you to translate from Enlgish to Braille and vice versa.
-"""
+A command-line application to translate from English to Braille and vice versa.
+'''
 
 english_to_braille = {  
     # This dictionary maps English (+ numbers) characters to their correspondent 
     # Braille characters
-    '1': '100000',
-    '2': '110000',
-    '3': '100100',
-    '4': '100110',
-    '5': '100010',
-    '6': '110100',
-    '7': '110110',
-    '8': '123124',
-    '9': '4412124',
-    '10': '123124',
+    '1': 'O.....',
+    '2': 'O.O...',
+    '3': 'OO....',
+    '4': 'OO.O..',
+    '5': 'O..O..',
+    '6': 'OOO...',
+    '7': 'OOOO..',
+    '8': 'O.OO..',
+    '9': '.OO...',
+    '0': '.OOO..',
+    'a': 'O.....',
+    'b': 'O.O...',
+    'c': 'OO....',
+    'd': 'OO.O..',
+    'e': 'O..O..',
+    'f': 'OOO...',
+    'g': 'OOOO..',
+    'h': 'O.OO..',
+    'i': '.OO...',
+    'j': '.OOO..',
+    'k': 'O...O.',
+    'l': 'O.O.O.',
+    'm': 'OO..O.',
+    'n': 'OO.OO.',
+    'o': 'O..OO.',
+    'p': 'OOO.O.',
+    'q': 'OOOOO.',
+    'r': 'O.OOO.',
+    's': '.OO.O.',
+    't': '.OOOO.',
+    'u': 'O...OO',
+    'v': 'O.O.OO',
+    'w': '.OOO.O',
+    'x': 'OO..OO',
+    'y': 'OO.OOO',
+    'z': 'O..OOO',
 }
-
 
 
 def translate_language () -> bool:  # Returns 1 to translate to English, 0 to translate to Braille
@@ -37,16 +62,17 @@ def translate_text_to_english () -> str:  # Returns the translated text
     # Use the dictionary to translate each section, and append to string
     pass
 
+
 def divide_string_into_sections(input_text: str) -> list:  # Returns a list of braille charachters
     sections = []
-    for i in range(0, len(s), 6):
-        section = s[i:i + 6]
+    for i in range(0, len(input_text), 6):
+        section = input_text[i:i + 6]
         sections.append(section)
     return sections
 
+
 def main():
     pass
-
 
 
 if __name__ == '__main__':
