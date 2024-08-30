fba4b3f178cc6653eb159ca87e1c59d5486ff024
Jonathan Lee <jonathan.k.lee9@gmail.com>
2024-08-30 19:53:27 -0400

    added constants

--- ruby/translator.rb
+++ ruby/translator.rb
@@ -1,9 +1,34 @@
+require_relative 'constants'
+
 def determine_if_input_is_braille_or_english()
   input_string = ARGV[0]
 
-  if input_string.include?(".")
-    return "braille"
+  if input_string.nil?
+    return puts "Please provide an input string"
+  else
+    if input_string.include?(".")
+      return BRAILLE_TYPE
+    else
+      return ENGLISH_TYPE
+    end
   end
-  return "english"
 end
 
+
+def translate_braille_to_english(braille_string)
+  result = ""
+end
+
+def main()
+  input_type = determine_if_input_is_braille_or_english()
+
+  if input_type == BRAILLE_TYPE
+    puts "hello"
+  else
+    puts "world"
+  end
+
+
+end
+
+main()
