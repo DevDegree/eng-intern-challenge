94a1f8dc4848c7b63756bc4fc2db8a71184177ab
KhushPatel2003 <khushbhavesh147@gmail.com>
2024-08-30 16:41:00 -0400

    [FIX] add tests + update readme

--- ruby/README.md
+++ ruby/README.md
@@ -1,10 +1,54 @@
-# Few notes
+# Braille Converter
 
-- The test case fails due to a mismatch between number to Braille conversion in the README and test case.
-- There were no explicit instructions on handling "decimal follows" input. I assumed that after this input, only the next character would be one of the following: `.,?!:;-/<>()`.
+### Overview
 
-# Contact info
+This Ruby application translates English text to Braille and vice versa. The application uses predefined mappings for English characters, Braille characters, and control codes (for capitalization, numbers, and punctuation).
+
+### Command Line Execution
+
+To run the application from the command line:
+
+```shell
+ruby braille_converter.rb "Your input text here"
+```
+### Examples
+
+- **English to Braille**:
+
+```shell
+>> ruby braille_converter.rb "Hello world"
+>> .....O.O.....O.O..OOO..O..O....O..O......O...O..OO...O..O......
+```
+
+- **Braille to English**:
+
+```shell
+>> ruby braille_converter.rb ".....O.O.....O.O..OOO..O..O....O..O......O...O..OO...O..O......"
+>> Hello, World!
+
+```
+
+### Run tests
+
+- Run tests by running
+
+``` shell
+>> rspec translator.test.rb 
+```
+### Code Structure
+
+- `ENGLISH_TO_BRAILLE_MAP`: Maps English letters, numbers, and punctuation to their Braille equivalents.
+- `BRAILLE_TO_ENGLISH_MAP`: Inverts the Braille map for reverse conversion.
+
+### Functions:
+
+- **`english_to_braille(string)`**: Converts an English string into its Braille equivalent.
+- **`braille_to_english(string)`**: Converts a Braille string back into English text.
+- **`check_if_input_english(user_input)`**: Determines if the input is in English or Braille based on pattern matching.
+- **`main`**: The main function that integrates input detection and conversion.
+
+### Contact info
 
 - Email: khush.patel@uwaterloo.ca
 - Phone: 4319988848
-- LinkedIn: https://www.linkedin.com/in/khush-patel-uw/ 
+- LinkedIn: https://www.linkedin.com/in/khush-patel-uw/
--- ruby/translator.test.rb
+++ ruby/translator.test.rb
@@ -3,4 +3,140 @@ RSpec.describe 'Braille Translator Script' do
     output = `ruby translator.rb Abc 123 xYz`
     expect(output.strip).to eq('.....OO.....O.O...OO...........O.OOOO.....O.O...OO..........OO..OO.....OOO.OOOO..OOO')
   end
+
+  # ******************************
+  # *  ENGLISH TO BRAILLE TESTS  *
+  # ******************************
+
+  # Number to braille tests
+  it 'number to braille test1' do
+    output = `ruby translator.rb 0`
+    expect(output.strip).to eq('.O.OOO.OOO..')
+  end
+  it 'number to braille test2' do
+    output = `ruby translator.rb 0123456789`
+    expect(output.strip).to eq('.O.OOO.OOO..O.....O.O...OO....OO.O..O..O..OOO...OOOO..O.OO...OO...')
+  end
+  it 'number to braille test3' do
+    output = `ruby translator.rb 987 654 321 0`
+    expect(output.strip).to eq('.O.OOO.OO...O.OO..OOOO.........O.OOOOOO...O..O..OO.O.........O.OOOOO....O.O...O............O.OOO.OOO..')
+  end
+  it 'number to braille test4' do
+    output = `ruby translator.rb 0 1 2 3 4 5 6 7 8 9`
+    expect(output.strip).to eq('.O.OOO.OOO.........O.OOOO............O.OOOO.O..........O.OOOOO...........O.OOOOO.O.........O.OOOO..O.........O.OOOOOO..........O.OOOOOOO.........O.OOOO.OO.........O.OOO.OO...')
+  end
+
+  # Alphabet to braille tests (lowercase)
+  it 'alphabet to braille test1' do
+    output = `ruby translator.rb a`
+    expect(output.strip).to eq('O.....')
+  end
+  it 'alphabet to braille test2' do
+    output = `ruby translator.rb abcdefghijklmnopqrstuvwxyz`
+    expect(output.strip).to eq('O.....O.O...OO....OO.O..O..O..OOO...OOOO..O.OO...OO....OOO..O...O.O.O.O.OO..O.OO.OO.O..OO.OOO.O.OOOOO.O.OOO..OO.O..OOOO.O...OOO.O.OO.OOO.OOO..OOOO.OOOO..OOO')
+  end
+  it 'alphabet to braille test3' do
+    output = `ruby translator.rb abcd efg hijk lmnop qrs tuv wxyz`
+    expect(output.strip).to eq('O.....O.O...OO....OO.O........O..O..OOO...OOOO........O.OO...OO....OOO..O...O.......O.O.O.OO..O.OO.OO.O..OO.OOO.O.......OOOOO.O.OOO..OO.O........OOOO.O...OOO.O.OO.......OOO.OOO..OOOO.OOOO..OOO')
+  end
+  it 'alphabet to braille test4' do
+    output = `ruby translator.rb a b c d e f g h i j k l m n o p q s r t u v w x y z`
+    expect(output.strip).to eq('O...........O.O.........OO..........OO.O........O..O........OOO.........OOOO........O.OO.........OO..........OOO........O...O.......O.O.O.......OO..O.......OO.OO.......O..OO.......OOO.O.......OOOOO........OO.O.......O.OOO........OOOO.......O...OO......O.O.OO.......OOO.O......OO..OO......OO.OOO......O..OOO')
+  end
+
+  # Alphabet to braille tests (uppercase)
+  it 'alphabet to braille test5' do
+    output = `ruby translator.rb A`
+    expect(output.strip).to eq('.....OO.....')
+  end
+  it 'alphabet to braille test6' do
+    output = `ruby translator.rb ABCDEFGHIJKLMNOPQRSTUVWXYZ`
+    expect(output.strip).to eq('.....OO..........OO.O........OOO.........OOO.O.......OO..O.......OOOO........OOOOO.......OO.OO.......O.OO........O.OOO.......OO...O......OO.O.O......OOO..O......OOO.OO......OO..OO......OOOO.O......OOOOOO......OO.OOO......O.OO.O......O.OOOO......OO...OO.....OO.O.OO.....O.OOO.O.....OOO..OO.....OOO.OOO.....OO..OOO')
+  end
+  it 'alphabet to braille test7' do
+    output = `ruby translator.rb ABCD EFG HIJK LMNOP QRS TUV WXYZ`
+    expect(output.strip).to eq('.....OO..........OO.O........OOO.........OOO.O.............OO..O.......OOOO........OOOOO.............OO.OO.......O.OO........O.OOO.......OO...O............OO.O.O......OOO..O......OOO.OO......OO..OO......OOOO.O............OOOOOO......OO.OOO......O.OO.O............O.OOOO......OO...OO.....OO.O.OO...........O.OOO.O.....OOO..OO.....OOO.OOO.....OO..OOO')
+  end
+  it 'alphabet to braille test8' do
+    output = `ruby translator.rb A B C D E F G H I J K L M N O P Q S R T U V W X Y Z`
+    expect(output.strip).to eq('.....OO................OO.O..............OOO...............OOO.O.............OO..O.............OOOO..............OOOOO.............OO.OO.............O.OO..............O.OOO.............OO...O............OO.O.O............OOO..O............OOO.OO............OO..OO............OOOO.O............OOOOOO............O.OO.O............OO.OOO............O.OOOO............OO...OO...........OO.O.OO...........O.OOO.O...........OOO..OO...........OOO.OOO...........OO..OOO')
+  end
+
+  # Mixed alphabet and number to braille tests
+  it 'mixed alphabet and number to braille test1' do
+    output = `ruby translator.rb abc 123 abc`
+    expect(output.strip).to eq('O.....O.O...OO...........O.OOOO.....O.O...OO..........O.....O.O...OO....')
+  end
+  it 'mixed alphabet and number to braille test2' do
+    output = `ruby translator.rb aBc 1 2 DeF 3 4`
+    expect(output.strip).to eq('O..........OO.O...OO...........O.OOOO............O.OOOO.O..............OOO.O..O..O.......OOOO..........O.OOOOO...........O.OOOOO.O..')
+  end
+
+  # ******************************
+  # *  BRAILLE TO ENGLISH TESTS  *
+  # ******************************
+
+  # Braille to number tests
+  it 'braille to number test1' do
+    output = `ruby translator.rb .O.OOO.OOO..`
+    expect(output.strip).to eq('0')
+  end
+  it 'braille to number test2' do
+    output = `ruby translator.rb .O.OOO.OOO..O.....O.O...OO....OO.O..O..O..OOO...OOOO..O.OO...OO...`
+    expect(output.strip).to eq('0123456789')
+  end
+  it 'braille to number test3' do
+    output = `ruby translator.rb .O.OOO.OO...O.OO..OOOO.........O.OOOOOO...O..O..OO.O.........O.OOOOO....O.O...O............O.OOO.OOO..`
+    expect(output.strip).to eq('987 654 321 0')
+  end
+  it 'braille to number test4' do
+    output = `ruby translator.rb .O.OOO.OOO.........O.OOOO............O.OOOO.O..........O.OOOOO...........O.OOOOO.O.........O.OOOO..O.........O.OOOOOO..........O.OOOOOOO.........O.OOOO.OO.........O.OOO.OO...`
+    expect(output.strip).to eq('0 1 2 3 4 5 6 7 8 9')
+  end
+
+  # Braille to alphabet tests (lowercase)
+  it 'braille to alphabet test1' do
+    output = `ruby translator.rb O.....`
+    expect(output.strip).to eq('a')
+  end
+  it 'braille to alphabet test2' do
+    output = `ruby translator.rb O.....O.O...OO....OO.O..O..O..OOO...OOOO..O.OO...OO....OOO..O...O.O.O.O.OO..O.OO.OO.O..OO.OOO.O.OOOOO.O.OOO..OO.O..OOOO.O...OOO.O.OO.OOO.OOO..OOOO.OOOO..OOO`
+    expect(output.strip).to eq('abcdefghijklmnopqrstuvwxyz')
+  end
+  it 'braille to alphabet test3' do
+    output = `ruby translator.rb O.....O.O...OO....OO.O........O..O..OOO...OOOO........O.OO...OO....OOO..O...O.......O.O.O.OO..O.OO.OO.O..OO.OOO.O.......OOOOO.O.OOO..OO.O........OOOO.O...OOO.O.OO.......OOO.OOO..OOOO.OOOO..OOO`
+    expect(output.strip).to eq('abcd efg hijk lmnop qrs tuv wxyz')
+  end
+  it 'braille to alphabet test4' do
+    output = `ruby translator.rb O...........O.O.........OO..........OO.O........O..O........OOO.........OOOO........O.OO.........OO..........OOO........O...O.......O.O.O.......OO..O.......OO.OO.......O..OO.......OOO.O.......OOOOO........OO.O.......O.OOO........OOOO.......O...OO......O.O.OO.......OOO.O......OO..OO......OO.OOO......O..OOO`
+    expect(output.strip).to eq('a b c d e f g h i j k l m n o p q s r t u v w x y z')
+  end
+
+  # Braille to alphabet tests (uppercase)
+  it 'braille to uppercase alphabet test1' do
+    output = `ruby translator.rb .....OO.....`
+    expect(output.strip).to eq('A')
+  end
+  it 'braille to uppercase alphabet test2' do
+    output = `ruby translator.rb .....OO..........OO.O........OOO.........OOO.O.......OO..O.......OOOO........OOOOO.......OO.OO.......O.OO........O.OOO.......OO...O......OO.O.O......OOO..O......OOO.OO......OO..OO......OOOO.O......OOOOOO......OO.OOO......O.OO.O......O.OOOO......OO...OO.....OO.O.OO.....O.OOO.O.....OOO..OO.....OOO.OOO.....OO..OOO`
+    expect(output.strip).to eq('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+  end
+  it 'braille to uppercase alphabet test3' do
+    output = `ruby translator.rb .....OO..........OO.O........OOO.........OOO.O.............OO..O.......OOOO........OOOOO.............OO.OO.......O.OO........O.OOO.......OO...O............OO.O.O......OOO..O......OOO.OO......OO..OO......OOOO.O............OOOOOO......OO.OOO......O.OO.O............O.OOOO......OO...OO.....OO.O.OO...........O.OOO.O.....OOO..OO.....OOO.OOO.....OO..OOO`
+    expect(output.strip).to eq('ABCD EFG HIJK LMNOP QRS TUV WXYZ')
+  end
+  it 'braille to uppercase alphabet test4' do
+    output = `ruby translator.rb .....OO................OO.O..............OOO...............OOO.O.............OO..O.............OOOO..............OOOOO.............OO.OO.............O.OO..............O.OOO.............OO...O............OO.O.O............OOO..O............OOO.OO............OO..OO............OOOO.O............OOOOOO............O.OO.O............OO.OOO............O.OOOO............OO...OO...........OO.O.OO...........O.OOO.O...........OOO..OO...........OOO.OOO...........OO..OOO`
+    expect(output.strip).to eq('A B C D E F G H I J K L M N O P Q S R T U V W X Y Z')
+  end
+
+  # Braille to mixed alphabet and number tests
+  it 'braille to mixed alphabet and number test1' do
+    output = `ruby translator.rb O.....O.O...OO...........O.OOOO.....O.O...OO..........O.....O.O...OO....`
+    expect(output.strip).to eq('abc 123 abc')
+  end
+  it 'braille to mixed alphabet and number test2' do
+    output = `ruby translator.rb O..........OO.O...OO...........O.OOOO............O.OOOO.O..............OOO.O..O..O.......OOOO..........O.OOOOO...........O.OOOOO.O..`
+    expect(output.strip).to eq('aBc 1 2 DeF 3 4')
+  end
 end
