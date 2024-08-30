68c77c51607b01e5c86577d246de71679f87ab12
Azaria <askelman5@gmail.com>
2024-08-29 22:08:40 -0400

    Created file structure

---
+++ python/translator.py
@@ -0,0 +1,26 @@
+"""Shopify Eng Intern Challenge Fall - Winter 2025
+
+Author: Azaria Kelman
+
+File Description:
+This file allows you to translate from Enlgish to Braille and vice versa.
+"""
+
+
+
+def translate_language () -> bool:  # Returns the language to translate to
+    pass
+
+def translate_text_to_english () -> str  # Returns the translated text
+    pass
+
+def translate_text_to_braille () -> str:  # Returns the translated text
+    pass
+
+def main():
+    pass
+
+
+
+if __name__ == '__main__':
+    main()
