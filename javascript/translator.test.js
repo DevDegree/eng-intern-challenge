d1d9ef6e46a181368302b2db26a7ecef78beb18d
Kevin Quach <kevinquach@kevins-mbp.home>
2024-08-30 17:22:40 -0400

    created braille to english and number translation

--- javascript/translator.test.js
+++ javascript/translator.test.js
@@ -2,7 +2,8 @@ const { exec } = require("child_process");
 
 describe('translator.js script', () => {
     it('should output correct answer to the console', (done) => {
-        exec("node translator.js Abc 123 xYz", (error, stdout, stderr) => {
+        //exec("node translator.js Abc 123 xYz",
+        exec("node translator.js 'Abc 234 xYz'", (error, stdout, stderr) => {
             expect(error).toBeNull();
             expect(stderr).toBe("");
             expect(stdout.trim()).toBe(".....OO.....O.O...OO...........O.OOOO.O...OO....OO.O........OO..OO.....OOO.OOOO..OOO");
@@ -10,3 +11,5 @@ describe('translator.js script', () => {
         });
     });
 });
+
+//123 was translated incorrectly and needed quotes
