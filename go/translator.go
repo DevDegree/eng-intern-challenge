89adb8a6be8462f56a75fdf903705a273e9200cb
nick-z-h <n.zhanghoule@gmail.com>
2024-08-30 17:56:39 -0400

    feat: create translator constructor

--- go/translator.go
+++ go/translator.go
@@ -10,6 +10,62 @@ import (
 
 var ErrMissingArguments = errors.New("missing required cli arguments")
 
+type Translator struct {
+	text               string
+	letterToBrailleMap map[string]string
+	numberToBrailleMap map[string]string
+}
+
+// creates new translator unit
+func NewTranslator(text string) *Translator {
+	var t Translator
+	t.text = text
+	t.letterToBrailleMap = map[string]string{
+		"CAPITAL_FOLLOWS": ".....O",
+		"NUMBER_FOLLOWS":  ".O.OOO",
+		"a":               "O.....",
+		"b":               "O.O...",
+		"c":               "OO....",
+		"d":               "OO.O..",
+		"e":               "O..O..",
+		"f":               "OOO...",
+		"g":               "OOOO..",
+		"h":               "O.OO..",
+		"i":               ".OO...",
+		"j":               ".OOO..",
+		"k":               "O...O.",
+		"l":               "O.O.O.",
+		"m":               "OO..O.",
+		"n":               "OO.OO.",
+		"o":               "O..OO.",
+		"p":               "OOO.O.",
+		"q":               "OOOOO.",
+		"r":               "O.OOO.",
+		"s":               ".OO.O.",
+		"t":               ".OOOO.",
+		"u":               "O...OO",
+		"v":               "O.O.OO",
+		"w":               ".OOO.O",
+		"x":               "OO..OO",
+		"y":               "OO.OOO",
+		"z":               "O..OOO",
+		" ":               "......",
+	}
+	t.numberToBrailleMap = map[string]string{
+		"0": ".OOO..",
+		"1": "O.....",
+		"2": "O.O...",
+		"3": "OO....",
+		"4": "OO.O..",
+		"5": "O..O..",
+		"6": "OOO...",
+		"7": "OOOO..",
+		"8": "O.OO..",
+		"9": ".OO...",
+	}
+	return &t
+}
+
 // processes arguments, joins them together, and removes leading/trailing whitespace
 func handleArguments() (string, error) {
 	if len(os.Args) < 2 {
