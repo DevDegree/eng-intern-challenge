19a59cd90f06705d2dd844ffc2143768339c1e86
Aaron -victus <aaronforsocial@gmail.com>
2024-08-30 00:08:04 -0600

    braille to english implemented

--- python/translator.py
+++ python/translator.py
@@ -1,3 +1,5 @@
+import sys
+
 # lets first map all the alphabets to their respective braille codes
 braille_mapping = {
     'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..', 
@@ -13,3 +15,46 @@ braille_mapping = {
     '(': '.O.OO.', ')': '.O.OOO' 
 }
 
+
+def braille_to_english(braille):
+    out = []
+    i = 0
+    number = False
+    
+    while i < len(braille):
+        symbol = braille[i:i+6]
+        if symbol == braille_mapping['capital']:
+            i += 6 
+            next_symbol = braille[i:i+6]
+            for key, value in braille_mapping.items():
+                if value == next_symbol:
+                    char = key.upper() 
+                    out.append(char)
+                    break
+        elif symbol == braille_mapping['number']:
+            number = True 
+        elif symbol in braille_mapping.values():
+            for key, value in braille_mapping.items():
+                if value == symbol:
+                    char = key
+                    if number:
+                        out.append(char)
+                        number = False if char == ' ' else number
+                    else:
+                        out.append(char)
+                    break
+        
+        i += 6
+    return ''.join(out)
+
+
+def main():
+    input_str = sys.argv[1]
+
+    if all(c in 'Oo.' for c in input_str):
+        print(braille_to_english(input_str))
+    else: 
+        pass
+
+if __name__ == "__main__":
+    main()
