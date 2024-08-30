93463b6a68483367e1420db56bac48af5273abbb
Hematullah Karimzada <hematullahkarimzada7@gmail.com>
2024-08-30 02:36:43 -0400

    Final and Added Comments

--- python/translator.py
+++ python/translator.py
@@ -1,5 +1,6 @@
 import sys
 
+# Making dictioniary for chars to values
 eng_to_b = {
     'a':'O.....', 'b':'O.O...', 'c':'OO....', 'd':'OO.O..', 'e':'O..O..', 'f':'OOO...', 'g':'OOOO..', 'h':'O.OO..',
     'i':'.OO...', 'j':'.OOO..', 'k':'O...O.', 'l':'O.O.O.', 'm':'OO..O.', 'n':'OO.OO.', 'o':'O..OO.', 'p':'OOO.O.',
@@ -10,6 +11,8 @@ eng_to_b = {
     '>':'.OO..O', '(':'O.O..O', ')':'.O.OO.', ' ':'......'
 }
 
+# Adding values from the eng_to_b dictioniary and any chars with the same braille text will be appending in the order of letter then number
+
 b_to_eng ={}
 for key, value in eng_to_b.items():
     if value in b_to_eng:
@@ -17,32 +20,35 @@ for key, value in eng_to_b.items():
     else:
         b_to_eng[value] = [key]
 
+# Checking if input is eiher in braille or english text.
+
 def is_text_braille(text):
     for char in text:
         if char not in 'O.':
             return False
-    return True
+    return True # If all charcters in the input are O or . it will be in braille so return true
+
 
 def english_to_braille(text):
     res = []
     is_number = False
 
     for char in text:
-        if char.isalpha():
+        if char.isalpha(): # Checks if the current character is a letter
             is_number = False
-            if char.isupper():
-                res.append(eng_to_b['cap'])
+            if char.isupper(): # Checks if the current character is a capital
+                res.append(eng_to_b['cap']) # Appends the braille text for capital letter
                 char = char.lower()
             res.append(eng_to_b[char])
-        elif char.isdigit():
+        elif char.isdigit(): # Checks if the character is a number
             if is_number == False:
                 is_number = True
-                res.append(eng_to_b['num'])
+                res.append(eng_to_b['num']) # Sends the braille text that the next character is a number
             res.append(eng_to_b[char])
         else:
-            res.append(eng_to_b[char])
+            res.append(eng_to_b[char]) # Anything that is not a number or letter can be sent (there is no complications with these)
 
-    return ''.join(res)
+    return ''.join(res) # Puts everything in one text
 
 def braille_to_english(braille):
     res = []
@@ -50,34 +56,35 @@ def braille_to_english(braille):
     is_number = False
 
     while i < len(braille):
-        brailleChar = braille[i:i+6]
+        brailleChar = braille[i:i+6] # Grabs the first 6 available braille characters and puts them in a variable 
         
         if brailleChar == eng_to_b['cap']:
             nextChar = braille[i+6:i+12]
-            res.append(b_to_eng[nextChar][0].upper())
+            res.append(b_to_eng[nextChar][0].upper()) # If the current braille value is a capital value then it sends the next brailler value as a capital letter
             i += 12
-        elif brailleChar == eng_to_b['num']:
+        elif brailleChar == eng_to_b['num']: # Lets the code know that the next character is going to be a number
             is_number = True
             i += 6
         elif brailleChar == "......":
             res.append(' ')
-            is_number = False
+            is_number = False # in the case the current value is a number and it ends to send a letter, this lets the user know the next chacrter is either a new number or letter
             i += 6
         else:
-            nextChar = b_to_eng[brailleChar]
+            nextChar = b_to_eng[brailleChar] # takes the value of the the current 6 braille characters
             if is_number:
-                res.append(nextChar[1])
+                res.append(nextChar[1]) # sends the 2nd value in the array which was previously set for only numbers ( check b_to_eng comment line 14)
             else:
                 is_number = False
-                res.append(nextChar[0])
+                res.append(nextChar[0]) # sends the 1st value in the array which was previously set for only letters ( check b_to_eng comment line 14)
             i += 6
     
-    return ''.join(res)
+    return ''.join(res) # sends the new english text to user
 
 
 def main():
-    inputString = " ".join(sys.argv[1:])
+    inputString = " ".join(sys.argv[1:]) # takes input from terminal
 
+    # Checks if the input is in braille or english and uses its respective function for conversion 
     if is_text_braille(inputString):
         print(braille_to_english(inputString))
     else:
