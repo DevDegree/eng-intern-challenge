3ed0fde5e61d82323b7ef2c0792f59b213a64edb
Saadmanswork <saadman.rahman.w@gmail.com>
2024-08-30 13:29:07 -0600

    hashmaps made

--- ruby/translator.rb
+++ ruby/translator.rb
@@ -1,3 +1,64 @@
+# make the hashmap to be used for the translation
+# Since there is just a fixed amount of characters, hardcoding the hashmap is fine as it will take constant time to access the value of a key
+
+letters = {
+    "a" => "O.....",
+    "b" => "O.O...",
+    "c" => "OO....",
+    "d" => "OO.O..",
+    "e" => "O..O..",
+    "f" => "OOO...",
+    "g" => "OOOO..",
+    "h" => "O.OO..",
+    "i" => ".OO...",
+    "j" => ".OOO..",
+    "k" => "O...O.",
+    "l" => "O.O.O.",
+    "m" => "OO..O.",
+    "n" => "OO.OO.",
+    "o" => "O..OO.",
+    "p" => "OOO.O.",
+    "q" => "OOOOO.",
+    "r" => "O.OOO.",
+    "s" => ".OO.O.",
+    "t" => ".OOOO.",
+    "u" => "O...OO",
+    "v" => "O.O.OO",
+    "w" => ".OOO.O",
+    "x" => "OO..OO",
+    "y" => "OO.OOO",
+    "z" => "O..OOO",
+}
+
+non_letters = {
+    "." => "..OO.O",
+    "," => "..O...",
+    "?" => "..O.OO",
+    "!" => "..OOO.",
+    ":" => "..OO..",
+    ";" => "..O.O.",
+    "-" => "....OO",
+    "/" => ".O..O.",
+    "<" => ".OO..O",
+    ">" => "O..OO.",
+    "(" => "O.O..O",
+    ")" => ".O.OO.",
+    " " => "......",
+}
+
+numbers = {
+    "1" => "O.....",
+    "2" => "O.O...",
+    "3" => "OO....",
+    "4" => "OO.O..",
+    "5" => "O..O..",
+    "6" => "OOO...",
+    "7" => "OOOO..",
+    "8" => "O.OO..",
+    "9" => ".OO...",
+    "0" => ".OOO..",
+}
+
 # take the input, assuming the full input needs to be translated
 word = ARGV.join(" ")
 
