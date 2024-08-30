bbf8e70b9c1b012d6ecccd654236d8fb76c6883c
Ian Yu <zhiyaoyu19@gmail.com>
2024-08-30 19:58:22 -0400

    Implemented function to check whether user message is in English or Braille

--- python/translator.py
+++ python/translator.py
@@ -1,5 +1,12 @@
 import sys
 
+def isEnglish(message: str) -> bool:
+    for char in message:
+        if (char != 'O' or char != '.'):
+            return False
+    
+    return True
+    
 if __name__ == "__main__":
     message = sys.argv[1]
     print(message)
