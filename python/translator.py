a65c0b30154330a8a17445b364b82b5f9d472ab1
Tudor Barsan <tudor.barsan46@gmail.com>
2024-08-30 11:52:19 -0400

    read input and store text string and unique chars

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,14 @@
 
+if __name__ == '__main__':
+    import sys
+    args = sys.argv
+    args.pop(0)
+
+    # Get the input text as a string
+    text = ' '.join(args)
+    # Get the unique characters in the input
+    characters = set(text)
+
+    
+
+
