241450f2c90982d04aaf368e6ef909b8dfd807e0
Alex Balan <cs.k.alexanderbalan@gmail.com>
2024-08-30 10:25:04 -0400

    Create alphabet and rules dictionaries

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,24 @@
+import sys
 
+
+class Translator:
+    BRAILLE_ALPHABET = {
+        'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..', 'f': 'OOO...', 'g': 'OOOO..',
+        'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..', 'k': 'O...O.', 'l': 'O.O.O.', 'm': 'OO..O.', 'n': 'OO.OO.',
+        'o': 'O..OO.', 'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.', 'u': 'O...OO',
+        'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO', 'y': 'OO.OOO', 'z': 'O..OOO', '1': 'O.....', '2': 'O.O...',
+        '3': 'OO....', '4': 'OO.O..', '5': 'O..O..', '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...',
+        ' ': '......'
+    }
+    CAPITAL_FOLLOWS = '.....O'
+    NUMBER_FOLLOWS = '.O.OOO'
+
+
+def main():
+    args = sys.argv[1:]  # Skip the first argument (script name)
+    translator = Translator
+    print(translator.CAPITAL_FOLLOWS)
+
+
+if __name__ == '__main__':
+    main()
