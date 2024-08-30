ed34a2a9bd5231d484968c4392ad901d00ec560d
Jatin Mehta <mehta.r.jatin@gmail.com>
2024-08-30 17:33:16 -0400

    init ruby braille

---
+++ ruby/braille_to_visual.rb
@@ -0,0 +1,61 @@
+def braille_to_visual(string, lang)
+
+  if lang == "english"
+    require_relative "data/english"
+  else 
+    raise "Unknown language: #{lang}"
+  end
+
+  number_mode = false
+  capital_left = 0
+
+  num_segments = (string.length / 6) - 1
+
+  output = ""
+
+  special_inv = SPECIAL.invert
+  letters_inv = LETTERS.invert
+  nums_inv = NUMS.invert
+
+  for i in 0..num_segments
+      segment = "#{string[i*6, 6]}"
+      capital_left = [0, capital_left - 1].max
+
+      
+    if special_inv.has_key?(segment)
+      if special_inv[segment] == ' '
+          number_mode = false
+          output.concat(' ')
+
+      elsif special_inv[segment] == 'number'
+          number_mode = true
+
+      elsif special_inv[segment] == 'capital'
+          capital_left = 2
+      end
+
+      elsif number_mode
+          if nums_inv.has_key?(segment)
+              output.concat(nums_inv[segment])
+              
+          else
+              raise "Expected number, got '#{segment}'. Translated so far: '#{output}'"
+          end
+
+      elsif capital_left == 1
+          if letters_inv.has_key?(segment)
+              output.concat(letters_inv[segment].upcase)
+
+          else
+              raise "Expected letter to capitalize, got '#{segment}'. Translated so far: '#{output}'"
+          end
+      elsif letters_inv.has_key?(segment)
+          output.concat(letters_inv[segment])
+
+      else
+          raise "Translated so far '#{output}', but encountered unexpected segment '#{segment}'"
+      end
+  end
+
+  return output
+end
---
+++ ruby/visual_to_braille.rb
@@ -0,0 +1,43 @@
+def visual_to_braille(string, lang)
+
+  if lang == "english"
+    require_relative "data/english"
+  else 
+    raise "Unknown language: #{lang}"
+  end
+
+  output = ""
+  number_mode = false
+
+  string.each_char do |char|
+      if LETTERS.has_key?(char)
+          if number_mode
+              raise "In number mode but attempting to translate letter"
+          end
+          output.concat(LETTERS[char])
+          
+      elsif LETTERS.has_key?(char.downcase)
+          if number_mode
+              raise "In number mode but attempting to translate letter"
+          end
+          output.concat(SPECIAL['capital'])
+          output.concat(LETTERS[char.downcase])
+
+      elsif NUMS.has_key?(char)
+          if !number_mode
+              number_mode = true
+              output.concat(SPECIAL['number'])
+          end
+
+          output.concat(NUMS[char])
+          
+      elsif char == ' '
+          number_mode = false
+          output.concat(SPECIAL[char])
+      else
+          raise "Unexpected character received: '#{char}'"
+     end
+  end
+
+  return output
+end
