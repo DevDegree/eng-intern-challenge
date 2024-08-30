a6361205856636d289cdd28dbda841303918271d
Azaria <askelman5@gmail.com>
2024-08-29 22:28:37 -0400

    Added translate_language()

--- python/translator.py
+++ python/translator.py
@@ -6,10 +6,25 @@ File Description:
 This file allows you to translate from Enlgish to Braille and vice versa.
 """
 
+english_to_braille = {  
+    # This dictionary maps English (+ numbers) characters to their correspondent 
+    # Braille characters
+    '1': '100000',
+    '2': '110000',
+    '3': '100100',
+    '4': '100110',
+    '5': '100010',
+    '6': '110100',
+    '7': '110110',
+    '8': '123124',
+    '9': '4412124,
+    '10': '123124',
+}
 
-
-def translate_language () -> bool:  # Returns the language to translate to
-    pass
+def translate_language () -> bool:  # Returns 1 to translate to English, 0 to translate to Braille
+    for char in input_text:
+        if char not in ['.', 'O']:
+            return 0
 
 def translate_text_to_english () -> str  # Returns the translated text
     pass
