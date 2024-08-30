3a19114908ee5ddc4ab245d35fc7e9beeaf8a0ba
Aaron -victus <aaronforsocial@gmail.com>
2024-08-29 22:08:34 -0600

    map braille to alphabets

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,15 @@
-# test
+# lets first map all the alphabets to their respective braille codes
+braille_mapping = {
+    'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..', 
+    'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..',
+    'k': 'O...O.', 'l': 'O.O.O.', 'm': 'OO..O.', 'n': 'OO.OO.', 'o': 'O..OO.',
+    'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.',
+    'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO', 'y': 'OO.OOO', 
+    'z': 'O..OOO',
+    'capital': '.....O',
+    'number': '.O.OOO',
+    ' ': '......',
+    '1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..', '5': 'O..O..',
+    '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...', '0': '.OOO..'
+}
+
