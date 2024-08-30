ee64f173562b06c4913dd19d6a993fc96bba7da1
tomfucodes <tsa127@sfu.ca>
2024-08-30 09:30:54 -0700

    Update translator.py

--- python/translator.py
+++ python/translator.py
@@ -2,7 +2,7 @@ import sys
 
 # Defining Braille to English dictionary
 braille_to_english = {
-    'O.....': 'a', 'O.O...': 'b', 'OO....': 'c', 'OO.O..': 'd', 'O..O..': 'e',
+    '.....O': 'a', 'O.....': 'b', 'OO....': 'c', 'OO.O..': 'd', 'O..O..': 'e',
     'OOO...': 'f', 'OOOO..': 'g', 'O.OO..': 'h', '.OO...': 'i', '.OOO..': 'j',
     'O...O.': 'k', 'O.O.O.': 'l', 'OO..O.': 'm', 'OO.OO.': 'n', 'O..OO.': 'o',
     'OOO.O.': 'p', 'OOOOO.': 'q', 'O.OOO.': 'r', '.OO.O.': 's', '.OOOO.': 't',
@@ -12,13 +12,16 @@ braille_to_english = {
 }
 
 # Defining English to Braille dictionary
-english_to_braille = {x: y for y, x in braille_to_english.items() if x not in ['capital', 'number']}
-english_to_braille.update({
-    'A': '.....O' + 'O.....', 'B': '.....O' + 'O.O...', 'C': '.....O' + 'OO....',
+english_to_braille = {
+    'a': '.....O', 'b': 'O.....', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..',
+    'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..',
+    'k': 'O...O.', 'l': 'O.O.O.', 'm': 'OO..O.', 'n': 'OO.OO.', 'o': 'O..OO.',
+    'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.',
+    'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO', 'y': 'OO.OOO',
+    'z': 'O..OOO', ' ': '......', '.': '.....O.O...',
     '1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..', '5': 'O..O..',
-    '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...', '0': '.OOO..',
-    ' ': '......', '.': '.....O.O...'
-})
+    '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...', '0': '.OOO..'
+}
 
 # Function to translate Braille to English
 def braille_to_text(braille_input):
@@ -63,7 +66,7 @@ def text_to_braille(text_input):
 
     for char in text_input:
         if char.isupper():
-            br_output += '.....O'  # Capital
+            br_output += '.....O'  # Capital symbol
             char = char.lower()
         elif char.isdigit():
             if not number_mode:
@@ -85,7 +88,7 @@ def main():
         return
     
     mode = sys.argv[1]
-    input_text = " ".join(sys.argv[2:])
+    input_text = sys.argv[1:]
     
     if mode == "text-to-braille":
         output = text_to_braille(input_text)
