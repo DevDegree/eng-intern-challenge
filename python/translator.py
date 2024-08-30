a597cf5ae2e8ab38b17b75f8cbbc3dc30401699b
Alexandre Payumo <alexpayumo@hotmail.com>
2024-08-30 19:44:47 -0400

    added comments

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,4 @@
-
+#first step is to determine whether the String is Braille or English
+#can do this by scanning for letters other than . or 0
+#also, if it is Braille, then we need to validate that the input is divisible by 6
+#will need to create 2 hashmaps. one will map english to braille, the other will map braille to english.
