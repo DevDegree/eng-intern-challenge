8d7beb8f43840a9535c19d9652d3c4d6f4505718
Anmol Tyagi <anmolty2@gmail.com>
2024-08-30 03:06:01 -0400

    fix: add testcase clarification

--- ruby/translator.test.rb
+++ ruby/translator.test.rb
@@ -1,6 +1,6 @@
 RSpec.describe 'Braille Translator Script' do
   it 'outputs the correct string' do
-    output = `ruby translator.rb Abc 123 xYz`
+    output = `ruby translator.rb Abc 123 xYz` # 'ruby translator.rb Abc 234 xYz' should be the corrected command for the given output
     expect(output.strip).to eq('.....OO.....O.O...OO...........O.OOOO.O...OO....OO.O........OO..OO.....OOO.OOOO..OOO')
   end
 end
