8615f18d909b4c33b5ba2b9267da1516a8146c14
AyhaamB <ayh132@gmail.com>
2024-08-30 17:42:15 -0400

    Javascript Braille Translator Ayhaam B

--- javascript/translator.test.js
+++ javascript/translator.test.js
@@ -2,7 +2,7 @@ const { exec } = require("child_process");
 
 describe('translator.js script', () => {
     it('should output correct answer to the console', (done) => {
-        exec("node translator.js 'Abc 234 xYz'", (error, stdout, stderr) => {
+        exec("node translator.js Abc 123 xYz", (error, stdout, stderr) => {
             expect(error).toBeNull();
             expect(stderr).toBe("");
             expect(stdout.trim()).toBe(".....OO.....O.O...OO...........O.OOOO.....O.O...OO..........OO..OO.....OOO.OOOO..OOO");
