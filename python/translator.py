721d4c981f352df2e15dd5552eb508ecb34afb79
Arafat Olayiwola <harof.dev@gmail.com>
2024-08-31 11:10:00 +0100

    add braille alphabets map'

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,23 @@
 # Python version 3.8
+import sys
+
+# Braille encoding for English letters and numbers
+braille_alphabet = {
+    'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..',
+    'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..',
+    'k': 'O...O.', 'l': 'O.O.O.', 'm': 'OO..O.', 'n': 'OO.OO.', 'o': 'O..OO.',
+    'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.',
+    'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO', 'y': 'OO.OOO',
+    'z': 'O..OOO', ' ': '......', 'cap': '.....O', 'num': '.O.OOO',
+    '1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..', '5': 'O..O..',
+    '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...', '0': '.OOO..'
+}
+
+inverse_braille_alphabet = {
+    v: k for k, v in braille_alphabet.items()
+}
+
+def is_braille(input_str: str):
+    return all(char in 'O.' for char in input_str)
+
+
