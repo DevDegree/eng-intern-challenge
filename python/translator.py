e840e4ae1541e7d9cefbf368ac329e8a905f01a0
Samudra Perera <samudrapup@gmail.com>
2024-08-31 16:04:14 -0400

    Added translate_to_english function

--- python/translator.py
+++ python/translator.py
@@ -20,6 +20,7 @@ BRAILLE_TO_NUMBERS = {
 
 NUMBERS_TO_BRAILLE = {v: k for k, v in BRAILLE_TO_NUMBERS.items()}
 
+# Special Case characters
 CAPITALIZE = '.....O'
 
 NUMBER_FOLLOWS = '.O.OOO'
@@ -43,12 +44,54 @@ def is_braille(input_string):
     return valid_chars and valid_length    
 
 def translate_to_english(input_string):
-    print('Its braille')
-    pass
+    '''
+    Function that takes in a valid braille input string and translates it to english
 
-def translate_to_braille(input_string):
-    print('Its english')
-    pass
+    Args:
+    input_string: string to translate to english
+
+    Returns:
+    output_string: translated string
+    '''
+    translated_chars = []
+    number_follows = False
+    capitalize_next = False
+
+    for i in range(0, len(input_string), 6):
+        braille_char = input_string[i:i+6]
+
+        #Toggle switches for number or capitalize
+        if braille_char == CAPITALIZE:
+            capitalize_next = True
+            continue
+        elif braille_char == NUMBER_FOLLOWS:
+            number_follows = True
+            continue
+
+        #Determine the translated character based on number toggle (ie 'j' or 1)
+        if number_follows:
+            translated_character = BRAILLE_TO_NUMBERS[braille_char]
+        else:
+            translated_character = BRAILLE_TO_ENGLISH[braille_char]
+
+        #if the character is a letter, toggle capitalize is on capitalize it
+        if capitalize_next and translated_character.isalpha():
+            translated_character = translated_character.upper()
+            capitalize_next = False
+        
+        translated_chars.append(translated_character)
+
+        #if a space is found, reset the number toggle
+        if translated_character == ' ':
+            number_follows = false
+
+    return ''.join(translated_chars)
+            
+
+
+    def translate_to_braille(input_string):
+        print('Its english')
+        pass
 
 
 
@@ -59,7 +102,7 @@ def main():
     input_string = ' '.join(sys.argv[1:]) 
 
     if is_braille(input_string):
-        translate_to_english(input_string)
+        print(translate_to_english(input_string))
     else:
         translate_to_braille(input_string)
 
