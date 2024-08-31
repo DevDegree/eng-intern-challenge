5fc1daab4dd0f7740b6ca30cf5ee83ba82a657ff
Mayank808 <mayankmehra628@gmail.com>
2024-08-30 22:07:29 -0400

    feat: added basic exception handling and errors

--- python/translator.py
+++ python/translator.py
@@ -3,6 +3,12 @@ import argparse
 from enum import Enum
 from collections import Counter
 
+# Exceptions
+class TranslationError(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
 # Enums
 class SpecialBrailleChar(Enum):
     CAPITAL = ".....O"
@@ -28,10 +34,10 @@ english_numbers_to_braille = {
 braille_to_english = {value : key for key, value in english_to_braille.items()}
 braille_to_english_numbers = {value : key for key, value in english_numbers_to_braille.items()}
 
+# Helper Functions
 def is_text_supported_in_language(string, supported_language_chars):
     for letter in string:
         if not(letter.isdigit() or letter.lower() in supported_language_chars):
-            print(f"invalid english letter {letter}")
             return False
     
     return True
@@ -51,7 +57,6 @@ def is_text_braille(string):
     
     return True
     
-
 def parse_braille_string_to_language(braille_text, braille_to_language, braille_to_numbers):
     i = 0
     translated_text = ''
@@ -61,20 +66,15 @@ def parse_braille_string_to_language(braille_text, braille_to_language, braille_
     while i < total_letters:
         letter = letters[i]
         if letter not in braille_to_language or letter not in special_braille_chars:
-            # throw error
-            print(f"Throw error: letter not translatable {letter}")
-            break
+            raise TranslationError(f"Unable to translate the following letter {letter}")
         
         letter_translation = braille_to_language[letter]
         if letter_translation == SpecialBrailleChar.CAPITAL.value:
             if i + 1 >= total_letters:
-                # throw error
-                print("Invalid use of captial in braille: Require characters after capital follows character.")
-                break
+                raise TranslationError(f"Invalid use of captial follows in braille. Requires a letter to follow the use of the capital follows character")
             
             i += 1
             cap_letter = braille_to_language[letters[i]]
-            
             translated_text += cap_letter.upper()
         elif letter_translation == SpecialBrailleChar.NUMBER.value:
             while i + 1 < total_letters and letters[i + 1] in braille_to_numbers:
@@ -84,10 +84,8 @@ def parse_braille_string_to_language(braille_text, braille_to_language, braille_
             translated_text += letter_translation
         
         i += 1
-    
-    print(f"Final String:{translated_text}")
-    return translated_text
 
+    return translated_text
 
 def parse_language_to_braille(text, language_to_braille, number_to_braille):
     i = 0
@@ -96,9 +94,7 @@ def parse_language_to_braille(text, language_to_braille, number_to_braille):
         letter = text[i]
         
         if letter not in number_to_braille and letter.lower() not in language_to_braille :
-             # throw error
-            print(f"Throw error: letter not translatable {letter}")
-            break
+            raise TranslationError(f"Unable to translate the following letter {letter}")
         
         if letter.isdigit():
             braille_translation += SpecialBrailleChar.NUMBER.value
@@ -115,17 +111,15 @@ def parse_language_to_braille(text, language_to_braille, number_to_braille):
         i += 1
     
     return braille_translation
-     
+         
 def translate_text(text):
     if is_text_braille(text):
         return parse_braille_string_to_language(text, braille_to_english, braille_to_english_numbers)
     elif is_text_supported_in_language(text, english_to_braille):
         return parse_language_to_braille(text, english_to_braille, english_numbers_to_braille)
     else:
-        # Throw error
-        print("Error: Unable to currently translate text")   
+        raise TranslationError(f"Unable to translate input text. Note that only basic Braille/English are supported.")
     
-
 def main():
     parser = argparse.ArgumentParser(
         prog="Braille Translator",
@@ -138,16 +132,20 @@ def main():
     
     arguments = parser.parse_args()
     
-    # should be defined with a valid string
-    if not arguments.input_text_to_translate:
+    # added below but note that argparse enforces that an argument is passed in. 
+    if not arguments.input_text_to_translate:    
         return
     
     text = ' '.join(arguments.input_text_to_translate).strip()
     
-    translated_text = translate_text(text)
+    try:
+        translated_text = translate_text(text)
+    except TranslationError as e:
+        # Capture and print the error message
+        print(f"Error: {e.message}")
+        return
     
     print(f"{translated_text}")
     
-
 if __name__ == "__main__":
     main()
