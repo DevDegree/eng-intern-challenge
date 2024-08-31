1f59e9180ab570c8f198182246b4b90a40b5b3bb
Jonathan Lee <jonathan.k.lee9@gmail.com>
2024-08-31 13:51:41 -0400

    added braille to english feature

--- ruby/translate_braille_to_english.rb
+++ ruby/translate_braille_to_english.rb
@@ -1,3 +1,41 @@
+require_relative "constants"
+
 def translate_braille_to_english(braille_string)
+  if braille_string.length % 6 != 0
+    puts "Braille input length must be divisible by 6"
+    return
+  end
+
   result = ""
+  is_number = false
+  index = 0
+
+  while index < braille_string.length
+    braille_symbol = braille_string[index, 6]
+
+    if !BRAILLE_TO_ENGLISH_DICT.key?(braille_symbol) &&
+      braille_symbol != BRAILLE_CAPITAL_FOLLOWS &&
+      braille_symbol != BRAILLE_NUMBER_FOLLOWS
+      puts "#{braille_symbol} is not valid braille"
+      return
+    end
+
+    case braille_symbol
+    when BRAILLE_CAPITAL_FOLLOWS
+      index += 6
+      braille_symbol = braille_string[index, 6]
+      result += BRAILLE_TO_ENGLISH_DICT[braille_symbol].upcase
+    when BRAILLE_NUMBER_FOLLOWS
+      is_number = true
+    when "......"
+      is_number = false
+      result += BRAILLE_TO_ENGLISH_DICT[braille_symbol]
+    else
+      result += is_number ? BRAILLE_TO_NUMBERS_DICT[braille_symbol] : BRAILLE_TO_ENGLISH_DICT[braille_symbol]
+    end
+
+    index += 6
+  end
+
+  return result
 end
--- ruby/translator.rb
+++ ruby/translator.rb
@@ -1,5 +1,6 @@
 require_relative 'constants'
 require_relative 'get_input_type'
+require_relative 'translate_braille_to_english'
 
 def translate_english_to_braille(english_string)
   result = ""
@@ -13,11 +14,11 @@ def main()
   input_type = get_input_type()
 
   if input_type == BRAILLE_TYPE
-    puts "hello"
+    puts translate_braille_to_english(ARGV[0])
+    return
   else
-    puts "world"
+    puts "English"
   end
-
 
 end
 
