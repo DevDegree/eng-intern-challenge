6d54d4924f6a67fa5bf1978e00e783314e758a12
alex-h-sun <alexhsn.05@gmail.com>
2024-08-31 17:11:12 -0600

    Update translator.py
    final

--- python/translator.py
+++ python/translator.py
@@ -1,4 +1,129 @@
-braille_dict = {}
+import sys
 
-engl_dict = {}
+b_to_e_special_chars = {'.....O':"cap", '......':" ", '.O.OOO':'num'}
+
+braille_to_engl_dict = {'O.....':'a', 'O.O...':'b',
+                'OO....':'c', 'OO.O...':'d', 
+                'O..O..':'e', 'OOO...':'f', 
+                'OOOO..':'g', 'O.OO..':'h', 
+                '.OO...':'i', '.OOO..':'j',
+                'O...O.':'k', 'O.O.O.':'l',
+                'OO..O.':'m', 'OO.OO.':'n',
+                'O..OO.':'o', 'OOO.O.':'p',
+                'OOOOO.':'q', 'O.OOO.':'r',
+                '.OO.O.':'s', '.OOOO.':'t',
+                'O...OO':'u', 'O.O.OO':'v',
+                '.OOO.O':'w', 'OO..OO':'x',
+                'OO.OOO':'y', 'O..OOO':'z',
+                '..OO.O':'.', '..O...':',',
+                '..O.OO':'?', '..OOO.':'|',
+                '..OO..':':', '..O.O.':';',
+                '....OO':'-', '.O..O.':'/',
+                '.OO..O':'<', 'O..OO.':'>',
+                'O.O..O':'(', '.O.OO.':')'}
+
+b_to_e_numbers = {'O.....':'1', 'O.O...':'2',
+                'OO....':'3', 'OO.O...':'4', 
+                'O..O..':'5', 'OOO...':'6', 
+                'OOOO..':'7', 'O.OO..':'8', 
+                '.OO...':'9', '.OOO..':'0'}
+
+
+e_to_b_special_chars = {"cap":'.....O', "space":'......', 'num':'.O.OOO'}
+
+engl_to_braille_dict = {'a': 'O.....', 'b': 'O.O...', 
+             'c': 'OO....', 'd': 'OO.O..', 
+             'e': 'O..O..', 'f': 'OOO...', 
+             'g': 'OOOO..', 'h': 'O.OO..', 
+             'i': '.OO...', 'j': '.OOO..', 
+             'k': 'O...O.', 'l': 'O.O.O.', 
+             'm': 'OO..O.', 'n': 'OO.OO.', 
+             'o': 'O..OO.', 'p': 'OOO.O.', 
+             'q': 'OOOOO.', 'r': 'O.OOO.', 
+             's': '.OO.O.', 't': '.OOOO.', 
+             'u': 'O...OO', 'v': 'O.O.OO', 
+             'w': '.OOO.O', 'x': 'OO..OO', 
+             'y': 'OO.OOO', 'z': 'O..OOO',
+             '.': '..OO.O', ',': '..O...',
+             '?': '..O.OO', '|': '..OOO.', 
+             ':': '..OO..', ';': '..O.O.', 
+             '-': '....OO', '/': '.O..O.', 
+             '<': '.OO..O', '>': 'O..OO.', 
+             '(': 'O.O..O', ')': '.O.OO.'}
+
+e_to_b_numbers = {'1': 'O.....', '2': 'O.O...', 
+             '3': 'OO....', '4': 'OO.O..', 
+             '5': 'O..O..', '6': 'OOO...', 
+             '7': 'OOOO..', '8': 'O.OO..', 
+             '9': '.OO...', '0': '.OOO..'}
+
+def check_for_braille(text):
+    if all(i in '.O' for i in text):
+        return True
+    return False
+
+def translate_braille_to_engl(text):
+    ans = []
+    capital = False
+    num = False
+    
+    for i in range(0, len(text), 6):
+        if num:
+            if text[i:i+6] in b_to_e_special_chars:
+                num = False
+            else:
+                ans.append(b_to_e_numbers[text[i:i+6]])
+        else:
+            if text[i:i+6] in b_to_e_special_chars:
+                if b_to_e_special_chars[text[i:i+6]] == 'cap':
+                    capital = True
+                elif b_to_e_special_chars[text[i:i+6]] == 'num':
+                    num = True
+                else:
+                    ans.append(b_to_e_special_chars[text[i:i+6]])
+            elif text[i:i+6] in braille_to_engl_dict:
+                if capital:
+                    ans.append(braille_to_engl_dict[text[i:i+6]].upper())
+                    capital = False
+                else:
+                    ans.append(braille_to_engl_dict[text[i:i+6]])
+                
+    return ''.join(ans)
+        
+
+def translate_engl_to_braille(text):
+    ans = []
+    curr_nums = False
+
+    for i in text:
+        if i == " ":
+            if curr_nums == True:
+                curr_nums = False
+            else:
+                ans.append(e_to_b_special_chars['space'])
+                continue
+
+        if i.lower() in engl_to_braille_dict:
+            if i.isupper():
+                ans.append(e_to_b_special_chars['cap'])
+                ans.append(engl_to_braille_dict[i.lower()])
+            else:
+                ans.append(engl_to_braille_dict[i.lower()])
+            continue
+
+        if i.isdigit():
+            if not curr_nums:
+                curr_nums = True
+                ans.append(e_to_b_special_chars['num'])
+            ans.append(e_to_b_numbers[i])
+            
+    return ''.join(ans)
+
+text = ' '.join(sys.argv[1:])
+
+if check_for_braille(text):
+    print(translate_braille_to_engl(text))
+else:
+    print(translate_engl_to_braille(text))
+
 
