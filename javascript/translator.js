9a482f270603e798621b7f194958065e87cae410
Yvette de Sereville <ydesereville@uwaterloo.ca>
2024-08-31 09:27:40 -0400

    build braille to english dictionary

--- javascript/translator.js
+++ javascript/translator.js
@@ -15,6 +15,7 @@ const [message] = arg; // the argument is the message provided in the command li
 
 // English to Braille mapping
 const engToBraille = new Map();
+
 engToBraille.set("a", "O.....");
 engToBraille.set("b", "O.O...");
 engToBraille.set("c", "OO....");
@@ -57,8 +58,46 @@ engToBraille.set("9", ".OO...");
 // Braille to English mapping
 const brailleToEng = new Map();
 
+brailleToEng.set("O.....", "a");
+brailleToEng.set("O.O...", "b");
+brailleToEng.set("OO....", "c");
+brailleToEng.set("OO.O..", "d");
+brailleToEng.set("O..O..", "e");
+brailleToEng.set("OOO...", "f");
+brailleToEng.set("OOOO..", "g");
+brailleToEng.set("O.OO..", "h");
+brailleToEng.set(".OO...", "i");
+brailleToEng.set(".OOO..", "j");
+brailleToEng.set("O...O.", "k");
+brailleToEng.set("O.O.O.", "l");
+brailleToEng.set("OO..O.", "m");
+brailleToEng.set("OO.OO.", "n");
+brailleToEng.set("O..OO.", "o");
+brailleToEng.set("OOO.O.", "p");
+brailleToEng.set("OOOOO.", "q");
+brailleToEng.set("O.OOO.", "r");
+brailleToEng.set(".OO.O.", "r");
+brailleToEng.set(".OOOO.", "t");
+brailleToEng.set("O...OO", "u");
+brailleToEng.set("O.O.OO", "v");
+brailleToEng.set(".OOO.O", "w");
+brailleToEng.set("OO..OO", "x");
+brailleToEng.set("OO.OOO", "y");
+brailleToEng.set("O..OOO", "z");
+brailleToEng.set(".....O", "capitalize");
+brailleToEng.set("......", "space");
+brailleToEng.set(".OOO..", "0");
+brailleToEng.set("O.....", "1");
+brailleToEng.set("O.O...", "2");
+brailleToEng.set("OO....", "3");
+brailleToEng.set("OO.O..", "4");
+brailleToEng.set("O..O..", "5");
+brailleToEng.set("OOO...", "6");
+brailleToEng.set("OOOO..", "7");
+brailleToEng.set("O.OO..", "8");
+brailleToEng.set(".OO...", "9");
+
 // 1. check if converting from braille to english or vice versa
-if (message.split("")[0] === ".") {
+if (message.split("").includes(".")) {
   // braille to english
-  console.log(engToBraille.get("a"));
 }
