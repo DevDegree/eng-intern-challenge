65ef09ba8f6278b4530e7c35752c9df7004a6a5a
Mayank808 <mayankmehra628@gmail.com>
2024-08-30 21:52:46 -0400

    feat: added translation logic - english to braille, braille to english supported

--- python/translator.py
+++ python/translator.py
@@ -4,42 +4,39 @@ from enum import Enum
 from collections import Counter
 
 # Enums
-class SpecialChar(Enum):
-    CAPITAL = "CAPITAL"
-    NUMBER = "NUMBER"
-    SPACE = " "
+class SpecialBrailleChar(Enum):
+    CAPITAL = ".....O"
+    NUMBER = ".O.OOO"
+
+special_braille_chars = {char.value for char in SpecialBrailleChar}
 
 # Constants
-english_to_braille_dict = {
+english_to_braille = {
     'a': "O.....", 'b': "O.O...", 'c': "OO....", 'd': "OO.O..", 'e': "O..O..",
     'f': "OOO...", 'g': "OOOO..", 'h': "O.OO..", 'i': ".OO...", 'j': ".OOO..",
     'k': "O...O.", 'l': "O.O.O.", 'm': "OO..O.", 'n': "OO.OO.", 'o': "O..OO.",
     'p': "OOO.O.", 'q': "OOOOO.", 'r': "O.OOO.", 's': ".OO.O.", 't': ".OOOO.",
     'u': "O...OO", 'v': "O.O.OO", 'w': ".OOO.O", 'x': "OO..OO", 'y': "OO.OOO",
-    'z': "O..OOO",
-    
-    SpecialChar.CAPITAL.value: ".....O", SpecialChar.NUMBER.value: ".O.OOO",
-    SpecialChar.SPACE.value: "......"
+    'z': "O..OOO", ' ': "......"
 }
 
-english_numbers_braille_dict = {
+english_numbers_to_braille = {
     '1': "O.....", '2': "O.O...", '3': "OO....", '4': "OO.O..", '5': "O..O..",
     '6': "OOO...", '7': "OOOO..", '8': "O.OO..", '9': ".OO...", '0': ".OOO..",
 }
 
-braille_to_english_dict = {value : key for key, value in english_to_braille_dict.items()}
-braille_to_english_numbers = {value : key for key, value in english_numbers_braille_dict.items()}
-validBrailleChar = {'.', 'O'}
+braille_to_english = {value : key for key, value in english_to_braille.items()}
+braille_to_english_numbers = {value : key for key, value in english_numbers_to_braille.items()}
 
-def isInputStringContainValidChar(string, language_chars):
+def is_text_supported_in_language(string, supported_language_chars):
     for letter in string:
-        if not(letter.isdigit() or letter.lower() in language_chars):
+        if not(letter.isdigit() or letter.lower() in supported_language_chars):
             print(f"invalid english letter {letter}")
             return False
     
     return True
 
-def isInputStringBaraille(string):
+def is_text_braille(string):
     if not (len(string) % 6 == 0):
         return False
 
@@ -48,21 +45,85 @@ def isInputStringBaraille(string):
     if len(letters) > 2:
         return False
 
-    for letter, count in letters.items():
-        if letter not in validBrailleChar:
+    for letter in letters.keys():
+        if letter not in {'.', 'O'}:
             return False
     
     return True
     
 
 def parse_braille_string_to_language(braille_text, braille_to_language, braille_to_numbers):
-    pass
+    i = 0
+    translated_text = ''
+    letters = [braille_text[i: i + 6] for i in range(0, len(braille_text), 6)]
+    total_letters = len(letters)
+    
+    while i < total_letters:
+        letter = letters[i]
+        if letter not in braille_to_language or letter not in special_braille_chars:
+            # throw error
+            print(f"Throw error: letter not translatable {letter}")
+            break
+        
+        letter_translation = braille_to_language[letter]
+        if letter_translation == SpecialBrailleChar.CAPITAL.value:
+            if i + 1 >= total_letters:
+                # throw error
+                print("Invalid use of captial in braille: Require characters after capital follows character.")
+                break
+            
+            i += 1
+            cap_letter = braille_to_language[letters[i]]
+            
+            translated_text += cap_letter.upper()
+        elif letter_translation == SpecialBrailleChar.NUMBER.value:
+            while i + 1 < total_letters and letters[i + 1] in braille_to_numbers:
+                i += 1
+                translated_text += braille_to_numbers[letters[i]]
+        else:
+            translated_text += letter_translation
+        
+        i += 1
+    
+    print(f"Final String:{translated_text}")
+    return translated_text
 
 
 def parse_language_to_braille(text, language_to_braille, number_to_braille):
-    pass
-    
+    i = 0
+    braille_translation = ""
+    while i < len(text):
+        letter = text[i]
         
+        if letter not in number_to_braille and letter.lower() not in language_to_braille :
+             # throw error
+            print(f"Throw error: letter not translatable {letter}")
+            break
+        
+        if letter.isdigit():
+            braille_translation += SpecialBrailleChar.NUMBER.value
+            braille_translation += number_to_braille[letter]
+            while i + 1 < len(text) and text[i + 1].isdigit():
+                i += 1
+                braille_translation += number_to_braille[text[i]]
+        elif letter.isupper():
+            braille_translation += SpecialBrailleChar.CAPITAL.value
+            braille_translation += language_to_braille[letter.lower()]
+        else:
+            braille_translation += language_to_braille[letter]
+        
+        i += 1
+    
+    return braille_translation
+     
+def translate_text(text):
+    if is_text_braille(text):
+        return parse_braille_string_to_language(text, braille_to_english, braille_to_english_numbers)
+    elif is_text_supported_in_language(text, english_to_braille):
+        return parse_language_to_braille(text, english_to_braille, english_numbers_to_braille)
+    else:
+        # Throw error
+        print("Error: Unable to currently translate text")   
     
 
 def main():
@@ -82,15 +143,8 @@ def main():
         return
     
     text = ' '.join(arguments.input_text_to_translate).strip()
-    translated_text = ""
-    if isInputStringBaraille(text):
-        translated_text = parse_braille_string_to_language(text, braille_to_english_dict, braille_to_english_numbers)
-    elif isInputStringContainValidChar(text, english_to_braille_dict):
-        translated_text = parse_language_to_braille(text, english_to_braille_dict, english_numbers_braille_dict)
-    else:
-        # Throw error
-        print("Error: Unable to currently translate text")
     
+    translated_text = translate_text(text)
     
     print(f"{translated_text}")
     
