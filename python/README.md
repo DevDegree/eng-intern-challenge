800e414ab3e46de7d6edea17a225376eb5767799
Samudra Perera <samudrapup@gmail.com>
2024-08-31 14:48:58 -0400

    Assumptions + Dictionaries for challenge

--- python/README.md
+++ python/README.md
@@ -1,3 +1,11 @@
 # Python Instructions
 
 Note that the Python version used is 3.8
+
+# Pseudo Code
+-> Create a dictionary to map all the keys to their respective values
+
+
+#Assumptions
+Im under the assumption that the input will always be valid and that the user will only provide values that are either in English or in braille. 
+Furthermore, I am assuming that the user will only use characters that were specified in the requirements. 
--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,26 @@
+import sys
+
+# Create all the key-value pairs
+BRAILLE_TO_ENGLISH = {
+    'O.....': 'a', 'O.O...': 'b', 'OO....': 'c', 'OO.O..': 'd', 'O..O..': 'e',
+    'OOO...': 'f', 'OOOO..': 'g', 'O.OO..': 'h', '.OO...': 'i', '.OOO..': 'j',
+    'O...O.': 'k', 'O.O.O.': 'l', 'OO..O.': 'm', 'OO.OO.': 'n', 'O..OO.': 'o',
+    'OOO.O.': 'p', 'OOOOO.': 'q', 'O.OOO.': 'r', '.OO.O.': 's', '.OOOO.': 't',
+    'O...OO': 'u', 'O.O.OO': 'v', '.OOO.O': 'w', 'OO..OO': 'x', 'OO.OOO': 'y',
+    'O..OOO': 'z',
+    '......': ' '
+}
+
+ENGLISH_TO_BRAILLE = {v: k for k, v in BRAILLE_TO_ENGLISH.items()}
+
+BRAILLE_TO_NUMBERS = {
+    '.OOO..': '1', 'O.OO..': '2', 'OOO...': '3', 'OOO.O.': '4', 'O..OO.': '5',
+    'OOOO..': '6', 'OOOOO.': '7', 'O.OOO.': '8', '.OOOO.': '9', '.OO.O.': '0'
+}
+
+NUMBERS_TO_BRAILLE = {v: k for k, v in BRAILLE_TO_NUMBERS.items()}
+
+CAPITALIZE = '.....O'
+
+NUMBER_FOLLOWS = '.O.OOO'
 
