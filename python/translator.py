0dd0edd77cb123a622a3b3270b76427a13340376
Arafat Olayiwola <harof.dev@gmail.com>
2024-08-31 11:19:24 +0100

    add english translation functionality

--- python/translator.py
+++ python/translator.py
@@ -21,6 +21,11 @@ def is_braille(input_str: str):
     return all(char in 'O.' for char in input_str)
 
 def translate_to_braille(input_str: str):
+    # translate an input string to braille characters wise
+    """
+        At worst scenario, time and space complexity should be linear 
+        considering the characters of input string
+    """
     result = []
     number_mode = False
     for char in input_str:
@@ -33,4 +38,33 @@ def translate_to_braille(input_str: str):
         elif char == ' ':
             number_mode = False
         result.append(braille_alphabet.get(char, ''))
+    return ''.join(result)
+
+
+def translate_to_english(input_str):
+    # translate the input string to english characters
+    """
+        At worst scenario, time and space complexity should be linear 
+        considering the characters of input string
+    """
+    result = []
+    i = 0
+    number_mode = False
+    while i < len(input_str):
+        braille_char = input_str[i:i+6]
+        i += 6
+        if braille_char == braille_alphabet['cap']:
+            char = inverse_braille_alphabet[input_str[i:i+6]].upper()
+            result.append(char)
+            i += 6
+        elif braille_char == braille_alphabet['num']:
+            number_mode = True
+        elif braille_char == '......':
+            result.append(' ')
+            number_mode = False
+        else:
+            char = inverse_braille_alphabet[braille_char]
+            if number_mode:
+                char = str('abcdefghij'.index(char) + 1)
+            result.append(char)
     return ''.join(result)
