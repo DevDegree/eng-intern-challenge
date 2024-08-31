3680021da093b890efa021c44a4a0bccd2833b78
pritish911 <NfdgxDx9LhUnn3V>
2024-08-31 00:21:14 -0700

    Added code for Braille <-> English CLI Translator

---
+++ python/braille_dict.py
@@ -0,0 +1,24 @@
+# braille_dict.py
+
+BRAILLE_LETTERS = {
+    'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..',
+    'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..',
+    'k': 'O...O.', 'l': 'O.O.O.', 'm': 'OO..O.', 'n': 'OO.OO.', 'o': 'O..OO.',
+    'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.',
+    'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO', 'y': 'OO.OOO',
+    'z': 'O..OOO'
+}
+
+BRAILLE_CAPITAL_PREFIX = '.....O'
+BRAILLE_NUMBER_PREFIX = '.O.OOO'
+BRAILLE_SPACE = '......'
+
+BRAILLE_NUMBERS = {
+    '1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..', '5': 'O..O..',
+    '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...', '0': '.OOO..'
+}
+
+ENGLISH_LETTERS = {v: k for k, v in BRAILLE_LETTERS.items()
+}
+ENGLISH_NUMBERS = {v: k for k, v in BRAILLE_NUMBERS.items()
+}
