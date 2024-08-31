5018e1ac52ea731ee961a1d4c28245f091410b65
Justin Zhang <jing.justin@gmail.com>
2024-08-31 11:03:14 -0400

    define braille-english mappings

--- typescript/translator.ts
+++ typescript/translator.ts
@@ -1,3 +1,59 @@
+const brailleToEnglish = new Map<string, string>([
+  ["O.....", "a"],
+  ["O.O...", "b"],
+  ["OO....", "c"],
+  ["OO.O..", "d"],
+  ["O..O..", "e"],
+  ["OOO...", "f"],
+  ["OOOO..", "g"],
+  ["O.OO..", "h"],
+  [".OO...", "i"],
+  [".OOO..", "j"],
+  ["O...O.", "k"],
+  ["O.O.O.", "l"],
+  ["OO..O.", "m"],
+  ["OO.OO.", "n"],
+  ["O..OO.", "o"],
+  ["OOO.O.", "p"],
+  ["OOOOO.", "q"],
+  ["O.OOO.", "r"],
+  [".OO.O.", "s"],
+  [".OOOO.", "t"],
+  ["O...OO", "u"],
+  ["O.O.OO", "v"],
+  [".OOO.O", "w"],
+  ["OO..OO", "x"],
+  ["OO.OOO", "y"],
+  ["O..OOO", "z"],
+  [".....O", "capital"],
+  [".O.OOO", "number"],
+  ["......", " "],
+]);
+
+const brailleToNum = new Map<string, string>([
+  ["O.....", "1"],
+  ["O.O...", "2"],
+  ["OO....", "3"],
+  ["OO.O..", "4"],
+  ["O..O..", "5"],
+  ["OOO...", "6"],
+  ["OOOO..", "7"],
+  ["O.OO..", "8"],
+  [".OO...", "9"],
+  [".OOO..", "0"],
+]);
+
+const englishToBraille = new Map<string, string>();
+const numToBraille = new Map<string, string>();
+
+brailleToNum.forEach((key: string, val: string) => {
+  numToBraille.set(val, key);
+});
+
+brailleToEnglish.forEach((key: string, val: string) => {
+  englishToBraille.set(val, key);
+});
+
 /**
  * check if a string is or isn't valid Braille such that it only contains O or .
  *
