e81cebabd750a59731cd9a90278588545baab613
Grace Kan <gracex1@Graces-MacBook-Air.local>
2024-08-31 14:07:59 -0700

    changed 0 to O

--- python/translator.py
+++ python/translator.py
@@ -1,81 +1,91 @@
 import re
 
-TRANSLATOR = {"alpha_mode":{
-    "a": "0.....",
-    "b": "0.0...",
-    "c": "00....",
-    "d": "00.0..",
-    "e": "0..0..",
-    "f": "000...",
-    "g": "0000..",
-    "h": "0.00..",
-    "i": ".00...",
-    "j": ".000..",
-    "k": "0...0.",
-    "l": "0.0.0.",
-    "m": "00..0.",
-    "n": "00.00.",
-    "o": "0..00.",
-    "p": "000.0.",
-    "q": "00000.",
-    "r": "0.000.",
-    "s": ".00.0.",
-    "t": ".0000.",
-    "u": "0...00",
-    "v": "0.0.00",
-    "w": ".000.0",
-    "x": "00..00",
-    "y": "00.000",
-    "z": "0..000",
-    ".": "..00.0",
-    ",": "..0...",
-    "?": "..0.00",
-    "!": "..000.",
-    ":": "..00..",
-    ";": "..0.0.",
-    "-": "....00",
-    "/": ".0..0.",
-    "<": ".00..0",
-    ">": "0..00.",
-    "(": "0.0..0",
-    ")": ".0.00.",
+TRANSLATOR = {
+    "a": "O.....",
+    "b": "O.O...",
+    "c": "OO....",
+    "d": "OO.O..",
+    "e": "O..O..",
+    "f": "OOO...",
+    "g": "OOOO..",
+    "h": "O.OO..",
+    "i": ".OO...",
+    "j": ".OOO..",
+    "k": "O...O.",
+    "l": "O.O.O.",
+    "m": "OO..O.",
+    "n": "OO.OO.",
+    "o": "O..OO.",
+    "p": "OOO.O.",
+    "q": "OOOOO.",
+    "r": "O.OOO.",
+    "s": ".OO.O.",
+    "t": ".OOOO.",
+    "u": "O...OO",
+    "v": "O.O.OO",
+    "w": ".OOO.O",
+    "x": "OO..OO",
+    "y": "OO.OOO",
+    "z": "O..OOO",
+    ".": "..OO.O",
+    ",": "..O...",
+    "?": "..O.OO",
+    "!": "..OOO.",
+    ":": "..OO..",
+    ";": "..O.O.",
+    "-": "....OO",
+    "/": ".O..O.",
+    "<": ".OO..O",
+    ">": "O..OO.",
+    "(": "O.O..O",
+    ")": ".O.OO.",
     " ": "......",
-    "cap": ".....0",
-    "dec": ".0...0",
-    "num": ".0.000"
-},
-"num_mode": 
-    {
-    "1": "0.....",
-    "2": "0.0...",
-    "3": "00....",
-    "4": "00.0..",
-    "5": "0..0..",
-    "6": "000...",
-    "7": "0000..",
-    "8": "0.00..",
-    "9": ".00...",
-    "0": ".000..",
-    ".": "..00.0",
-    ",": "..0...",
-    "?": "..0.00",
-    "!": "..000.",
-    ":": "..00..",
-    ";": "..0.0.",
-    "-": "....00",
-    "/": ".0..0.",
-    "<": ".00..0",
-    ">": "0..00.",
-    "(": "0.0..0",
-    ")": ".0.00.",
-    " ": "......",
-    "cap": ".....0",
-    "dec": ".0...0",
-    "num": ".0.000"
-    }
+    "cap": ".....O",
+    "dec": ".O...O",
+    "num": ".O.OOO"
 }
 
+NUM_MODE = {
+    "1": "a",
+    "2": "b",
+    "3": "c",
+    "4": "d",
+    "5": "e",
+    "6": "f",
+    "7": "g",
+    "8": "h",
+    "9": "i",
+    "0": "j"
+}
 
+def translate_to_braille(input, mode):
+    result = ""
+    for c in input:
+        if c.isdigit():
+            if mode == "alpha_mode":
+                mode = "num_mode"
+                result = result + TRANSLATOR["num"]
+            result = result + TRANSLATOR[NUM_MODE[c]]
+        else:
+            mode = "alpha_mode"
+            if c.isupper():
+                result = result + TRANSLATOR["cap"] + TRANSLATOR[c.lower()]
+            elif c == ".":
+                result = result + TRANSLATOR["dec"] + TRANSLATOR["."]
+            else:
+                result = result + TRANSLATOR[c]
+        
+    
+    return result
+
+def translate_to_eng(input): 
+    return ""
+
+def translate(input): 
+    if (re.match(r'^[O.]+$', input) is not None and len(input) % 6 == 0):
+        return translate_to_eng(input)
+    else:
+        return translate_to_braille(input, "alpha_mode")
     
 while True:    
     user_input = input()
