f545ef617276ab30c6a4dcfce053ffcd4050c74f
gexu13neu <xu.ge1@northeastern.edu>
2024-08-31 01:03:53 -0700

    add ENGLISH_TO_BRAILLE dict

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,31 @@
+'''
+    Braille Translator:
+    This command_line program takes a string and 
+    determines if the string is either Braille or English
+    and coverts it to the correct opposite
+    Gavin Xu
+    Aug 30, 2024
+'''
+
+import sys
+
+ENGLISH_TO_BRAILLE = {
+    'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..',
+    'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..',
+    'k': 'O...O.', 'l': 'O.O.O.', 'm': 'OO..O.', 'n': 'OO.OO.', 'o': 'O..OO.',
+    'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.',
+    'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO', 'y': 'OO.OOO', 
+    'z': 'O..OOO',
+
+    '1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..', '5': 'O..O..',
+    '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...', '0': '.OOO..',
+
+    'capital_follows': '....O', 
+    'decimal_follows': '.O...O', 
+    'number_follows': '.O.OOO',
+
+    '.': '..OO.O', ',': '..O...', '?': '..O.OO', '!': '..OOO.', ':': '..OO..',
+    ';': '..O.O.', '-': '....OO', '/': '.O..O.', '<': '.OO..O', '>': 'O..OO.',
+    '(': 'O.O..O', ')': '.O.OO.', ' ': '......'
+}
 
