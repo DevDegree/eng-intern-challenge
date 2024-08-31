3b2dd16a75007800f9b9aa4f32a372696ad40ac9
mishareyes <hellomboreyes@gmail.com>
2024-08-31 12:54:31 -0700

    implemented translator

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,89 @@
+import sys
 
+char_to_braille = {
+    'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..',
+    'e': 'O..O..', 'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..',
+    'i': '.OO...', 'j': '.OOO..', 'k': 'O...O.', 'l': 'O.O.O.',
+    'm': 'OO..O.', 'n': 'OO.OO.', 'o': 'O..OO.', 'p': 'OOO.O.',
+    'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.',
+    'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO',
+    'y': 'OO.OOO', 'z': 'O..OOO',
+    
+    '1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..',
+    '5': 'O..O..', '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..',
+    '9': '.OO...', 'O': '.OOO..',
+
+    'capital': '.....O', 'number': '.O.OOO', ' ': '......'
+}
+
+braille_to_number = {
+    'O.....': '1', 'O.O...': '2', 'OO....': '3', 'OO.O..': '4',
+    'O..O..': '5', 'OOO...': '6', 'OOOO..': '7', 'O.OO..': '8',
+    '.OO...': '9', '.OOO..': 'O',
+}
+
+braille_to_letter = {
+    'O.....': 'a', 'O.O...': 'b', 'OO....': 'c', 'OO.O..': 'd',
+    'O..O..': 'e', 'OOO...': 'f', 'OOOO..': 'g', 'O.OO..': 'h',
+    '.OO...': 'i', '.OOO..': 'j', 'O...O.': 'k', 'O.O.O.': 'l',
+    'OO..O.': 'm', 'OO.OO.': 'n', 'O..OO.': 'o', 'OOO.O.': 'p',
+    'OOOOO.': 'q', 'O.OOO.': 'r', '.OO.O.': 's', '.OOOO.': 't',
+    'O...OO': 'u', 'O.O.OO': 'v', '.OOO.O': 'w', 'OO..OO': 'x',
+    'OO.OOO': 'y', 'O..OOO': 'z',
+}
+
+def translate_to_braille(text: str) -> str:
+    output = ""
+    number_mode = False
+    for char in text:
+        if char.isupper():
+            output += char_to_braille['capital']
+            output += char_to_braille[char.lower()]
+        elif char.islower() or number_mode:
+            output += char_to_braille[char]
+        elif char.isdigit() and not number_mode:
+            number_mode = True
+            output += char_to_braille['number']
+            output += char_to_braille[char]
+        else: 
+            number_mode = False
+            output += char_to_braille[' ']
+    
+    return output
+
+def translate_to_english(braille: str) -> str:
+    output = ""
+    number_mode = False
+    capitalize_next = True
+    for i in range(0, len(braille), 6):
+        b_char = braille[i:i+6]
+
+        if b_char == char_to_braille['number']:
+            number_mode = True
+        elif b_char == char_to_braille[' ']:
+            number_mode = False
+            output += ' '
+        elif b_char == char_to_braille['capital']:
+            capitalize_next = True
+        elif number_mode:
+            output += braille_to_number[b_char]
+        elif capitalize_next:
+            output += braille_to_letter[b_char].upper()
+            capitalize_next = False
+        else:
+            output += braille_to_letter[b_char]
+
+    return output
+
+def process_input():
+    arguments = sys.argv[1:]
+    string = (" ".join(arguments))
+
+    is_braille = all(char in 'O.' for char in string) and len(string) % 6 == 0
+
+    if is_braille:
+        print(translate_to_english(string))
+    else:
+        print(translate_to_braille(string))
+
+process_input()
--- python/translator.test.py
+++ python/translator.test.py
@@ -2,17 +2,29 @@ import unittest
 import subprocess
 
 class TestTranslator(unittest.TestCase):
-    def test_output(self):
+    def test_original(self):
         # Command to run translator.py script
         command = ["python3", "translator.py", "Abc", "123", "xYz"]
-        
+
         # Run the command and capture output
         result = subprocess.run(command, capture_output=True, text=True)
-        
+
         # Expected output without the newline at the end
         expected_output = ".....OO.....O.O...OO...........O.OOOO.....O.O...OO..........OO..OO.....OOO.OOOO..OOO"
-        
+
         # Strip any leading/trailing whitespace from the output and compare
+        self.assertEqual(result.stdout.strip(), expected_output)
+    
+    def test_alphabet_to_braille(self):
+        command = ["python3", "translator.py", "abcdefghijklmnopqrstuvwxyz"]
+        result = subprocess.run(command, capture_output=True, text=True)
+        expected_output = "O.....O.O...OO....OO.O..O..O..OOO...OOOO..O.OO...OO....OOO..O...O.O.O.O.OO..O.OO.OO.O..OO.OOO.O.OOOOO.O.OOO..OO.O..OOOO.O...OOO.O.OO.OOO.OOO..OOOO.OOOO..OOO"
+        self.assertEqual(result.stdout.strip(), expected_output)
+
+    def test_alphabet_to_english(self):
+        command = ["python3", "translator.py", ".....OO.....O.O...OO...........O.OOOO.....O.O...OO..........OO..OO.....OOO.OOOO..OOO"]
+        result = subprocess.run(command, capture_output=True, text=True)
+        expected_output = "Abc 123 xYz"
         self.assertEqual(result.stdout.strip(), expected_output)
 
 if __name__ == '__main__':
