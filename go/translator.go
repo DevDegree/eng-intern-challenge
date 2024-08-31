faa30129e3e3c2e7064a88cc47d056d82c79e80f
Danny Liu <dannyjialiliu@gmail.com>
2024-08-31 13:11:08 -0700

    update translations and readme

--- go/translator.go
+++ go/translator.go
@@ -15,9 +15,9 @@ var englishToBraille = map[string]string{
 	"u": "O...OO", "v": "O.O.OO", "w": ".OOO.O", "x": "OO..OO", "y": "OO.OOO",
 	"z": "O..OOO",
 
-	"O": ".OOO..", "1": "O.....", 
-	" ": "......", 
-	"capital_follows": ".....O","number_follows": ".O.OOO",
+	"1": "O.....", "2": "O.O...", "3": "OO....", "4": "OO.O..", "5": "O..O..", "6": "OOO...", "7": "OOOO..", "8": "O.OO..", "9": ".OO...", "0": ".OOO..", 
+	"capital_follows": ".....O", "decimal_follows": ".O...O", "number_follows": ".O.OOO",
+	".": "..OO.O", ",": "..O...", "?": "..O.OO", "!": "..OOO.", ":": "..OO..", ";": "..O.O.", "-": "....OO", "/": ".O..O.", "<": ".OO..O", ">": "O..OO.", "(": "O.O..O", ")": ".O.OO.", " ": "......",
 }
 
 func main() {
@@ -34,5 +34,6 @@ func main() {
 		braille := englishToBraille[string(unicode.ToLower(char))]
 		fmt.Print(braille)
 	}
+	
 	fmt.Println()
 }
