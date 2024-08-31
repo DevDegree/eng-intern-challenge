07733f861af9104d35134907ef6724d677282ba1
Harry Chen <harry.chen@uwaterloo.ca>
2024-08-30 19:39:44 -0700

    Add implementation for translation

--- python/translator.py
+++ python/translator.py
@@ -62,13 +62,31 @@ def translate(phrase):
     Main function, determines if a phrase is english or braille then accordingly iterates through phrase to translate it
     Returns the translated result
     """
-    # Setup strings to store result
+    # Setup string to store result
     res = ""
+    # set up flags indicating number or capital is next
+    number_follows = False
+    capital_follows = False
 
     if check_if_english(phrase):
-        # We have confirmed it is an english phrase, now we change each character into braille
         for c in phrase:
-            res += to_braille(c)
+            # Pre-operations for each character
+            if c.isupper():
+                # if upper, add identifier first
+                res += ".....O"
+                c = c.lower()
+            elif c.isnumeric():
+                # if adding a number and not in a number sequence yet, add identifier before adding number
+                if not number_follows:
+                    res += ".O.OOO"
+                    number_follows = True
+                res += num_to_braille[c]
+                continue
+            elif c == " ":
+                # if there is a space, set the numerical sequence flag to false
+                number_follows = False
+            # finally, add the digit
+            res += eng_to_braille[c]
     else:
         # We have confirmed it is a braille phrase, now we change every 6 characters into english
         cur = ""
@@ -76,13 +94,34 @@ def translate(phrase):
             # Add characters to cur until 6 characters, then convert it into english and add to res, and clear cur
             cur += c
             if len(cur) == 6:
-                res += to_english(cur)
+                if cur == ".O.OOO":
+                    # if braille indicates number follows, set flag
+                    number_follows = True
+                elif cur == ".....O":
+                    # if braille indicates capital follows, set flag
+                    capital_follows = True
+                elif cur == "......":
+                    # if braille indicates space, remove number follows flag, and add a space
+                    number_follows = False
+                    res += " "
+                else:
+                    if number_follows:
+                        # if we expect numbers, use braille to num dict
+                        res += braille_to_num[cur]
+                    elif capital_follows:
+                        # if we expect a capital, change letter added to uppercase and remove capital flag
+                        res += braille_to_eng[cur].upper()
+                        capital_follows = False
+                    else:
+                        # otherwise, just add the letter
+                        res += braille_to_eng[cur]
                 cur = ""
 
     return res
 
 if __name__ == '__main__':
+    # join everything after calling the script with a space (as it appears visually
     if len(sys.argv) > 1:
-        translate(" ".join(sys.argv[1:]))
+        print(translate(" ".join(sys.argv[1:])))
     else:
         print("Welcome to braille translator: enter a braille phrase or english phrase as an argument to translate")
