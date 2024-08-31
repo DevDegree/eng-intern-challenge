8c45661e8385d8c91255570fbc1c395961caa33b
tterrytang <73598033+tterrytang@users.noreply.github.com>
2024-08-30 22:00:46 -0600

    review and update translator.py

--- python/translator.py
+++ python/translator.py
@@ -1,80 +1,88 @@
 import sys
 
-# Braille mappings
-BRAILLE_DICT = {
-    'a': 'O.....',    'b': 'O.O...',    'c': 'OO....',    'd': 'OO.O..',
-    'e': 'O..O..',    'f': 'OOO...',    'g': 'OOOO..',    'h': 'O.OO..',
-    'i': '.OO...',    'j': '.OOO..',    'k': 'O...O.',    'l': 'O.O.O.',
-    'm': 'OO..O.',    'n': 'OO.OO.',    'o': 'O..OO.',    'p': 'OOO.O.',
-    'q': 'OOOOO.',    'r': 'O.OOO.',    's': '.OO.O.',    't': '.OOOO.',
-    'u': 'O...OO',    'v': 'O.O.OO',    'w': '.OOO.O',    'x': 'OO..OO',
-    'y': 'OO.OOO',    'z': 'O..OOO',    ' ': '......',
-    'capital': '.....O',    'number': '.O.OOO'
+# Braille mappings for letters, digits, and special indicators
+ENG_TO_BRAILLE_ALPHABET = {
+    'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..',
+    'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..',
+    'k': 'O...O.', 'l': 'O.O.O.', 'm': 'OO..O.', 'n': 'OO.OO.', 'o': 'O..OO.',
+    'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.',
+    'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO', 'y': 'OO.OOO', 
+    'z': 'O..OOO', ' ': '......',
+
+    # Indicators for capital, number follows
+    'cap': '.....O', 'num': '.O.OOO', 
 }
 
-# Reverse the Braille dictionary for Braille to English conversion
-ENGLISH_DICT = {v: k for k, v in BRAILLE_DICT.items()}
+ENG_TO_BRAILLE_DIGITS = {
+    '1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..', '5': 'O..O..',
+    '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...', '0': '.OOO..',
+}
 
-def english_to_braille(text):
-    result = []
-    number_mode = False
-    
-    for char in text:
-        if char.isalpha():
-            if char.isupper():
-                result.append(BRAILLE_DICT['capital'])
-            if number_mode:
-                number_mode = False
-            result.append(BRAILLE_DICT[char.lower()])
-        elif char.isdigit():
-            if not number_mode:
-                result.append(BRAILLE_DICT['number'])
-                number_mode = True
-            result.append(BRAILLE_DICT[chr(ord(char) - ord('0') + ord('a'))])
-        elif char == ' ':
-            result.append(BRAILLE_DICT[char])
-            number_mode = False
-    
-    return ''.join(result)
+# Reverse mappings for translation from Braille to English
+BRAILLE_TO_ENG_ALPHABET = {braille: eng for eng, braille in ENG_TO_BRAILLE_ALPHABET.items()}
+BRAILLE_TO_ENG_DIGITS = {braille: eng for eng, braille in ENG_TO_BRAILLE_DIGITS.items()}
 
+def is_braille(input_str: str) -> bool:
+    """Check if the input string is in Braille format (contains only 'O' and '.' characters)."""
+    return all(char in 'O.' for char in input_str) and len(input_str) % 6 == 0
 
-def braille_to_english(braille):
-    result = []
-    i = 0
-    capitalize_next = False
-    number_mode = False
-    
-    while i < len(braille):
-        chunk = braille[i:i+6]
-        if chunk == BRAILLE_DICT['capital']:
-            capitalize_next = True
-        elif chunk == BRAILLE_DICT['number']:
-            number_mode = True
-        elif chunk in ENGLISH_DICT:
-            char = ENGLISH_DICT[chunk]
-            if char.isalpha():
-                if capitalize_next:
-                    char = char.upper()
-                    capitalize_next = False
-                if number_mode:
-                    char = str(ord(char) - ord('a'))
-                    number_mode = False
-            elif char == ' ':
-                number_mode = False
-            result.append(char)
-        i += 6
-    
-    return ''.join(result)
+def translate_braille_to_eng(input_str: str) -> str:
+    """Translate a Braille string to its English equivalent."""
+    english = ""
+    cap_follows = False
+    num_follows = False
 
-def translate(input_string):
-    if set(input_string).issubset({'O', '.'}):
-        return braille_to_english(input_string)
-    else:
-        return english_to_braille(input_string)
+    for i in range(0, len(input_str), 6):
+        braille_char = input_str[i:i+6]
+        eng_char = BRAILLE_TO_ENG_ALPHABET.get(braille_char, '')
+
+        if eng_char == 'cap':
+            cap_follows = True
+        elif eng_char == 'num':
+            num_follows = True
+        elif eng_char == ' ':
+            english += ' '
+            num_follows = False
+        elif num_follows:
+            english += BRAILLE_TO_ENG_DIGITS.get(braille_char, '')
+        elif cap_follows:
+            english += eng_char.upper()
+            cap_follows = False
+        else:
+            english += eng_char
+
+    return english
+
+def translate_eng_to_braille(input_str: str) -> str:
+    """Translate an English string to its Braille equivalent."""
+    braille = ""
+    num_follows = False
+
+    for eng_char in input_str:
+        if eng_char.isdigit():
+            if not num_follows:
+                braille += ENG_TO_BRAILLE_ALPHABET['num']
+                num_follows = True
+            braille += ENG_TO_BRAILLE_DIGITS[eng_char]
+        elif eng_char.isupper():
+            braille += ENG_TO_BRAILLE_ALPHABET['cap']
+            braille += ENG_TO_BRAILLE_ALPHABET[eng_char.lower()]
+            num_follows = False
+        elif eng_char == ' ':
+            braille += ENG_TO_BRAILLE_ALPHABET[' ']
+            num_follows = False
+        else:
+            braille += ENG_TO_BRAILLE_ALPHABET[eng_char]
+            num_follows = False
+
+    return braille
 
 if __name__ == "__main__":
-    input_string = ' '.join(sys.argv[1:])
-    if input_string:
-        result = translate(input_string)
-        print(result, end='')  # Print without newline
+    input_str = " ".join(sys.argv[1:])
 
+    if is_braille(input_str):
+        output_str = translate_braille_to_eng(input_str)
+    else:
+        output_str = translate_eng_to_braille(input_str)
+
+    print(output_str, end='')
