943ddd20f31329c9091c2a3e49e2eb88ec81064c
Hasan-Al-Khazraji <37431365+Hasan-Al-Khazraji@users.noreply.github.com>
2024-09-01 02:12:05 +0300

    Add translate to braille feature with reading from CLI

--- python/translator.py
+++ python/translator.py
@@ -1,3 +1,6 @@
+import sys
+
+# English to braille
 braille_map = {
     'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..',
     'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..',
@@ -11,3 +14,30 @@ braille_map = {
     '-': '....OO', '/': '.O..O.', '>': 'O..OO.', '<': '.OO..O', '(': 'O.O..O',
     ')': '.O.OO.', ' ': '......'
 }
+
+def translate_to_braille(text):
+    braille_text = ""
+    isNumber = False
+    for char in text:
+        if char.isnumeric() and not isNumber:
+            braille_text += braille_map['NUMBER']
+            isNumber = True
+        if char.isupper():
+            braille_text += braille_map['CAPITAL']
+        if char.lower() in braille_map:
+            braille_text += braille_map[char.lower()]
+        elif char == " ":
+            braille_text += braille_map[' ']
+            isNumber = False
+    return braille_text
+
+def main():
+    if len(sys.argv) < 2:
+        print("Translator requires at least one argument")
+        sys.exit(1)
+    input_text = ' '.join(sys.argv[1:])
+
+    print(translate_to_braille(input_text))
+
+if __name__ == "__main__":
+    main()
