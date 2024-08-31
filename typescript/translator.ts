54aa3e84eae6aa6bbbdcb1f0c37e66ee45434f7c
yevheniia.shabalova <11918485+yevheniia-shabalova@users.noreply.github.com>
2024-08-31 11:42:50 -0400

    Added code to create translate function for Braille alphabet, added configs

---
+++ typescript/translator.ts
@@ -0,0 +1,86 @@
+// to run: ts-node translator Hello world
+
+const brailleDictionary: Record<string, string> = {
+  "a": "O.....",
+  "b": "O.O...",
+  "c": "OO....",
+  "d": "OO.O..",
+  "e": "O..O..",
+  "f": "OOO...",
+  "g": "OOOO..",
+  "h": "O.OO..",
+  "i": ".OO...",
+  "j": ".OOO..",
+  "k": "O...O.",
+  "l": "O.O.O.",
+  "m": "OO..O.",
+  "n": "OO.OO.",
+  "o": "O..OO.",
+  "p": "OOO.O.",
+  "q": "OOOOO.",
+  "r": "O.OOO.",
+  "s": ".OO.O.",
+  "t": ".OOOO.",
+  "u": "O...OO",
+  "v": "O.O.OO",
+  "w": ".OOO.O",
+  "x": "OO..OO",
+  "y": "OO.OOO",
+  "z": "O..OOO",
+  "capital": ".....O",
+  "number": ".O.OOO",
+  "0": ".OOO..",
+  "1": "O.....",
+  "2": "O.O...",
+  "3": "OO....",
+  "4": "OO.O..",
+  "5": "O..O..",
+  "6": "OOO...",
+  "7": "OOOO..",
+  "8": "O.OO..",
+  "9": ".OO...",
+  "space": "......",
+};
+
+function preprocessParams(obj: Array<string>): string {
+  obj.shift();
+  obj.shift();
+
+  return obj.join(' ')
+}
+
+function translate(text: string): string {
+  let result = '';
+  let isNumeric = false;
+
+  for (const char of text) {
+    if (char === ' ') {
+      result += brailleDictionary["space"];
+      isNumeric = false;
+    } else if (isNaN(+char)) {
+      if (char === char.toUpperCase()) {
+        // Handle capital letters
+        result += brailleDictionary["capital"];
+        result += brailleDictionary[char.toLowerCase()] || '';
+        isNumeric = false;
+      } else {
+        // Handle letters
+        result += brailleDictionary[char];
+        isNumeric = false;
+      }
+    } else if (!isNaN(+char)) {
+      // Handle numbers
+      if (!isNumeric) {
+        result += brailleDictionary["number"];
+        isNumeric = true;
+      }
+      result += brailleDictionary[char];
+    }
+  }
+
+  return result.trim();
+}
+
+const args = preprocessParams(process.argv);
+const result = translate(args);
+console.log(result);
