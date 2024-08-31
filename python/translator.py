97bc26b3438f533642bfb84d5d4109232e9db739
IshaHaider <66149883+IshaHaider@users.noreply.github.com>
2024-08-30 22:04:24 -0600

    Remove multiple spaces

--- python/translator.py
+++ python/translator.py
@@ -50,8 +50,15 @@ class EnglishToBrailleStrategy(TranslationStrategy):
     def translate(self, english_text):
         braille_list = [] # Using list to append characters and then join them at the end, because string concatentations have a time complexity of (O(n^2))
         number_flag = False
+        last_char_was_space = False
         
         for char in english_text:
+            if char == ' ':
+                if last_char_was_space: continue  # Skip if the last character was also a space
+                last_char_was_space = True
+            else:
+                last_char_was_space = False
+                
             if char.lower() in self.translate_dict: # ignores all characters not in dictionary
                 if char.isupper(): # if capital, add "C" before braille and lowercase the letter
                     braille_list.append(self.translate_dict['C']) 
@@ -70,7 +77,7 @@ class EnglishToBrailleStrategy(TranslationStrategy):
         
         braille_text = ''.join(braille_list) 
         return braille_text
-
+    
 class BrailleToEnglishStrategy(TranslationStrategy):
     # Total Time Complexity: O(n) because 2 for loops: 1 for reverse dict and 1 for braille_text, but not nested
     def __init__(self, translate_dict):
