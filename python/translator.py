14c6b2ec133de86c6bdfc7bf2e1cb52c0a91b1dc
Prithee Roy <prithee.roy@mail.utoronto.ca>
2024-08-31 17:51:28 -0400

    adhered to oop

--- python/translator.py
+++ python/translator.py
@@ -1,32 +1,34 @@
 import sys
 
-letters_to_braille = {
-    'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 
-    'e': 'O..O..', 'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..', 
-    'i': '.OO...', 'j': '.OOO..', 'k': 'O...O.', 'l': 'O.O.O.', 
-    'm': 'OO..O.', 'n': 'OO.OO.', 'o': 'O..OO.', 'p': 'OOO.O.',
-    'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.',
-    'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO', 
-    'y': 'OO.OOO', 'z': 'O..OOO',
-}
+class BrailleTranslator:
+    def __init__(self):
+        self.letters_to_braille = {
+            'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 
+            'e': 'O..O..', 'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..', 
+            'i': '.OO...', 'j': '.OOO..', 'k': 'O...O.', 'l': 'O.O.O.', 
+            'm': 'OO..O.', 'n': 'OO.OO.', 'o': 'O..OO.', 'p': 'OOO.O.',
+            'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.',
+            'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO', 
+            'y': 'OO.OOO', 'z': 'O..OOO',
+        }
 
-numbers_to_braille = {
-    '1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..', 
-    '5': 'O..O..', '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', 
-    '9': '.OO...', '0': '.OOO..',
-}
+        self.numbers_to_braille = {
+            '1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..', 
+            '5': 'O..O..', '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', 
+            '9': '.OO...', '0': '.OOO..',
+        }
 
-punctuation_to_braille = {
-    '.': '.O..OO', ',': '.O....', '?': '.O.OO.', '!': '.OO.O.', 
-    ':': 'OO....', ';': 'O.O...', '-': '......', '/': 'O.O...', 
-    '<': 'O..OO.', '>': '.OOO.O', '(': 'OO.OO.', ')': '.OO.OO',
-}
+        self.punctuation_to_braille = {
+            '.': '.O..OO', ',': '.O....', '?': '.O.OO.', '!': '.OO.O.', 
+            ':': 'OO....', ';': 'O.O...', '-': '......', '/': 'O.O...', 
+            '<': 'O..OO.', '>': '.OOO.O', '(': 'OO.OO.', ')': '.OO.OO',
+        }
 
-special_symbols = {
-    'cap_follows': '.....O', 'num_follows': '.O.OOO', 'dec_follows': '.O...O', ' ': '......',
-}
+        self.special_symbols = {
+            'cap_follows': '.....O', 'num_follows': '.O.OOO', 'dec_follows': '.O...O', ' ': '......',
+        }
 
-# Inverse mappings for braille to english
-braille_to_letters = {braille: letter for letter, braille in letters_to_braille.items()}
-braille_to_numbers = {braille: letter for letter, braille in numbers_to_braille.items()}
-braille_to_punctuation = {braille: letter for letter, braille in punctuation_to_braille.items()}
+        # Inverse mappings for braille to english
+        self.braille_to_letters = {braille: letter for letter, braille in self.letters_to_braille.items()}
+        self.braille_to_numbers = {braille: letter for letter, braille in self.numbers_to_braille.items()}
+        self.braille_to_punctuation = {braille: letter for letter, braille in self.punctuation_to_braille.items()}
