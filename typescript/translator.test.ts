4054ac36594716a8d41589f06d8f65f1c6523bf7
cass <cassandraptrs@gmail.com>
2024-08-31 20:54:51 -0400

    Adding unit test cases as per the expectations outlined

--- typescript/translator.test.ts
+++ typescript/translator.test.ts
@@ -18,5 +18,23 @@ describe('translator.ts output', () => {
     const expected = '.....OO.....O.O...OO...........O.OOOO.O...OO....OO.O........OO..OO.....OOO.OOOO..OOO';
     expect(stdout.trim()).toBe(expected);
   });
+  it('should print the correct output to the console', async () => {
+    const { stdout } = await execAsync('ts-node translator.ts Hello world');
+
+    const expected = '.....OO.OO..O..O..O.O.O.O.O.O.O..OO........OOO.OO..OO.O.OOO.O.O.O.OO.O..';
+    expect(stdout.trim()).toBe(expected);
+  });
+  it('should print the correct output to the console', async () => {
+    const { stdout } = await execAsync('ts-node translator.ts 42');
+
+    const expected = '.O.OOOOO.O..O.O...';
+    expect(stdout.trim()).toBe(expected);
+  });
+  it('should print the correct output to the console', async () => {
+    const { stdout } = await execAsync('ts-node translator.ts .....OO.....O.O...OO...........O.OOOO.....O.O...OO....');
+
+    const expected = 'Abc 123';
+    expect(stdout.trim()).toBe(expected);
+  });
 });
 
