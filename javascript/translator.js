6206d6b94aa82cc13031b416a0f76fcf93ca6d87
Yunpeng Shan <shanyunp@sheridancollege.ca>
2024-09-01 16:20:04 -0400

    Completed Braille Translator Challenge

--- javascript/translator.js
+++ javascript/translator.js
@@ -1,71 +1,125 @@
-// need a dictionary to store key paired values for english to braille
-const brailleMap = {
-    'A': 'O.....', 'B': 'O.O...', 'C': 'OO....', 'D': 'OO.O..', 'E': 'O..O..',
-    'F': 'OOO...', 'G': 'OOOO..', 'H': 'O.OO..', 'I': '.OO...', 'J': '.OOO..',
-    'K': 'O...O.', 'L': 'O.O.O.', 'M': 'OO..O.', 'N': 'OO.OO.', 'O': 'O..OO.',
-    'P': 'OOO.O.', 'Q': 'OOOOO.', 'R': 'O.OOO.', 'S': '.OO.O.', 'T': '.OOOO.',
-    'U': 'O...OO', 'V': 'O.O.OO', 'W': '.OOO.O', 'X': 'OO..OO', 'Y': 'OO.OOO',
-    'Z': 'O..OOO',
+// a dictionary to map english and braille
+const brailleDictionary = {
+    "a": "O.....",
+    "b": "O.O...",
+    "c": "OO....",
+    "d": "OO.O..",
+    "e": "O..O..",
+    "f": "OOO...",
+    "g": "OOOO..",
+    "h": "O.OO..",
+    "i": ".OO...",
+    "j": ".OOO..",
+    "k": "O...O.",
+    "l": "O.O.O.",
+    "m": "OO..O.",
+    "n": "OO.OO.",
+    "o": "O..OO.",
+    "p": "OOO.O.",
+    "q": "OOOOO.",
+    "r": "O.OOO.",
+    "s": ".OO.O.",
+    "t": ".OOOO.",
+    "u": "O...OO",
+    "v": "O.O.OO",
+    "w": ".OOO.O",
+    "x": "OO..OO",
+    "y": "OO.OOO",
+    "z": "O..OOO",
+    "1": "O.....",
+    "2": "O.O...",
+    "3": "OO....",
+    "4": "OO.O..",
+    "5": "O..O..",
+    "6": "OOO...",
+    "7": "OOOO..",
+    "8": "O.OO..",
+    "9": ".OO...",
+    "0": ".OOO..",
+    "capitalFollows": ".....O",
+    "decimalFollows": ".O...O",
+    "numberFollows": ".O.OOO",
+    ".": "..OO.O",
+    ",": "..O...",
+    "?": "..O.OO",
+    "!": "..OO..",
+    ":": "..OO..",
+    ";": "..O.O.",
+    "-": "....OO",
+    "/": ".O..O.",
+    "<": ".OO..O",
+    ">": "O..OO.",
+    "(": "O.O..O",
+    ")": ".O.OO.",
+    " ": "......", 
+};
 
-    '1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..', '5': 'O..O..',
-    '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...', '0': '.OOO..',
-
-    'capitalFollows': '.....O', 'decimalFollows': '.O...O', 'numberFollows': 'O.....', 
-    ' ': '......', '.': '..O.OO', ',': '..O...', '?': '..OO.O',
-    '!': '..OOO.', ':': '..OO..', ';': '..OOO.', '-': '..O..O', 
-    '/': '..O.O.', '<': '..O.O.', '>': '..OOO.', '(': '...O..', ')': '...OO.'
-}
-
-const brailleToEnglish = Object.entries(brailleMap).reduce((obj, [key, value]) => {
-    obj[value] = key;
-    return obj;
+// Reverse map to decode Braille back to English
+const ReversebrailleDictionary = Object.entries(brailleDictionary).reduce((acc, [letter, braille]) => {
+    acc[braille] = letter;
+    return acc;
 }, {});
 
-const capitalFollows = brailleMap['capitalFollows'];
-const numberFollows = brailleMap['numberFollows'];
-
+// Function to determine if the input is Braille or English
 function isBraille(input) {
-    return /^[O\.]+$/.test(input);
+    return /^[O.]+$/.test(input);
 }
 
-function translateToBraille(text) {
-    let result = '';
-    for (let i = 0; i < text.length; i++) {
-        let char = text[i];
-        if(/[A-Z]/.test(char)) {
-            result += capitalFollows;
-        } else if (/[0-9]/.test(char)) {
-            result += numberFollows;
+// Function to translate from English to Braille
+function translateToBraille(input) {
+    let braille = '';
+    let isNumber = false;
+
+    for (let char of input) {
+        if (/[A-Z]/.test(char)) {
+            braille += brailleDictionary["capitalFollows"];
+            char = char.toLowerCase();
+        } 
+        
+        if (/[0-9]/.test(char)) {
+            if (!isNumber) {
+                braille += brailleDictionary["numberFollows"];
+                isNumber = true;
+            }
+        } else {
+            isNumber = false;
         }
-        result += brailleMap[char.toUpperCase()];
+
+        braille += brailleDictionary[char] || brailleDictionary[' '];
     }
-    return result;
+
+    return braille;
 }
 
-function translateToEnglish(braille) {
-    let result = '';
-    let capital = false;
-    let number = false;
-    for (let i = 0; i < braille.length; i += 6) {
-        let symbol = braille.slice(i, i + 6);
-        if (symbol === capitalFollows) {
-            capital = true;
+// Function to translate from Braille to English
+function translateToEnglish(input) {
+    let english = '';
+    let isCapital = false;
+    let isNumber = false;
+
+    for (let i = 0; i < input.length; i += 6) {
+        const brailleChar = input.slice(i, i + 6);
+        if (brailleChar === brailleDictionary["capitalFollows"]) {
+            isCapital = true;
+            continue;
+        } else if (brailleChar === brailleDictionary["numberFollows"]) {
+            isNumber = true;
             continue;
         }
 
-        let char = brailleToEnglish[symbol];
-        if (capital) {
-            char = char.toUpperCase();
-            capital = false;
-        } else {
-            char = char.toLowerCase();
+        let letter = ReversebrailleDictionary[brailleChar] || ' ';
+        if (isCapital) {
+            letter = letter.toUpperCase();
+            isCapital = false;
         }
-        result += char;
+
+        english += letter;
     }
-    return result;
+
+    return english;
 }
 
-function brailleTranslator(input) {
+function translate(input) {
     if (isBraille(input)) {
         return translateToEnglish(input);
     } else {
@@ -74,4 +128,6 @@ function brailleTranslator(input) {
 }
 
 const input = process.argv.slice(2).join(' ');
-console.log(brailleTranslator(input));
+const output = translate(input);
+
+console.log(output);
