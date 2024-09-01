64b706994da59d6307f1ab1b5ee08106b1a86731
Thushen Mohanarajah <thushenarriyam@gmail.com>
2024-09-01 17:05:18 -0400

    Changed from regular dictionary to bidirectional dict

--- python/translator.py
+++ python/translator.py
@@ -1,19 +1,79 @@
 import sys
+
+from bidirectional_dict import BidirectionalDict
 # Constants
 
-BASE_BRAILLE = {
-    "a": "O.....", "b": "O.O...", "c": "OO....", "d": "OO.O..", "e": "O..O..",
-    "f": "OOO...", "g": "OOOO..", "h": "O.OO..", "i": ".OO...", "j": ".OOO..",
-    "k": "O...O.", "l": "O.O.O.", "m": "OO..O.", "n": "OO.OO.", "o": "O..OO.",
-    "p": "OOO.O.", "q": "OOOOO.", "r": "O.OOO.", "s": ".OO.O.", "t": ".OOOO.",
-    "u": "O...OO", "v": "O.O.OO", "w": ".OOO.O", "x": "OO..OO", "y": "OO.OOO",
-    "z": "O..OOO", "1": "O.....", "2": "O.O...", "3": "OO....",
-    "4": "OO.O..", "5": "O..O..", "6": "OOO...", "7": "OOOO..",
-    "8": "O.OO..", "9": ".OO...", "0": ".OOO..", " ": "......"
-}
+# NOTE: Thought about using a biderectional 
+# ENGLISH_TO_BRAILLE = {
+#     "a": "O.....", "b": "O.O...", "c": "OO....", "d": "OO.O..", "e": "O..O..",
+#     "f": "OOO...", "g": "OOOO..", "h": "O.OO..", "i": ".OO...", "j": ".OOO..",
+#     "k": "O...O.", "l": "O.O.O.", "m": "OO..O.", "n": "OO.OO.", "o": "O..OO.",
+#     "p": "OOO.O.", "q": "OOOOO.", "r": "O.OOO.", "s": ".OO.O.", "t": ".OOOO.",
+#     "u": "O...OO", "v": "O.O.OO", "w": ".OOO.O", "x": "OO..OO", "y": "OO.OOO",
+#     "z": "O..OOO", "1": "O.....", "2": "O.O...", "3": "OO....",
+#     "4": "OO.O..", "5": "O..O..", "6": "OOO...", "7": "OOOO..",
+#     "8": "O.OO..", "9": ".OO...", "0": ".OOO..", " ": "......"
+# }
 
+# BRAILLE_TO_ENGLISH = {
+#     "O.....": "a", "O.O...": "b", "OO....": "c", "OO.O..": "d", "O..O..": "e",
+#     "OOO...": "f", "OOOO..": "g", "O.OO..": "h", ".OO...": "i", ".OOO..": "j",
+#     "O...O.": "k", "O.O.O.": "l", "OO..O.": "m", "OO.OO.": "n", "O..OO.": "o",
+#     "OOO.O.": "p", "OOOOO.": "q", "O.OOO.": "r", ".OO.O.": "s", ".OOOO.": "t",
+#     "O...OO": "u", "O.O.OO": "v", ".OOO.O": "w", "OO..OO": "x", "OO.OOO": "y",
+#     "O..OOO": "z", "O.....": "1", "O.O...": "2", "OO....": "3",
+#     "OO.O..": "4", "O..O..": "5", "OOO...": "6", "OOOO..": "7",
+#     "O.OO..": "8", ".OO...": "9", ".OOO..": "0", "......": " "
+# }
+
+# Constants
 CAPITAL_PREFIX = "O.OOOO"
 NUMBER_PREFIX = ".O.OOO"
+SPACE = "......" # ' '
+
+def create_braille_alphabet_bidict():
+    braille_alphabet_bidict = BidirectionalDict()
+    braille_alphabet_bidict.add("a", "O.....")
+    braille_alphabet_bidict.add("b", "O.O...")
+    braille_alphabet_bidict.add("c", "OO....")
+    braille_alphabet_bidict.add("d", "OO.O..")
+    braille_alphabet_bidict.add("e", "O..O..")
+    braille_alphabet_bidict.add("f", "OOO...")
+    braille_alphabet_bidict.add("g", "OOOO..")
+    braille_alphabet_bidict.add("h", "O.OO..")
+    braille_alphabet_bidict.add("i", ".OO...")
+    braille_alphabet_bidict.add("j", ".OOO..")
+    braille_alphabet_bidict.add("k", "O...O.")
+    braille_alphabet_bidict.add("l", "O.O.O.")
+    braille_alphabet_bidict.add("m", "OO..O.")
+    braille_alphabet_bidict.add("n", "OO.OO.")
+    braille_alphabet_bidict.add("o", "O..OO.")
+    braille_alphabet_bidict.add("p", "OOO.O.")
+    braille_alphabet_bidict.add("q", "OOOOO.")
+    braille_alphabet_bidict.add("r", "O.OOO.")
+    braille_alphabet_bidict.add("s", ".OO.O.")
+    braille_alphabet_bidict.add("t", ".OOOO.")
+    braille_alphabet_bidict.add("u", "O...OO")
+    braille_alphabet_bidict.add("v", "O.O.OO")
+    braille_alphabet_bidict.add("w", ".OOO.O")
+    braille_alphabet_bidict.add("x", "OO..OO")
+    braille_alphabet_bidict.add("y", "OO.OOO")
+    braille_alphabet_bidict.add("z", "O..OOO")
+    return braille_alphabet_bidict
+
+def create_braille_numerical_bidict():
+    braille_numerical_bidict = BidirectionalDict()
+    braille_numerical_bidict.add("1", "O.....")
+    braille_numerical_bidict.add("2", "O.O...")
+    braille_numerical_bidict.add("3", "OO....")
+    braille_numerical_bidict.add("4", "OO.O..")
+    braille_numerical_bidict.add("5", "O..O..")
+    braille_numerical_bidict.add("6", "OOO...")
+    braille_numerical_bidict.add("7", "OOOO..")
+    braille_numerical_bidict.add("8", "O.OO..")
+    braille_numerical_bidict.add("9", ".OO...")
+    braille_numerical_bidict.add("0", ".OOO..")
+    return braille_numerical_bidict
 
 '''
 In Braille Alphabet in the technical requirements it does not state that '.' can be a vaild alphabet input in this program
@@ -43,13 +103,21 @@ def get_system_arguments(args):
 
     return text
 
+# def convert_brallie_to_english(text):
+
+
+
 # Runs the Translator
 def main():
-    text = get_system_arguments(sys.argv)
-    print(text)
-    if is_braille(text) and vaild_braille(text):
-        print(True)
-    print("RAN")
+    # text = get_system_arguments(sys.argv)
+    # print(text)
+    # if is_braille(text) and vaild_braille(text):
+    #     print(convert_brallie_to_english(text))
+    # print("RAN")
+    braille_alphabet_bidict = create_braille_alphabet_bidict()
+    braille_numerical_bidict = create_braille_numerical_bidict()
+    print(braille_alphabet_bidict.get('a'))
+    print(braille_alphabet_bidict.get_by_value('O.O...'))
 
 if __name__ == "__main__":
     main() 
