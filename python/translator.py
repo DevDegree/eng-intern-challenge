5b0679fe87a2e876b4835c09ad7eec9f340cda06
Razi Syed <r33syed@uwaterloo.ca>
2024-09-01 00:41:23 -0400

    finish braille to english

--- python/translator.py
+++ python/translator.py
@@ -8,19 +8,27 @@ output = ""
 if '.' in args[1]:
   capital = False
   numbers = False
+  brailleToLetters = {v: k for k, v in lettersToBraille.items()}
+  brailleToNumbers = {v: k for k, v in numbersToBraille.items()}
 
   for i in range(0, len(args[1]), 6):
-    char = args[i][i:i+6]
+    char = args[1][i:i+6]
 
-    if char == ".....O":
+    if char == specialSymbols['capital']:
       capital = True
-    elif char == ".O.OOO":
+    elif char == specialSymbols['number']:
       numbers = True
-    elif char == "......": 
+    elif char == specialSymbols['space']: 
       numbers = False
       output += " "
     else: 
-      print("kjansd")
+      if numbers:
+        output += brailleToNumbers[char]
+      elif capital: 
+        output += brailleToLetters[char].upper()
+        capital = False
+      else:
+        output += brailleToLetters[char]
 else:
   string = " ".join(args[1:])
   numbers = False
@@ -33,11 +41,10 @@ else:
       if not numbers:
         numbers = True
         output += specialSymbols['number'] 
-      output += numbersToBraille[char.lower()]
+      output += numbersToBraille[char]
     else: 
       if char.isupper():
         output += specialSymbols['capital']
       output += lettersToBraille[char.lower()]
   
-
 print(output)
