f7145430c8e43b046d5786ab683e3fd80513be98
Soroush Taslimi <soroushtaslimi@yahoo.com>
2024-09-01 18:18:39 -0400

    add language detection

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,11 @@
+import sys
 
+def check_braille(sentence):
+    return all(ch in '.O' for ch in sentence)
+
+def main():
+    sentence = ' '.join(sys.argv[1:])
+    is_braille = check_braille(sentence)
+
+if __name__ == "__main__":
+    main()
