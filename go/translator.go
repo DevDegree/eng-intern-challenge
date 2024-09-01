752db229650e531e9ac19b3bb12c9fffc3b5704b
ahdernasr <44983175+ahdernasr@users.noreply.github.com>
2024-09-01 15:02:28 +0200

    feat(main): integrate translation functions into main program logic

--- go/translator.go
+++ go/translator.go
@@ -158,24 +158,33 @@ func translateToEnglish(input string) string {
 	return result.String()
 }
 
+// isBraille checks if the input string is in Braille format
+func isBraille(input string) bool {
+	return strings.ContainsAny(input, "O.")
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Please provide a string to translate.")
 		return
 	}
 
-	input := os.Args[1]
+	// Combine all arguments into a single input string
+	input := strings.Join(os.Args[1:], " ")
 
-	fmt.Println(input)
-
-	// if isBraille(input) {
-	// 	fmt.Println(translateToEnglish(input))
-	// } else {
-	// 	fmt.Println(translateToBraille(input))
-	// }
-}
-
-// isBraille checks if the input string is in Braille format
-func isBraille(input string) bool {
-	return strings.ContainsAny(input, "O.")
+	if isBraille(input) {
+		output := translateToEnglish(input)
+		if output == "" {
+			fmt.Println("Invalid Braille input.")
+		} else {
+			fmt.Println(output)
+		}
+	} else {
+		output := translateToBraille(input)
+		if output == "" {
+			fmt.Println("Invalid English input.")
+		} else {
+			fmt.Println(output)
+		}
+	}
 }
