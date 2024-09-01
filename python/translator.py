fad64e630107d795a334e47708c79d6e4b9ff34c
Thomas Wu <thomaswu@getathelas.com>
2024-09-01 06:11:48 -0400

    add debug message for output tracking

--- python/translator.py
+++ python/translator.py
@@ -272,13 +272,13 @@ class BrailleToEnglishTranslator(Translator):
 
 def translate(input_string: str) -> str:
     # Deterine whether we are translating from english or braille
-    if len(input_string) % 6 == 0 and all(c in ".O" for c in input_string) :
-        # Case 1: We are working with braille data
-        translator = BrailleToEnglishTranslator(input_string)
-    else:
-        # Case 2: We are working with english data
-        translator = EnglishToBrailleTranslator(input_string)
-    sys.stdout.write("bar")
+    # if len(input_string) % 6 == 0 and all(c in ".O" for c in input_string) :
+    #     # Case 1: We are working with braille data
+    #     translator = BrailleToEnglishTranslator(input_string)
+    # else:
+    #     # Case 2: We are working with english data
+    translator = EnglishToBrailleTranslator(input_string)
+    print("bar")
 
 print("foo")
 translate(" ".join(sys.argv[1:]))
