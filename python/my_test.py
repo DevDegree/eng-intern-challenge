a3ec0a69c7ecccde8cf77a4214921a3ee0e875fc
steven huang <114288160+stevenh17@users.noreply.github.com>
2024-09-01 14:02:57 -0700

    translate to english + tests added

--- python/my_test.py
+++ python/my_test.py
@@ -1,5 +1,5 @@
 import unittest
-from translator import translate_to_braille
+from translator import translate_to_braille, translate_to_english
 
 class TestBrailleTranslator(unittest.TestCase):
 
@@ -16,6 +16,32 @@ class TestBrailleTranslator(unittest.TestCase):
             translate_to_braille("Hello world"),
             ".....OO.OO..O..O..O.O.O.O.O.O.O..OO........OOO.OO..OO.O.OOO.O.O.O.OO.O.."
         )
+        self.assertEqual(
+            translate_to_braille(".,?!:;"),
+            "..OO.O..O.....O.OO..OOO...OO....O.O."
+        )
+    
+    def test_english_to_braille(self):
+        self.assertEqual(
+            translate_to_english(
+                ".....OO.OO..O..O..O.O.O.O.O.O.O..OO........OOO.OO..OO.O.OOO.O.O.O.OO.O.."),
+            "Hello world"
+        )
+        self.assertEqual(
+            translate_to_english(".O.OOOOO.O..O.O..."),
+            "42"
+        )
+        self.assertEqual(
+            translate_to_english(
+                ".....OO.....O.O...OO...........O.OOOO.....O.O...OO...."),
+            "Abc 123"
+        )
+        self.assertEqual(
+            translate_to_english(
+                "..OO.O..O.....O.OO..OOO...OO....O.O."),
+            ".,?!:;"
+        )
+        
 
 if __name__ == "__main__":
     unittest.main()
--- python/translator.py
+++ python/translator.py
@@ -1,22 +1,24 @@
 import sys
 
-base_mapping = {
+english_to_braille = {
     "a": "O.....", "b": "O.O...", "c": "OO....", "d": "OO.O..", "e": "O..O..",
     "f": "OOO...", "g": "OOOO..", "h": "O.OO..", "i": ".OO...", "j": ".OOO..",
     "k": "O...O.", "l": "O.O.O.", "m": "OO..O.", "n": "OO.OO.", "o": "O..OO.",
     "p": "OOO.O.", "q": "OOOOO.", "r": "O.OOO.", "s": ".OO.O.", "t": ".OOOO.",
     "u": "O...OO", "v": "O.O.OO", "w": ".OOO.O", "x": "OO..OO", "y": "OO.OOO",
-    "z": "O..OOO", "1": "O.....", "2": "O.O...", "3": "OO....", "4": "OO.O..",
-    "5": "O..O..", "6": "OOO...", "7": "OOOO..", "8": "O.OO..", "9": ".OO...",
-    "0": ".OOO..", " ": "......", ".": "..OO.O", ",": "..O...", "?": "..O.OO",
+    "z": "O..OOO", " ": "......", ".": "..OO.O", ",": "..O...", "?": "..O.OO",
     "!": "..OOO.", ":": "..OO..", ";": "..O.O.", "-": "....OO", "/": ".O..O.",
-    "<": ".OO..O", "(": "O.O..O", ")": ".O.OO.", "Capital follows": ".....O",
-    "Number follows": ".O.OOO"
+    "<": ".OO..O", "(": "O.O..O", ")": ".O.OO.",
+    "Capital follows": ".....O", "Number follows": ".O.OOO", "Decimal follows": ".O...O"
+}
+braille_to_english = {v: k for k, v in english_to_braille.items()}
+
+number_to_braille = {
+    "1": "O.....", "2": "O.O...", "3": "OO....", "4": "OO.O..", "5": "O..O..",
+    "6": "OOO...", "7": "OOOO..", "8": "O.OO..", "9": ".OO...", "0": ".OOO.."
 }
 
-english_to_braille = base_mapping
-braille_to_english = {v: k for k, v in base_mapping.items()}
-
+braille_to_number = {v: k for k, v in number_to_braille.items()}
 
 def translate_to_braille(english):
     output = []
@@ -31,7 +33,7 @@ def translate_to_braille(english):
             if not number_state:
                 output.append(english_to_braille["Number follows"])
                 number_state = True
-            output.append(english_to_braille[char])
+            output.append(number_to_braille[char])
         elif char == ' ':
             output.append(english_to_braille[char])
             number_state = False
@@ -42,12 +44,39 @@ def translate_to_braille(english):
 
     return ''.join(output)
 
+def translate_to_english(braille):
+    output = []
+    number_state = False
+    capital_next = False
+    
+    for i in range(0, len(braille), 6):
+        braille_unit = braille[i:i+6]
+        print(braille_to_english[braille_unit])
+
+        if braille_to_english[braille_unit] == "Capital follows":
+            capital_next = True
+        elif braille_to_english[braille_unit] == "Number follows":
+            number_state = True
+        elif braille_to_english[braille_unit] == " ":
+            output.append(" ")
+            number_state = False  
+        else:
+            if number_state:
+                output.append(braille_to_number[braille_unit])
+            else:
+                char = braille_to_english[braille_unit]
+                if capital_next:
+                    char = char.upper()
+                    capital_next = False
+                output.append(char)
+    
+    return ''.join(output)
 
 def main():
     input_string = sys.argv[1]
 
     if all(char in 'O.' for char in input_string):
-        print("oops")
+        print(translate_to_english(input_string))
     else:
         print(translate_to_braille(input_string))
 
