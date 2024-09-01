c42016734c1c9e9cbf36d47ce31951dbb1b17f60
cass <cassandraptrs@gmail.com>
2024-08-31 20:54:25 -0400

    Fixing unit test - the input doesn't align with the expected output

--- typescript/translator.test.ts
+++ typescript/translator.test.ts
@@ -4,10 +4,19 @@ import { promisify } from 'util';
 const execAsync = promisify(exec);
 
 describe('translator.ts output', () => {
+
+  // This test case is incorrect, the input should read 'ABC 234 xYz' in order to match the expected output
+  // it('should print the correct output to the console', async () => {
+  //   const { stdout } = await execAsync('ts-node translator.ts Abc 123 xYz');
+
+  //   const expected = '.....OO.....O.O...OO...........O.OOOO.O...OO....OO.O........OO..OO.....OOO.OOOO..OOO';
+  //   expect(stdout.trim()).toBe(expected);
+  // });
   it('should print the correct output to the console', async () => {
-    const { stdout } = await execAsync('ts-node translator.ts Abc 123 xYz');
+    const { stdout } = await execAsync('ts-node translator.ts Abc 234 xYz');
 
     const expected = '.....OO.....O.O...OO...........O.OOOO.O...OO....OO.O........OO..OO.....OOO.OOOO..OOO';
     expect(stdout.trim()).toBe(expected);
   });
 });
+
