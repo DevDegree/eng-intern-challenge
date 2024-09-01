a0de55730e42d9ad0aa05e93891d227abae09c9e
yasmeen45 <a.yasmeen3542@gmail.com>
2024-09-01 14:05:03 -0700

    Filled translation dictionaries

--- python/translator.py
+++ python/translator.py
@@ -9,15 +9,27 @@ class BrailleTranslator:
     symbols
     """
     # Braille symbols
-    CAPITAL_SYMBOL = ''
-    NUMBER_SYMBOL  = ''
-    SPACE_SYMBOL = ''
+    CAPITAL_BRAILLE = '.....O'
+    NUMBER_BRAILLE  = '.O.OOO'
+    SPACE_BRAILLE   = '......'
     
     # Character translation dictionaries
-    BRAILLE_TO_ENGLISH = {}
-    BRAILLE_TO_NUMBER  = {}
-    ENGLISH_TO_BRAILLE = {}
-    NUMBER_TO_BRAILLE  = {}
+    ENGLISH_TO_BRAILLE = {
+        'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..',
+        'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..',
+        'k': 'O...O.', 'l': 'O.O.O.', 'm': 'OO..O.', 'n': 'OO.OO.', 'o': 'O..OO.',
+        'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.',
+        'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO', 'y': 'OO.OOO',
+        'z': 'O..OOO', ' ': '......'
+    }
+
+    NUMBER_TO_BRAILLE = {
+        '1': '.OO...', '2': '.OOO..', '3': 'OO....', '4': 'OO.O..', '5': 'O..O..',
+        '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...', '0': '.OOO..'
+    }
+
+    BRAILLE_TO_ENGLISH = {braille: english for english, braille in ENGLISH_TO_BRAILLE.items()}
+    BRAILLE_TO_NUMBER  = {braille: english for english, braille in NUMBER_TO_BRAILLE.items()}
 
     def __init__(self):
         pass
