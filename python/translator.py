8f0a822929f6d47dfe4c886ac3e7bb8997e22d1e
TiaElMasry <71573553+TiaElMasry@users.noreply.github.com>
2024-09-01 17:49:56 -0400

    Updated braille_to_english function

--- python/translator.py
+++ python/translator.py
@@ -8,13 +8,23 @@ alphabet = {
     'k': 'O...O.','l': 'O.O.O.','m': 'OO..O.','n': 'OO.OO.','o': 'O..OO.','p': 'OOO.O.','q': 'OOOOO.','r': 'O.OOO.','s': '.OO.O.','t': '.OOOO.',
     'u': 'O...OO','v': 'O.O.OO','w': '.OOO.O','x': 'OO..OO','y': 'OO.OOO','z': 'O..OOO',' ': '......','capital': '.....O','number': '.O.OOO',
     }
-# Dictionary to stroe numbers
+# Dictionary to store numbers
 numbers = {
     '1': 'O.....','2': 'O.O...','3': 'OO....','4': 'OO.O..','5': 'O..O..','6': 'OOO...','7': 'OOOO..','8': 'O.OO..','9': '.OO...','0': '.OOO..'
     }
+# Dictionary for braille alphabet
+braille_alphabet = {
+    'O.....':'a','O.O...':'b','OO....':'c','OO.O..':'d','O..O..':'e','OOO...':'f','OOOO..':'g','O.OO..':'h','.OO...':'i','.OOO..':'j',
+    'O...O.':'k','O.O.O.':'l','OO..O.':'m','OO.OO.':'n','O..OO.':'o','OOO.O.':'p','OOOOO.':'q','O.OOO.':'r','.OO.O.':'s','.OOOO.':'t',
+    'O...OO':'u','O.O.OO':'v','.OOO.O':'w','OO..OO':'x','OO.OOO':'y','O..OOO':'z','......':' ','.....O':'capital','.O.OOO':'number'
+    }
+# Dictionary for braille numbers
+braille_numbers = {
+    'O.....':'1','O.O...':'2','OO....':'3','OO.O..':'4','O..O..':'5','OOO...':'6','OOOO..':'7','O.OO..':'8','.OO...':'9','.OOO..':'0'
+    }
 
 # User input is in English
-def english_to_braille(user_input: str):
+def english_to_braille(user_input):
     '''Takes string in english and transforms it into braille and prints it to the terminal'''
 
     response = ''
@@ -59,46 +69,41 @@ def braille_to_english(user_input):
         response = 'Invalid input'
         print(response)
     else:
-        # Turn dictionaries into lists to be able to reverse search with values
-        key_list = list(alphabet.keys())
-        value_list = list(alphabet.values())
-        number_key_list = list(numbers.keys())
         # Flags to indicate if captial or number
         capital = False
         number = False
 
         # Edge case: Check if braille text ends with capital or number, therefore it is invalid
-        if key_list[value_list.index(user_input[-6:])].__eq__('capital') or key_list[value_list.index(user_input[-6:])].__eq__('number'):
+        if braille_alphabet.get(user_input[-6:]).__eq__('capital') or braille_alphabet.get(user_input[-6:]).__eq__('number'):
             response = 'Invalid input'
             print(response)
         else:
             #For loop jumps every 6 as every 6 characters in braille represent 1 character
             for i in range(0, len(user_input), 6):
-                x = user_input[i:i+6]
-                index = value_list.index(x)
+                letter = user_input[i:i+6]
 
                 # Flag capital
-                if key_list[index].__eq__('capital'):
+                if braille_alphabet.get(letter).__eq__('capital'):
                     capital = True
                 # Flag number
-                elif key_list[index].__eq__('number'):
+                elif braille_alphabet.get(letter).__eq__('number'):
                     number = True
                 # If space, flag number to be false
-                elif key_list[index].__eq__(' '):
-                    response += key_list[index]
+                elif braille_alphabet.get(letter).__eq__(' '):
+                    response += braille_alphabet.get(letter)
                     number = False
                 else:
                     # Capital letter
                     if capital == True:
-                        response += key_list[index].upper()
+                        response += braille_alphabet.get(letter).upper()
                         capital = False
                     # Number
                     elif number == True:
-                        response += number_key_list[index]
+                        response += braille_numbers.get(letter)
                     # simple letter
                     else:
                         number = False
-                        response += key_list[index]
+                        response += braille_alphabet.get(letter)
             print(response)
 
 
@@ -112,11 +117,14 @@ def main(arg1):
         braille_to_english(user_input)
     # Edge case: if input is invalid
     else:
-        print("!!")
         print("Invalid input")
     
 if __name__ == '__main__':
     # Take every arugment after the file name and turn into string
     arg1 = sys.argv[1:]
     arg1 = ' '.join(arg1)
-    main(arg1)
+    # Edge case: if the input is empty: user hasn't inputted anything
+    if not arg1:
+        print("Invalid input")
+    else:
+        main(arg1)
