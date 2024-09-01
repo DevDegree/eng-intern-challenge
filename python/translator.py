4039790c6aa8a15baf879bfb2d9cd2e78ac7fb4f
yasmeen45 <a.yasmeen3542@gmail.com>
2024-09-01 13:56:35 -0700

    BrailleTranslator class skeleton

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,65 @@
+import sys
 
+class BrailleTranslator:
+    """
+    A class used to translate between English and Braille
+
+    Supports the entire English alphabet, capitalization, 
+    spaces, and numbers. Can be extended to support other 
+    symbols
+    """
+    # Braille symbols
+    CAPITAL_SYMBOL = ''
+    NUMBER_SYMBOL  = ''
+    SPACE_SYMBOL = ''
+    
+    # Character translation dictionaries
+    BRAILLE_TO_ENGLISH = {}
+    BRAILLE_TO_NUMBER  = {}
+    ENGLISH_TO_BRAILLE = {}
+    NUMBER_TO_BRAILLE  = {}
+
+    def __init__(self):
+        pass
+
+    def is_braille(self, input_str: str) -> bool:
+        """
+        Checks if the input string is in Braille
+        """
+        pass
+
+    def translate_to_english(self, braille_str: str) -> str:
+        """
+        Translates a Braille string to English
+        """
+        pass
+
+    def translate_to_braille(self, english_str: str) -> str:
+        """
+        Translates an English string to Braille
+        """
+        pass
+
+    def translate(self, input_str: str) -> str:
+        """
+        Translates input to the appropriate output, based on input type
+        """
+        pass
+
+
+def main():
+    # Read command line arguments
+    if len(sys.argv) > 1:
+        input_str = ' '.join(sys.argv[1:])
+        translator = BrailleTranslator()
+        
+        try:
+            output = translator.translate(input_str)
+            print(output)
+        except ValueError as e:
+            print(f"Error: {e}")
+    else:
+        print("Please provide a string to translate")
+
+if __name__ == "__main__":
+    main()
