770b48a09436846d1a08e867dcfa97bd97acab79
jazeelius <42842694+jazeelius@users.noreply.github.com>
2024-09-01 15:03:13 -0400

    solution

--- python/translator.py
+++ python/translator.py
@@ -1,5 +1,4 @@
 import sys
-
 alphabet = {
     'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..', 'f': 'OOO...', 'g': 'OOOO..', 
     'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..', 'k': 'O...O.', 'l': 'O.O.O.', 'm': 'OO..O.', 'n': 'OO.OO.',
