29763abec575c21bb73d88d49eeadb696683abe3
SebastianN3 <sebastian.navasch@gmail.com>
2024-09-01 18:07:49 -0400

    Read command line args

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,7 @@
+import sys
 
+if __name__ == "__main__":
+    if (len(sys.argv) <= 1):
+        print("Error: Not enough arguments.")
+        print("Usage: python3 translator.py <words to translate>")
+        exit()
