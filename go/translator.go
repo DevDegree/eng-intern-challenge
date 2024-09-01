1d1de4616bb14e335d9eb37934431899cb040bd5
ahdernasr <44983175+ahdernasr@users.noreply.github.com>
2024-09-01 13:35:41 +0200

    feat(cli): add command-line argument handling

--- go/translator.go
+++ go/translator.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Please provide a string to translate.")
+		return
+	}
+
+	input := os.Args[1]
+
+	fmt.Println(input)
+
+	// if isBraille(input) {
+	// 	fmt.Println(translateToEnglish(input))
+	// } else {
+	// 	fmt.Println(translateToBraille(input))
+	// }
+}
