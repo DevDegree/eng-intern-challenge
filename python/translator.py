8fa6cfad288d43e6b74a06acf3fa6ce872d756f4
Hasan-Al-Khazraji <37431365+Hasan-Al-Khazraji@users.noreply.github.com>
2024-09-01 14:19:45 +0300

    Create braille to english translator

--- python/translator.py
+++ python/translator.py
@@ -1,19 +1,21 @@
-import sys
+import sys, re
 
 # English to braille
 braille_map = {
-    'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..',
-    'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..',
     '1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..', '5': 'O..O..',
     '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...', '0': '.OOO..',
+    'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..',
+    'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..',
     'k': 'O...O.', 'l': 'O.O.O.', 'm': 'OO..O.', 'n': 'OO.OO.', 'o': 'O..OO.',
     'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.',
     'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO', 'y': 'OO.OOO',
     'z': 'O..OOO', 'CAPITAL': '.....O', 'DECIMAL': '.O...O', 'NUMBER': '.O.OOO', '.': '..OO.O',
     ',': '..O...', '?': '..O.OO', '!': '..OOO.', ':': '..OO..', ';': '..O.O.',
-    '-': '....OO', '/': '.O..O.', '>': 'O..OO.', '<': '.OO..O', '(': 'O.O..O',
+    '-': '....OO', '/': '.O..O.', '(': 'O.O..O',
     ')': '.O.OO.', ' ': '......'
 }
+
+english_map = {n: i for i, n in braille_map.items()}
 
 def translate_to_braille(text):
     braille_text = ""
@@ -30,6 +32,34 @@ def translate_to_braille(text):
             braille_text += braille_map[' ']
             isNumber = False
     return braille_text
+
+def translate_to_english(text):
+    english_text = ""
+    braille_chars = [text[i:i+6] for i in range(0, len(text), 6)]
+    isCapital, isNumber = False, False
+    for braille in braille_chars:
+        if braille == braille_map['CAPITAL']:
+            isCapital = True
+            continue
+        elif braille == braille_map['NUMBER']:
+            isNumber = True
+            continue
+        elif braille == braille_map[' ']:
+            english_text += " "
+            isNumber = False
+            continue
+        else:
+            if(isNumber):
+                english_text += str(ord(english_map[braille]) - 96)
+                continue
+            elif(isCapital):
+                english_text += english_map[braille].upper()
+                isCapital = False
+                continue
+            else:
+                english_text += english_map[braille]
+    return english_text
+
 
 def main():
     if len(sys.argv) < 2:
