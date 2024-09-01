882ce897d843a8d6e97c88abf889e12f4cdc8740
Sameer Fettes <sameer.fettes@gmail.com>
2024-09-01 11:23:39 -0400

    Created skeleton and isBraille function

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,36 @@
+# Requirements: 
+'''
+-
+
+'''
+
+# Imports
+import sys
+
+# Constants 
+
+# Helpers
+
+# Returns true if the list of strings should be parsed as a Braille string
+def isBraille(args: list[str]) -> bool: 
+    rawString = "".join(args)
+    numOfO = 0
+    numOfDot = 0
+    for char in rawString: 
+        if char == 'O': 
+            numOfO += 1
+        elif char == '.': 
+            numOfDot +=1
+    return numOfO + numOfDot == len(rawString) and len(rawString)%6==0
+    
+        
+
+# main
+def main(): 
+    args = sys.argv[1:]
+    print(isBraille(args)) 
+
+# Entry
+if __name__ == '__main__':
+    main()
 
