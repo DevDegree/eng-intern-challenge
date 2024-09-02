b8d68170b87a05a150dfd6a0cf9e181f5fd8dbc8
Vivian-Tung <viviantung@live.ca>
2024-09-02 12:19:10 -0700

    fix bugs

--- ruby/translator.rb
+++ ruby/translator.rb
@@ -24,7 +24,7 @@ class Translator
     @braille_translation = ""
     @braille_letter_map = {
       "a" => "O.....", "b" => "O.O...", "c" => "OO....", "d" => "OO.O..", "e" => "O..O..",
-      "f" => "OOO...", "g" => "OOOO..", "h" => "O.OO..", "i" => ".O.O..", "j" => ".OOO..",
+      "f" => "OOO...", "g" => "OOOO..", "h" => "O.OO..", "i" => ".OO...", "j" => ".OOO..",
       "k" => "O...O.", "l" => "O.O.O.", "m" => "OO..O.", "n" => "OO.OO.", "o" => "O..OO.",
       "p" => "OOO.O.", "q" => "OOOOO.", "r" => "O.OOO.", "s" => ".OO.O.", "t" => ".OOOO.",
       "u" => "O...OO", "v" => "O.O.OO", "w" => ".OOO.O", "x" => "OO..OO", "y" => "OO.OOO",
@@ -34,19 +34,14 @@ class Translator
       "1" => "O.....", "2" => "O.O...", "3" => "OO....", "4" => "OO.O..", "5" => "O..O..",
       "6" => "OOO...", "7" => "OOOO..", "8" => "O.OO..", "9" => ".O.O..", "0" => ".OOO..",
     }
-    @braille_non_alphanumeric_map = {
-      "." => "OO..O.", "," => "OO.OOO", "?" => "OO.O..", "!" => "OO...O", ":" => "OO..OO",
-      "-" => "OOOO..", "/" => "O.OO.O", "<" => "O..OO.", ">" => ".OO..O", "(" => ".O.OO.",
-      ")" => "O.O..O", " " => "OOOOOO",
-    }
     @applied_braille = {
       "upper" => ".....O",
-      "decimal" => ".O...O",
       "number" =>  ".O.OOO",
+	  " " => "......",
     }
     @english_letter_map = {
     "O....." => "a", "O.O..." => "b", "OO...." => "c", "OO.O.." => "d", "O..O.." => "e",
-    "OOO..." => "f", "OOOO.." => "g", "O.OO.." => "h", ".O.O.." => "i", ".OOO.." => "j",
+    "OOO..." => "f", "OOOO.." => "g", "O.OO.." => "h", ".OO..." => "i", ".OOO.." => "j",
     "O...O." => "k", "O.O.O." => "l", "OO..O." => "m", "OO.OO." => "n", "O..OO." => "o",
     "OOO.O." => "p", "OOOOO." => "q", "O.OOO." => "r", ".OO.O." => "s", ".OOOO." => "t",
     "O...OO" => "u", "O.O.OO" => "v", ".OOO.O" => "w", "OO..OO" => "x", "OO.OOO" => "y",
@@ -57,17 +52,11 @@ class Translator
       "O....." => "1", "O.O..." => "2", "OO...." => "3", "OO.O.." => "4", "O..O.." => "5",
       "OOO..." => "6", "OOOO.." => "7", "O.OO.." => "8", ".O.O.." => "9", ".OOO.." => "0"
     }
-
-    @english_non_alphanumeric = {
-      "OO..O." => ".", "OO.OOO" => ",", "OO.O.." => "?", "OO...O" => "!", "OO..OO" => ":",
-      "OOOO.." => "-", "O.OO.O" => "/", "O..OO." => "<", ".OO..O" => ">", ".O.OO." => "(",
-      "O.O..O" => ")", "OOOOOO" => " "
-    }
     
     @applied_english = {
       ".....O" => "upper",
-      ".O...O" => ".",
       ".O.OOO" => "number", 
+	  "......" => " ",
     }
   end
 
@@ -84,7 +73,6 @@ class Translator
     #validate input string and which function to call
     if @input_string.match?(@braille_pattern)
       if @input_string.length % 6 == 0
-        puts "braille to engl"
         braille_to_english
         puts @english_translation
         return
@@ -92,7 +80,6 @@ class Translator
         return "Invalid Braille"
       end
     else
-      puts "engl to braille"
       english_to_braille
       puts @braille_translation
       return
@@ -100,7 +87,6 @@ class Translator
   end
 
   def braille_to_english()
-    #iterate for every 6 characters and check if its in alpha num or just applied braille 
     #clean input string into arrays 
     @input_string = @input_string.chars
     split_chars = @input_string.each_slice(6)
@@ -109,39 +95,32 @@ class Translator
     i = 0
     while i < cells.length() do 
       cell = cells[i]
-      #puts cell
+      #check for applied symbols
       if @applied_english.has_key?(cell)
-        #puts "in applied braille"
-        #puts applied_english[cell].to_s
-        #if upper
         if @applied_english[cell].to_s == "upper"
-          #puts "in upper case"
-          word = @english_letter_map[cells[i+1]].upcase
+		      i += 1
+          word = @english_letter_map[cells[i]].upcase
           @english_translation += word
-          i += 1
         elsif @applied_english[cell].to_s == "number"
           i += 1
-          while i < cells.length() and cells[i] != "OOOOOO" do #iterate number until thre is a space then turn flag off
-            #puts "in number case"
+          while @applied_english[cell].to_s != " " do #iterate until there is space or end of string 
+            if i == cells.length() #the sequence ends on a numerical value
+              return
+            end
+            #add number 
             @english_translation += @english_numbers_map[cells[i]].to_s
             i += 1
           end
           #it must be a space now 
           @english_translation += @english_letter_map[cells[i]].to_s
           i += 1 
-        else #its a decimal 
-          #puts "in deciamal"
-          @english_translation += @applied_english[cell].to_s
         end
-      elsif @english_letter_map.has_key?(cell)
-        #puts "in letter"
+      elsif @english_letter_map.has_key?(cell) #it is a letter
         @english_translation += @english_letter_map[cell].to_s
-      elsif @english_non_alphanumeric.has_key?(cell)
-        @english_translation += @english_non_alphanumeric[cell].to_s
       end
       i += 1
     end
-    puts @english_translation
+    return @english_translation
   end
 
 
@@ -153,11 +132,11 @@ class Translator
       #alphanumeric 
       if char.match(/^[[:alnum:]]$/)
         #check if number or letter 
-        if char.is_a? Numeric 
-          #need to put number following sign, then keep going and check if previous is 
-          if @input_string[i-1].is_a? Numeric
+        if char.match(/[[:digit:]]/)
+          #check if previous was number, otherwise add the number symbol 
+          if i > 0 and @input_string[i-1].match?(/[[:digit:]]/)
             @braille_translation += @braille_number_map[char].to_s
-          else #need to add the 
+          else #need to add the number symbol
             @braille_translation += @applied_braille["number"].to_s
             @braille_translation += @braille_number_map[char].to_s
           end
@@ -166,16 +145,16 @@ class Translator
           if char == char.upcase  #check case 
             @braille_translation += @applied_braille["upper"].to_s 
           end 
-          @braille_translation += @braille_letter_map[char].to_s
+          @braille_translation += @braille_letter_map[char.downcase].to_s
         end
       else #its non alphanumeric
         @braille_translation += @applied_braille[char].to_s
       end
     end
-    puts @braille_translation
     return @braille_translation 
   end
 end  
+
 
 
 
@@ -191,6 +170,6 @@ end
 #input = "O.OOO."
 #input = ".....OO.....O.O...OO....OOOOOO.O.OOOO.....O.O...OO...." #Abc 123
 #input = ".OOOO.O..O...OO.O..OOOO..O.O..OO.OO.OOOO...O.OOOO......O.O..O....." #testing191
-#translate_to_english(input)
+
 test = Translator.new
 test.translate
