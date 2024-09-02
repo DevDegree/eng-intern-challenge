11c965d585ee2cbd0a53a5e365ffc0dda04ce533
Shaun S <shaunseeram@hotmail.com>
2024-09-01 20:07:52 -0400

    Additional clean up, fixed bug

--- javascript/translator.js
+++ javascript/translator.js
@@ -3,43 +3,48 @@ const query = process.argv.slice(2).join(" ").trim().split("");
 const dictionaryEnglish = {};
 const dictionaryBraille = {};
 
+const english = [..."abcdefghijklmnopqrstuvwxyz1234567890 "];
 const braille = ["O.....", "O.O...", "OO....", "OO.O..", "O..O..", "OOO...", "OOOO..", "O.OO..", ".OO...", ".OOO..", "O...O.", "O.O.O.", "OO..O.", "OO.OO.", "O..OO.", "OOO.O.", "OOOOO.", "O.OOO.", ".OO.O.", ".OOOO.", "O...OO", "O.O.OO", ".OOO.O", "OO..OO", "OO.OOO", "O..OOO", "O.....", "O.O...", "OO....", "OO.O..", "O..O..", "OOO...", "OOOO..", "O.OO..", ".OO...", ".OOO..", "......"];
-
-[..."abcdefghijklmnopqrstuvwxyz1234567890 "].forEach((l, i) => {
-    dictionaryEnglish[l] = braille[i];
-    dictionaryBraille[braille[i]] ? dictionaryBraille[braille[i]] += l : dictionaryBraille[braille[i]] = l;
-});
 
 const options = {
     capital: ".....O",
     number: ".O.OOO",
 };
 
-const determinator = query.every(l => l === "." || l === "O") ? "Braille" : "English"
+english.forEach((l, i) => {
+    dictionaryEnglish[l] = braille[i];
+    dictionaryBraille[braille[i]] ? dictionaryBraille[braille[i]] += l : dictionaryBraille[braille[i]] = l;
+});
 
-if (determinator === "English") {
+const englishToBraille = () => {
 
-    const englishToBraille = query.map((l, i) => {
+    const brailleString = query.map((l, i) => {
+
+        if (!english.includes(l.toLowerCase())) {
+            throw new Error(`Invalid character included: ${l}`)
+        }
 
         if (l === " ") {
             return "......"
-        }
-
-        if ( l === l.toUpperCase() && (i === 0 || query[i - 1] !== query[i - 1].toUpperCase()) ) {
-            return options.capital + dictionaryEnglish[l.toLowerCase()]
         }
 
         if (!isNaN(+l) && (isNaN(+query[i - 1]) || query[i - 1] === " " )) {
             return options.number + dictionaryEnglish[l]
         }
 
+        if ( l === l.toUpperCase() && (i === 0 || query[i - 1] !== query[i - 1].toUpperCase()) ) {
+            return options.capital + dictionaryEnglish[l.toLowerCase()]
+        }
+
         return dictionaryEnglish[l.toLowerCase()]
 
     }).join("")
 
-    console.log(englishToBraille)
+    console.log(brailleString)
 
-} else {
+}
+
+const brailleToEnglish = () => {
 
     const brailleSegments = query.reduce((cT, cV) => {
         cT[cT.length - 1].length === 6 ? cT.push(cV) : cT[cT.length - 1] += cV
@@ -49,7 +54,11 @@ if (determinator === "English") {
     let capital = false
     let number = false
 
-    const brailleToEnglish = brailleSegments.map(segment => {
+    const englishString = brailleSegments.map(segment => {
+
+        if (!braille.includes(segment) && segment !== options.capital && segment !== options.number) {
+            throw new Error(`Invalid braille character included. Please check input.`)
+        }
 
         if (segment === options.capital || capital) { 
             if (capital) {
@@ -77,6 +86,8 @@ if (determinator === "English") {
 
     }).filter(char => char !== "SKIP").join("")
 
-    console.log(brailleToEnglish)
+    console.log(englishString)
 
 }
+
+query.every(l => l === "." || l === "O") ? brailleToEnglish() : englishToBraille();
