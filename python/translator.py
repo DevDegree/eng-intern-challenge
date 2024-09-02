626b2a438fde9fdc1b10ed6eb4a9ae1eb6404f9b
AshwinM1523 <ashwinmallik@hotmail.com>
2024-09-01 21:10:10 -0400

    final solution

--- python/translator.py
+++ python/translator.py
@@ -1,3 +1,4 @@
+# O(n) time complexity, where n is the length of the input string.
 import sys
 
 braille_to_english_letters = {
@@ -37,24 +38,76 @@ english_to_braille = {
 }
 
 def verify_braille(input_string):
+    """Check if the input string is in Braille format."""
     for char in input_string:
         if char not in ('.', 'O'):
             return False
     return True
 
 def braille_to_eng(braille_string):
-    return 42
+    """Translate Braille input to English."""
+    translated = []
+    is_capital_letter = False
+    is_number = False
+
+    for i in range(0, len(braille_string), 6):
+        char = braille_string[i:i+6]
+
+        if char == "......":
+            # Append space if 6 dots are empty
+            translated.append(' ')
+            is_number = False
+        elif char == ".O.OOO":
+            # Is number indicator
+            is_number = True
+        elif char == ".....O":
+            # Is Capital letter indicator
+            is_capital_letter = True
+        else:
+            if is_number:
+                letter = braille_to_numbers.get(char)
+            else:
+                letter = (braille_to_english_letters.get(char) or braille_to_special.get(char))
+            if is_capital_letter and letter.isalpha():
+                letter = letter.upper()  # Capitalize letter if needed
+                is_capital_letter = False
+            translated.append(letter)
+
+    return ''.join(translated)
 
 
 def eng_to_braille(english_string):
-    return 42
+    """Translate English input to Braille without spaces between chunks."""
+    translated = []
+    is_number = False
+
+    for char in english_string:
+        if char.isdigit():
+            if not is_number:
+                # Undicate that a number follows
+                translated.append(english_to_braille['number follows'])
+                is_number = True
+            translated.append(english_to_braille.get(char))
+        else:
+            is_number = False
+            if char.isupper():
+                # Indicate that a capital letter follows
+                translated.append(english_to_braille['capital follows'])
+                translated.append(english_to_braille.get(char.lower()))
+            else:
+                translated.append(english_to_braille.get(char))
+    return ''.join(translated)
 
 def main():
     if len(sys.argv) != 2:
         print("Please use the format of: translator.py argument")
         sys.exit(1)
 
-    return 42
+    input_string = sys.argv[1]
+    if verify_braille(input_string):
+        print(braille_to_eng(input_string))
+    else:
+        print(eng_to_braille(input_string))
 
 if __name__ == "__main__":
     main()
