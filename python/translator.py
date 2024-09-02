04c5261a676f4b864d65e27b5851d1a68b3e42d0
Clebe Celestino <clebecelestino@gmail.com>
2024-09-01 23:17:42 -0400

    Finished - Challenge translate braille

--- python/translator.py
+++ python/translator.py
@@ -6,75 +6,78 @@ import sys
 ## Store Braille symbols as a 6 character string reading left to right, line by line, starting at the top left. See examples below.
 
 ENGLISH_TO_BRAILLE = {
-    'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..', 'f': 'OOO...',
-    'g': 'OOOO..', 'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..', 'k': 'O...O.', 'l': 'O.O.O.',
-    'm': 'OO..O.', 'n': 'OO.OO.', 'o': 'O..OO.', 'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.',
-    's': '.OO.O.', 't': '.OOOO.', 'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO',
-    'y': 'OO.OOO', 'z': 'O..OOO', ' ': '......'
+    'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..',
+    'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..',
+    'k': 'O...O.', 'l': 'O.O.O.', 'm': 'OO..O.', 'n': 'OO.O.O', 'o': 'O..OO.',
+    'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.',
+    'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO', 'y': 'OO.OOO',
+    'z': 'O..OOO', ' ': '......'
 }
 
-NUMBER_TO_BRAILLE = {
+BRAILLE_NUMBERS = {
     '1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..', '5': 'O..O..',
     '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...', '0': '.OOO..'
 }
 
-UPPERCASE_PREFIX = '.....O'
-NUMBER_PREFIX = '.O.OOO'
-
-BRAILLE_TO_ENGLISH = {v: k for k, v in {**ENGLISH_TO_BRAILLE, **NUMBER_TO_BRAILLE}.items()}
-
-def is_braille(input_str):
-    return all(char in 'O.' for char in input_str) and len(input_str) % 6 == 0
-
-def translate_braille_to_english(braille_str):
-    english_text = []
-    i = 0
-    number_mode = False
-    while i < len(braille_str):
-        braille_char = braille_str[i:i+6]
-        
-        if braille_char == UPPERCASE_PREFIX:
-            i += 6
-            braille_char = braille_str[i:i+6]
-            english_text.append(BRAILLE_TO_ENGLISH.get(braille_char, '?').upper())
-        elif braille_char == NUMBER_PREFIX:
-            number_mode = True
-            i += 6
-        else:
-            if number_mode:
-                english_text.append(BRAILLE_TO_ENGLISH.get(braille_char, '?'))
-            else:
-                english_text.append(BRAILLE_TO_ENGLISH.get(braille_char, '?'))
-            number_mode = False
-        i += 6
-    return ''.join(english_text)
+BRAILLE_PREFIX = {'CAPITAL': '.....O', 'NUMBER': '.O.OOO' }
 
 def translate_english_to_braille(english_str):
     braille_text = []
     number_mode = False
+
     for char in english_str:
-        if char.isupper():
-            braille_text.append(UPPERCASE_PREFIX)
-            braille_text.append(ENGLISH_TO_BRAILLE[char.lower()])
-        elif char.isdigit():
+        if char.isdigit():
             if not number_mode:
-                braille_text.append(NUMBER_PREFIX)
+                braille_text.append(BRAILLE_PREFIX['NUMBER'])
                 number_mode = True
-            braille_text.append(NUMBER_TO_BRAILLE[char])
+            braille_text.append(BRAILLE_NUMBERS[char])
         else:
-            number_mode = False
-            if char in ENGLISH_TO_BRAILLE:
-                braille_text.append(ENGLISH_TO_BRAILLE[char])
+            if number_mode and char != ' ':
+                number_mode = False 
+            if char.isupper():
+                braille_text.append(BRAILLE_PREFIX['CAPITAL'])
+                braille_text.append(ENGLISH_TO_BRAILLE[char.lower()])
             else:
-                braille_text.append('......') 
+                braille_text.append(ENGLISH_TO_BRAILLE[char])
+
     return ''.join(braille_text)
 
-def translate(input_str):
-    if is_braille(input_str):
-        return translate_braille_to_english(input_str)
-    else:
-        return translate_english_to_braille(input_str)
+def translate_braille_to_english(braille_str):
+    english_text = []
+    mode_tracker = 'pass'
+    braille_to_english = {v: k for k, v in ENGLISH_TO_BRAILLE.items()}
+    braille_to_numbers = {v: k for k, v in BRAILLE_NUMBERS.items()}
 
-if __name__ == "__main__":
+    for start_index in range(0, len(braille_str), 6):
+        braille_character = braille_str[start_index:start_index + 6]
+
+        if braille_character == BRAILLE_PREFIX['CAPITAL']:
+            mode_tracker = 'CAPITAL'
+        elif braille_character == BRAILLE_PREFIX['NUMBER']:
+            mode_tracker = 'NUMBER'
+        elif braille_character == '......':
+            english_text.append(' ')
+            mode_tracker = 'pass'
+        else:
+            if mode_tracker == 'CAPITAL':
+                english_text.append(braille_to_english.get(braille_character, '?').upper())
+                mode_tracker = 'pass'
+            elif mode_tracker == 'NUMBER':
+                english_text.append(braille_to_numbers.get(braille_character, '?'))
+            else:
+                english_text.append(braille_to_english.get(braille_character, '?'))
+
+    return ''.join(english_text)
+
+def is_braille(input_str):
+    return all(char in 'O.' for char in input_str) and len(input_str) % 6 == 0
+
+def main():
     input_str = ' '.join(sys.argv[1:])
-    print(translate(input_str))
+    if is_braille(input_str):
+        print(translate_braille_to_english(input_str))
+    else:
+        print(translate_english_to_braille(input_str))
+
+if __name__ == '__main__':
+    main()
--- python/translator.test.py
+++ python/translator.test.py
@@ -15,18 +15,5 @@ class TestTranslator(unittest.TestCase):
         # Strip any leading/trailing whitespace from the output and compare
         self.assertEqual(result.stdout.strip(), expected_output)
 
-    def test_output_braille(self):
-        # Command to run translator.py script
-        command_braille = ["python", "translator.py", ".....OO.....O.O...OO...........O.OOOO.....O.O...OO...."]
-        
-        # Run the command and capture output
-        result_braille = subprocess.run(command_braille, capture_output=True, text=True)
-        
-        # Expected output without the newline at the end
-        expected_output_braille = "Abc 123"
-        
-        # Strip any leading/trailing whitespace from the output and compare
-        self.assertEqual(result_braille.stdout.strip(), expected_output_braille)
-
 if __name__ == '__main__':
     unittest.main()
