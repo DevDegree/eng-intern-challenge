9f6f6f803113acfa4aee22ea5a9c0d8035a305ea
rupokghosh <rupokghosh291@gmail.com>
2024-09-02 01:57:02 -0700

    finish translator' '

--- python/translator.py
+++ python/translator.py
@@ -1,55 +1,120 @@
 import sys
 
-# all mappings
-braille_to_english = {
-    'O.....': 'a', 'O.O...': 'b', 'OO...O': 'c', 'OO.O.O': 'd', 'O.O.O.': 'e',
-    'O..OO.': 'f', 'O..OOO': 'g', 'O..O.O': 'h', '.O.O.O': 'i', '.O.OOO': 'j',
-    'O...O.': 'k', 'O.O..O': 'l', 'OO...O': 'm', 'OO.O.O': 'n', 'O.O.O.': 'o',
-    'O..OO.': 'p', 'O..OOO': 'q', 'O..O.O': 'r', '.O.O.O': 's', '.O.OOO': 't',
-    'O...O.': 'u', 'O.O..O': 'v', '.O.O.O': 'w', 'OO...O': 'x', 'OO.O.O': 'y',
-    'O.O.O.': 'z', '......': ' ', # space
-    # numbers
-    '.....O.': '1', '.....OO': '2', '.....O.O': '3', '.....O.OO': '4', '.....O.O.': '5',
-    '.....OOO': '6', '.....OOOO': '7', '.....OO.O': '8', '.....O.OOO': '9', '.....O....': '0'
+# cnstants 
+CAPITAL_LETTER = '.....O'
+NUMBER_PREFIX = '.O.OOO'
+BLANK = '......'
+
+# translation 
+BRAILLE_ALPHA = {
+    'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..', 'f': 'OOO...', 'g': 'OOOO..',
+    'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..', 'k': 'O...O.', 'l': 'O.O.O.', 'm': 'OO..O.', 'n': 'OO.OO.',
+    'o': 'O..OO.', 'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.', 'u': 'O...OO',
+    'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO', 'y': 'OO.OOO', 'z': 'O..OOO',
 }
 
-english_to_braille = {v: k for k, v in braille_to_english.items()}
+BRAILLE_NUM = {
+    '1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..', '5': 'O..O..', '6': 'OOO...', '7': 'OOOO..',
+    '8': 'O.OO..', '9': '.OO...', '0': '.OOO..',
+}
 
-def translate_to_braille(text):
-    braille = ""
+def create_reverse_dicts():
+    """
+    Generates reverse lookup dictionaries for Braille translation.
+    """
+    alpha_reverse = {}
+    num_reverse = {}
+
+    for letter, braille in BRAILLE_ALPHA.items():
+        alpha_reverse[braille] = letter
+        alpha_reverse[letter] = braille
+
+    for digit, braille in BRAILLE_NUM.items():
+        num_reverse[braille] = digit
+        num_reverse[digit] = braille
+
+    return alpha_reverse, num_reverse
+
+def text_to_braille(text, alpha_rev_dict, num_rev_dict):
+    """
+    Converts regular text to Braille representation.
+    """
+    braille_result = ''
+    is_number_sequence = False
+
     for char in text:
-        if char.lower() in english_to_braille:
-            braille += english_to_braille[char.lower()]
+        if char.isalpha():
+            if char.isupper():
+                braille_result += CAPITAL_LETTER
+                char = char.lower()
+            braille_result += alpha_rev_dict[char]
+            is_number_sequence = False
+        elif char.isdigit():
+            if not is_number_sequence:
+                braille_result += NUMBER_PREFIX
+                is_number_sequence = True
+            braille_result += num_rev_dict[char]
         elif char == ' ':
-            braille += '......'
+            braille_result += BLANK
+            is_number_sequence = False
         else:
-            braille += '' 
-    return braille
+            print(f"Character '{char}' is not supported.")
+            sys.exit(1)
 
-def translate_to_english(braille):
-    english = ""
-    for i in range(0, len(braille), 6):
-        char = braille[i:i+6]
-        if char in braille_to_english:
-            english += braille_to_english[char]
-        elif char == '......':
-            english += ' '
-    return english
+    return braille_result
 
-def is_braille(text):
-    return 'O' in text or '.' in text
+def braille_to_text(braille, alpha_rev_dict, num_rev_dict):
+    """
+    Converts Braille representation back to regular text.
+    """
+    text_result = ''
+    current_braille = ''
+    is_capital = False
+    is_number = False
+
+    for index, char in enumerate(braille):
+        current_braille += char
+
+        if (index + 1) % 6 == 0:
+            if current_braille == BLANK:
+                text_result += ' '
+                is_number = False
+            elif current_braille == CAPITAL_LETTER:
+                is_capital = True
+            elif current_braille == NUMBER_PREFIX:
+                is_number = True
+            else:
+                try:
+                    if is_number:
+                        text_result += num_rev_dict[current_braille]
+                    else:
+                        if is_capital:
+                            text_result += alpha_rev_dict[current_braille].upper()
+                            is_capital = False
+                        else:
+                            text_result += alpha_rev_dict[current_braille]
+                except KeyError:
+                    print(f"Braille character '{current_braille}' is not recognized.")
+                    sys.exit(1)
+            current_braille = ''
+
+    return text_result
 
 def main():
-    if len(sys.argv) != 2:
-        print('Error! Please use: python3 translator.py "text"')
+    alpha_rev_dict, num_rev_dict = create_reverse_dicts()
+
+    if len(sys.argv) < 2:
+        print("No input provided.")
         sys.exit(1)
-    
-    text = sys.argv[1]
 
-    if is_braille(text):
-        print(translate_to_english(text))
+    input_str = ' '.join(sys.argv[1:])
+    if input_str[0] in {'.', 'O'}:
+        if len(input_str) % 6 != 0:
+            print("Invalid Braille format.")
+            sys.exit(1)
+        print(braille_to_text(input_str, alpha_rev_dict,num_rev_dict))
     else:
-        print(translate_to_braille(text))
+        print(text_to_braille(input_str, alpha_rev_dict, num_rev_dict))
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
