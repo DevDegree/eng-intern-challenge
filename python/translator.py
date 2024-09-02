af9035e8ad792431c3f96e1e2821d05fd8b5ca91
Tanuj <tanuj.devjani@mail.utoronto.ca>
2024-09-01 22:46:54 -0400

    Added a README file with instructions, description & assumptions. Added a few comments to Translator.

--- python/translator.py
+++ python/translator.py
@@ -1,26 +1,26 @@
 import sys
 
 # Braille dictionary (alphabet -> braille)
-braille_dict = {
+english_to_braille_dict = {
     'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..',
     'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..',
     'k': 'O...O.', 'l': 'O.O.O.', 'm': 'OO..O.', 'n': 'OO.OO.', 'o': 'O..OO.',
     'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.',
     'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO', 'y': 'OO.OOO', 'z': 'O..OOO',
     ' ': '......',
-    'cap': '.....O',  # Capitalization leadd
+    'cap': '.....O',  # Capitalization lead
     'num': '.O.OOO',  # New number lead
 }
 
 # Reverse mapping of Braille to English
-english_dict = {b: e for e, b in braille_dict.items()}
+braille_to_english_dict = {b: e for e, b in english_to_braille_dict.items()}
 
-number_braille_dict = {
+number_to_braille_dict = {
     'O.....': '1', 'O.O...': '2', 'OO....': '3', 'OOO...': '4', 'O..O..': '5',
     'OO.O..': '6', 'OOO.O.': '7', 'O.OO..': '8', '.OO...': '9', '.OOO..': '0'
 }
 
-num_to_braille = {b: e for e, b in number_braille_dict.items()}
+braille_to_number_dict = {b: e for e, b in number_to_braille_dict.items()}
 
 
 def is_braille(input_string : str) -> bool:
@@ -60,11 +60,11 @@ def braille_to_english(braille_string: str ) -> str:
 
 
 def handle_special_braille(braille_char: str, english_output: str, capitalize_next: bool, number_mode: bool) -> tuple:
-    if braille_char == braille_dict['cap']:
+    if braille_char == english_to_braille_dict['cap']:
         return True, True, number_mode  # Set capitalize_next to True
-    elif braille_char == braille_dict['num']:
+    elif braille_char == english_to_braille_dict['num']:
         return True, capitalize_next, True  # Set number_mode to True
-    elif braille_char == braille_dict[' ']:
+    elif braille_char == english_to_braille_dict[' ']:
         english_output.append(' ')
         return True, capitalize_next, False  # Set number_mode to False after space
     return False, capitalize_next, number_mode
@@ -72,12 +72,12 @@ def handle_special_braille(braille_char: str, english_output: str, capitalize_ne
 
 def convert_braille_to_char(braille_char: str, number_mode: False) -> str:
     if number_mode:
-        char = number_braille_dict.get(braille_char, '')
+        char = number_to_braille_dict.get(braille_char, '')
         if not char:
             number_mode = False  # Exit number mode if invalid number character
         return char
     else:
-        return english_dict.get(braille_char, '')
+        return braille_to_english_dict.get(braille_char, '')
 
 
 def english_to_braille(english_string: str) -> str:
@@ -87,22 +87,22 @@ def english_to_braille(english_string: str) -> str:
     for char in english_string:
 
         if char.isupper():
-            braille_output.append(braille_dict['cap'])
+            braille_output.append(english_to_braille_dict['cap'])
             char = char.lower()
         
         if char.isdigit() and not number_mode:
-            braille_output.append(braille_dict['num'])  # Add number marker once before digits
+            braille_output.append(english_to_braille_dict['num'])  # Add number marker once before digits
             number_mode = True
         
         if not char.isdigit():
             number_mode = False  # Exit number mode after digits
         
-        if char in braille_dict:
-            braille_output.append(braille_dict[char])
-        elif char in num_to_braille:
-            braille_output.append(num_to_braille[char]) 
+        if char in english_to_braille_dict:
+            braille_output.append(english_to_braille_dict[char])
+        elif char in braille_to_number_dict:
+            braille_output.append(braille_to_number_dict[char]) 
         else:
-            braille_output.append(braille_dict[' '])  # Treat any non-letter, non-number as space
+            braille_output.append(english_to_braille_dict[' '])  # Treat any non-letter, non-number as space
     
     return ''.join(braille_output)
 
@@ -115,14 +115,9 @@ def translate(input_string : str) -> str:
         return english_to_braille(input_string)
 
 
-# if __name__ == 'main':
-#     input_string = sys.argv[1]
-#     print(translate(input_string))
 
 
-inp = "Abc 123 xYz"
-out = ".....OO.....O.O...OO...........O.OOOO.....O.O...OO..........OO..OO.....OOO.OOOO..OOO"
-result = translate(inp)
-gh = translate(out)
-assert result == out, "English to Braille FAILED"
-assert gh == inp, "Braille to English FAILED"
+
+if __name__ == '__main__':
+    input_string = " ".join(sys.argv[1:])
+    print(translate(input_string))
