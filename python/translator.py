6c393e85b377addd3aa9830e31dcdabd9082cf05
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2024-09-01 20:57:26 -0400

    docs: add module docstring with link to harness repo

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,13 @@
+"""CLI application to translate messages between Braille and English.
 
+--------------------------------------------------------------------------------
+IMPORTANT
+--------------------------------------------------------------------------------
+I've created a companion repo to store my additional test cases
+and documentation. This way, I don't accidentally break the
+automated validation by modifying other files in this repo.
+
+You can find the companion repo here:
+https://github.com/callumcurtis/shopify-intern-challenge-harness
+--------------------------------------------------------------------------------
+"""
