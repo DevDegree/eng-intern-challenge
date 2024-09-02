fefb8e0e54d19be7fd25eb25d6d94a24dcbf5d19
Kianuu <krezazadeh@hotmail.com>
2024-09-01 20:28:30 -0400

    Finish core functionality of braille to english Add english to braille conversion next, then complete.

--- python/translator.py
+++ python/translator.py
@@ -4,7 +4,7 @@
 
 import sys
 
-input_str = sys.argv[1]
+#input_str = sys.argv[1]
 
 global_dict = {'a':"0.....", 'b':'0.0...', 'c':'00....', 'd':'00.0..', 'e':'0..0..', 'f':'000...', 
                'g':'0000..', 'h':'0.00..', 'i':'.00...', 'j':".000..", 'k':'0....0', 'l':'0.0.0.', 
@@ -16,8 +16,8 @@ global_dict = {'a':"0.....", 'b':'0.0...', 'c':'00....', 'd':'00.0..', 'e':'0..0
 test_str_e = "XOXOXO" ## --> 00..00 0..00. 00..00 0..00. 00..00 0..00. 
 test_str_b = "0.....0.0....0.0000....." ## --> ab1
 
-keys = global_dict.keys()
-values = global_dict.values() # use this string to index the translation of the characters. Example: '0.....' indexes 0, thrown into keys[0] gives 'a'. 
+keys = list(global_dict.keys())
+values = list(global_dict.values()) # use this string to index the translation of the characters. Example: '0.....' indexes 0, thrown into keys[0] gives 'a'. 
 
 
 def main(val):
@@ -33,8 +33,8 @@ def main(val):
 
 def lang_detect(string):
 
-    if set('0','.') == set(string): # This means it is in braille
-        print(set('0','.') + " and " + set(string))
+    if set(['0','.']) == set(string): # This means it is in braille
+#        print(str(set(['0','.'])) + " and " + str(set(string)))
         return 1 
 
     else: # This means it is in english
@@ -43,44 +43,45 @@ def lang_detect(string):
 def lang_converter(convertee, val): # receives 0 or 1 and terminal value
     
     if convertee == 1:
-        if (len(val) % 6) == 0: 
-        
-            char_end = (len(val) // 6) - 1 
-            char_start = 0
-            message = ''
-            mode = 0 # 0 is lowercase alpha, 1 is uppercase alpha, 2 is numeric. 
+        char_end = 6 
+        char_start = 0
+        message = ''
+        mode = 0 # 0 is lowercase alpha, 1 is uppercase alpha, 2 is numeric. 
 
-            while char_start < len(convertee): # grabs set of 6 characters to be converted from braille to english
-                snippet = val[char_start:char_end]
+        while char_end <= len(val): # grabs set of 6 characters to be converted from braille to english
+            snippet = val[char_start:char_end]
+            
+            if snippet == '.....0':
+                mode = 1
+                if (char_end + 6) <= len(val):
+                    char_end += 6 
+                    char_start += 6
+
+
+            elif snippet == '.0.000':
+                mode = 2
+                if (char_end + 6) <= len(val):
+                    char_end += 6 
+                    char_start += 6
+
+
+            else:
+                char_end += 6 
+                char_start += 6
+                message += char_retrieval(snippet, mode)
                 mode = 0
-                if snippet == '.....0':
-                    mode = 1
-                    if (char_end + 7) <= len(val):
-                        char_end += 6 
-                        char_start += 6
-                        snippet = val[char_start:char_end]
-                        message += char_retrieval(snippet, mode)
-
-                elif snippet == '.0.0000':
-                    mode = 2
-                    if (char_end + 7) <= len(val):
-                        char_end += 6 
-                        char_start += 6
-                        snippet = val[char_start:char_end]
-                        message += char_retrieval(snippet, mode)
+        return message
 
 
 def char_retrieval(snippet, mode):
     if mode == 1: # returns uppercase
-        print(keys[values.index(snippet)].upper())
         return keys[values.index(snippet)].upper()
     elif mode == 2: #returns numeric value
-        print(keys[keys[values.index(snippet)]])
-        return keys[keys[values.index(snippet)]]
+        return keys[values.index(keys[values.index(snippet)])]
     else: # base case
         return keys[values.index(snippet)]
 
 
-main(input_str)
+main(test_str_b)
 
 
