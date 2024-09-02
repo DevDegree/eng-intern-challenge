3434c87a96101cbe5735629c5542e291b130ae67
Roxie Reginold <roxie.reginold@torontomu.ca>
2024-09-01 20:56:17 -0400

    adding dictionaries for conversion

--- python/translator.py
+++ python/translator.py
@@ -1,7 +1,28 @@
-def braille_to_english_translator(string):
+# Mapping dictionaries for Braille to English and English to Braille conversions
+braille_to_english = {
+    "O.....": "A", "O.O...": "B", "OO....": "C", "OO.O..": "D", "O..O..": "E",
+    "OOO...": "F", "OOOO..": "G", "O.OO..": "H", ".OO...": "I", ".OOO..": "J",
+    "O...O.": "K", "O.O.O.": "L", "OO..O.": "M", "OO.OO.": "N", "O..OO.": "O",
+    "OOO.O.": "P", "OOOOO.": "Q", "O.OOO.": "R", ".OO.O.": "S", ".OOOO.": "T",
+    "O...OO": "U", "O.O.OO": "V", ".OOO.O": "W", "OO..OO": "X", "OO.OOO": "Y",
+    "O..OOO": "Z", ".OOOOO": "0", "O.....": "1", "O.O...": "2", "OO....": "3", 
+    "OO.O..": "4", "O..O..": "5", "OOO...": "6", "OOOO..": "7", "O.OO..": "8",
+    ".OOO..": "9", 
+}
+
+# Create reverse mapping for English to Braille conversion
+english_to_braille = {v: k for k, v in braille_to_english.items()}
 
 
-
-
-def english_to_braille_translator(string):
+def braille_to_english_translator(text):
+    final_result = ''
     
+    return final_result
+
+
+
+
+def english_to_braille_translator(text):
+    final_result = ''
+    
+    return final_result
