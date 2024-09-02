f6c9fed2d8a596fca7d86143fe22ca8e783669f2
Tanuj <tanuj.devjani@mail.utoronto.ca>
2024-09-01 20:32:52 -0400

    Added initial skeleton

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,9 @@
+import sys
 
+def translate(input_string : str) -> str:
+    # TODO: Implement this function
+    return ""
+
+if __name__ == 'main':
+    input_string = sys.argv[1]
+    print(translate(input_string))
