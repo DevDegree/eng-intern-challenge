f6107024d70da92245c6f6a32ea2aba949a6fe48
liran <ashbel.liran@gmail.com>
2024-09-02 19:05:00 -0400

    Fixes to overall code and braille to eng

--- python/translator.py
+++ python/translator.py
@@ -1,24 +1,24 @@
 import sys
 
-# braille to english + punctuation map
+# mapping of Braille to english letters and punctuation
 braille_to_english = {
     "O.....": "a", "O.O...": "b", "OO....": "c", "OO.O..": "d", "O..O..": "e",
     "OOO...": "f", "OOOO..": "g", "O.OO..": "h", ".OO...": "i", ".OOO..": "j",
     "O...O.": "k", "O.O.O.": "l", "OO..O.": "m", "OO.OO.": "n", "O..OO.": "o",
     "OOO.O.": "p", "OOOOO.": "q", "O.OOO.": "r", ".OO.O.": "s", ".OOOO.": "t",
     "O...OO": "u", "O.O.OO": "v", ".OOO.O": "w", "OO..OO": "x", "OO.OOO": "y",
-    "O..OOO": "z", "..OO.O": ".", "..O...": ",", "..O.OO": "?", "..OOO.": "!", "..OO..": ":",
-    "..O.O.": ";", "....OO": "-", ".O..O.": "/", ".OO..O": "<", "O..OO.": ">",
-    "O.O..O": "(", ".O.OO.": ")", "......": " ",
+    "O..OOO": "z", "..OO.O": ".", "..O...": ",", "..O.OO": "?", "..OOO.": "!",
+    "..OO..": ":", "..O.O.": ";", "....OO": "-", ".O..O.": "/", ".OO..O": "<",
+    "O..OO.": ">", "O.O..O": "(", ".O.OO.": ")", "......": " ",
 }
 
-# braille to number map
+# mapping of Braille to numbers (0-9)
 braille_to_num = {
     "O.....": "1", "O.O...": "2", "OO....": "3", "OO.O..": "4", "O..O..": "5",
     "OOO...": "6", "OOOO..": "7", "O.OO..": "8", ".OO...": "9", ".OOO..": "0",
 }
 
-# reverse lookup dictionaries
+# reverse lookup dictionaries to convert english letters and nums to braille
 english_to_braille = {v: k for k, v in braille_to_english.items()}
 num_to_braille = {v: k for k, v in braille_to_num.items()}
 
@@ -30,34 +30,40 @@ decimal_prefix = ".O...O"  # indicates that a decimal follows
 # function to check if the given string is braille or english
 def braille_check(string):
 
-    # return true if the string is braille, otherwise return false (english string)
+    # check if the string is braille, otherwise return false (english string)
     return all(c in ".O" for c in string) and len(string) % 6 == 0
 
 # function to convert braille to english
 def braille_to_eng(braille):
 
     english_text = ""
-    braille_chars = [braille[i:i + 6] for i in range(0, len(braille), 6)]
+    braille_chars = []
+
+    for i in range(0, len(braille), 6):
+        braille_chars.append(braille[i:i + 6])
 
     capitalize_next = False
-    is_mode = False  # flag to track if we are in number mode
+    is_mode = False  # flag to track if in mode
 
+    # iterate over each braille chunk/pattern and translate it to english
     for char in braille_chars:
+
+        # set the capital and/or number mode flags to true if the braille pattern is seen
         if char == capital_prefix:
             capitalize_next = True
             continue
         elif char == number_prefix:
             is_mode = True
             continue
-        elif char == decimal_prefix:
-            continue
-
+        
+        # translate the braille to a number if in mode
         if is_mode:
             translated_char = braille_to_num.get(char)
-            english_text += translated_char
-            continue
-        
-        translated_char = braille_to_english.get(char)
+            if translated_char is None:
+                is_mode = False  # exit number mode if not a valid number
+                translated_char = braille_to_english.get(char, "")
+        else: # translate to english letters or punctutation if not in mode
+            translated_char = braille_to_english.get(char, "")
 
         if capitalize_next:
             translated_char = translated_char.upper()
@@ -73,26 +79,27 @@ def eng_to_braille(english):
     braille_text = ""
     is_mode = False  # flag to track if in number mode
 
+    # iterate over each character in the english string
     for char in english:
+
         if char.isupper():
             braille_text += capital_prefix
             char = char.lower()
             is_mode = False  # exit num mode before letters
 
         if char.isdigit():
-            if not is_mode:  # only prepend the number prefix if not already in number mode #########
+            if not is_mode:  # only prepend the number prefix to the braille string if not in mode
                 braille_text += number_prefix
                 is_mode = True
-            braille_text += num_to_braille.get(char, "")
-        else:
+            braille_text += num_to_braille.get(char, "") # if it is a number then get the number translation to braille
+        else: # otherwise get the english letter/punctuation translation
             braille_text += english_to_braille.get(char, "")
-            is_mode = False
+            is_mode = False  # exit num mode after non-digit
 
     return braille_text
 
 # main function to detect input and perform translation
 def main():
-
     # join all arguments into a single string to handle multiple inputs correctly
     string = " ".join(sys.argv[1:])
 
