01db9344fbd89736b2974bc493cc815571ecbbc4
Hadi <hadi.jafar@torontomu.ca>
2024-09-01 23:01:52 -0400

    Setting up braille codes and checking if input is english or braille

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,67 @@
+import sys
 
+braille_english_letters = {
+    "a":"O.....",
+    "b":"O.O...",
+    "c":"OO....",
+    "d":"OO.O..",
+    "e":"O..O..",
+    "f":"OOO...",
+    "g":"OOOO..",
+    "h":"O.OO..",
+    "i":".OO...",
+    "j":".OOO..",
+    "k":"O...O.",
+    "l":"O.O.O.",
+    "m":"OO..O.",
+    "n":"OO.OO.",
+    "o":"O..OO.",
+    "p":"OOO.O.",
+    "q":"OOOOO.",
+    "r":"O.OOO.",
+    "s":".OO.O.",
+    "t":".OOOO.",
+    "u":"O...OO",
+    "v":"O.O.OO",
+    "w":".OOO.O",
+    "x":"OO..OO",
+    "y":"OO.OOO",
+    "z":"O..OOO",
+    ".":"..OO.O",
+    ",":"..O...",
+    "?":"..O.OO",
+    "!":"..OOO.",
+    ":":"..OO..",
+    ";":"..O.O.",
+    "-":"....OO",
+    "/":".O..O.",
+    "<":".OO..O",
+    ">":"O..OO.",
+    "(":"O,O,,O",
+    ")":".O.OO.",
+}
+braille_english_numbers={
+    "1":"O.....",
+    "2":"O.O...",
+    "3":"OO....",
+    "4":"OO.O..",
+    "5":"O..O..",
+    "6":"OOO...",
+    "7":"OOOO..",
+    "8":"O.OO..",
+    "9":".OO...",
+    "O":".OOO..",
+}
+
+braille_english_specials={
+    "capital":".....O",
+    "decimal":".O...O",
+    "number":".O.OOO",
+    " ":"......"
+}
+
+def input_type_checker(line):
+    if len(line) % 6 == 0:
+        if all(char in {'O', '.'} for char in line):
+            return True #represents braille to english
+    return False # represents english to braille
