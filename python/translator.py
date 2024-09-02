7deb9ec5370ba1a878e1862fc23f3e2365e26f46
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2024-09-01 23:24:04 -0400

    feat: add cli message parser

--- python/translator.py
+++ python/translator.py
@@ -11,3 +11,49 @@ You can find the companion repo here:
 https://github.com/callumcurtis/shopify-intern-challenge-harness
 --------------------------------------------------------------------------------
 """
+
+from __future__ import annotations
+
+
+class CliMessageParser:
+
+    def __init__(
+        self,
+        *,
+        separator: str = " ",
+        split_compound_args: bool = True,
+    ):
+        """
+        Optional keyword arguments:
+            separator: the character(s) to place between tokens in the output message.
+            split_compound_args: whether to split quoted arguments containing multiple tokens.
+        """
+        self.separator = separator
+        self.split_compound_args = split_compound_args
+
+    def parse(self, args: list[str] | None = None):
+        """Returns the message formed by the given cli arguments.
+
+        The first argument is assumed to be the name of the script and
+        is excluded from the message.
+
+        Arguments:
+            args: list of cli arguments
+        """
+        if not args:
+            import sys
+            args = sys.argv
+        if self.split_compound_args:
+            args = [
+                arg
+                for compound in args
+                for arg in compound.split()
+            ]
+        return self.separator.join(args[1:])
+
+
+if __name__ == "__main__":
+    message_parser = CliMessageParser()
+    message = message_parser.parse()
+    print(message)
+
