d2e0e12ad4b45e21ad040e1d804ceb8f1510aea2
Clebe Celestino <clebecelestino@gmail.com>
2024-09-01 22:26:16 -0400

    Challenge translate braille

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,80 @@
+import sys
 
+##Translator
+## Given arguments passed into the program at runtime, determine if the given string should be translated to English or Braille.
+## For Braille, each character is stored as a series of O (the letter O) or . (a period).
+## Store Braille symbols as a 6 character string reading left to right, line by line, starting at the top left. See examples below.
+
+ENGLISH_TO_BRAILLE = {
+    'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..', 'f': 'OOO...',
+    'g': 'OOOO..', 'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..', 'k': 'O...O.', 'l': 'O.O.O.',
+    'm': 'OO..O.', 'n': 'OO.OO.', 'o': 'O..OO.', 'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.',
+    's': '.OO.O.', 't': '.OOOO.', 'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO',
+    'y': 'OO.OOO', 'z': 'O..OOO', ' ': '......'
+}
+
+NUMBER_TO_BRAILLE = {
+    '1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..', '5': 'O..O..',
+    '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...', '0': '.OOO..'
+}
+
+UPPERCASE_PREFIX = '.....O'
+NUMBER_PREFIX = '.O.OOO'
+
+BRAILLE_TO_ENGLISH = {v: k for k, v in {**ENGLISH_TO_BRAILLE, **NUMBER_TO_BRAILLE}.items()}
+
+def is_braille(input_str):
+    return all(char in 'O.' for char in input_str) and len(input_str) % 6 == 0
+
+def translate_braille_to_english(braille_str):
+    english_text = []
+    i = 0
+    number_mode = False
+    while i < len(braille_str):
+        braille_char = braille_str[i:i+6]
+        
+        if braille_char == UPPERCASE_PREFIX:
+            i += 6
+            braille_char = braille_str[i:i+6]
+            english_text.append(BRAILLE_TO_ENGLISH.get(braille_char, '?').upper())
+        elif braille_char == NUMBER_PREFIX:
+            number_mode = True
+            i += 6
+        else:
+            if number_mode:
+                english_text.append(BRAILLE_TO_ENGLISH.get(braille_char, '?'))
+            else:
+                english_text.append(BRAILLE_TO_ENGLISH.get(braille_char, '?'))
+            number_mode = False
+        i += 6
+    return ''.join(english_text)
+
+def translate_english_to_braille(english_str):
+    braille_text = []
+    number_mode = False
+    for char in english_str:
+        if char.isupper():
+            braille_text.append(UPPERCASE_PREFIX)
+            braille_text.append(ENGLISH_TO_BRAILLE[char.lower()])
+        elif char.isdigit():
+            if not number_mode:
+                braille_text.append(NUMBER_PREFIX)
+                number_mode = True
+            braille_text.append(NUMBER_TO_BRAILLE[char])
+        else:
+            number_mode = False
+            if char in ENGLISH_TO_BRAILLE:
+                braille_text.append(ENGLISH_TO_BRAILLE[char])
+            else:
+                braille_text.append('......') 
+    return ''.join(braille_text)
+
+def translate(input_str):
+    if is_braille(input_str):
+        return translate_braille_to_english(input_str)
+    else:
+        return translate_english_to_braille(input_str)
+
+if __name__ == "__main__":
+    input_str = ' '.join(sys.argv[1:])
+    print(translate(input_str))
--- python/translator.test.py
+++ python/translator.test.py
@@ -4,7 +4,7 @@ import subprocess
 class TestTranslator(unittest.TestCase):
     def test_output(self):
         # Command to run translator.py script
-        command = ["python3", "translator.py", "Abc", "123", "xYz"]
+        command = ["python", "translator.py", "Abc", "123", "xYz"]
         
         # Run the command and capture output
         result = subprocess.run(command, capture_output=True, text=True)
@@ -14,6 +14,19 @@ class TestTranslator(unittest.TestCase):
         
         # Strip any leading/trailing whitespace from the output and compare
         self.assertEqual(result.stdout.strip(), expected_output)
+
+    def test_output_braille(self):
+        # Command to run translator.py script
+        command_braille = ["python", "translator.py", ".....OO.....O.O...OO...........O.OOOO.....O.O...OO...."]
+        
+        # Run the command and capture output
+        result_braille = subprocess.run(command_braille, capture_output=True, text=True)
+        
+        # Expected output without the newline at the end
+        expected_output_braille = "Abc 123"
+        
+        # Strip any leading/trailing whitespace from the output and compare
+        self.assertEqual(result_braille.stdout.strip(), expected_output_braille)
 
 if __name__ == '__main__':
     unittest.main()
