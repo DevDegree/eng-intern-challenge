a7fafb2da27e35844376cd5a300d678c3d610475
Hadi <hadi.jafar@torontomu.ca>
2024-09-01 23:23:42 -0400

    adding in english to braille and vice versa functions

--- python/translator.py
+++ python/translator.py
@@ -27,18 +27,6 @@ braille_english_letters = {
     "x":"OO..OO",
     "y":"OO.OOO",
     "z":"O..OOO",
-    ".":"..OO.O",
-    ",":"..O...",
-    "?":"..O.OO",
-    "!":"..OOO.",
-    ":":"..OO..",
-    ";":"..O.O.",
-    "-":"....OO",
-    "/":".O..O.",
-    "<":".OO..O",
-    ">":"O..OO.",
-    "(":"O,O,,O",
-    ")":".O.OO.",
 }
 braille_english_numbers={
     "1":"O.....",
@@ -57,7 +45,19 @@ braille_english_specials={
     "capital":".....O",
     "decimal":".O...O",
     "number":".O.OOO",
-    " ":"......"
+    " ":"......",
+     ".":"..OO.O",
+    ",":"..O...",
+    "?":"..O.OO",
+    "!":"..OOO.",
+    ":":"..OO..",
+    ";":"..O.O.",
+    "-":"....OO",
+    "/":".O..O.",
+    "<":".OO..O",
+    ">":"O..OO.",
+    "(":"O,O,,O",
+    ")":".O.OO."
 }
 
 def input_type_checker(line):
@@ -65,3 +65,89 @@ def input_type_checker(line):
         if all(char in {'O', '.'} for char in line):
             return True #represents braille to english
     return False # represents english to braille
+
+def braille_to_english(line):
+    braille_to_char = {v: k for k, v in braille_english_letters.items()}
+    braille_to_number = {v: k for k, v in braille_english_numbers.items()}
+    braille_to_special = {v: k for k, v in braille_english_specials.items()}
+
+    # Split the Braille text by 6-character chunks
+    braille_chunks = [line[i:i+6] for i in range(0, len(line), 6)]
+    
+    result = []
+    is_number_mode = False
+    is_capital_mode = False
+    
+    for chunk in braille_chunks:
+        if chunk in braille_to_special:
+            special_char = braille_to_special[chunk]
+            if special_char == "capital":
+                is_capital_mode = True
+                continue
+            elif special_char == "number":
+                is_number_mode = True
+                continue
+            elif special_char == " ":
+                is_number_mode = False
+                is_capital_mode = False
+                result.append(" ")
+                continue
+        if is_number_mode:
+            if chunk in braille_to_number:
+                result.append(braille_to_number[chunk])
+            else:
+                result.append("?")  # In case of an unknown chunk
+        else:
+            if chunk in braille_to_char:
+                char = braille_to_char[chunk]
+                if is_capital_mode:
+                    result.append(char.upper())
+                    is_capital_mode = False
+                else:
+                    result.append(char)
+            else:
+                sys.stderr.write("braille to english failed.\n")  # In case of an unknown chunk
+    
+    return ''.join(result)
+
+def english_to_braille(line):
+    char_to_braille = braille_english_letters
+    number_to_braille = braille_english_numbers
+    special_to_braille = braille_english_specials
+
+    result = []
+    is_number_mode = False
+    
+    for char in line:
+        if char.isalpha():
+            if char.isupper():
+                result.append(special_to_braille["capital"])
+                result.append(char_to_braille[char.lower()])
+            else:
+                if is_number_mode:
+                    result.append(special_to_braille["number"])
+                    is_number_mode = False
+                result.append(char_to_braille[char])
+        elif char.isdigit():
+            if not is_number_mode:
+                result.append(special_to_braille["number"])
+                is_number_mode = True
+            result.append(number_to_braille[char])
+        elif char in braille_english_letters:
+            result.append(char_to_braille.get(char, '?'))
+        elif char == ' ':
+            result.append(special_to_braille[" "])
+            is_number_mode = False
+        elif char in ".?!:;,/-<>()":
+            result.append(char_to_braille.get(char, '?'))
+        else:
+            sys.stderr.write("english to braille failed.\n")
+
+    return ''.join(result)
+
+if __name__ == "__main__":
+    line = input()
+    if input_type_checker(line) == True:
+        print(braille_to_english(line))
+    else:
+        print(english_to_braille(line))
