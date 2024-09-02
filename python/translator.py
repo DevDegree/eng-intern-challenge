fc53661b7450506751e3c661fb7e894eb9719f47
AshwinM1523 <ashwinmallik@hotmail.com>
2024-09-01 20:42:15 -0400

    create dict of braille

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,58 @@
+import sys
 
+braille_to_english_letters = {
+    "O.....": "a", "O.O...": "b", "OO....": "c", "OO.O..": "d", "O..O..": "e",
+    "OOO...": "f", "OOOO..": "g", "O.OO..": "h", ".OO...": "i", ".OOO..": "j",
+    "O...O.": "k", "O.O.O.": "l", "OO..O.": "m", "OO.OO.": "n", "O..OO.": "o",
+    "OOO.O.": "p", "OOOOO.": "q", "O.OOO.": "r", ".OO.O.": "s", ".OOOO.": "t",
+    "O...OO": "u", "O.O.OO": "v", ".OOO.O": "w", "OO..OO": "x", "OO.OOO": "y",
+    "O..OOO": "z",
+    ".....O": "capital follows", ".O...O": "decimal follows", ".O.OOO": "number follows",
+}
+
+braille_to_numbers = {
+    "O.....": "1", "O.O...": "2", "OO....": "3", "OO.O..": "4", "O..O..": "5",
+    "OOO...": "6", "OOOO..": "7", "O.OO..": "8", ".OO...": "9", ".OOO..": "0",
+}
+
+braille_to_special = {
+    "..OO.O": ".", "..O...": ",", "..O.OO": "?", "..OOO.": "!", "..OO..": ":",
+    "..O.O.": ";", "....OO": "-", ".O..O.": "/", ".OO..O": "<", "O..OO.": ">",
+    "O.O..O": "(", ".O.OO.": ")", "......": " ",
+}
+
+english_to_braille = {
+    'a': "O.....", 'b': "O.O...", 'c': "OO....", 'd': "OO.O..", 'e': "O..O..",
+    'f': "OOO...", 'g': "OOOO..", 'h': "O.OO..", 'i': ".OO...", 'j': ".OOO..",
+    'k': "O...O.", 'l': "O.O.O.", 'm': "OO..O.", 'n': "OO.OO.", 'o': "O..OO.",
+    'p': "OOO.O.", 'q': "OOOOO.", 'r': "O.OOO.", 's': ".OO.O.", 't': ".OOOO.",
+    'u': "O...OO", 'v': "O.O.OO", 'w': ".OOO.O", 'x': "OO..OO", 'y': "OO.OOO",
+    'z': "O..OOO",
+    '1': "O.....", '2': "O.O...", '3': "OO....", '4': "OO.O..", '5': "O..O..",
+    '6': "OOO...", '7': "OOOO..", '8': "O.OO..", '9': ".OO...", 'O': ".O..",
+    'capital follows': ".....O", 'number follows': ".O.OOO", 'decimal follows': ".O...O",
+    '.': "..OO.O", ',': "..O...", '?': "..O.OO", '!': "..OOO.", ':': "..OO..",
+    ';': "..O.O.", '-': "....OO", '/': ".O..O.", '<': ".OO..O", '>': "O..OO.",
+    '(': "O.O..O", ')': ".O.OO.", ' ': "......",
+}
+
+def verify_braille(input_string):
+    return 42
+
+def braille_to_eng(braille_string):
+    return 42
+
+
+def eng_to_braille(english_string):
+    """Translate English input to Braille without spaces between chunks."""
+    return 42
+
+def main():
+    if len(sys.argv) != 2:
+        print("Please use the format of: translator.py argument")
+        sys.exit(1)
+
+    return 42
+
+if __name__ == "__main__":
+    main()
