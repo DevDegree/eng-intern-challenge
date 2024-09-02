f030446ee1a72adca0c6515701ded5dc8e669022
Parita Dhola <dholaparita@gmail.com>
2024-09-02 12:16:29 -0400

    add translator

--- python/translator.py
+++ python/translator.py
@@ -1,71 +1,86 @@
-brailleToEnglish = {
-    'O.....': 'a', 'O.O...': 'b', 'OO....': 'c', 'OO.O..': 'd', 'O..O..': 'e',
-    'OOO...': 'f', 'OOOO..': 'g', 'O.OO..': 'h', '.OO...': 'i', '.OOO..': 'j',
-    'O...O.': 'k', 'O.O.O.': 'l', 'OO..O.': 'm', 'OO.OO.': 'n', 'O..OO.': 'o',
-    'OOO.O.': 'p', 'OOOOO.': 'q', 'O.OOO.': 'r', '.OO.O.': 's', '.OOOO.': 't',
-    'O...OO': 'u', 'O.O.OO': 'v', '.OOO.O': 'w', 'OO..OO': 'x', 'OO.OOO': 'y',
-    'O..OOO': 'z', '......': ' ', '.....O': '0', '.....O.O': '1', '.....O.OO': '2', 
-    '.....O.OOO': '3', '.....O.OOOO': '4', '.....O.OOOOO': '5', '.....O.OOOOOO': '6',
-    '.....O.OOOOOOO': '7', '.....O.OOOOOOOO': '8', '.....O.OOOOOOOOO': '9', 
-    '..O...': 'capital'  
+import sys
+
+
+BRAILLE_CHARS = {
+    "letters": {
+        chr(i): dots for i, dots in enumerate("O.....", ord('a'))
+    },
+    "numbers": {
+        chr(i): dots for i, dots in enumerate("O.....", ord('1'))
+    },
 }
 
-englishToBraille = {
-    'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..',
-    'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..',
-    'k': 'O...O.', 'l': 'O.O.O.', 'm': 'OO..O.', 'n': 'OO.OO.', 'o': 'O..OO.',
-    'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.',
-    'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO', 'y': 'OO.OOO',
-    'z': 'O..OOO', ' ': '......', '0': '.....O', '1': '.....O.', '2': '.....O.O',
-    '3': '.....O.OO', '4': '.....O.OOO', '5': '.....O.OOOO', '6': '.....O.OOOOO',
-    '7': '.....O.OOOOOO', '8': '.....O.OOOOOOO', '9': '.....O.OOOOOOOO',
-    'capital': '..O...'  
-}
+CAPITAL_FOLLOWS = ".....O"
+NUMBER_FOLLOWS = ".O.OOO"
 
-# Function to check if the input string is Braille
-def isBraille(inputText):
-    for c in inputText:
-        if c != 'O' and c != '.':  # Verify each character is 'O' or '.'
-            return False
-    return True
 
-# Convert English to Braille
-def convertEnglishToBraille(inputEnglish):
-    convertText = ""
-    for char in inputEnglish:
-        if char.isupper():
-            convertText += englishToBraille['capital']  # Add capital marker
-            char = char.lower()  # Convert to lowercase for Braille lookup
-        if char in englishToBraille:
-            convertText += englishToBraille[char]
+def is_braille(text):
+    """Checks if the text consists only of valid Braille characters."""
+    return all(c in BRAILLE_CHARS for c in text)
+
+
+def translate(text, to_braille=True):
+
+    translated_text = ""
+    in_number_mode = False
+
+    for char in text:
+        if to_braille:
+            if char.isupper():
+                translated_text += CAPITAL_FOLLOWS + BRAILLE_CHARS["letters"][char.lower()]
+            elif char.isalpha():
+                translated_text += BRAILLE_CHARS["letters"][char]
+            elif char.isdigit():
+                if not in_number_mode:
+                    translated_text += NUMBER_FOLLOWS
+                    in_number_mode = True
+                translated_text += BRAILLE_CHARS["numbers"][char]
+            elif char == " ":
+                translated_text += BRAILLE_CHARS["space"]
+                in_number_mode = False
+            else:
+                raise ValueError(f"Unknown character: {char}")
         else:
-            convertText += '......'  # For space or unsupported characters
-    return convertText
+            letter = text[:6]
+            if letter == CAPITAL_FOLLOWS:
+                translated_text += BRAILLE_CHARS["letters"][text[6:12]].upper()
+                text = text[12:]
+            elif letter == BRAILLE_CHARS["space"]:
+                translated_text += " "
+                in_number_mode = False
+                text = text[6:]
+            else:
+                if in_number_mode:
+                    translated_text += next(
+                        num
+                        for num, braille in BRAILLE_CHARS["numbers"].items()
+                        if braille == letter
+                    )
+                else:
+                    translated_text += next(
+                        char
+                        for char, braille in BRAILLE_CHARS["letters"].items()
+                        if braille == letter
+                    )
+                text = text[6:]
 
-# Convert Braille to English
-def convertBrailleToEnglish(inputBraille):
-    convertText = ""
-    i = 0
-    while i < len(inputBraille):
-        brailleNumbers = inputBraille[i:i+6]
-        if brailleNumbers == englishToBraille['capital']:
-            i += 6
-            brailleNumbers = inputBraille[i:i+6]
-            convertText += brailleToEnglish.get(brailleNumbers, '`~`').upper()  # Capitalize the letter
-        else:
-            convertText += brailleToEnglish.get(brailleNumbers, '`~`')
-        i += 6
-    return convertText
+    return translated_text
 
-# Main function
-if __name__ == "__main__":
-    inputString = input("Enter the String: ")
 
-    if isBraille(inputString):
-        print("Braille")
-        print("Converted to English: ", convertBrailleToEnglish(inputString))
+def main():
+
+    if len(sys.argv) < 2:
+        sys.exit(1)
+
+    text = " ".join(sys.argv[1:])
+
+    if is_braille(text):
+        translated_text = translate(text, to_braille=False)
     else:
-        print("English")
-        print("Converted to Braille: ", convertEnglishToBraille(inputString))
+        translated_text = translate(text)
+
+    print(translated_text)
 
 
+if __name__ == "__main__":
+    main()
