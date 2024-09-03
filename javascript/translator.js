bbcacdcaaf9d8ddfe15db8b5f578949209e1b236
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-09-03 19:25:16 -0400

    Add the logic to isolate each individual braille character

--- javascript/translator.js
+++ javascript/translator.js
@@ -1,9 +1,21 @@
-const input = '42';
+const input = '.....OO.OO..O..O..O.O.O.O.O.O.O..OO........OOO.OO..OO.O.OOO.O.O.O.OO.O..';
 let output;
 
 if (input.includes('.')) {
-  output = 'Braille';
+  // Braille
+  const brailleCharacters = [];
+  const lengthOfInput = input.length;
+  let index = 0;
+
+  while (index <= lengthOfInput - 1) {
+    const brailleCharacter = input.substring(index, index + 6);
+    brailleCharacters.push(brailleCharacter);
+    index += 6;
+  }
+
+  output = brailleCharacters;
 } else {
+  // English
   output = 'English';
 }
 
