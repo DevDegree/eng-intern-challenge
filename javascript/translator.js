a9af46fc4ea294df54bc7aba651b5df965c7715c
Arnab Das <arnabdas1234@hotmail.com>
2024-09-02 21:51:02 -0400

    works for letters

--- javascript/translator.js
+++ javascript/translator.js
@@ -5,7 +5,6 @@
 //translate braille -> english.
 //how to know if we need to run this function?
 
-
 //this function checks if the string entered is english or braille.
 function natureOfString(str){
     let isEnglish = false
@@ -42,42 +41,67 @@ function computeEngLetter(str){
    return value;
 }
 
+//will be necessary when translating
+function flipString(str){
+    // const arr = Array.from(str)
+    let newstr = "";
+    for(let i = str.length-1; i>-1; i--){
+      newstr += str[i];
+    }
+    return newstr;
+}
+// console.log(flipString("HELLO"));
+
+function convertBrailledigits(str){
+    let newStr = ""
+    for(let i = 0; i<str.length; i++){
+        if(str[i] === "1"){
+            newStr += "O"
+        }else{ //if str[i] === "0"
+            newStr += "."
+        }
+    }
+    return newStr;
+}
+
+
+
 //just to help me copy paste all of the thing below because im too lazy to write it all
 // const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 // for(let i = 0; i<26; i++){
-//     console.log(`engLetters[${i}] = \"${alphabet[i]}\"`)
+//     console.log(`engLetters[${i}] = \"${alphabet[i].toLowerCase()}\"`)
 // }
 
 //computeEngLetter("O...OO")
 const engLetters = new Array(111111)
 //now we translate
 engLetters[0] = " "; //space character
-engLetters[1] = "A"
-engLetters[101] = "B"
-engLetters[11] = "C"
-engLetters[1011] = "D"
-engLetters[1001] = "E"
-engLetters[111] = "F"
-engLetters[1111] = "G"
-engLetters[1101] = "H"
-engLetters[110] = "I"
-engLetters[1110] = "J"
-engLetters[10001] = "K"
-engLetters[10101] = "L"
-engLetters[10011] = "M"
-engLetters[11011] = "N"
-engLetters[11001] = "O"
-engLetters[10111] = "P"
-engLetters[11111] = "Q"
-engLetters[11101] = "R"
-engLetters[10110] = "S"
-engLetters[11110] = "T"
-engLetters[110001] = "U"
-engLetters[110101] = "V"
-engLetters[101110] = "W"
-engLetters[110011] = "X"
-engLetters[111011] = "Y"
-engLetters[111001] = "Z"
+engLetters[1] = "a"
+engLetters[101] = "b"
+engLetters[11] = "c"
+engLetters[1011] = "d"
+engLetters[1001] = "e"
+engLetters[111] = "f"
+engLetters[1111] = "g"
+engLetters[1101] = "h"
+engLetters[110] = "i"
+engLetters[1110] = "j"
+engLetters[10001] = "k"
+engLetters[10101] = "l"
+engLetters[10011] = "m"
+engLetters[11011] = "n"
+engLetters[11001] = "o"
+engLetters[10111] = "p"
+engLetters[11111] = "q"
+engLetters[11101] = "r"
+engLetters[10110] = "s"
+engLetters[11110] = "t"
+engLetters[110001] = "u"
+engLetters[110101] = "v"
+engLetters[101110] = "w"
+engLetters[110011] = "x"
+engLetters[111011] = "y"
+engLetters[111001] = "z"
 
 //To test translation from braille to english
 //Bonjour = "101 11001 11011 1110 11001 110001 11101"
@@ -100,35 +124,37 @@ const EngToBrArr = new Array(26);
 const EngToBrArrIndexReference = new Array(26);
 // const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 // for(let i = 0; i<26; i++){
-//     //console.log(`EngToBrArr[${i}] = \"${alphabet[i]}\"`)
-//     console.log(`EngToBrArrIndexReference[${i}] = ${engLetters.indexOf(alphabet[i])}`)
+//     console.log(`EngToBrArr[${i}] = \"${alphabet[i].toLowerCase()}\"`)
+//     // console.log(`EngToBrArrIndexReference[${i}] = ${engLetters.indexOf(alphabet[i])}`)
 // }
-EngToBrArr[0] = "A"
-EngToBrArr[1] = "B"
-EngToBrArr[2] = "C"
-EngToBrArr[3] = "D"
-EngToBrArr[4] = "E"
-EngToBrArr[5] = "F"
-EngToBrArr[6] = "G"
-EngToBrArr[7] = "H"
-EngToBrArr[8] = "I"
-EngToBrArr[9] = "J"
-EngToBrArr[10] = "K"
-EngToBrArr[11] = "L"
-EngToBrArr[12] = "M"
-EngToBrArr[13] = "N"
-EngToBrArr[14] = "O"
-EngToBrArr[15] = "P"
-EngToBrArr[16] = "Q"
-EngToBrArr[17] = "R"
-EngToBrArr[18] = "S"
-EngToBrArr[19] = "T"
-EngToBrArr[20] = "U"
-EngToBrArr[21] = "V"
-EngToBrArr[22] = "W"
-EngToBrArr[23] = "X"
-EngToBrArr[24] = "Y"
-EngToBrArr[25] = "Z"
+
+EngToBrArr[0] = "a"
+EngToBrArr[1] = "b"
+EngToBrArr[2] = "c"
+EngToBrArr[3] = "d"
+EngToBrArr[4] = "e"
+EngToBrArr[5] = "f"
+EngToBrArr[6] = "g"
+EngToBrArr[7] = "h"
+EngToBrArr[8] = "i"
+EngToBrArr[9] = "j"
+EngToBrArr[10] = "k"
+EngToBrArr[11] = "l"
+EngToBrArr[12] = "m"
+EngToBrArr[13] = "n"
+EngToBrArr[14] = "o"
+EngToBrArr[15] = "p"
+EngToBrArr[16] = "q"
+EngToBrArr[17] = "r"
+EngToBrArr[18] = "s"
+EngToBrArr[19] = "t"
+EngToBrArr[20] = "u"
+EngToBrArr[21] = "v"
+EngToBrArr[22] = "w"
+EngToBrArr[23] = "x"
+EngToBrArr[24] = "y"
+EngToBrArr[25] = "z"
+EngToBrArr[26] = " "
 
 EngToBrArrIndexReference[0] = 1
 EngToBrArrIndexReference[1] = 101
@@ -156,23 +182,54 @@ EngToBrArrIndexReference[22] = 101110
 EngToBrArrIndexReference[23] = 110011
 EngToBrArrIndexReference[24] = 111011
 EngToBrArrIndexReference[25] = 111001
-
-// function translateEngToBr(str){
-
-// }
+EngToBrArrIndexReference[26] = 0
 
 
 
+function translateEngToBr(str){
+    let brailMessage = "";
+    for (let i = 0; i< str.length; i++){
+        const englishLetter = str[i];
+        const brailleIndex = EngToBrArr.indexOf(englishLetter);
+        const brailleNumber = EngToBrArrIndexReference[brailleIndex];
+
+        let prepend = ".".repeat(6 - String(brailleNumber).length)
+        const brailTextwrongFlipped = convertBrailledigits(String(brailleNumber))
+        let correctLetter = flipString(prepend + brailTextwrongFlipped);
+        brailMessage += correctLetter
+    }
+    return brailMessage;
+}
+console.log(translateEngToBr("hello world"));
+
+
+//need to add number functionality
 function translateBrToEng(str){
     if(str.length % 6 !== 0){
         return "invalid braille string, please try again";
     }
+
+    let capitalize = false;
     let decryptedMessage = "";
+    
     for (let i = 0; i < str.length; i += 6) {
-        const letter = str.slice(i, i + 6);
-        // computeEngLetter(letter);
-        decryptedMessage += engLetters[computeEngLetter(letter)];
+        const letter = flipString(str.slice(i, i + 6));
+        const num = computeEngLetter(letter);
+
+        if(num === 100000){ //100000 represents capital follows.
+            capitalize = true;
+            continue //no need to add anything, skip current iteration
+        }
+
+        if(capitalize){
+            decryptedMessage += engLetters[num].toUpperCase();
+            capitalize = false; //set it back to false
+        }
+        else{
+            decryptedMessage += engLetters[num];
+        }
     }
+    // console.log(decryptedMessage)
 }
-translateBrToEng("123456HELLOOBONJOULALALA")
+//translateBrToEng(".....OO.OO..O..O..O.O.O.O.O.O.O..OO........OOO.OO..OO.O.OOO.O.O.O.OO.O..");
 
