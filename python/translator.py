e49e8e59fd16d88281a55420c1fe468aedc84d7e
Vraj Bhavsar <vraj137@hotmail.com>
2024-09-02 22:58:26 -0400

    Added more test cases

--- python/translator.py
+++ python/translator.py
@@ -4,7 +4,7 @@ import sys
 # Braille to English Mapping
 braille_to_english = {
     'O.....': 'a', 'O.O...': 'b', 'OO....': 'c', 'OO.O..': 'd', 'O..O..': 'e',
-    'OOO...': 'f', 'OOOO..': 'g', 'O.OO..': 'h', '.O.O..': 'i', '.OOO..': 'j',
+    'OOO...': 'f', 'OOOO..': 'g', 'O.OO..': 'h', '.OO...': 'i', '.OOO..': 'j',
     'O...O.': 'k', 'O.O.O.': 'l', 'OO..O.': 'm', 'OO.OO.': 'n', 'O..OO.': 'o',
     'OOO.O.': 'p', 'OOOOO.': 'q', 'O.OOO.': 'r', '.OO.O.': 's', '.OOOO.': 't',
     'O...OO': 'u', 'O.O.OO': 'v', '.OOO.O': 'w', 'OO..OO': 'x', 'OO.OOO': 'y',
@@ -14,7 +14,7 @@ braille_to_english = {
 # Braille to Numbers Mapping
 braille_to_numbers = {
     'O.....': '1', 'O.O...': '2', 'OO....': '3', 'OO.O..': '4', 'O..O..': '5',
-    'OOO...': '6', 'OOOO..': '7', 'O.OO..': '8', '.O.O..': '9', '.OOO..': '0'
+    'OOO...': '6', 'OOOO..': '7', 'O.OO..': '8', '.OO...': '9', '.OOO..': '0'
 }
 
 # Braille to Special Characters Mapping
@@ -27,13 +27,13 @@ braille_to_special_chars = {
 # English to Braille Mapping
 english_to_braille = {
     'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..',
-    'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..', 'i': '.O.O..', 'j': '.OOO..',
+    'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..',
     'k': 'O...O.', 'l': 'O.O.O.', 'm': 'OO..O.', 'n': 'OO.OO.', 'o': 'O..OO.',
     'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.',
     'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO', 'y': 'OO.OOO',
     'z': 'O..OOO', ' ': '......',
     '1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..', '5': 'O..O..',
-    '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', '9': '.O.O..', '0': '.OOO..'
+    '6': 'OOO...', '7': 'OOOO..', '8': '.OO...', '9': '.O.O..', '0': '.OOO..'
 }
 
 # English to Braille Special Characters Mapping
