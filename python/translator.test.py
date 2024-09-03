918c59fdc6c8ea5705cfa35e5162786b3258c3ec
bh1090 <trivedibhavya23@gmail.com>
2024-09-03 02:03:54 -0700

    Completed the technical challenge

--- python/translator.test.py
+++ python/translator.test.py
@@ -4,11 +4,11 @@ import subprocess
 class TestTranslator(unittest.TestCase):
     def test_output(self):
         # Command to run translator.py script
-        command = ["python3", "translator.py", "Abc", "123", "xYz"]
-        
+        command = ["python", "translator.py", "Abc", "123", "xYz"]
+        print(command)
         # Run the command and capture output
         result = subprocess.run(command, capture_output=True, text=True)
-        
+        print(result)
         # Expected output without the newline at the end
         expected_output = ".....OO.....O.O...OO...........O.OOOO.....O.O...OO..........OO..OO.....OOO.OOOO..OOO"
         
