ea61459780b29ee8c25a0d135a140fa9e85863d5
MarinEstrada <adrian.marin.estrada@gmail.com>
2024-09-04 14:00:11 -0700

    ADD: add dictonaries for translating to and from braille

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,62 @@
+eng2braille_dict = {
+    'a': 'O.....',
+    'b': 'O.O...',
+    'c': 'OO....',
+    'd': 'OO.O..',
+    'e': 'O..O..',
+    'f': 'OOO...',
+    'g': 'OOOO..',
+    'h': 'O.OO..',
+    'i': '.OO...',
+    'j': '.OOO..',
+    'k': 'O...O.',
+    'l': 'O.O.O.',
+    'm': 'OO..O.',
+    'n': 'OO.OO.',
+    'o': 'O..OO.',
+    'p': 'OOO.O.',
+    'q': 'OOOOO.',
+    'r': 'O.OOO.',
+    's': '.OO.O.',
+    't': '.OOOO.',
+    'u': 'O...OO',
+    'v': 'O.O.OO',
+    'w': '.OOO.O',
+    'x': 'OO..OO',
+    'y': 'OO.OOO',
+    'z': 'O..OOO',
+    '.': '..OO.O',
+    ',': '..O...',
+    '?': '..O.OO',
+    '!': '..OOO.',
+    ':': '..OO..',
+    ';': '..O.O.',
+    '-': '....OO',
+    '/': '.O..O.',
+    '<': '.OO..O',
+    '>': 'O..OO.',
+    '(': 'O.O..O',
+    ')': '.O.OO.',
+    ' ': '......',
+}
 
+# reverse mapping of dictionary from 'char': 'braille' to 'braille': 'char'
+braille2eng_dict = {v: k for k, v in eng2braille_dict.items()}
+
+num2braille_dict = {
+    '1': 'O.....',
+    '2': 'O.O...',
+    '3': 'OO....',
+    '4': 'OO.O..',
+    '5': 'O..O..',
+    '6': 'OOO...',
+    '7': 'OOOO..',
+    '8': 'O.OO..',
+    '9': '.OO...',
+    '0': '.OOO..',
+    '.': '.O...O', # decimal point
+}
+
+# reverse mapping of dictionary from 'num': 'braille' to 'braille': 'num'
+braille2num_dict = {v: k for k, v in num2braille_dict.items()}
+
