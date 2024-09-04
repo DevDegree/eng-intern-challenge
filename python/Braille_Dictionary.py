bccc5f99a16f04226d36bc035a4b9e4a6d6260b4
Thomas Nguyen <34043095+ThomasNLy@users.noreply.github.com>
2024-09-04 19:56:52 -0400

    translating only letters, numbers and spaces at the moment

---
+++ python/Braille_Dictionary.py
@@ -0,0 +1,141 @@
+#--------------------alphabet------------------
+A ={
+    "char": "a",
+    "braille": "O....."
+}
+B = {"char": "b",
+    "braille": "O.O..."
+     }
+C = {"char": "c",
+    "braille": "OO...."
+     }
+D = {"char": "d",
+    "braille": "OO.O.."
+     }
+E = {"char": "e",
+    "braille": "O..O.."
+     }
+F = {"char": "f",
+    "braille": "OOO..."
+     }
+G = {"char": "g",
+    "braille": "OOOO.."
+     }
+H = {"char": "h",
+    "braille": "O.OO.."
+     }
+I = {"char": "i",
+    "braille": ".OO..."
+     }
+J = {"char": "j",
+    "braille": ".OOO.."
+     }
+K = {"char": "k",
+    "braille": "O...O."
+     }
+L = {"char": "l",
+    "braille": "O.O.O."
+     }
+M = {"char": "m",
+    "braille": "OO..O."
+     }
+N = {"char": "n",
+    "braille": "OO.OO."
+     }
+O = {"char": "o",
+    "braille": "O..OO."
+     }
+P = {"char": "p",
+    "braille": "OOO.O."
+     }
+Q = {"char": "q",
+    "braille": "OOOOO."
+     }
+R = {"char": "r",
+    "braille": "O.OOO."
+     }
+S = {"char": "s",
+    "braille": ".OO.O."
+     }
+T = {"char": "t",
+    "braille": ".OOOO."
+     }
+U = {"char": "u",
+    "braille": "O...OO"
+     }
+V = {"char": "v",
+    "braille": "O.O.OO"
+     }
+W = {"char": "w",
+    "braille": ".OOO.O"
+     }
+X = {"char": "x",
+    "braille": "OO..OO"
+     }
+Y = {"char": "y",
+    "braille": "OO.OOO"
+     }
+Z = {"char": "z",
+    "braille": "O..OOO"
+     }
+#-----------numbers----------
+num_1 = {"char": "1",
+    "braille": "O....."
+     }
+num_2 = {"char": "2",
+    "braille": "O.O..."
+     }
+num_3 = {"char": "3",
+    "braille": "OO...."
+     }
+num_4 = {"char": "4",
+    "braille": "OO.O.."
+     }
+num_5 = {"char": "5",
+    "braille": "O..O.."
+     }
+num_6 = {"char": "6",
+    "braille": "OOO..."
+     }
+num_7 = {"char": "7",
+    "braille": "OOOO.."
+     }
+num_8 = {"char": "8",
+    "braille": "O.OO.."
+     }
+num_9 = {"char": "9",
+    "braille": ".OO..."
+     }
+num_zero = {"char": "0",
+    "braille": ".OOO.."
+     }
+#------------------------follows_marker----------
+cap_follows = {
+    "braille": ".....O"
+     }
+decimal_follows = {
+    "braille": ".O...O"
+     }
+num_follows = {
+    "braille": ".O.OOO"
+     }
+#------------symbols-----------
+# period =
+# comma =
+# question_mrk =
+# exclamation_mrk =
+# colon =
+# semi_colon =
+# dash =
+# slash =
+# less_than =
+# greater_than =
+# left_bracket =
+# right_bracket =
+space = {"char": " ",
+    "braille": "......"
+     }
+
+
+LETTERS = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z]
+NUMBERS = [num_1, num_2, num_3, num_4, num_5, num_6, num_7, num_8, num_9]
--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,83 @@
+from Braille_Dictionary import LETTERS, NUMBERS, cap_follows, decimal_follows, num_follows, space
+
+
+
+arg1 = "HeLLo WorlD"
+
+def translate_to_braille(msg):
+    chars = list(str(msg))
+    """
+    loop through each char in the message, check if it is a specific letter and have a separate string translate that letter
+    to braile along with checking whether or not it is a captial letter or not, and apply the correct braille marker
+    
+    also check if it is a number and apply the correct braille marker before translating it to a number
+    """
+
+    #letters only translating with captials and spaces and numbers as well
+    translated_msg = []
+
+    number_found = False #checks if first time it found a number and assume all symbols after are numbers till a space is found
+    for i in range(len(chars)):
+        for j in range(len(LETTERS)):
+            if chars[i] == LETTERS[j]["char"].upper():
+                translated_msg.append(cap_follows["braille"])
+                translated_msg.append(LETTERS[j]["braille"])
+                break
+            elif chars[i] == LETTERS[j]["char"]:
+                translated_msg.append(LETTERS[j]["braille"])
+                break
+            elif chars[i] == space["char"]:
+                translated_msg.append(space["braille"])
+                number_found = False
+                break
+        # checking if it there is a number
+        for k in range(len(NUMBERS)):
+            if chars[i] == NUMBERS[k]["char"]:
+                if number_found is False:
+                    translated_msg.append(num_follows["braille"])
+                    number_found = True
+                translated_msg.append(NUMBERS[k]["braille"])
+
+    print(translated_msg)
+    print("".join(translated_msg))
+    # print("".join(translated_msg) == ".O.OOOOO.O..O.O...")
+    # print("".join(translated_msg) == ".....OO.OO..O..O..O.O.O.O.O.O.O..OO........OOO.OO..OO.O.OOO.O.O.O.OO.O..")
+
+translate_to_braille(arg1)
+translate_to_braille(42)
+
+# only translates from braille to letters with spacing and capitals no numbers yet
+def translate_from_braille(msg):
+    braille_counter = 0
+    single_char = []
+    braille_chars = []
+    translated_chars = []
+    for i in range(len(msg)):
+        braille_counter += 1
+        single_char.append(msg[i])
+        if braille_counter == 6:
+            braille_chars.append("".join(single_char))
+            single_char = []
+            braille_counter = 0
+
+    capital = False
+    for i in range(len(braille_chars)):
+        for j in range(len(LETTERS)):
+            if braille_chars[i] == cap_follows["braille"]:
+                capital = True
+                break
+            if capital and braille_chars[i] == LETTERS[j]["braille"]:
+                translated_chars.append(LETTERS[j]["char"].upper())
+                capital = False
+                break
+            elif braille_chars[i] == LETTERS[j]["braille"]:
+                translated_chars.append(LETTERS[j]["char"])
+
+                break
+            elif braille_chars[i] == space["braille"]:
+                translated_chars.append(space["char"])
+                break
+    print(translated_chars)
+
+translate_from_braille(".....OO.OO..O..O.......OO.O.O......OO.O.O.O..OO............O.OOO.OO..OO.O.OOO.O.O.O......OOO.O..")
 
