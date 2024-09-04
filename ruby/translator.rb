6415122a342f2e6b16d6358d2a6e2c88e9f810f5
Emma Shortt <eshortt@uoguelph.ca>
2024-09-04 13:57:11 -0400

    Define dictionaries for conversions

--- ruby/translator.rb
+++ ruby/translator.rb
@@ -1,3 +1,21 @@
+# Define the Braille Dictionary
+BRAILLE_DICT = {
+  "a" => "O.....", "b" => "O.O...", "c" => "OO....", "d" => "OO.O..", "e" => "O..O..",
+  "f" => "OOO...", "g" => "OOOO..", "h" => "O.OO..", "i" => ".OO...", "j" => ".OOO..",
+  "k" => "O...O.", "l" => "O.O.O.", "m" => "OO..O.", "n" => "OO.OO.", "o" => "O..OO.",
+  "p" => "OOO.O.", "q" => "OOOOO.", "r" => "O.OOO.", "s" => ".OO.O.", "t" => ".OOOO.",
+  "u" => "O...OO", "v" => "O.O.OO", "w" => ".OOO.O", "x" => "OO..OO", "y" => "OO.OOO",
+  "z" => "O..OOO",
+  " " => "......",
+  "capital" => ".....O",
+  "number" => ".O.OOO",
+  "0" => ".OOO..", "1" => "O.....", "2" => "O.O...", "3" => "OO....", "4" => "OO.O..",
+  "5" => "O..O..", "6" => "OOO...", "7" => "OOOO..", "8" => "O.OO..", "9" => ".OO..."
+}
+
+# Reverse Dictionary for English Translation
+ENGLISH_DICT = BRAILLE_DICT.invert
+
 # Determine if the input string is Braille
 def braille?(input_string)
 
