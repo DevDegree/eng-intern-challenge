4ccd4dadcf6a7a82f66a89ac4cd28535171fb650
Susie Liu <susie.liu97@gmail.com>
2024-09-04 16:41:36 -0400

    Creating enums and helper variables for determining whether input is in English or Braille

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,12 @@
+from enum import Enum
 
+str input = ''
+bool foundNonBrailleSymbol = False
+int inputLength = len(input)
+
+public class dictionaries:
+    brailleSymbols = {'o', '.'}
+
+class inputLanguage (Enum):
+    BRAILLE = 0
+    ENGLISH = 1
