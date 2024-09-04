9461cd0cee68c94abf10459f8e96c7aad2447bb7
Jekflvn <jeffreyyu.school@gmail.com>
2024-09-04 03:46:13 -0800

    wfd

--- python/translator.py
+++ python/translator.py
@@ -20,59 +20,71 @@ reverse_braille_dict = {
     'OOO.O.': 'p', 'OOOOO.': 'q', 'O.OOO.': 'r', '.OO.O.': 's', '.OOOO.': 't',
     'O...OO': 'u', 'O.O.OO': 'v', '.OOO.O': 'w', 'OO..OO': 'x', 'OO.OOO': 'y',
     'O..OOO': 'z',
-    'O.....': '1', 'O.O...': '2', 'OO....': '3', 'OO.O..': '4', 'O..O..': '5',
-    'OOO...': '6', 'OOOO..': '7', 'O.OO..': '8', '.OO...': '9', '.OOO..': '0',
     '......': ' ',  # space
     '.O....': ',', '.O.OOO': '?', 'OO.O.O': '!', '.O.OO.': '.', 
     '....O.': '-', '.OO...': ':', '.OO.O.': ';', 'O..O.O': '/', 
     'O..O..': '<', '.OO.OO': '>', 'OO.O..': '(', 'OO.O..': ')'
 }
-
+Reverse_braille_nums = {
+    'O.....': '1', 'O.O...': '2', 'OO....': '3', 'OO.O..': '4', 'O..O..': '5',
+    'OOO...': '6', 'OOOO..': '7', 'O.OO..': '8', '.OO...': '9', '.OOO..': '0',
+    '......': ' ' # space
+}
 
 capital_next = '.....O'  
 number_next = '.O.OOO'
+space = '......' 
 
-def translate(input_string):
+def translator(input_string):
 
-    stripped_input = input_string.replace(' ', '')
     english = True
-    
-    if all(c in '0.' for c in stripped_input) and len(stripped_input) % 6 == 0:
+    stripped_input = input_string.replace(' ', '')
+    if all(c in 'O.' for c in stripped_input) and len(stripped_input) % 6 == 0: #check if brialle or english
         english = False
     number_mode = False
     answer = []
+
     if english:
         for char in input_string:
-            if char.isdigit():
+            if char.isdigit():      #if number
                 if not number_mode:
                     answer.append(number_next)
                     number_mode = True
                 answer.append(braille_dict[char])
-            elif char.isalpha():
-                if number_mode:
-                    number_mode = False
-                if char.isupper():
+
+            elif char.isalpha():    #if letter
+                number_mode = False
+                if char.isupper():  #if Capital
                     answer.append(capital_next)
                 answer.append(braille_dict[char.upper()])
-            elif char in braille_dict:
+
+            elif char in braille_dict:  #if symbol
+                number_mode = False
                 answer.append(braille_dict[char])
     else:
         capital_mode = False
-        for i in range(0, len(input_string), 6):
+        for i in range(0, len(input_string), 6):    #For each brialle word
             char_braille = input_string[i:i+6]
 
-            if char_braille == capital_next:
+            if char_braille == capital_next:    #If Capital Symbol
                 capital_mode = True
                 continue
-            elif char_braille == number_next:
+
+            elif char_braille == number_next:   #If Num Symbol
+                number_mode = True
                 continue
-            if capital_mode:
+
+            if capital_mode:        #Capitalize if needed
                 answer.append(reverse_braille_dict[char_braille].upper())
                 capital_mode = False
+            elif number_mode:   #Nums if needed
+                answer.append(Reverse_braille_nums[char_braille])
             else:
                 answer.append(reverse_braille_dict[char_braille])
+
+            if char_braille == space:   #End nums if space
+                number_mode = False
     
-    return ''.join(answer) 
+    return ''.join(answer)  #make string
 
 
-    
