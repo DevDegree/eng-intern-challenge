fcaf866895a969ad89537fd2c0af4905247bd7ab
trtrgfh <73056232+trtrgfh@users.noreply.github.com>
2024-09-04 12:19:19 -0400

    finish

--- python/translator.py
+++ python/translator.py
@@ -1,13 +1,100 @@
 
-english_braille = {'a': '0.....', 'b': '0.0...', 'c': '00....', 'd': '00.0..', 'e': '0..0..', 
-                   'f': '000...', 'g': '0000..', 'h': '0.00..', 'i': '.00...', 'j': '.000..',
-                   'k': '0...0.', 'l': '0.0.0.', 'm': '00..0.', 'n': '00.00.', '0': '0..00.',
-                   'p': '000.0.', 'q': '00000.', 'r': '0.000.', 's': '.00.0.', 't': '.0000.',
-                   'u': '0...00', 'v': '0.0.00', 'w': '.000.0', 'x': '00..00', 'y': '00.000',
-                   'z': '0..000', '1': '0.....', '2': '0.0...', '3': '00....', '4': '00.0..', 
-                   '5': '0..0..', '6': '000...', '7': '0000..', '8': '0.00..', '9': '.00...', 
-                   '0': '.000..', 'cap': '.....0', 'dec': '.0...0', 'num': '.0.000', '.': '..00.0',
-                   ',': '..0...', '?': '..0.00', '!': '..000.', ':': '..00..', ';': '..0.0.', 
-                   '-': '....00', '/': '.0..0.', '<': '.0.0.0', '>': '0.0.0.', '(': '0.0..0', 
-                   ')': '.0.00.', ' ': '......'}
+import sys
+
+english_braille = {'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..', 
+                   'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..',
+                   'k': 'O...O.', 'l': 'O.O.O.', 'm': 'OO..O.', 'n': 'OO.OO.', 'o': 'O..OO.',
+                   'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.',
+                   'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO', 'y': 'OO.OOO',
+                   'z': 'O..OOO', 'cap': '.....O', 'dec': '.O...O', 'num': '.O.OOO'}
+braille_english = {v: k for k, v in english_braille.items()}
+
+braille_num = {'1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..', '5': 'O..O..', 
+               '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...', '0': '.OOO..', 
+               '.': '..OO.O', ',': '..O...', '?': '..O.OO', '!': '..OOO.', ':': '..OO..', 
+               ';': '..O.O.', '-': '....OO', '/': '.O..O.', '<': '.OO..O', '>': 'O..OO.', 
+               '(': 'O.O..O', ')': '.O.OO.', ' ': '......'}
+english_num = {v: k for k, v in braille_num.items()}
+
+def english_to_braille(text):
+    res = []
+    isnum = False
+    for char in text:
+        # print(char)
+        if char.isupper():
+            res.append(english_braille['cap'])
+            # print(english_braille['cap'])
+            char = char.lower()
+        if char.isdigit():
+            if not isnum:
+                res.append(english_braille['num'])
+            isnum = True
+            res.append(braille_num[char])
+            # print(english_braille['num'])
+            # print(braille_num[char])
+            
+        elif not char.isalpha():
+            res.append(braille_num[char])
+            isnum = False
+            # print(braille_num[char])
+        else:
+            res.append(english_braille[char])
+            isnum = False
+            # print(english_braille[char])
+    return ''.join(res)
+
+def braille_to_english(text):
+    res = []
+    iscap = False
+    isnum = False
+    for i in range(0, len(text), 6):
+        braille = text[i:i+6]
+        # print(braille)
+        if braille in braille_english:
+            char = braille_english[braille]
+        elif braille in english_num:
+            char = english_num[braille]
+        # print(char)
+        if char == 'cap':
+            iscap = True
+            continue
+        if char == 'num':
+            isnum = True
+            continue
+        if char == ' ':
+            isnum = False
+
+        if iscap:
+            res.append(char.upper())
+            iscap = False
+        elif isnum:
+            res.append(english_num[braille])
+        else:
+            res.append(char)
+        
+    return ''.join(res)
+
+def is_braille(text):
+    for char in text:
+        if char not in 'O.':
+            return False
+    return True
+
+def main():
+    if len(sys.argv) < 2:
+        print("invalid input")
+        sys.exit(1)
+    
+    input_text = ' '.join(sys.argv[1:])
+
+    if is_braille(input_text):
+        translated_text = braille_to_english(input_text)
+    else:
+        translated_text = english_to_braille(input_text)
+    
+    print(translated_text)
+
+if __name__ == "__main__":
+    main()
+
 
