015a9e6b18f9abfd3cee5f16144a59dc033447e3
Keegan Liu <k342liu@uwaterloo.ca>
2024-09-03 21:06:55 -0400

    Complete brailleToEnglish

--- python/translator.py
+++ python/translator.py
@@ -1,5 +1,6 @@
+import sys
+
 CAPITALFOLLOWS = 'C'
-DECIMALFOLLOWS = 'D'
 NUMBERFOLLOWS = 'N'
 
 brailleToAlphabetMap = {
@@ -31,7 +32,7 @@ brailleToAlphabetMap = {
   "O..OOO": 'z',
 }
 
-brailleToSymbols = {
+brailleToNumberMap = {
   "O.....": '1',
   "O.O...": '2',
   "OO....": '3',
@@ -42,22 +43,78 @@ brailleToSymbols = {
   "O.OO..": '8',
   ".OO...": '9',
   ".OOO..": '0',
+}
+
+brailleToSymbolMap = {
   ".....O": CAPITALFOLLOWS,
-  ".O...O": DECIMALFOLLOWS,
   ".O.OOO": NUMBERFOLLOWS,
-  "..OO.O": '.',
-  "..O...": ',',
-  "..O.OO": '?',
-  "..OOO.": '!',
-  "..OO..": ':',
-  "..O.O.": ';',
-  "....OO": '-',
-  ".O..O.": '/',
-  ".OO..O": '<',
-  "O..OO.": '>',
-  "O.O..O": '(',
-  ".O.OO.": ')',
   "......": ' ',
 }
 
+def isBraille(s: str):
+  if len(s) % 6 != 0:
+    return False # Braille consists of 6 character strings and each character consists of at least one unraised rea
 
+  for c in s:
+    if c != '.' and c != 'O':
+      return False # Braille only consists of '.' and 'O'
+    
+  if '.' not in s:
+    return False # Braille characters contain at least one unraised area
+
+  return True
+
+def brailleToEnglish(s: str):
+  res = ""
+
+  addCapital = False
+  addNumber = False
+  
+  for currCharStart in range(0, len(s) - 5, 6):
+    currCharEnd = currCharStart + 6
+    currBraille = s[currCharStart: currCharEnd]
+
+    # CASE 1: SYMBOL
+    if currBraille in brailleToSymbolMap:
+      symbol = brailleToSymbolMap[currBraille]
+
+      if symbol == CAPITALFOLLOWS:
+        addCapital = True
+      elif symbol == NUMBERFOLLOWS:
+        addNumber = True
+      else:
+        res += symbol
+      
+      continue
+
+    # CASE 2: NUMBER
+    if addNumber:
+      res += brailleToNumberMap[currBraille]
+      addNumber = False
+      continue
+    
+    # CASE 3: LETTER
+    if currBraille in brailleToAlphabetMap:
+      res += brailleToAlphabetMap[currBraille].upper() if addCapital else brailleToAlphabetMap[currBraille]
+      addCapital = False
+
+  return res
+
+def englishToBraille(s: str):
+  
+  return None
+
+def main():
+  arg = sys.argv[1] # Extract the provided argument
+
+  print(f"Provided argument: {arg}")
+
+  if isBraille(arg):
+    print("Braille->English Result:")
+    print(brailleToEnglish(arg))
+  else:
+    print("English->Braille Result:")
+    print(englishToBraille(arg))
+
+if __name__ == "__main__":
+  main()
