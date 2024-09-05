24bec418b4adc1a21bdd10c7426c775a6f25f97f
maxkmy <maxkoh03@gmail.com>
2024-09-05 00:45:15 -0400

    generalize test cases

--- python/translator.test.py
+++ python/translator.test.py
@@ -1,19 +1,48 @@
 import unittest
 import subprocess
 
+class EnglishBraillePair:
+    def __init__(self, english_text: str, braille_text: str):
+        self.english_text = english_text
+        self.braille_text = braille_text
+
 class TestTranslator(unittest.TestCase):
-    def test_output(self):
-        # Command to run translator.py script
-        command = ["python3", "translator.py", "Abc", "123", "xYz"]
-        
-        # Run the command and capture output
-        result = subprocess.run(command, capture_output=True, text=True)
-        
-        # Expected output without the newline at the end
-        expected_output = ".....OO.....O.O...OO...........O.OOOO.....O.O...OO..........OO..OO.....OOO.OOOO..OOO"
-        
-        # Strip any leading/trailing whitespace from the output and compare
-        self.assertEqual(result.stdout.strip(), expected_output)
+    english_braille_pairs = [
+        EnglishBraillePair(
+            "Abc 123 xYz",
+            ".....OO.....O.O...OO...........O.OOOO.....O.O...OO..........OO..OO.....OOO.OOOO..OOO"
+        ),
+    ]
+    
+    def test_english_to_braille(self):
+        for english_braille_pair in self.english_braille_pairs:
+            # Split English text into list of arguments to pass to command
+            english_text = english_braille_pair.english_text
+            english_text_args = english_text.split(" ")
+
+            # Command to run translator.py script
+            command = ["python3", "translator.py"] + english_text_args
+
+            expected_braille_text = english_braille_pair.braille_text
+            
+            # Run the command and capture output
+            result = subprocess.run(command, capture_output=True, text=True)
+
+            # Strip any leading/trailing whitespace from the output and compare
+            self.assertEqual(result.stdout.strip(), expected_braille_text)
+
+    def test_braille_to_english(self):
+        for english_braille_pair in self.english_braille_pairs:
+            # Command to run translator.py script
+            command = ["python3", "translator.py", english_braille_pair.braille_text]
+
+            expected_english_text = english_braille_pair.english_text
+
+            # Run the command and capture output
+            result = subprocess.run(command, capture_output=True, text=True)
+
+            # Strip any leading/trailing whitespace from the output and compare
+            self.assertEqual(result.stdout.strip(), expected_english_text)
 
 if __name__ == '__main__':
     unittest.main()
