9de548404f416196118a3aa2529dff677cafee54
mariyatur27 <mariyatur27@gmail.com>
2024-09-04 21:24:30 -0400

    Create a function that converts english to brail

--- javascript/translator.js
+++ javascript/translator.js
@@ -40,7 +40,7 @@ const brailleAlphabet = {
     '>': 'O..OO.',
     '(': 'O.O..O',
     ')': '.O.OO.',
-    ' ': '',
+    ' ': '......',
     '1': 'O.....',
     '2': 'O.O...',
     '3': 'OO....',
@@ -51,4 +51,63 @@ const brailleAlphabet = {
     '8': 'O.OO..',
     '9': '.OO...',
     '0': '.OOO..'
+}
+
+const input = process.argv.slice(2);
+const inputString = input.join(' ');
+
+const isNumber = (val) => {
+    return !isNaN(parseInt(val)) && isFinite(val)
+}
+
+const isDecimal = (val) => {
+    return !isNaN(parseFloat(val)) && isFinite(val)
+}
+
+const brailToEnglish = (val) => {
+    let output = '';
+
+    let inputArr = val.split('');
+    console.log(inputArr);
+
+    for(let i = 0; i < inputArr.length; i++){
+        //if the letter is uppercase
+        if(inputArr[i] == inputArr[i].toUpperCase() && inputArr[i] != ' ' && /^[a-zA-Z]+$/.test(inputArr[i])){
+            output += brailleAlphabet['capital'];
+        }
+        //if the letter is a number
+        else if(isNumber(inputArr[i])){
+            if(i == 0 || (i-1 > 0 && !isNumber(inputArr[i-1]))){
+                output += brailleAlphabet['number']
+            }
+        }
+        //if the letter is a decimal
+        else if(isDecimal(inputArr[i])){
+            if(i == 0 || (i-1 >= 0 && !isDecimal(inputArr[i-1]))){
+                output += brailleAlphabet['decimal']
+            }
+        }
+
+        output += brailleAlphabet[inputArr[i].toLowerCase()]
+    }
+    return output
+}
+
+
+if(inputString){
+    let output;
+
+    output = brailToEnglish(inputString)
+    console.log(output)
+
+    if(output == 'Abc 123'){
+        console.log('passed')
+    }
+    
+    // output = brailToEnglish(inputString)
+    // console.log(output)
+
+    // if(output == '.O.OOOOO.O..O.O...'){
+    //     console.log('passed')
+    // }
 }
