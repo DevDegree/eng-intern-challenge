2c82e3dae9668dc389a741bbc9a6b03f6e1baed4
gloai <glorialim.dev@gmail.com>
2024-09-05 19:25:36 -0400

    Simplify constants

--- javascript/constants.js
+++ javascript/constants.js
@@ -1,14 +1,14 @@
-const braille_dict = {
-    "O.....": ["a", "1"],
-    "O.O...": ["b", "2"],
-    "OO....": ["c", "3"],
-    "OO.O..": ["d", "4"],
-    "O..O..": ["e", "5"],
-    "OOO...": ["f", "6"],
-    "OOOO..": ["g", "7"],
-    "O.OO..": ["h", "8"],
-    ".OO...": ["i", "9"],
-    ".OOO..": ["j", "0"],
+const braille_alphabet_dict = {
+    "O.....": "a",
+    "O.O...": "b",
+    "OO....": "c",
+    "OO.O..": "d",
+    "O..O..": "e",
+    "OOO...": "f",
+    "OOOO..": "g",
+    "O.OO..": "h",
+    ".OO...": "i",
+    ".OOO..": "j",
     "O...O.": "k",
     "O.O.O.": "l",
     "OO..O.": "m",
@@ -25,6 +25,19 @@ const braille_dict = {
     "OO..OO": "x",
     "OO.OOO": "y",
     "O..OOO": "z",
+}
+
+const braille_num_dict = {
+    "O.....": "1",
+    "O.O...": "2",
+    "OO....": "3",
+    "OO.O..": "4",
+    "O..O..": "5",
+    "OOO...": "6",
+    "OOOO..": "7",
+    "O.OO..": "8",
+    ".OO...": "9",
+    ".OOO..": "0",
 }
 
 const braille_symbols_dict = {
@@ -46,7 +59,7 @@ const braille_precursor_dict = {
     ".....O": "capital follows",
     ".O...O": "decimal follows",
     ".O.OOO": "number follows",
-    "......": " ",
+    "......": "space",
 }
 
-module.exports = { braille_dict, braille_symbols_dict, braille_precursor_dict }
+module.exports = { braille_alphabet_dict, braille_num_dict, braille_symbols_dict, braille_precursor_dict }
