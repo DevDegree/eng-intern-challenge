2dfd8e08460ba92a52887098945dbc85eff3e348
David Langill <david.langill@outlook.com>
2024-09-04 23:39:38 -0400

    Had map use lowercase o instead of uppercase O

--- python/translator.py
+++ python/translator.py
@@ -4,17 +4,17 @@ input = sys.argv
 # create alpha to braille mapping
 # no need for #s since we'll ascii map them to a-z
 alphaToBraille = {
-'a': 'o.....', 'b': 'o.o...', 'c': 'oo....', 'd': 'oo.o..', 'e': 'o..o..', 'f': 'ooo...', 'g': 'oooo..', 'h': 'o.oo..', 'i': '.oo...',
-'j': '.ooo..', 'k': 'o...o.', 'l': 'o.o.o.', 'm': 'oo..o.', 'n': 'oo.oo.', 'o': 'o..oo.', 'p': 'ooo.o.', 'q': 'ooooo.', 'r': 'o.ooo.',
-'s': '.oo.o.', 't': '.oooo.', 'u': 'o...oo', 'v': 'o.o.oo', 'w': '.ooo.o', 'x': 'oo..oo', 'y': 'oo.ooo', 'z': 'o..ooo', 'cap': '.....o',
-'dec': '.o...o', '#': '.o.ooo', '.': '..oo.o', ':': '..o...', '?': '..o.oo', '!': '..ooo.', ':': '..oo..', ';': '..o.o.', '-': '....oo',
-'/': '.o..o.', '<': '.oo..o', '>': 'o..oo.', '(': 'o.o..o', ')': '.o.oo.', ' ': '......'}
+'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..', 'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..', 'i': '.OO...',
+'j': '.OOO..', 'k': 'O...O.', 'l': 'O.O.O.', 'm': 'OO..O.', 'n': 'OO.OO.', 'O': 'O..OO.', 'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.',
+'s': '.OO.O.', 't': '.OOOO.', 'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO', 'y': 'OO.OOO', 'z': 'O..OOO', 'cap': '.....O',
+'dec': '.O...O', '#': '.O.OOO', '.': '..OO.O', ':': '..O...', '?': '..O.OO', '!': '..OOO.', ':': '..OO..', ';': '..O.O.', '-': '....OO',
+'/': '.O..O.', '<': '.OO..O', '>': 'O..OO.', '(': 'O.O..O', ')': '.O.OO.', ' ': '......'}
 
 
 # reverse dictionary
 brailleToAlpha = {}
 for key in alphaToBraille:
-    value = {alphaToBraille[key]: key}
+    value = {alphaToBraille[key]: key} # lower to enforce no O
     brailleToAlpha.update(value)
 
 """
@@ -24,25 +24,38 @@ for i in range(1, len(input)):
 """
 
 def processBraille():
-    if (len(input) <= 2):
+    if (len(input) != 2):
         return False
     
     potentialBraille = input[1]
     # break up input after every 6th character
-    splits = [potentialBraille[i:i+6] for i in range(0, len(potentialBraille, 6))]
+    splits = [potentialBraille[i:i+6] for i in range(0, len(potentialBraille), 6)]
     translation = ""
     follows = "low" # denotes whether caps follows, decimal follows, or number follows (low means lowercase)
     for i in range (0, len(splits), 1):
         try:
-            value = brailleToAlpha[splits[i]]
-            if value == "cap":
-                follows = "cap"
-            elif value == "dec":
-                follows = "dec"
-            elif value == "num"
+            key = splits[i]
+            value = brailleToAlpha[key]
+            if (len(value) != 1):
+                follows = value
+                continue
+            elif (value == ' '):
+                translation += value
+                follows = "low" # reset
+
+            if (follows == "low"):
+                translation += value
+            elif (follows == "cap"):
+                translation += value.upper()
+            elif (follows == "dec"):
+                value -= 'a'
+                if (value == 'j' - 'a'):
+                    value = '0'
+                translation += value - 'a'
         except:
             # not Braille!
             return False
+    return translation
 
 
 def processEnglish():
@@ -52,6 +65,6 @@ def processEnglish():
 brailleOut = processBraille()
 if processBraille() == False:
     # not Braille, so translate from English to Braille
-    print(processEnglish)
+    print(1)
 else:
     print(brailleOut)
