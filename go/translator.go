72d38717aaebc9f85f59d48dbfffe2356d0a800b
raeeceip <chisomchibogu@cmail.carleton.ca>
2024-09-05 17:35:22 -0400

    hm

--- go/translator.go
+++ go/translator.go
@@ -7,19 +7,23 @@ import (
 	"unicode"
 )
 
-var englishToBrailleMap = map[rune]string{
-	'a': "O.....", 'b': "O.O...", 'c': "OO....", 'd': "OO.O..", 'e': "O..O..",
-	'f': "OOO...", 'g': "OOOO..", 'h': "O.OO..", 'i': ".OO...", 'j': ".OOO..",
-	'k': "O...O.", 'l': "O.O.O.", 'm': "OO..O.", 'n': "OO.OO.", 'o': "O..OO.",
-	'p': "OOO.O.", 'q': "OOOOO.", 'r': "O.OOO.", 's': ".OO.O.", 't': ".OOOO.",
-	'u': "O...OO", 'v': "O.O.OO", 'w': ".OOO.O", 'x': "OO..OO", 'y': "OO.OOO",
-	'z': "O..OOO", ' ': "......",
-}
+const (
+	brailleCharLength = 6
+	numberPrefix      = ".O.OOO"
+	capitalPrefix     = ".....O"
+)
 
-var brailleToEnglishMap = make(map[string]rune)
-
-var numberPrefix = ".O.OOO"
-var capitalPrefix = ".....O"
+var (
+	englishToBrailleMap = map[rune]string{
+		'a': "O.....", 'b': "O.O...", 'c': "OO....", 'd': "OO.O..", 'e': "O..O..",
+		'f': "OOO...", 'g': "OOOO..", 'h': "O.OO..", 'i': ".OO...", 'j': ".OOO..",
+		'k': "O...O.", 'l': "O.O.O.", 'm': "OO..O.", 'n': "OO.OO.", 'o': "O..OO.",
+		'p': "OOO.O.", 'q': "OOOOO.", 'r': "O.OOO.", 's': ".OO.O.", 't': ".OOOO.",
+		'u': "O...OO", 'v': "O.O.OO", 'w': ".OOO.O", 'x': "OO..OO", 'y': "OO.OOO",
+		'z': "O..OOO", ' ': "......",
+	}
+	brailleToEnglishMap = make(map[string]rune)
+)
 
 func init() {
 	for k, v := range englishToBrailleMap {
@@ -29,65 +33,81 @@ func init() {
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("Please provide a string to translate")
-		return
+		fmt.Println("Please provide strings to translate")
+		os.Exit(1)
 	}
 
-	input := strings.Join(os.Args[1:], " ")
+	inputs := os.Args[1:]
+	var results []string
 
-	if isBraille(input) {
-		fmt.Println(brailleToEnglish(input))
-	} else {
-		fmt.Println(englishToBraille(input))
+	for _, input := range inputs {
+		if isBraille(input) {
+			result, _ := brailleToEnglish(input)
+			results = append(results, result)
+		} else {
+			result, _ := englishToBraille(input)
+			results = append(results, result)
+		}
 	}
+
+	// Join the results with the Braille representation of a space
+	fmt.Print(strings.Join(results, "......"))
 }
 
 func isBraille(s string) bool {
-	return strings.Count(s, "O")+strings.Count(s, ".") == len(s) && len(s)%6 == 0
+	return strings.Count(s, "O")+strings.Count(s, ".") == len(s) && len(s)%brailleCharLength == 0
 }
 
-func brailleToEnglish(s string) string {
+func brailleToEnglish(s string) (string, error) {
 	var result strings.Builder
 	isCapital := false
 	isNumber := false
 
-	for i := 0; i < len(s); i += 6 {
-		symbol := s[i : i+6]
+	for i := 0; i < len(s); i += brailleCharLength {
+		symbol := s[i : i+brailleCharLength]
 
-		if symbol == capitalPrefix {
+		switch symbol {
+		case capitalPrefix:
 			isCapital = true
 			continue
-		}
-
-		if symbol == numberPrefix {
+		case numberPrefix:
 			isNumber = true
 			continue
 		}
 
-		if char, ok := brailleToEnglishMap[symbol]; ok {
-			if isNumber {
-				result.WriteRune(rune(char - 'a' + '1'))
-				if char == 'j' {
-					result.WriteRune('0')
-				}
-			} else {
-				if isCapital {
-					char = unicode.ToUpper(char)
-					isCapital = false
-				}
-				result.WriteRune(char)
-			}
+		char, ok := brailleToEnglishMap[symbol]
+		if !ok {
+			return "", fmt.Errorf("invalid Braille symbol: %s", symbol)
 		}
 
-		if symbol == "......" {
+		if isNumber && unicode.IsLetter(char) {
+			result.WriteRune(convertBrailleToNumber(char))
+			continue
+		}
+
+		if isCapital && unicode.IsLetter(char) {
+			char = unicode.ToUpper(char)
+			isCapital = false
+		}
+
+		result.WriteRune(char)
+
+		if char == ' ' {
 			isNumber = false
 		}
 	}
 
-	return result.String()
+	return result.String(), nil
 }
 
-func englishToBraille(s string) string {
+func convertBrailleToNumber(char rune) rune {
+	if char == 'j' {
+		return '0'
+	}
+	return rune(char - 'a' + '1')
+}
+
+func englishToBraille(s string) (string, error) {
 	var result strings.Builder
 	isNumber := false
 
@@ -102,21 +122,25 @@ func englishToBraille(s string) string {
 				result.WriteString(numberPrefix)
 				isNumber = true
 			}
-			if char == '0' {
-				result.WriteString(englishToBrailleMap['j'])
-			} else {
-				result.WriteString(englishToBrailleMap[rune(char-'1'+'a')])
-			}
+			result.WriteString(convertNumberToBraille(char))
 		} else {
 			if isNumber && char != ' ' {
-				result.WriteString("......")
 				isNumber = false
 			}
-			if braille, ok := englishToBrailleMap[char]; ok {
-				result.WriteString(braille)
+			braille, ok := englishToBrailleMap[char]
+			if !ok {
+				return "", fmt.Errorf("invalid character: %c", char)
 			}
+			result.WriteString(braille)
 		}
 	}
 
-	return result.String()
+	return result.String(), nil
+}
+
+func convertNumberToBraille(char rune) string {
+	if char == '0' {
+		return englishToBrailleMap['j']
+	}
+	return englishToBrailleMap[rune(char-'1'+'a')]
 }
