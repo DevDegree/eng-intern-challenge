413b6800451fa1572d4233643b0669acacecd094
SaoodAM <saoodalimughal@gmail.com>
2024-09-04 22:58:39 -0400

    Update translator.py

--- python/translator.py
+++ python/translator.py
@@ -1,7 +1,7 @@
 import sys
 letter_to_braille = {'a':'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..', 'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..', 'k': 'O...O.', 'l': 'O.O.O.', 'm': 'OO..O.', 'n': 'OO.OO.', 'o': 'O..OO.', 'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.', 'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO', 'y': 'OO.OOO', 'z': 'O..OOO', ' ': '......'}
 charnum_to_braille = {'1':'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..', '5': 'O..O..', '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...', '0': '.OOO..'}
-text = ' '.join(sys.argv[1::])
+text = ' '.join(sys.argv[1::]).strip()
 result = ''
 # Variables for english to braille
 capital_follows = '.....O'
