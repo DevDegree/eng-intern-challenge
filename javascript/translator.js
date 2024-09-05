9d0e89eccd3ad13734491cad1ae192165f2b2f3f
JeanJoblin <invisibleorbs@gmail.com>
2024-09-05 11:19:52 -0400

    hardcode braille chars

--- javascript/translator.js
+++ javascript/translator.js
@@ -1,5 +1,7 @@
 
 const inputString = formatInput();
+
+console.log(inputString);
 idLang();
 
 function formatInput() {
@@ -24,3 +26,56 @@ function idLang() {
     console.log("String is not Braille")
   }
 }
+
+const brailleAlphanum = [
+  "O.....",
+  "O.O...",
+  "OO....",
+  "OO.O..",
+  "O..O..",
+  "OOO...",
+  "OOOO..",
+  "O.OO..",
+  ".OO...",
+  ".OOO..",
+  "O...O.",
+  "O.O.O.",
+  "OO..O.",
+  "OO.OO.",
+  "O..OO.",
+  "OOO.O.",
+  "OOOOO.",
+  "O.OOO.",
+  ".OO.O.",
+  ".OOOO.",
+  "O.OO..",
+  "O.O.OO",
+  ".OOO.O",
+  "OO..OO",
+  "OO.OOO",
+  "O..OOO"
+];
+
+const brailleMod = [
+  ".....O",
+  ".O...O",
+  ".O.OOO"
+];
+
+const brailleSmyb = [
+  "..OO.O",
+  "..O...",
+  "..O.OO",
+  "..OOO.",
+  "..OO..",
+  "..O.O.",
+  "....OO",
+  ".O..O.",
+  ".OO..O",
+  "O..OO.",
+  "O.O..O",
+  ".O.OO.",
+  "......"
+];
+
+
