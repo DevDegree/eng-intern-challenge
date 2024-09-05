1fadea5dbf9d96ab3463aa5d8e7b4b09c9c8f9c3
farooq-alk <farooq.alkhaffaf@gmail.com>
2024-09-05 01:43:18 -0400

    Initial commit

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,86 @@
 
+# English to Braille map
+ENGLISH_TO_BRAILLE = {
+    'A':'O.....',
+    'B':'O.O...',
+    'C':'OO....',
+    'D':'OO.O..',
+    'E':'O..O..',
+    'F':'OOO...',
+    'G':'OOOO..',
+    'H':'O.OO..',
+    'I':'.OO...',
+    'J':'.OOO..',
+    'K':'O...O.',
+    'L':'O.O.O.',
+    'M':'OO..O.',
+    'N':'OO.OO.',
+    'O':'O..OO.',
+    'P':'OOO.O.',
+    'Q':'OOOOO.',
+    'R':'O.OOO.',
+    'S':'.OO.O.',
+    'T':'.OOOO.',
+    'U':'O...OO',
+    'V':'O.O.OO',
+    'W':'.OOO.O',
+    'X':'OO..OO',
+    'Y':'OO.OOO',
+    'Z':'O..OOO',
+    '1':'O.....',
+    '2':'O.O...',
+    '3':'OO....',
+    '4':'OO.O..',
+    '5':'O..O..',
+    '6':'OOO...',
+    '7':'OOOO..',
+    '8':'O.OO..',
+    '9':'.OO...',
+    '0':'.OOO..',
+    '.':'..OO.O',
+    ',':'..O...',
+    '?':'..O.OO',
+    '!':'..OOO.',
+    ':':'..OO..',
+    ';':'..O.O.',
+    '-':'....OO',
+    '/':'.O..O.',
+    '<':'.OO..O',
+    '>':'O..OO.',
+    '(':'O.O..O',
+    ')':'.O.OO.',
+    ' ':'......',
+}
+
+# English to Braille numbers map
+ENGLISH_TO_BRAILLE_NUMS={
+    '1':'O.....',
+    '2':'O.O...',
+    '3':'OO....',
+    '4':'OO.O..',
+    '5':'O..O..',
+    '6':'OOO...',
+    '7':'OOOO..',
+    '8':'O.OO..',
+    '9':'.OO...',
+    '0':'.OOO..'
+}
+
+# Define other constants
+CAPITAL_FOLLOWS = '.....O'
+NUMBER_FOLLOWS  = '.O.OOO'
+BRAILLE_CHUNK = 6
+
+'''
+Input is English, output is Braille.
+Takes a string as an argument and uses it as the input string
+'''
+def translate_english_to_braille(_input):
+    pass
+
+'''
+Input is Braille, output is English.
+Takes a string as an argument and uses it as the input string
+'''
+def translate_braille_to_english(_input):
+    pass
