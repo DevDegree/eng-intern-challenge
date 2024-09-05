1a6b3593d6ebe66053555716d9bcbf736b2e823f
mariyatur27 <mariyatur27@gmail.com>
2024-09-05 15:33:57 -0400

    Finished the brailleToEnglish function and editing other functionalities

--- javascript/translator.js
+++ javascript/translator.js
@@ -28,18 +28,6 @@ const brailleAlphabet = {
     'capital': '.....O',
     'decimal': '.O...O',
     'number': '.O.OOO',
-    '.': '..OO.O',
-    ',': '..O...',
-    '?': '..O.OO',
-    '!': '..OOO.',
-    ':': '..OO..',
-    ';': '..O.O.',
-    '-': '....OO',
-    '/': '.O..O.',
-    '<': '.OO..O',
-    '>': 'O..OO.',
-    '(': 'O.O..O',
-    ')': '.O.OO.',
     ' ': '......',
     '1': 'O.....',
     '2': 'O.O...',
@@ -64,12 +52,10 @@ const isDecimal = (val) => {
     return !isNaN(parseFloat(val)) && isFinite(val)
 }
 
-const brailleToEnglish = (val) => {
+const englishToBraille = (val) => {
     let output = '';
 
     let inputArr = val.split('');
-    console.log(inputArr);
-
     for(let i = 0; i < inputArr.length; i++){
         //if the letter is uppercase
         if(inputArr[i] == inputArr[i].toUpperCase() && inputArr[i] != ' ' && /^[a-zA-Z]+$/.test(inputArr[i])){
@@ -93,57 +79,88 @@ const brailleToEnglish = (val) => {
     return output
 }
 
-const englishToBraille = (val) => {
+const brailleToEnglish = (val) => {
     let output = '';
     let valArr = val.split('')
     let inputArr = [];
     let capitalize = false;
     let number = false;
     let decimal = false;
+    let addToOutput = false;
 
     for(let i = 0; i < valArr.length; i += 6){
         let chunk = valArr.slice(i, i + 6);
         inputArr.push(chunk.join(''));
     }
 
-    console.log(inputArr)
-
     for(let m = 0; m < inputArr.length; m++){
         for(const key in brailleAlphabet){
             if(brailleAlphabet[key] == inputArr[m]){
-                if(capitalize){
-                    output += key.toUpperCase();
-                    capitalize = false;
-                }else if(number){
-                    
-                }
 
-                if(key == 'capital'){
-                    capitalize = true;
-                }else if(key == 'number'){
+                if(key == 'number'){
                     number = true;
                 }else if(key == 'decimal'){
                     decimal = true;
+                }else if(key == 'capital'){
+                    capitalize = true;
+                }else{
+                    addToOutput = true;
+                }
+
+                if(addToOutput){
+                    if(number || decimal){
+                        if(isNumber(key) || isDecimal(key)){
+                            output += key;
+                        }
+
+                        if(number){
+                            if(m+1 < inputArr.length && isNumber(brailleAlphabet[inputArr[m+1]])){
+                                number = false;
+                            }
+                        }
+
+                        if(decimal){
+                            if(m+1 < inputArr.length && isDecimal(brailleAlphabet[inputArr[m+1]])){
+                                decimal = false;
+                            }
+                        }
+                    }else{
+                        if(!isNumber(key) && !isDecimal(key)){
+                            if(capitalize){
+                                output += key.toUpperCase();
+                                capitalize = false;
+                            }else{
+                                output += key;
+                            }
+                        }
+                    }
+                    addToOutput = false;
                 }
             }
         }
     }
+    return output;
 }
 
 if(inputString){
     let output;
+    let filteredAlphabet = Object.keys(brailleAlphabet).filter(ele => !['capital', 'decimal', 'number', 'o', '.'].includes(ele));
+    let englishDetected = false;
 
-    output = englishToBraille(inputString)
-    // console.log(output)
+    filteredAlphabet.forEach(ele => {
+        if(inputString.toLocaleLowerCase().trim().includes(ele)){
+            englishDetected = true;
+        }
+    })
 
-    // if(output == 'Abc 123'){
-    //     console.log('passed')
-    // }
-    
-    // output = brailleToEnglish(inputString)
-    // console.log(output)
+    if(englishDetected){
+        output = englishToBraille(inputString)
+        englishDetected = false;
+    }else{
+        output = brailleToEnglish(inputString)
+    }
 
-    // if(output == '.O.OOOOO.O..O.O...'){
-    //     console.log('passed')
-    // }
+
+    console.log(output)
+
 }
