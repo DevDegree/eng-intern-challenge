87b5eadae3fa8127798bef45faee35b9c6ab174f
Krystian Rusin <36743674+KrystianRusin@users.noreply.github.com>
2024-09-05 11:34:24 -0400

    initial setup and input handling

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,16 @@
+import sys
 
+def translate_to_braille(text):
+    return text
+
+def translate_to_english(braille):
+    return braille
+
+
+if __name__ == '__main__':
+    input_text = sys.argv[1]
+
+    if all(c in 'O.' for c in input_text):
+        print(translate_to_english(input_text))
+    else:
+        print(translate_to_braille(input_text))
