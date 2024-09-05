dedcb52a1a025a8ff6372efa29ccebd75f8b11cf
samuel <leumasmontreal@hotmail.com>
2024-09-05 15:14:06 -0400

    Convertion algorithm done

--- python/conversion.py
+++ python/conversion.py
@@ -1,52 +1,80 @@
+class BrailleTranslator:
+    # Number positions are:
+    #  1 4 
+    #  2 5
+    #  3 6
 
-# Number positions are:
-#  1 4 
-#  2 5
-#  3 6
+    # Using information from https://www.pharmabraille.com/pharmaceutical-braille/the-braille-alphabet/
+    # More scalable using numbers than strings for positioning
+    braille_to_letters = {
+        1: 'a',
+        12: 'b',
+        14: 'c',
+        145: 'd',
+        15: 'e',
+        124: 'f',
+        1245: 'g',
+        125: 'h',
+        24: 'i',
+        245: 'j',
+        13: 'k',
+        123: 'l',
+        134: 'm',
+        1345: 'n',
+        135: 'o',
+        1234: 'p',
+        12345: 'q',
+        1235: 'r',
+        234: 's',
+        2345: 't',
+        136: 'u',
+        1236: 'v',
+        2456: 'w',
+        1346: 'x',
+        13456: 'y',
+        1356: 'z',
+        256: '.',
+        2: ',',
+        236: '?',
+        235: '!',
+        25: ':',
+        23: ';',
+        36: '-',
+        34: '/',
+        246: '<',
+        135: '>',
+        126: '(',
+        345: ')',
+        6: 'capital',
+        46: 'decimal',
+        3456: 'number'
+    }
 
-# Using information from https://www.pharmabraille.com/pharmaceutical-braille/the-braille-alphabet/
-braille_to_letters = {
-    1 : 'a',
-    12 : 'b',
-    14: 'c',
-    145: 'd',
-    15: 'e',
-    124: 'f',
-    1245: 'g',
-    125: 'h',
-    24: 'i',
-    245: 'j',
-    13: 'k',
-    123: 'l',
-    134: 'm',
-    1345: 'n',
-    135: 'o',
-    1234: 'p',
-    12345: 'q',
-    1235: 'r',
-    234: 's',
-    2345: 't',
-    136: 'u',
-    1236: 'v',
-    2456: 'w',
-    1346: 'x',
-    13456: 'y',
-    1356: 'z',
-    256: '.',
-    2: ',',
-    236: '?',
-    235: '!',
-    25: ':',
-    23: ';',
-    36: '-',
-    34: '/',
-    246: '<',
-    135: '>',
-    126: '(',
-    345: ')'
-}
+    def __init__(self):
+        self.letters_to_braille = {v: k for k, v in self.braille_to_letters.items()}
 
-letters_to_braille = {}
+    def char_to_braille(self, c):
+        braille = ''
+        if c.isupper():
+            braille += self.position_to_braille(self.letters_to_braille['capital'])
+        braille += self.position_to_braille(self.letters_to_braille[c.lower()])
+        return braille
+    
+    def braille_to_char(self, s):
+        if s == '......':
+            return ' '
+        num = ''
+        pos = [1, 4, 2, 5, 3, 6]
+        for i in range(len(pos)):
+            if s[pos[i] - 1] == 'O':
+                num += str(i + 1)
+        return self.braille_to_letters[int(num)]
 
-def charToBraille(c):
-    pass
+    def position_to_braille(self, pos):
+        res = str(pos)
+        braille = ['.','.','.','.','.','.']
+        pos_map = [1, 4, 2, 5, 3, 6]
+        for i in range(len(pos_map)):
+            if str(pos_map[i]) in res:
+                braille[i] = "O"
+        return ''.join(braille)
--- python/translator.py
+++ python/translator.py
@@ -1,10 +1,64 @@
 import sys
+from conversion import BrailleTranslator
 
+class BrailleProcessor:
+    def __init__(self):
+        self.translator = BrailleTranslator()
 
-for key in braille_to_letters:
-    letters_to_braille[braille_to_letters[key]] = key
+    def braille_to_text(self, s):
+        is_numbers = False
+        result = ''
+        for i in range(0, len(s), 6):
+            c = self.translator.braille_to_char(s[i: i + 6])
+            if c == 'decimal' or c == 'number':
+                is_numbers = True
+            elif c == ' ':
+                is_numbers = False
+                result += ' '
+            else:
+                if is_numbers:
+                    num = (ord(c) - ord('a') + 1) % 10
+                    result += str(num)
+                else:
+                    result += c
+        return result
 
-args = sys.argv[1:]
+    def text_to_braille(self, s):
+        result = ''
+        for c in s:
+            result += self.translator.char_to_braille(c)
+        return result
 
+    def num_to_braille(self, s):
+        result = ''
+        for c in s:
+            if c != '.':
+                c = chr(int(c) + ord('a') - 1)
+            result += self.translator.char_to_braille(c)
+        return result
 
+    def process_args(self, args):
+        result = []
+        for arg in args:
+            if arg.count('.') + arg.count('O') == len(arg) and len(arg) % 6 == 0:
+                result.append(self.braille_to_text(arg))
+            else:
+                try:
+                    float(arg)
+                    braille_str = ''
+                    if '.' in arg:
+                        braille_str += '.O...O'
+                    else:
+                        braille_str += '.O.OOO'
+                    braille_str += self.num_to_braille(arg)
+                    result.append(braille_str)
+                except ValueError:
+                    result.append(self.text_to_braille(arg))
+        return '......'.join(result)
 
+def main(args):
+    processor = BrailleProcessor()
+    print(processor.process_args(args))
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
