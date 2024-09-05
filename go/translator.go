a4922c66172af4e8f1d42ca02b61f81f4527dbc6
Sam Anthony <sam@samanthony.xyz>
2024-09-05 09:42:57 -0400

    main()

--- go/translator.go
+++ go/translator.go
@@ -1,1 +1,49 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		return
+	}
+	if isBraille(os.Args[1]) {
+		english, err := decode(strings.Join(os.Args[1:], ""))
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(english)
+	} else { // Input is English.
+		braille, err := encode(strings.Join(os.Args[1:], " "))
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(braille)
+	}
+}
+
+// isBraille returns true if str is a valid Braille string. A valid Braille string
+// contains only 'O' (uppercase letter O), and '.' (period) characters.
+func isBraille(str string) bool {
+	// TODO
+	return false
+}
+
+// decode converts a Braille string to English. braille must contain only 'O' (uppercase
+// letter O), and '.' (period) characters.
+func decode(braille string) (string, error) {
+	// TODO
+	return "", nil
+}
+
+// encode converts an English string to Braille. english must contain only a-z, A-Z, 0-9,
+// and ' ' (space) characters. If english contains any other characters, encode returns
+// ErrInvalidChar.
+func encode(english string) (string, error) {
+	// TODO
+	return "", nil
+}
