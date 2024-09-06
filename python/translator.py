fbef84b48c612ea16f4b1b3cf949f2466bbcd8c8
jessicach4n <96037905+jessicach4n@users.noreply.github.com>
2024-09-06 00:32:42 -0400

    Refactor code by separating tasks of translation from braille to latin and vice versa
    CHANGES:
    Instead of a convert function that has to constantly check the mode, I made two functions to perform each translation separately (translate_to_braille and translate_to_latin).

--- python/translator.py
+++ python/translator.py
@@ -6,16 +6,17 @@ class CharacterType(Enum):
     DECIMAL = '.O...O'
     NUMBER = '.O.OOO' 
     SPACE = '......'
-    
-class Mode(Enum):
-    TO_LATIN = 0
-    TO_BRAILLE = 1
 
 class Translator:
     def __init__(self):
         self.next_capital = False
         self.next_number = False
         self.next_decimal = False
+
+class BrailleTranslator(Translator):
+    def __init__(self):
+        super().__init__()
+        
         self.latin_alphabet = { 'A': 'O.....', 'B': 'O.O...', 'C': 'OO....',
                                 'D': 'OO.O..', 'E': 'O..O..', 'F': 'OOO...',
                                 'G': 'OOOO..', 'H': 'O.OO..', 'I': '.OO...',
@@ -46,7 +47,6 @@ class Translator:
         for key, value in self.latin_numbers.items():
             self.braille_numbers[value] = key
             
-    
     def is_braille(self, text) -> bool:
         if len(text) % 6 == 0:
             for charac in text:
@@ -55,64 +55,83 @@ class Translator:
             return True
         return False
     
-    def is_capital(self, charac, mode) -> bool:
-        if mode == Mode.TO_LATIN:
-            return charac == CharacterType.CAPITAL.value
-        return charac.isupper()
+    # def is_capital(self, charac, mode) -> bool:
+    #     if mode == Mode.TO_LATIN:
+    #         return charac == CharacterType.CAPITAL.value
+    #     return charac.isupper()
     
-    def is_decimal(self, charac, mode) -> bool:
-        if mode == Mode.TO_LATIN:
-            return charac == CharacterType.DECIMAL.value
-        return charac == self.latin_symbols.get(charac)
+    # def is_decimal(self, charac, mode) -> bool:
+    #     if mode == Mode.TO_LATIN:
+    #         return charac == CharacterType.DECIMAL.value
+    #     return charac == self.latin_symbols.get(charac)
     
-    def is_number(self, charac, mode) -> bool:
-        if mode == Mode.TO_LATIN:
-            return charac == CharacterType.NUMBER.value
-        return self.latin_numbers.get(charac)
+    # def is_number(self, charac, mode) -> bool:
+    #     if mode == Mode.TO_LATIN:
+    #         return charac == CharacterType.NUMBER.value
+    #     return self.latin_numbers.get(charac)
     
-    def is_space(self, charac, mode) -> bool:
-        if mode == Mode.TO_LATIN:
-            return charac == CharacterType.SPACE.value
-        return charac == ' '
+    # def is_space(self, charac, mode) -> bool:
+    #     if mode == Mode.TO_LATIN:
+    #         return charac == CharacterType.SPACE.value
+    #     return charac == ' '
     
-    def get_value(self, charac, mode) -> str:
-        pass
+    # def convert(self, characs, mode) -> str:
+    #     converted_text = ''
+    #     for charac in characs:
+    #         if self.is_space(charac, mode):
+    #             if mode == Mode.TO_LATIN:
+    #                 converted_text += ' '
+    #             else:
+    #                 converted_text += CharacterType.SPACE.value
+    #         elif self.is_capital(charac, mode):
+    #             next_capital = True
+    #             continue
+    #         elif next_capital:
+    #             next_capital = False
+    #             if mode == Mode.TO_LATIN:
+    #                 converted_text += self.braille_alphabet.get(charac)
+    #             else:
+    #                 converted_text += self.latin_alphabet.get(charac.upper())
+    #         else:
+    #             if mode == Mode.TO_LATIN:
+    #                 converted_text += self.braille_alphabet.get(charac).lower()
+    #             else:
+    #                 converted_text += self.latin_alphabet.get(charac.upper())
+    #     return converted_text
     
-    def convert(self, characs, mode) -> str:
+    def __translate_to_braille(self, latin_text: str) -> str:
         converted_text = ''
-        for charac in characs:
-            if self.is_space(charac, mode):
-                if mode == Mode.TO_LATIN:
-                    converted_text += ' '
-                else:
-                    converted_text += self.braille_alphabet.get(charac)
-            elif self.is_capital(charac, mode):
-                next_capital = True
-                continue
-            elif next_capital:
-                next_capital = False
-                converted_text += self.braille_alphabet.get(charac).upper()
+        for charac in latin_text:
+            if charac.isupper():
+                converted_text += CharacterType.CAPITAL.value
+            elif charac.isnumeric():
+                converted_text += (CharacterType.NUMBER.value + self.latin_numbers.get(charac))
+            elif charac in self.latin_symbols:
+                converted_text += self.latin_symbols.get(charac) # TODO: check for decimal
+            elif charac == ' ':
+                converted_text += CharacterType.SPACE.value
             else:
-                converted_text += self.braille_alphabet.get(charac).lower()
+                converted_text += self.latin_alphabet.get(charac.upper())
         return converted_text
+    
+    def __translate_to_latin(self, braille_text: str) -> str:
+        pass
             
    
     def translate(self, text: str) -> str:
         characs = []
-        mode = None
+       
         if self.is_braille(text):
-            mode = Mode.TO_LATIN
             for i in range(0, len(text), 6):
                 characs.append(text[i:i+6])
+            print(self.__translate_to_latin(text))
         else:
-            mode = Mode.TO_BRAILLE
-    
-        word = self.convert(characs, mode)
-        print(word) 
+            characs = list(text)
+            print(self.__translate_to_braille(text))
 
 def main():
     if len(argv) > 1:
-        translator = Translator()
+        translator = BrailleTranslator()
         text = ' '.join(argv[1:])
         translator.translate(text)
     else:
