f6faf2e344d238129ad525bd3f92f11ec11d3614
Aayush-Bhan <55848844+Aayush-Bhan@users.noreply.github.com>
2024-09-06 02:23:04 -0400

    Final translator.py
    Removed comments extra space to make code look cleaner.

--- python/translator.py
+++ python/translator.py
@@ -1,70 +1,26 @@
 import sys
 
-# Create mappings for text to Braille representation
-LETTER_TO_BRAILLE_MOD = {
-    'a': 'O.....',
-    'b': 'O.O...',
-    'c': 'OO....',
-    'd': 'OO.O..',
-    'e': 'O..O..',
-    'f': 'OOO...',
-    'g': 'OOOO..',
-    'h': 'O.OO..', 
-    'i': '.OO...',
-    'j': '.OOO..',
-    'k': 'O...O.',
-    'l': 'O.O.O.',
-    'm': 'OO..O.',
-    'n': 'OO.OO.',
-    'o': 'O..OO.',
-    'p': 'OOO.O.',
-    'q': 'OOOOO.',
-    'r': 'O.OOO.',
-    's': '.OO.O.',
-    't': '.OOOO.',
-    'u': 'O...OO',
-    'v': 'O.O.OO',
-    'w': '.OOO.O',
-    'x': 'OO..OO', 
-    'y': 'OO.OOO',
-    'z': 'O..OOO'
-}
+LETTER_TO_BRAILLE_MOD = {'a': 'O.....','b': 'O.O...','c': 'OO....','d': 'OO.O..','e': 'O..O..', 'f': 'OOO...','g': 'OOOO..','h': 'O.OO..', 
+                         'i': '.OO...','j': '.OOO..','k': 'O...O.','l': 'O.O.O.', 'm': 'OO..O.','n': 'OO.OO.','o': 'O..OO.', 'p': 'OOO.O.', 
+                         'q': 'OOOOO.','r': 'O.OOO.','s': '.OO.O.','t': '.OOOO.','u': 'O...OO','v': 'O.O.OO','w': '.OOO.O','x': 'OO..OO', 
+                         'y': 'OO.OOO','z': 'O..OOO'}
 
-NUMBER_TO_BRAILLE_MOD = {
-    '1': 'O.....',
-    '2': 'O.O...',
-    '3': 'OO....',
-    '4': 'OO.O..',
-    '5': 'O..O..',
-    '6': 'OOO...',
-    '7': 'OOOO..',
-    '8': 'O.OO..', 
-    '9': '.OO...',
-    '0': '.OOO..',
-}
+NUMBER_TO_BRAILLE_MOD = {'1': 'O.....', '2': 'O.O...','3': 'OO....','4': 'OO.O..','5': 'O..O..','6': 'OOO...','7': 'OOOO..','8': 'O.OO..', '9': '.OO...','0': '.OOO..', }
 
-WHITESPACE_TO_BRAILLE_MOD = {
-    ' ': '......'
-}
+WHITESPACE_TO_BRAILLE_MOD = {' ': '......'}
 
-# Reverse mappings for Braille to text
-BRAILLE_TO_LETTER_MOD = {braille: letter for letter, braille in LETTER_TO_BRAILLE_MOD.items()}
-BRAILLE_TO_NUMBER_MOD = {braille: number for number, braille in NUMBER_TO_BRAILLE_MOD.items()}
+BRAILLE_TO_LETTER_MOD = {braille: i for i, braille in LETTER_TO_BRAILLE_MOD.items()}
+BRAILLE_TO_NUMBER_MOD = {braille: n for n, braille in NUMBER_TO_BRAILLE_MOD.items()}
 BRAILLE_TO_WHITESPACE_MOD = {braille: ws for ws, braille in WHITESPACE_TO_BRAILLE_MOD.items()}
 
-# Special sequences for capitalization and numbers
 CAP_MARK_MOD = '.....O'
 NUM_MARK_MOD = '.O.OOO'
 
-# HELPER FUNCTIONS
 def verify_braille(input_braille: str) -> bool:
-    '''
-    Check if the input string is valid Braille
-    '''
+    
     if len(input_braille) % 6 != 0:
         return False
 
-    # Read through 6 character chunks
     start, end = 0, 6
     while end <= len(input_braille):
         braille_chunk = input_braille[start:end]
@@ -78,42 +34,33 @@ def verify_braille(input_braille: str) -> bool:
     return True
 
 def verify_text(input_text: str) -> bool:
-    '''
-    Check if the input string consists of valid text
-    '''
-    for char in input_text.lower():
-        if char not in LETTER_TO_BRAILLE_MOD and char not in NUMBER_TO_BRAILLE_MOD and char not in WHITESPACE_TO_BRAILLE_MOD:
+    for x in input_text.lower():
+        if x not in LETTER_TO_BRAILLE_MOD and x not in NUMBER_TO_BRAILLE_MOD and x not in WHITESPACE_TO_BRAILLE_MOD:
             return False
 
     return True
 
 def text_to_braille(input_text: str) -> str:
-    '''
-    Convert the input text to Braille
-    '''
     output_braille = ''
     is_number_active = False
 
-    for char in input_text:
-        if char.lower() in LETTER_TO_BRAILLE_MOD:
-            if char.isupper():
+    for x in input_text:
+        if x.lower() in LETTER_TO_BRAILLE_MOD:
+            if x.isupper():
                 output_braille += CAP_MARK_MOD
-            output_braille += LETTER_TO_BRAILLE_MOD[char.lower()]
-        elif char in NUMBER_TO_BRAILLE_MOD:
+            output_braille += LETTER_TO_BRAILLE_MOD[x.lower()]
+        elif x in NUMBER_TO_BRAILLE_MOD:
             if not is_number_active:
                 is_number_active = True
                 output_braille += NUM_MARK_MOD
-            output_braille += NUMBER_TO_BRAILLE_MOD[char]
-        elif char in WHITESPACE_TO_BRAILLE_MOD:
-            output_braille += WHITESPACE_TO_BRAILLE_MOD[char]
+            output_braille += NUMBER_TO_BRAILLE_MOD[x]
+        elif x in WHITESPACE_TO_BRAILLE_MOD:
+            output_braille += WHITESPACE_TO_BRAILLE_MOD[x]
             is_number_active = False
 
     return output_braille
 
 def braille_to_text(input_braille: str) -> str:
-    '''
-    Convert the Braille input back to text
-    '''
     output_text = ''
     capitalize_flag = False
     number_flag = False
