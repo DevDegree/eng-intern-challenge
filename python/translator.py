23520161a96bacb5696132d772115768250a16ce
nidhiVm6 <nidhivm6@gmail.com>
2024-09-06 17:47:33 -0400

    first commit

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,21 @@
+def braille_to_english(inpt):
+    #inpt is in braille, convert to english
+    print("braille input: ", inpt)
 
+def english_to_braille(inpt):
+    #inpt is in english, convert to braille
+    print("english input: ", inpt)
+
+def main():
+    print("Running")
+    inpt = input()
+    braille = False
+    i = 0 #character counter
+    while i < 6:
+        if inpt[i] == ".":
+            braille = True
+            break
+    if braille == True:
+        braille_to_english(inpt)
+    else:
+        english_to_braille(inpt)
