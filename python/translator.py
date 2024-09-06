a571b14ef4afbdea326ab63f42f48b1951f9862e
Kai Adams <kaiadams66@gmail.com>
2024-09-06 01:14:25 -0400

    Finished English to Braille translator plus added some tests
    Redesigned the script a bit. Added the ability to detect if braille or English text then send to the right function. Buit the English to Braille function. Added some tests to ensure functionality as intended

--- python/translator.py
+++ python/translator.py
@@ -1,9 +1,8 @@
 import sys
 
-def translate(to_trans):
-    
-    char_map = {
-        "a": "O......",
+#English to braille character map
+eng_char_map = {
+        "a":"O.....",
         "b":"O.O...",
         "c":"OO....",
         "d":"OO.O..",
@@ -44,8 +43,9 @@ def translate(to_trans):
         " ":"......"
     }
 
-    num_map = {
-        "1": "O......",
+#English to braille number map
+eng_num_map = {
+        "1":"O.....",
         "2":"O.O...",
         "3":"OO....",
         "4":"OO.O..",
@@ -57,32 +57,85 @@ def translate(to_trans):
         "0":".OOO.."
     }
 
-    capf = ".....O"
-    decf = ".O...O"
-    numf = ".O.OOO"
+#Braille to english character map
+braille_char_map = {value: key for key, value in eng_char_map.items()}
 
-    #Determine if englsh or braile
-    #Split english into list of characters and braile into list of strings each 6 chars
-    #Use dictionary to map opposite word
-    #print completed word
+#Braille to english number map
+braille_num_map = {value: key for key, value in eng_num_map.items()}
+
+#Braille follows alphabet
+capf = ".....O"
+decf = ".O...O"
+numf = ".O.OOO"
+
+#Function to change Braille to English
+def braille_to_eng(to_trans):
+    
+
+    return
+
+
+#Function to change English to Braille
+def eng_to_braille(to_trans):
+
+    brailled = []
+    number = False
+
+    trans_str_eng = list(to_trans)
+    
+    for i  in range(len(trans_str_eng)):
+
+        if trans_str_eng[i].isupper(): #Adds upper Alpha follows is letter is capital then lowers the case
+            brailled.append(capf)
+            trans_str_eng[i] = trans_str_eng[i].lower()
+
+        elif trans_str_eng[i].isnumeric() and number == False: #Adds number Alpha if char is a number only adds it once
+            brailled.append(numf)
+            number = True
+
+        elif trans_str_eng[i] == "." and trans_str_eng[i-1].isnumeric(): #Adds decimal alpha if the 
+            brailled.append(decf)
+            continue
+
+        elif trans_str_eng[i] == " ": #Turns the numberic boolen off at the first space as specified 
+            number = False
+
+
+        if trans_str_eng[i] in eng_char_map:
+            brailled.append(eng_char_map[trans_str_eng[i]])
+       
+        elif trans_str_eng[i] in eng_num_map:
+            brailled.append(eng_num_map[trans_str_eng[i]]) 
+
+        else:
+            raise ValueError("Character not in Dictionarys")
+
+    
+    eng_result = ''.join(brailled) #takes all the results and put it in one string
+
+    return eng_result
 
 
 
-
-
-
-
-
+#Main function
 def main():
     
-    args = sys.argv[1:]
+    translated = []
 
-    to_trans = ''.join(args) 
+    for arg in sys.argv[1:]:
+        if all(c in {'.', 'O'} for c in arg): #check if its braille or english
+           translated.append(braille_to_eng(arg))
+        else:
+            translated.append(eng_to_braille(arg))
+            
+            if arg != sys.argv[-1]:
+                translated.append(eng_char_map[" "])
 
-    out = translate(to_trans)
+    result = ''.join(translated) #takes all the results and put it in one string
 
-    print(out)
+    print(result)
 
+    return
 
 if __name__ == "__main__":
     main()
