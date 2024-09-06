c69f4fb88ced8baa1e9cba61991ccc8ec38ac4dd
Victor Anogwih <anogwihvictor@gmail.com>
2024-09-06 00:24:41 -0400

    Add Braille alphabet mapping for character to Braille conversion

---
+++ javascript/translator.js
@@ -0,0 +1,7 @@
+const brailleLetters = {
+  'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O.O.O.', 'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..',
+  'i': '.OO...', 'j': '.OOO..', 'k': 'O...O.', 'l': 'O.O.O.', 'm': 'OO..O.', 'n': 'OO.OO.', 'o': 'O.O..O', 'p': 'OOO.O.',
+  'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.', 'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO',
+  'y': 'OO.OOO', 'z': 'O.OOOO', ' ': '......', '0': '.O.OO.', '1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..',
+  '5': 'O.O.O.', '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...'
+};
