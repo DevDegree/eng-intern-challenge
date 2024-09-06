fe385f95f4dcf572ada79fe56dbc8636de9e3537
Dsoesito <99083937+Dsoesito@users.noreply.github.com>
2024-09-06 10:27:46 -0400

    braille to english logic

--- typescript/translator.ts
+++ typescript/translator.ts
@@ -55,8 +55,114 @@ const brailleToNumber: { [braille: string]: string } = {
   ".OOO..": "0",
 };
 
+const lettersOrSpecialToBraille: { [braille: string]: string } = {
+  a: "O.....",
+  b: "O.O...",
+  c: "OO....",
+  d: "OO.O..",
+  e: "O..O..",
+  f: "OOO...",
+  g: "OOOO..",
+  h: "O.OO..",
+  i: ".OO...",
+  j: ".OOO..",
+  k: "O...O.",
+  l: "O.O.O.",
+  m: "OO..O.",
+  n: "OO.OO.",
+  o: "O..OO.",
+  p: "OOO.O.",
+  q: "OOOOO.",
+  r: "O.OOO.",
+  s: ".OO.O.",
+  t: ".OOOO.",
+  u: "O...OO",
+  v: "O.O.OO",
+  w: ".OOO.O",
+  x: "OO..OO",
+  y: "OO.OOO",
+  z: "O..OOO",
+  ".": "..OO.O",
+  ",": "..O...",
+  "?": "..O.OO",
+  "!": "..OOO.",
+  ":": "..OO..",
+  ";": "..O.O.",
+  "-": "....OO",
+  "/": ".O..O.",
+  "<": ".OO..O",
+  // ">": "O..OO.",
+  "(": "O.O..O",
+  ")": ".O.OO.",
+  " ": "......",
+};
+
+const numberToBraille: { [braille: string]: string } = {
+  "1": "O.....",
+  "2": "O.O...",
+  "3": "OO....",
+  "4": "OO.O..",
+  "5": "O..O..",
+  "6": "OOO...",
+  "7": "OOOO..",
+  "8": "O.OO..",
+  "9": ".OO...",
+  "0": ".OOO..",
+};
+
 if (/^[O.]+$/.test(input)) {
   // Braille is inputted
+  let beginning: number = 0;
+  let end: number = 6;
+  let output: string = "";
+  let isNumber: boolean = false;
+
+  while (end <= input.length) {
+    const currentSlice: string = input.slice(beginning, end);
+
+    if (currentSlice === ".O.OOO") {
+      // Number follows indicator
+      isNumber = true;
+    } else if (isNumber && currentSlice === "......") {
+      // isNumber is true and currentSlice is space
+      isNumber = false;
+      output += " ";
+    } else if (isNumber) {
+      // If isNumber process as number
+      output += brailleToNumber[currentSlice];
+    } else if (currentSlice === ".....O") {
+      // Capital follows indicator
+      beginning += 6;
+      end += 6;
+      const nextSlice: string = input.slice(beginning, end);
+      output += brailleToLettersOrSpecial[nextSlice]?.toUpperCase();
+    } else {
+      // Regular character
+      output += brailleToLettersOrSpecial[currentSlice];
+    }
+
+    beginning += 6;
+    end += 6;
+  }
+  console.log(output);
 } else {
   // Regular character inputted
+  console.log("Input is not in Braille format.");
+  let counter: number = 0;
+  let output: string = "";
+  let isCapital: boolean = false;
+
+  while (counter < input.length) {
+    const currentChar: string = input[counter];
+
+    if (currentChar === " ") {
+      output += "......";
+    } else if (currentChar === currentChar.toUpperCase()) {
+      console.log(true);
+    } else {
+      console.log(false);
+    }
+
+    counter++;
+  }
 }
