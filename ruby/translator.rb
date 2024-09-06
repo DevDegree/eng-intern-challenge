335319a322ffeb05b7d2cec30d81dbbfc7d367e5
Chengjie Mao <cj.mao@icloud.com>
2024-09-05 21:11:52 -0400

    Change definition of translation table to make code simpler

--- ruby/translator.rb
+++ ruby/translator.rb
@@ -2,32 +2,31 @@
 
 TRANSLATION_TABLE = {
   letters: {
-    a: [0],       b: [0, 2],       c: [0, 1],       d: [0, 1, 3],
-    e: [0, 3],    f: [0, 1, 2],    g: [0, 1, 2, 3], h: [0, 2, 3],
-    i: [1, 2],    j: [1, 2, 3],    k: [0, 4],       l: [0, 2, 4],
-    m: [0, 1, 4], n: [0, 1, 3, 4], o: [0, 3, 4],    p: [0, 1, 2, 4],
-
-    q: [0, 1, 2, 3, 4], r: [0, 2, 3, 4], s: [1, 2, 4],    t: [1, 2, 3, 4],
-    u: [0, 4, 5],       v: [0, 2, 4, 5], w: [1, 2, 3, 5], x: [0, 1, 4, 5],
-    y: [0, 1, 3, 4, 5], z: [0, 3, 4, 5],
+    "a" => "O.....", "b" => "O.O...", "c" => "OO....", "d" => "OO.O..",
+    "e" => "O..O..", "f" => "OOO..." ,"g" => "OOOO..", "h" => "O.OO..",
+    "i" => ".OO...", "j" => ".OOO..", "k" => "O...O.", "l" => "O.O.O.",
+    "m" => "OO..O.", "n" => "OO.OO.", "o" => "O..OO.", "p" => "OOO.O.",
+    "q" => "OOOOO.", "r" => "O.OOO.", "s" => ".OO.O.", "t" => ".OOOO.",
+    "u" => "O...OO", "v" => "O.O.OO", "w" => ".OOO.O", "x" => "OO..OO",
+    "y" => "OO.OOO", "z" => "O..OOO",
   },
 
   numbers: {
-    1 => [0],       2 => [0, 2],       3 => [0, 1],    4 => [0, 1, 3], 5 => [0, 3],
-    6 => [0, 1, 2], 7 => [0, 1, 2, 3], 8 => [0, 2, 3], 9 => [1, 2],    0 => [1, 2, 3],
+    1 => "O.....", 2 => "O.O...", 3 => "OO....", 4 => "OO.O..", 5 => "O..O..",
+    6 => "OOO...", 7 => "OOOO..", 8 => "O.OO..", 9 => ".OO...", 0 => ".OOO..",
   },
 
   special: {
-    capital_follows: [5],
-    decimal_follows: [1, 5],
-    number_follows: [1, 3, 4, 5],
+    capital_follows: ".....O",
+    decimal_follows: ".O...O",
+    number_follows: ".O.OOO",
   },
 
   punctuations: {
-    ".": [2, 3, 5], ",": [2],       "?": [2, 4, 5], "!": [2, 3, 4],
-    ":": [2, 3],    ";": [2, 4],    "-": [4, 5],    "/": [1, 4],
-    "<": [1, 2, 5], ">": [0, 3, 4], "(": [0, 2, 5], ")": [1, 3, 4],
-    " ": []
+    "." => "..OO.O", "," => "..O...", "?" => "..O.OO", "!" => "..OOO.",
+    ":" => "..OO..", ";" => "..O.O.", "-" => "....OO", "/" => ".O..O.",
+    "<" => ".OO..O", ">" => "O..OO.", "(" => "O.O..O", ")" => ".O.OO.",
+    " " => "......"
   },
 }
 
@@ -86,8 +85,8 @@ def braille_to_english(text)
 
   # scan through each Braille symbol where each symbol consists of 6 characters
   # of either "." or "O" (capital letter "o")
-  text.scan(/([.O]{6})/) do |match|
-    state = parse_braille_symbol (match[0] || "......"), state, english
+  text.scan(/([.O]{6})/) do |group|
+    state = parse_braille_symbol (group[0] || "......"), state, english
   end
 
   english
@@ -111,13 +110,8 @@ end
 # @param text [String]
 # @return [Symbol]
 def parse_braille_symbol(symbol, state, text)
-  braille_dots = []
-  symbol.chars.each_with_index do |c, i|
-    braille_dots << i if c == "O"
-  end
-
   # check if it is a special symbol
-  case look_up(:special, braille_dots)&.to_sym
+  case REVERSE_TRANSLATION_TABLE[:special][symbol]
   when :capital_follows then state = :capital_follows
   when :number_follows then state = :number_follows
   when :decimal_follows
@@ -131,23 +125,23 @@ def parse_braille_symbol(symbol, state, text)
     text << \
       case state
       when :letter_or_punctuation
-        look_up(:letters, braille_dots) \
-          || look_up(:punctuations, braille_dots) \
+        REVERSE_TRANSLATION_TABLE[:letters][symbol] \
+          || REVERSE_TRANSLATION_TABLE[:punctuations][symbol] \
           || ""
 
       when :capital_follows
         state = :letter_or_punctuation
-        look_up(:letters, braille_dots)&.upcase || ""
+        REVERSE_TRANSLATION_TABLE[:letters][symbol]&.upcase || ""
 
       when :number_follows
-        if look_up(:punctuations, braille_dots) == " "
+        if REVERSE_TRANSLATION_TABLE[:punctuations][symbol] == " "
           # a space represents the end of a number, so the state should be
           # changed and the space should be appended
           state = :letter_or_punctuation
           " "
         else
           # still reading a number, append the digit and the state does not change
-          look_up :numbers, braille_dots
+          REVERSE_TRANSLATION_TABLE[:numbers][symbol].to_s
         end
 
       else # unreachable
@@ -166,45 +160,27 @@ end
 def parse_english_character(current, previous, braille_text)
   if current.match? /[0-9]/ # current is a digit
     # add the number mark unless previous is already a digit or a decimal point
-    braille_text << braille_symbol(:special, :number_follows) \
+    braille_text << TRANSLATION_TABLE[:special][:number_follows] \
       unless previous.match?(/[0-9.]/)
 
-    braille_text << braille_symbol(:numbers, current.to_i)
+    braille_text << TRANSLATION_TABLE[:numbers][current.to_i]
 
   elsif current == "." && previous.match?(/[0-9]/) # current is a decimal point
-    braille_text << braille_symbol(:special, :decimal_follows)
+    braille_text << TRANSLATION_TABLE[:special][:decimal_follows]
 
   elsif current.match? /[a-z]/i # current is a letter
     # add the capital letter sign if the letter is in upper case
-    braille_text << braille_symbol(:special, :capital_follows) \
+    braille_text << TRANSLATION_TABLE[:special][:capital_follows] \
       if current == current.upcase
 
-    braille_text << braille_symbol(:letters, current.downcase.to_sym)
+    braille_text << TRANSLATION_TABLE[:letters][current.downcase]
 
   else # current is punctuation
-    braille_text << braille_symbol(:punctuations, current.to_sym)
+    braille_text << TRANSLATION_TABLE[:punctuations][current]
 
   end
 
   current
-end
-
-# Helpers
-
-# @param category [Symbol]
-# @param braille_dots [Array<Integer>]
-# @return [String, nil]
-def look_up(category, braille_dots)
-  REVERSE_TRANSLATION_TABLE[category][braille_dots]&.to_s
-end
-
-# @param category [Symbol]
-# @param key [Symbol, Integer]
-# @return [String]
-def braille_symbol(category, key)
-  braille = "......"
-  TRANSLATION_TABLE[category][key]&.each { |i| braille[i] = "O" }
-  braille
 end
 
 if __FILE__ == $0
