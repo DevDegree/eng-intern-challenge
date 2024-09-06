9c7298c41a93bbb10ab51eb386700ec47d3f5fff
NolanChan1 <nolanchan1@gmail.com>
2024-09-06 16:57:27 -0600

    Implement translate_from_braille()

--- python/translator.py
+++ python/translator.py
@@ -54,6 +54,67 @@ to_braille_dict = {
     ' ': '......',
 }
 
+from_braille_dict_special = {
+    '.....O': 'CAPITAL',
+    '.O.OOO': 'NUMBER',
+}
+
+from_braille_dict_char = {
+    'O.....': 'a',
+    'O.O...': 'b',
+    'OO....': 'c',
+    'OO.O..': 'd',
+    'O..O..': 'e',
+    'OOO...': 'f',
+    'OOOO..': 'g',
+    'O.OO..': 'h',
+    '.OO...': 'i',
+    '.OOO..': 'j',
+    'O...O.': 'k',
+    'O.O.O.': 'l',
+    'OO..O.': 'm',
+    'OO.OO.': 'n',
+    'O..OO.': 'o',
+    'OOO.O.': 'p',
+    'OOOOO.': 'q',
+    'O.OOO.': 'r',
+    '.OO.O.': 's',
+    '.OOOO.': 't',
+    'O...OO': 'u',
+    'O.O.OO': 'v',
+    '.OOO.O': 'w',
+    'OO..OO': 'x',
+    'OO.OOO': 'y',
+    'O..OOO': 'z',
+    '..OO.O': ',',
+    '..O...': '.',
+    '..O.OO': '?',
+    '..OOO.': '!',
+    '..OO..': ':',
+    '..O.O.': ';',
+    '....OO': '-',
+    '.O..O.': '/',
+    '.OO..O': '<',
+    'O..OO.': '>',
+    'O.O..O': '(',
+    '.O.OO.': ')',
+    '......': ' ',
+    '.O...O': '.',
+}
+
+from_braille_dict_num = {
+    '.OOO..': '0',
+    'O.....': '1',
+    'O.O...': '2',
+    'OO....': '3',
+    'OO.O..': '4',
+    'O..O..': '5',
+    'OOO...': '6',
+    'OOOO..': '7',
+    'O.OO..': '8',
+    '.OO...': '9',
+    '......': ' ',
+}
 
 def check_if_braille(input_text):
    # If input string is not a multiple of six characters, then it cannot be braille
@@ -86,7 +147,34 @@ def translate_to_braille(english_input):
     return translated_text
 
 def translate_from_braille(braille_input):
-    return ''
+    translated_text = ''
+    number_state = False
+    capitalized_state = False
+    num_cells = len(braille_input) // 6
+    for i in range(num_cells):
+        braille_cell = braille_input[(i * 6) : ((i + 1) * 6)]
+        special_type = from_braille_dict_special.get(braille_cell, None)
+        
+        if special_type == 'CAPITAL':
+            capitalized_state = True
+        elif special_type == 'NUMBER':
+            number_state = True
+        else:
+            # If current braille cell is not special (e.g., character insert)
+            if number_state:
+                c = from_braille_dict_num.get(braille_cell, '')
+                translated_text += c
+                if c == ' ':
+                    number_state = False
+            else:
+                c = from_braille_dict_char.get(braille_cell, '')
+                if capitalized_state and c.isalpha():
+                    c = c.upper()
+                    capitalized_state = False
+                
+                translated_text += c
+
+    return translated_text
 
 def main():
     # Parse input from command line
@@ -109,6 +197,6 @@ def main():
     # Output result
     print(output)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
 
