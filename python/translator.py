3a4d3e2f686123024f482c26ea321bbcad2d30ed
NolanChan1 <nolanchan1@gmail.com>
2024-09-06 13:39:33 -0600

    Implement program structure + check_if_braille()

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,40 @@
+import sys
+import re
 
+def check_if_braille(input_text):
+   # If input string is not a multiple of six characters, then it cannot be braille
+   if len(input_text) % 6 != 0:
+       return False
+
+   # If the input string only contains the characters 'O' and '.', then it is considered to be braille
+   return bool(re.match('^[O.]+$', input_text))
+
+def translate_to_braille(english_input):
+    return ''
+
+def translate_from_braille(braille_input):
+    return ''
+
+def main():
+    # Parse input from command line
+    if len(sys.argv) <= 1:
+        print('')
+        return
+    
+    input_text = ' '.join(sys.argv[1:])
+
+    # Check if command line input is braille or not
+    is_braille = check_if_braille(input_text)
+
+    # Perform translation
+    output = ''
+    if is_braille:
+        output = translate_from_braille(input_text)
+    else:
+        output = translate_to_braille(input_text)
+
+    # Output result
+    print(output)
+
+if __name__ == "__main__":
+    main()
