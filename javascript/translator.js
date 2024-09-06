0d691624d48af5147704367cd2ff845dc274dbb2
Shajee-j <Shajeejm@gmail.com>
2024-09-06 06:37:13 -0400

    implemented braille to english, added ability to captilize only the next letter following the 'capital follows' symbol and to translate only numbers until the next space following the 'number follows' symbol

--- javascript/translator.js
+++ javascript/translator.js
@@ -1,42 +1,45 @@
 
 const alpha = {
- a:'O.....',
- b:'O.O...', 
- c:'OO....',
- d:'OO.O..', 
- f:'OOO...',
- g:'OOOO..', 
- h:'O.OO..',
- i:'.OO...', 
- j:'.OOO..',
- l:'O.O.O.', 
- m:'OO..O.',
- n:'OO.OO.', 
- o:'O..OO.',
- p:'OOO.O.', 
- q:'OOOOO.',
- r:'O.OOO.', 
- s:'.OO.O.',
- t:'.OOOO.', 
- u:'O...OO',
- v:'O.O.OO', 
- w:'.OOO.O',
- x:'OO..OO', 
- y:'OO.OOO',
- z:'O..OOO', 
+ 'a':'O.....',
+ 'b':'O.O...', 
+ 'c':'OO....',
+ 'd':'OO.O..', 
+ 'e':'O..O..',
+ 'f':'OOO...',
+ 'g':'OOOO..', 
+ 'h':'O.OO..',
+ 'i':'.OO...', 
+ 'j':'.OOO..',
+ 'l':'O.O.O.', 
+ 'm':'OO..O.',
+ 'n':'OO.OO.', 
+ 'o':'O..OO.',
+ 'p':'OOO.O.', 
+ 'q':'OOOOO.',
+ 'r':'O.OOO.', 
+ 's':'.OO.O.',
+ 't':'.OOOO.', 
+ 'u':'O...OO',
+ 'v':'O.O.OO', 
+ 'w':'.OOO.O',
+ 'x':'OO..OO', 
+ 'y':'OO.OOO',
+ 'z':'O..OOO', 
+ ' ':'......'
 }
 
+
 const num = {
-  1:'O.....',
-  2:'O.O...',
-  3:'OO....',
-  4:'OO.O..',
-  5:'O..O..',
-  6:'OOO...',
-  7:'OOOO..',
-  8:'O.OO..',
-  9:'.OO...',
-  0:'.OOO..'
+  '1':'O.....',
+  '2':'O.O...',
+  '3':'OO....',
+  '4':'OO.O..',
+  '5':'O..O..',
+  '6':'OOO...',
+  '7':'OOOO..',
+  '8':'O.OO..',
+  '9':'.OO...',
+  '0':'.OOO..'
 }
 
 const modi = {
@@ -45,27 +48,25 @@ const modi = {
   numbers:'.O.OOO'
 }
 
-const sym = {
-  '.':'..OO.O',
-  ',':'..O...',
-  '?':'..O.OO',
-  '!':'..OOO.',
-  ':':'..OO..',
-  ';':'..O.O.',
-  '-':'....OO',
-  '/':'.O..O.',
-  '<':'.OO..O',
-  '>':'O..OO.',
-  '(':'O.O..O',
-  ')':'.O.OO.',
-}
-
-const spa = {
-  ' ':'......'
-}
+// const sym = {
+//   '.':'..OO.O',
+//   ',':'..O...',
+//   '?':'..O.OO',
+//   '!':'..OOO.',
+//   ':':'..OO..',
+//   ';':'..O.O.',
+//   '-':'....OO',
+//   '/':'.O..O.',
+//   '<':'.OO..O',
+//   '>':'O..OO.',
+//   '(':'O.O..O',
+//   ')':'.O.OO.',
+// }
 
 
-// one function isn't enough, so multiple will be used. one funtion to sort between english and braille, one function to split in-coming strings or inputs into 6-characters groups, one function to translate each 6-character group into the correct word or number of the opposite languge, and lastly one to concatinate all the seperated letter or numbers into one text. 
+
+
+// one function isn't enough, so multiple will be used. one funtion to sort between english and braille, one function to split in-coming strings or inputs into 6-characters groups, one function to translate each 6-character group into the correct word or number of the opposite languge, and lastly one to concatinate all the seperated letter or numbers into one text output. 
 
 let translator = function(input) {
   
@@ -101,10 +102,60 @@ let translator = function(input) {
     let result = x.match(/[\s\S]{1,6}/g) || [];
     console.log('after spliting:')
     console.log(result)
-
+    brailleToEnglsih(result)
 
   }
   
+  const brailleToEnglsih = (arr) => {
+
+    console.log('braille To English:')
+    let arr2 = []
+
+    let object = '';
+        object = alpha;
+
+
+for (let i = 0; i < arr.length; i++) {  
+
+    if (modi.numbers === arr[i]){
+            object = num
+          }
+
+    if ('......' === arr[i]) {
+      object = alpha
+    }
+  
+function getKeyByValue(object, value) {
+    for (let prop in object) {
+        if (object.hasOwnProperty(prop)) {
+          if ( modi.capitals === value) {
+            return '*';
+          } 
+
+          if (modi.numbers === arr[i]){
+             return '';
+          }
+        
+            if (object[prop] === value)
+                return prop;
+        }
+    }
+}
+
+ans = getKeyByValue(object, arr[i]);
+
+ arr2 = arr2 + ans
+
+
+    }
+    
+    let str2 = ''
+    for (let i = 0; i < arr2.length; i++) {
+    str2 = arr2.replace(/\*(\w)/g, (_, letter) => letter.toUpperCase())
+  }
+  
+  console.log(str2);
+  }
   
   
 
@@ -125,5 +176,11 @@ let translator = function(input) {
 // translator('cookie')
 // translator('425')
 // translator('Hello world')
-translator('.....OO.OO..O..O..O.O.O.O.O.O.O..OO........OOO.OO..OO.O.OOO.O.O.O.OO.O..')
+// translator('.....OO.OO..O..O..O.O.O.O.O.O.O..OO........OOO.OO..OO.O.OOO.O.O.O.OO.O..') // output: Hello world
+// translator('.....OO.OO..O..O..O.O.O.O.O.O.O..OO............O.OOO.OO..OO.O.OOO.O.O.O.OO.O..') // output: Hello World
+// translator('.....OO.....O.OOO.OO..O.OO.OOO') // output: Army
+// translator('O.....O.OOO.OO..O.OO.OOO') // output: army
+translator('.O.OOOOO.O..O.O..............OO.....O.OOO.OO..O.OO.OOO') // output: 42 Army
+// translator('.O.OOOOO.O..O.O...') // output: 42
+
 
