511d8193db1603ec8fb4cf6bd4f508f4ed39ce13
Kai Adams <kaiadams66@gmail.com>
2024-09-06 19:14:42 -0400

    Added a 15th Test case
    Tested a combo of both english and braille inputs at once.

--- python/translator.test.py
+++ python/translator.test.py
@@ -172,5 +172,17 @@ class TestTranslator(unittest.TestCase):
         # Strip any leading/trailing whitespace from the output and compare
         self.assertEqual(result.stdout.strip(), expected_output)
     
+
+    def test_output15(self):
+        # Command to run translator.py script
+        command = ["python3", "python/translator.py", "Kai", "12 x",".OO.O..OOOO.O..OO.OOO.O...O.OO" ]
+        # Run the command and capture output
+        result = subprocess.run(command, capture_output=True, text=True)
+        
+        # Expected output without the newline at the end
+        expected_output = ".....OO...O.O......OO..........O.OOOO.....O.O.........OO..OO......stop?"
+        # Strip any leading/trailing whitespace from the output and compare
+        self.assertEqual(result.stdout.strip(), expected_output)
+
 if __name__ == '__main__':
     unittest.main()
