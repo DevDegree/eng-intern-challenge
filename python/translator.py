de9e9c9d25c94b1cdb8fbaa6c2a891a1d6e342fe
isaiahL88 <isaiah.ib@outlook.com>
2024-09-06 02:23:46 -0400

    put messy code in main func, more comprehensive dict

--- python/translator.py
+++ python/translator.py
@@ -2,7 +2,7 @@ import sys
 print(sys.argv)
 
 # dictionary used to translate letters and numbers to brail
-letterNumDict = {
+letNumDict = {
     'a': 'O.....',
     'b': 'O.O...',
     'c': 'OO....',
@@ -28,25 +28,71 @@ letterNumDict = {
     'w': '.OOO.O',
     'x': 'OO..OO',
     'y': 'OO.OOO',
-    'z': 'O..OOO'
+    'z': 'O..OOO',
+    ' ': '......',
+    '1': '0.....',
+    '2': '0.0...',
+    '3': '00....',
+    '4': '00.0..',
+    '5': '0..0..',
+    '6': '000...',
+    '7': '0000..',
+    '8': '0.00..',
+    '9': '.00...',
+    '0': '.000..',
+    'A': '.....OO.....',
+    'B': '.....OO.O...',
+    'C': '.....OOO....',
+    'D': '.....OOO.O..',
+    'E': '.....OO..O..',
+    'F': '.....OOOO...',
+    'G': '.....OOOOO..',
+    'H': '.....OO.OO..',
+    'I': '.....O.OO...',
+    'J': '.....O.OOO..',
+    'K': '.....OO...O.',
+    'L': '.....OO.O.O.',
+    'M': '.....OOO..O.',
+    'N': '.....OOO.OO.',
+    'O': '.....OO..OO.',
+    'P': '.....OOOO.O.',
+    'Q': '.....OOOOOO.',
+    'R': '.....OO.OOO.',
+    'S': '.....O.OO.O.',
+    'T': '.....O.OOOO.',
+    'U': '.....OO...OO',
+    'V': '.....OO.O.OO',
+    'W': '.....O.OOO.O',
+    'X': '.....OOO..OO',
+    'Y': '.....OOO.OOO',
+    'Z': '.....OO..OOO'
 }
 
 #dictionary used to translate braile to numbers and letters
-braileDict = {}
-
-if sys.argv.length > 1:
-    input = ' '.join(sys.argv[1:])
-    if(letterOr)
-    output = ""
-    i = 1
-    while i < sys.argv.length:
-        output += braileDict[sys.argv[i]]
-        if i != sys.argv.length:
-            output += "......"
-        i += 1
-else:
-    print("empty input")
-
+brailleDict = {v: k for k, v in letNumDict}
 
 def isBraille(text): 
-    return text.count("O") + text.count(".") === text.len()
+    return text.count("O") + text.count(".") == text.len()
+
+
+def main():
+    print(brailleDict)
+
+    if sys.argv.length > 1:
+        input = ' '.join(sys.argv[1:])
+        if isBraille:
+            print('braille')
+        else:
+            print('letnum')
+            # output = ""
+            # i = 1
+            # while i < sys.argv.length:
+            #     output += braileDict[sys.argv[i]]
+            #     if i != sys.argv.length:
+            #         output += "......"
+            #     i += 1
+    else:
+        print("empty input")
+
+if __name__ == "__main__":
+    main()
