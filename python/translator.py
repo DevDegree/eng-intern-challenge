5b4883955d373099f694d095aaa65546723f8829
Anthony Vaccaro <anthony.200115@gmail.com>
2024-09-06 19:37:54 -0400

    implemented brail to word

--- python/translator.py
+++ python/translator.py
@@ -10,6 +10,19 @@ alphabet_translation = {
     '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...', '0': '.OOO..'
 }
 
+brail_to_nums={
+    'O.....': '1', 'O.O...': '2', 'OO....': '3', 'OO.O..': '4', 'O..O..': '5',
+    'OOO...': '6', 'OOOO..': '7', 'O.OO..': '8', '.OO...': '9', '.OOO..': '0'
+}
+
+baril_to_alphabet= {
+    'O.....': 'a', 'O.O...': 'b', 'OO....': 'c', 'OO.O..': 'd', 'O..O..': 'e',
+    'OOO...': 'f', 'OOOO..': 'g', 'O.OO..': 'h', '.OO...': 'i', '.OOO..': 'j',
+    'O...O.': 'k', 'O.O.O.': 'v', 'OO..O.': 'm', 'OO.OO.': 'n', 'O..OO.': 'o',
+    'OOO.O.': 'p', 'OOOOO.': 'q', 'O.OOO.': 'r', '.OO.O.': 's', '.OOOO.': 't',
+    'O...OO': 'u', 'OO..OO': 'x', 'OO.OOO': 'y', 'O..OOO': 'z', '.OOO.O': 'w'
+}
+
 
 
 def word_to_brail(str):
@@ -34,9 +47,31 @@ def word_to_brail(str):
 
     return ''.join(word)
 
+def brail_to_word(str):
+    word=[]
+    state = 'alph'
+    cells = [str[i:i + 6] for i in range(0, len(str), 6)]
+    nextUpper = False
 
-print((word_to_brail('Abc 123 xYz')))
-# for index, char in enumerate(".....OO.....O.O...OO...........O.OOOO.....O.O...OO..........OO..OO.....OOO.OOOO..OOO"):
-#     print(char, end="")
-#     if index != 0 and (index+1)%6 == 0:
-#         print()
+    for cell in cells:
+        if cell == '......':
+            word.append(" ")
+            continue
+        if state == 'alph':
+            if cell == '.O.OOO':
+                state = 'num'
+                continue
+            elif cell == '.....O':
+                nextUpper = True
+                continue
+            else:
+                if nextUpper:
+                    word.append(baril_to_alphabet[cell].upper())
+                    nextUpper=False
+                else:
+                    word.append(baril_to_alphabet[cell])
+        else:
+             word.append(brail_to_nums[cell])
+            
+    return ''.join(word)
+
