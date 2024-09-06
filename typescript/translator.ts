118d3c0211550b2a5015a8d5c2a48194ffaa2801
Dsoesito <99083937+Dsoesito@users.noreply.github.com>
2024-09-06 10:26:35 -0400

    created constants

---
+++ typescript/translator.ts
@@ -0,0 +1,62 @@
+const input = process.argv.slice(2).join(" ");
+
+const brailleToLettersOrSpecial: { [braille: string]: string } = {
+  "O.....": "a",
+  "O.O...": "b",
+  "OO....": "c",
+  "OO.O..": "d",
+  "O..O..": "e",
+  "OOO...": "f",
+  "OOOO..": "g",
+  "O.OO..": "h",
+  ".OO...": "i",
+  ".OOO..": "j",
+  "O...O.": "k",
+  "O.O.O.": "l",
+  "OO..O.": "m",
+  "OO.OO.": "n",
+  "O..OO.": "o",
+  "OOO.O.": "p",
+  "OOOOO.": "q",
+  "O.OOO.": "r",
+  ".OO.O.": "s",
+  ".OOOO.": "t",
+  "O...OO": "u",
+  "O.O.OO": "v",
+  ".OOO.O": "w",
+  "OO..OO": "x",
+  "OO.OOO": "y",
+  "O..OOO": "z",
+  "..OO.O": ".",
+  "..O...": ",",
+  "..O.OO": "?",
+  "..OOO.": "!",
+  "..OO..": ":",
+  "..O.O.": ";",
+  "....OO": "-",
+  ".O..O.": "/",
+  ".OO..O": "<",
+  // "O..OO.": ">",
+  "O.O..O": "(",
+  ".O.OO.": ")",
+  "......": " ",
+};
+
+const brailleToNumber: { [braille: string]: string } = {
+  "O.....": "1",
+  "O.O...": "2",
+  "OO....": "3",
+  "OO.O..": "4",
+  "O..O..": "5",
+  "OOO...": "6",
+  "OOOO..": "7",
+  "O.OO..": "8",
+  ".OO...": "9",
+  ".OOO..": "0",
+};
+
+if (/^[O.]+$/.test(input)) {
+  // Braille is inputted
+} else {
+  // Regular character inputted
+}
