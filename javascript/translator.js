e4773c43db622e624093d3cfcb04cc1d439f7750
tanyakhemani <tanyakhemani.12@gmail.com>
2024-09-06 20:43:12 -0400

    complete Braille to English

--- javascript/translator.js
+++ javascript/translator.js
@@ -1,4 +1,11 @@
-import engToBraille from "./engToBraille.json" assert { type: "json" };
+import fs from "fs";
+const engToBraille = JSON.parse(fs.readFileSync("engToBraille.json", "utf-8"));
+const brailleToEngLetters = JSON.parse(
+  fs.readFileSync("./brailleToEngLetters.json", "utf-8")
+);
+const brailleToEngNumbers = JSON.parse(
+  fs.readFileSync("./brailleToEngNumbers.json", "utf-8")
+);
 
 function convertEnglishToBraille(input) {
   let brailleOutput = "";
@@ -7,28 +14,103 @@ function convertEnglishToBraille(input) {
   for (let i = 0; i < input.length; i++) {
     const character = input[i];
 
-    // if character is capital, then add capital braille
+    // if character is capital, then add braille for capitalization
     if (character === character.toUpperCase() && isNaN(character)) {
       brailleOutput += engToBraille["capital"];
     }
 
-    // if character is number, then add number braille
-    if (!isNaN(character) && character !== " ") {
-      if (!numberMode) {
-        brailleOutput += engToBraille["number"];
-        numberMode = true;
-      }
-    } else {
+    // if character is number and we're not already in number mode, then add braille delim for numbers START
+    if (!isNaN(character) && character !== " " && !numberMode) {
+      brailleOutput += engToBraille["number"];
+      numberMode = true;
+    }
+    // if character is not number and character is not space and numberMode is ON, add the braille delim for numbers END
+    else if (isNaN(character) && character !== " " && numberMode) {
       brailleOutput += engToBraille[" "];
       numberMode = false;
+      // if character is space and numberMode is ON, skip the character conversion
+    } else if (character === " " && numberMode) {
+      brailleOutput += engToBraille[" "];
+      numberMode = false;
+      continue;
     }
 
     let braille = engToBraille[character.toLowerCase()];
+    // console.log("character:", character, "braile:", braille);
     brailleOutput += braille;
   }
 
   return brailleOutput;
 }
 
-let result = convertEnglishToBraille("hello123");
+function convertBrailleToEnglish(input) {
+  let englishOutput = "";
+  let numberMode = false;
+  let capitalMode = false;
+
+  for (let i = 0; i < input.length; i += 6) {
+    const brailleChar = input.slice(i, i + 6);
+
+    // check for number START delim
+    if (brailleChar === engToBraille["number"]) {
+      numberMode = true;
+      continue;
+    }
+
+    // check for capital delim
+    if (brailleChar === engToBraille["capital"]) {
+      capitalMode = true;
+      continue;
+    }
+
+    // check for space (number END) delim
+    if (brailleChar === engToBraille[" "]) {
+      if (numberMode) {
+        numberMode = false;
+      }
+      englishOutput += " ";
+      continue;
+    }
+
+    let translatedChar = "";
+    if (numberMode) {
+      translatedChar = brailleToEngNumbers[brailleChar];
+    } else {
+      translatedChar = brailleToEngLetters[brailleChar];
+      if (capitalMode && translatedChar) {
+        translatedChar = translatedChar.toUpperCase();
+        capitalMode = false;
+      }
+    }
+
+    if (translatedChar) {
+      englishOutput += translatedChar;
+    }
+  }
+
+  return englishOutput;
+}
+
+function isBraille(characters) {
+  for (let i = 0; i < characters.length; i++) {
+    const character = characters[i];
+
+    if (character === "O" || character === ".") {
+      continue;
+    } else {
+      return false;
+    }
+  }
+  return true;
+}
+
+let result = "";
+const args = process.argv.slice(2);
+const input = args.join(" ");
+
+if (isBraille(input)) {
+  result = convertBrailleToEnglish(input);
+} else {
+  result = convertEnglishToBraille(input);
+}
 console.log(result);
--- javascript/translator.test.js
+++ javascript/translator.test.js
@@ -1,12 +1,26 @@
 const { exec } = require("child_process");
 
-describe('translator.js script', () => {
-    it('should output correct answer to the console', (done) => {
-        exec("node translator.js Abc 123 xYz", (error, stdout, stderr) => {
-            expect(error).toBeNull();
-            expect(stderr).toBe("");
-            expect(stdout.trim()).toBe(".....OO.....O.O...OO...........O.OOOO.....O.O...OO..........OO..OO.....OOO.OOOO..OOO");
-            done();
-        });
+describe("translator.js script", () => {
+  it("should output correct answer to the console", (done) => {
+    exec("node translator.js Abc 123 xYz", (error, stdout, stderr) => {
+      expect(error).toBeNull();
+      expect(stderr).toBe("");
+      expect(stdout.trim()).toBe(
+        ".....OO.....O.O...OO...........O.OOOO.....O.O...OO..........OO..OO.....OOO.OOOO..OOO"
+      );
+      done();
     });
+  });
+
+  it("should translate braille to english to the console", (done) => {
+    exec(
+      "node translator.js .....OO.....O.O...OO...........O.OOOO.....O.O...OO..........OO..OO.....OOO.OOOO..OOO",
+      (error, stdout, stderr) => {
+        expect(error).toBeNull();
+        expect(stderr).toBe("");
+        expect(stdout.trim()).toBe("Abc 123 xYz");
+        done();
+      }
+    );
+  });
 });
