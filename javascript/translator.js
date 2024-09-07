631254f76e81f39f1c7d0d5bf9691e3a4fcd7c57
JeremyC <jeremy4322@gmail.com>
2024-09-07 02:22:49 -0400

    some changes

--- javascript/translator.js
+++ javascript/translator.js
@@ -21,63 +21,6 @@
 // well, 0.00.. translates to h, meaning yes, its read row by row.
 
 //so i could start with just a big fat object for each character.
-// const brailleLibrary = {
-//   a: "0.....",
-//   b: "0.0...",
-//   c: "00....",
-//   d: "00.0..",
-//   e: "0..0..",
-//   f: "000...",
-//   g: "0000..",
-//   h: "0.00..",
-//   i: ".00...",
-//   j: ".000..",
-//   k: "0...0.",
-//   l: "0.0.0.",
-//   m: "00..0.",
-//   n: "00.00.",
-//   o: "0..00.",
-//   p: "000.0.",
-//   q: "00000.",
-//   r: "0.000.",
-//   s: ".00.0.",
-//   t: ".0000.",
-//   u: "0...00",
-//   v: "0.0.00",
-//   w: ".000.0",
-//   x: "00..00",
-//   y: "00.000",
-//   z: "0..000",
-//   1: "0.....",
-//   2: "0.0...",
-//   3: "00....",
-//   4: "00.0..",
-//   5: "0..0..",
-//   6: "000...",
-//   7: "0000..",
-//   8: "0.00..",
-//   9: ".00...",
-//   0: ".000..",
-//   caps: ".....0",
-//   decimal: ".0...0",
-//   numberFollows: ".0.000",
-//   period: "..00.0",
-//   comma: "..0...",
-//   question: "..0.00",
-//   exclamation: "..000.",
-//   colon: "..00..",
-//   semicolon: "..0.0.",
-//   hyphen: "....00",
-//   slash: ".0..0.",
-//   left: ".00..0",
-//   right: "0..00.",
-//   openBracket: "0.0..0",
-//   closeBracket: ".0.00.",
-//   space: "......",
-//   capitalize: (character) => {
-//     character.toUppercase()
-//   }
-// }
 //threw in a capitalize method for now.
 //numberFollows is interesting. All following characters are numbers until space follows. whys that? 
 //ah. cause its the exact same in braille. i could probably refactor the code here so i dont double repeat f and 6.
@@ -183,6 +126,7 @@ const brailleToEngLibrary = {
   ".O.OO.": ")", 
   "......": " ",  
   "..OO.O": ".", //period
+  '.O...O': "." // decimal
 }
 
 const brailleOperators = {
@@ -202,54 +146,8 @@ const brailleToNumber = {
   "O.OO..": "8",
   ".OO...": "9",
   ".OOO..": "0",
+  ".O...O": "."
 }
-
-
-
-
-// const brailleToEngLibrary = {
-//   '.O..': 'j',
-//   'O.....': 'a',
-//   '0.O...': 'b',
-//   '00....': 'c',
-//   '00.0..': 'd',
-//   '0..0..': 'e',
-//   '000...': 'f',
-//   '0000..': 'g',
-//   '0.00..': 'h',
-//   '.00...': 'i',
-//   '0...0.': 'k',
-//   '0.0.0.': 'l',
-//   '00..0.': 'm',
-//   '00.00.': 'n',
-//   '0..00.': '>',
-//   '000.0.': 'p',
-//   '00000.': 'q',
-//   '0.000.': 'r',
-//   '.00.0.': 's',
-//   '.0000.': 't',
-//   '0...00': 'u',
-//   '0.0.00': 'v',
-//   '.000.0': 'w',
-//   '00..00': 'x',
-//   '00.000': 'y',
-//   '0..000': 'z',
-//   '.....0': 'caps',
-//   '.0...0': '.',
-//   '.0.000': 'numberFollows',
-//   '..00.0': 'period',
-//   '..0...': ',',
-//   '..0.00': '?',
-//   '..000.': '!',
-//   '..00..': ':',
-//   '..0.0.': ';',
-//   '....00': '-',
-//   '.0..0.': '/',
-//   '.00..0': '<',
-//   '0.0..0': '(',
-//   '.0.00.': ')',
-//   '......': ' '
-// } 
 
 
 const brailleString = ".....OO.OO..O..O..O.O.O.O.O.O.O..OO...OOO.......O.....O.O...OO...............O.OOOO.O.OO...OO....OO.O........OO....OO.O.......O............OOOO.O..O...OO.O..OOOO.......O.....O.O...OO...."
@@ -258,7 +156,7 @@ const englishString = "Hello, 123 World!"
 const brailleToEnglishTranslator = (message) => {
 
   //prep variables
-  let messageBox = [] //where strings are translated
+  let messageBox = [] 
   let capitalFollowsFlag = false //flag next braille chunk to be capitalized
   let numberFollowsFlag = false //flag for turning stuff into numbers
 
@@ -274,12 +172,12 @@ const brailleToEnglishTranslator = (message) => {
 
   //for each chunk
   const translatedMessage = messageBox.map((chunk) => {
-    console.log(chunk, 'chunk here')
+    // console.log(chunk, 'chunk here')
     let translatedChar
 
     //check if its a capitalizer first. 
     if (brailleOperators[chunk] === "capitalFollows") {
-      console.log('caps here')
+      // console.log('caps here')
       capitalFollowsFlag = true
       return ""
     } 
@@ -293,7 +191,7 @@ const brailleToEnglishTranslator = (message) => {
     if (numberFollowsFlag){
       
       if(brailleToEngLibrary[chunk] === " ") {
-        console.log('space found', brailleToEngLibrary[chunk], chunk)
+        // console.log('space found', brailleToEngLibrary[chunk], chunk)
         translatedChar = brailleToEngLibrary[chunk]
         numberFollowsFlag = false
         return translatedChar
@@ -304,7 +202,6 @@ const brailleToEnglishTranslator = (message) => {
         return translatedChar
       }
     }
-
 
     //get the translated character
     translatedChar = brailleToEngLibrary[chunk]
@@ -317,7 +214,7 @@ const brailleToEnglishTranslator = (message) => {
     return translatedChar
   })
   
-  console.log(translatedMessage.join(""), 'translated message')
+  return translatedMessage.join("")
 
 }
 
@@ -330,7 +227,7 @@ const englishToBrailleTranslator = (message) => {
 
   //for each character, translate it to braille
   chars.forEach((char) => {
-    console.log(char)
+    // console.log(char)
   
     //check if this character is uppercase. use regex to ensure its not flagged true for non alphabet stuff.
     if (char.match(/[a-zA-Z]/) && char === char.toUpperCase()){
@@ -347,22 +244,38 @@ const englishToBrailleTranslator = (message) => {
     } else {
         const brailleChar = engToBrailleLibrary[char] || "?";  // Fallback for unrecognized characters
         messageBox.push(brailleChar);
-        numberize = false;  // Exit number mode for non-number characters
+        if(char === " ") {
+          numberize = false;  
+        }
+       
     }
     
       
 
   })
   const finalMessage = messageBox.join("")
-
-  console.log(finalMessage, 'message here')
-  brailleToEnglishTranslator(finalMessage)
+  return finalMessage
+  // console.log(finalMessage, 'message here')
+  // brailleToEnglishTranslator(finalMessage)
 }
 
+const main = () => {
+  const data = process.argv.slice(2)
+  const inputString = data.join(" ")
+  // console.log(inputString)
+  // const inputString = "OOO....O...O.O.OOOOO...."
+  if (/^[\.O]+$/.test(inputString)){
+    console.log(brailleToEnglishTranslator(inputString))
+  } else {
+    console.log(englishToBrailleTranslator(inputString))
+  }
+
+}
+main()
 //? to do
 //? get decimal follows working
 //? then get engtoBraille() working and its operators (if any are needed)
 //? return ? if its nonbraille
 
-brailleToEnglishTranslator(brailleString)
+// brailleToEnglishTranslator(brailleString)
 // englishToBrailleTranslator(englishString)
