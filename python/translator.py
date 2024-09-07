ce4d880b36ed2317c34eaf682399dce7b73bb0e1
Bogdan Feher <bogdan.feher@gmail.com>
2024-09-07 17:21:37 -0400

    added pseudo code for first solution

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,24 @@
 
+'''
+first solution that comes to mind:
+initialize result string
+
+check if input braille or english (if characters in argument not in ['0', '.']
+input = braille:
+    create dict for braille to english
+    create list with each braille letter seperated
+    
+    loop through list
+        treat special cases (capital follows, decimal follows, number follows)
+        append english letter to result
+
+input = english:
+    create dict for english to braille
+    create list with each character seperated
+
+    loop through list
+        treat special cases (capital follows, decimal follows, number follows)
+        append braille letter to result
+
+return result
+'''
