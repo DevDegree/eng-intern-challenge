7f638b143903a142baf44f1c19bf0a7841d178d1
Alex Olowookere <alexandero@wingmateapp.com>
2024-09-07 10:16:08 -0400

    built braille patterns

--- python/translator.py
+++ python/translator.py
@@ -17,3 +17,14 @@ W is unique and has a different pattern altogether.
 In the end, I aimed for readability and simplicity of the code.
 '''
 
+# Braille patterns for each letter and special characters excluding punctuations as per the technical requirements.
+braille_patterns = {
+    'a': 'O.....',    'b': 'O.O...',    'c': 'OO....',    'd': 'OO.O..',
+    'e': 'O..O..',    'f': 'OOO...',    'g': 'OOOO..',    'h': 'O.OO..',
+    'i': '.OO...',    'j': '.OOO..',    'k': 'O...O.',    'l': 'O.O.O.',
+    'm': 'OO..O.',    'n': 'OO.OO.',    'o': 'O..OO.',    'p': 'OOO.O.',
+    'q': 'OOOOO.',    'r': 'O.OOO.',    's': '.OO.O.',    't': '.OOOO.',
+    'u': 'O...OO',    'v': 'O.O.OO',    'w': '.OOO.O',    'x': 'OO..OO',
+    'y': 'OO.OOO',    'z': 'O..OOO',    ' ': '......',
+    'capital': '.....O',  'number': '.O.OOO'
+}
