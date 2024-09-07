290ea1271286e35ff05c10786bc1e9ef502533a1
Alex Olowookere <alexandero@wingmateapp.com>
2024-09-07 10:14:33 -0400

    explaining thought process

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,19 @@
+'''
+Name: Oluwasina Olowookere,
+Email: oluwsinaalex@gmail.com,
+'''
+import sys
+
+'''
+My thought process is to create a dictionary 
+that maps each letter to its corresponding Braille pattern.
+I wanted to see if I could do this programmatically without hardcoding the patterns,
+because I researched and found that the Braille alphabet are repeating patterns. i.e 
+A-J only use a combination of the first 4 dots in the 3x2 matrix.
+K-T repeat the pattern of A-J but with an added dot in the bottom left corner.
+U-Z repeat the pattern of A-J but with the last row filled out.
+W is unique and has a different pattern altogether.
+
+In the end, I aimed for readability and simplicity of the code.
+'''
 
