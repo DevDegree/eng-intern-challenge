11f854cc135638ed26f2eb1fdbb8561119cf8b28
Khai Nguyen <khaivnguyen02@gmail.com>
2024-09-07 15:34:30 +0700

    Add mapping for English characters and their Braille representations

---
+++ ruby/translator.rb
@@ -0,0 +1,38 @@
+class BrailleTranslator
+  BRAILLE_MAPPING = {
+    'a' => 'O.....',
+    'b' => 'O.O...',
+    'c' => 'OO....',
+    'd' => 'OO.O..',
+    'e' => 'O..O..',
+    'f' => 'OOO...',
+    'g' => 'OOOO..',
+    'h' => 'O.OO..',
+    'i' => '.OO...',
+    'j' => '.OOO..',
+    'k' => 'O...O.',
+    'l' => 'O.O.O.',
+    'm' => 'OO..O.',
+    'n' => 'OO.OO.',
+    'o' => 'O..OO.',
+    'p' => 'OOO.O.',
+    'q' => 'OOOOO.',
+    'r' => 'O.OOO.',
+    's' => '.OO.O.',
+    't' => '.OOOO.',
+    'u' => 'O...OO',
+    'v' => 'O.O.OO',
+    'w' => '.OOO.O',
+    'x' => 'OO..OO',
+    'y' => 'OO.OOO',
+    'z' => 'O..OOO',
+    ' ' => '......',
+    'capital' => '.....O',
+    'number' => '..OO.O'
+  }
+
+  NUMBERS = {
+    'a' => '1', 'b' => '2', 'c' => '3', 'd' => '4', 'e' => '5',
+    'f' => '6', 'g' => '7', 'h' => '8', 'i' => '9', 'j' => '0'
+  }
+end
