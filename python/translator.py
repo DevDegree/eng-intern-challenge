51418ace276fb9a88151ff9db19596d57a2f00b1
Anthony Vaccaro <anthony.200115@gmail.com>
2024-09-06 21:36:53 -0400

    finished program
    I think because of the python version used its only works if translator.test.py is renamed to translator_test.py. Also, python is used to run instead of python3. I don't think this will be a problem when ran on the companys' system where they are using python 3.8 for some reason

--- python/translator.py
+++ python/translator.py
@@ -1,3 +1,6 @@
+import re
+import sys
+
 alphabet_translation = {
     'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..',
     'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..',
@@ -10,47 +13,49 @@ alphabet_translation = {
     '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...', '0': '.OOO..'
 }
 
-brail_to_nums={
+brail_to_nums = {
     'O.....': '1', 'O.O...': '2', 'OO....': '3', 'OO.O..': '4', 'O..O..': '5',
     'OOO...': '6', 'OOOO..': '7', 'O.OO..': '8', '.OO...': '9', '.OOO..': '0'
 }
 
-baril_to_alphabet= {
+brail_to_alphabet = {
     'O.....': 'a', 'O.O...': 'b', 'OO....': 'c', 'OO.O..': 'd', 'O..O..': 'e',
     'OOO...': 'f', 'OOOO..': 'g', 'O.OO..': 'h', '.OO...': 'i', '.OOO..': 'j',
-    'O...O.': 'k', 'O.O.O.': 'v', 'OO..O.': 'm', 'OO.OO.': 'n', 'O..OO.': 'o',
+    'O...O.': 'k', 'O.O.O.': 'l', 'OO..O.': 'm', 'OO.OO.': 'n', 'O..OO.': 'o',
     'OOO.O.': 'p', 'OOOOO.': 'q', 'O.OOO.': 'r', '.OO.O.': 's', '.OOOO.': 't',
-    'O...OO': 'u', 'OO..OO': 'x', 'OO.OOO': 'y', 'O..OOO': 'z', '.OOO.O': 'w'
+    'O...OO': 'u', 'O.O.OO': 'v', '.OOO.O': 'w', 'OO..OO': 'x', 'OO.OOO': 'y',
+    'O..OOO': 'z'
 }
 
-
-
-def word_to_brail(str):
-    word=[]
+def word_to_brail(s):
+    word = []
     state = 'alph'
-    str.strip()
-    for char in str:
+    s = s.strip()
+    
+    for char in s:
         if char == ' ':
             word.append("......")
             continue
-        if char.isdigit() and state!='num':
-            word.append('.O.OOO')
-            state = 'num'
         
+        if char.isdigit():
+            if state != 'num':
+                word.append('.O.OOO')  # Numeric indicator
+                state = 'num'
+            word.append(alphabet_translation[char])  # Translate the digit
         elif char.isalpha():
-            if state!='alpha':
-                state = "alpha"
-            
+            if state != 'alph':
+                state = 'alph'  # Reset back to alphabet state after numbers
             if char.isupper():
-                word.append(".....O")
-        word.append(alphabet_translation[char.lower()])
-
+                word.append(".....O")  # Capital letter indicator
+            word.append(alphabet_translation[char.lower()])
+    
     return ''.join(word)
 
-def brail_to_word(str):
-    word=[]
+
+def brail_to_word(s):
+    word = []
     state = 'alph'
-    cells = [str[i:i + 6] for i in range(0, len(str), 6)]
+    cells = [s[i:i + 6] for i in range(0, len(s), 6)]
     nextUpper = False
 
     for cell in cells:
@@ -66,16 +71,24 @@ def brail_to_word(str):
                 continue
             else:
                 if nextUpper:
-                    word.append(baril_to_alphabet[cell].upper())
-                    nextUpper=False
+                    word.append(brail_to_alphabet.get(cell, '?').upper())
+                    nextUpper = False
                 else:
-                    word.append(baril_to_alphabet[cell])
+                    word.append(brail_to_alphabet.get(cell, '?'))
         else:
-             word.append(brail_to_nums[cell])
+            word.append(brail_to_nums.get(cell, '?'))
             
     return ''.join(word)
 
-<<<<<<< Updated upstream
-=======
-print(brail_to_word('.....OO.....O.O...OO...........O.OOOO.....O.O...OO....'))
->>>>>>> Stashed changes
+
+if len(sys.argv) > 1:
+    test_string = ' '.join(sys.argv[1:])
+    # Check if the input is already in braille format (only dots and 'O')
+    if re.fullmatch(r'^[O.]+$', test_string):
+        # Input is in braille format, convert to word
+        print(brail_to_word(test_string))
+    else:
+        # Input is a word, convert to braille
+        print(word_to_brail(test_string))
+
+#not exactly sure how to format output so that it works with translator.test.py
