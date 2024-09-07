4672b6264fd61e8ace4c2066c476dd19cdc3dd23
Arathi Vallipuranathan <avallipuranathan@gmail.com>
2024-09-06 20:06:39 -0400

    create dict for braille/english translations

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,51 @@
 
+# Define symbols for following char being capital, decimal, or number
+CAPITAL_FOLLOWS = "....O"
+NUMBER_FOLLOWS = ".O.OOO"
+SPACE = "......"
+
+# Define Braille alphabet for translation
+braille_to_letters = {
+    'O.....' : 'a', 
+    'O.O...' : 'b',
+    'OO....' : 'c',
+    'OO.O..' : 'd',
+    'O..O..' : 'e',
+    'OOO...' : 'f',
+    'OOOO..' : 'g',
+    'O.OO..' : 'h',
+    '.OO...' : 'i',
+    '.OOO..' : 'j',
+    'O...O.' : 'k',
+    'O.O.O.' : 'l',
+    'OO..O.' : 'm',
+    'OO.OO.' : 'n',
+    'O..OO.' : 'o',
+    'OOO.O.' : 'p',
+    'OOOOO.' : 'q',
+    'O.OOO.' : 'r',
+    '.OO.O.' : 's',
+    '.OOOO.' : 't',
+    'O...OO' : 'u',
+    'O.O.OO' : 'v',
+    '.OOO.O' : 'w',
+    'OO..OO' : 'x',
+    'OO.OOO' : 'y',
+    'O..OOO' : 'z', 
+}
+
+braille_to_numbers = {
+    'O.....' : '1',
+    'O.O...' : '2',
+    'OO....' : '3',
+    'OO.O..' : '4',
+    'O..O..' : '5',
+    'OOO...' : '6',
+    'OOOO..' : '7',
+    'O.OO..' : '8',
+    '.OO...' : '9',
+    '.OOO..' : '0',
+}
+
+letters_to_braille = {v: k for k, v in braille_to_letters.items()}
+numbers_to_braille = {v: k for k, v in braille_to_numbers.items()}
