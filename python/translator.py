4361de94b84d0a59cd8616bc563ad830c3120a5d
fj-416 <fahad.jamil.94@gmail.com>
2024-09-07 00:13:10 -0400

    Barebones code for parsing command line argument and checking to see if Braille or text.

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,30 @@
+import argparse, re
+
+class translator:
+    def __init__(self) -> None:
+
+        parser = argparse.ArgumentParser(description='Translate a string into braille or braille to text.')
+
+        parser.add_argument('BrailleOrText', type=str)
+
+        givenArgument = parser.parse_args()
+
+        # set up regex
+        pattern = r'^(?=.*[O.])[O.]{2,}$'
+        compiledPattern = re.compile(pattern)
+        if re.match(compiledPattern, givenArgument.BrailleOrText):
+            print("braille") # we will call corresponding functions from here
+        else:
+            print("text")
+
+        self.text = givenArgument.BrailleOrText # we will only take the first argument because that's all that matters to us
+
+    
+
+
+
+
+if __name__ == "__main__":
+    translatorObject = translator()
+
 
