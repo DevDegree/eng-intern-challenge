f8dbb8549e04c025751596b944aec4ff5b77421f
Andy Tran <dev.andytran@gmail.com>
2024-09-07 17:16:54 -0400

    Translation from English to Braille implemented.

--- ruby/translator.rb
+++ ruby/translator.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 class Translator
-    ENGLISH_TO_BRAILLE = {
+    LETTERS_ENGLISH_TO_BRAILLE = {
       "a" => "O.....",
       "b" => "O.O...",
       "c" => "OO....",
@@ -29,11 +29,20 @@ class Translator
       "y" => "OO.OOO",
       "z" => "O..OOO"
     }
-    BRAILLE_TO_ENGLISH = ENGLISH_TO_BRAILLE.invert
-    BRAILLE_LETTER_TO_DIGIT = {
-      "a" => "1", "b" => "2", "c" => "3", "d" => "4", "e" => "5", "f" => "6",
-      "g" => "7", "h" => "8", "i" => "9", "j" => "10",
+    NUMBERS_ENGLISH_TO_BRAILLE = {
+      "1" => "O.....",
+      "2" => "O.O...",
+      "3" => "OO....",
+      "4" => "OO.O..",
+      "5" => "O..O..",
+      "6" => "OOO...",
+      "7" => "OOOO..",
+      "8" => "O.OO..",
+      "9" => ".OO...",
+      "0" => ".OOO.."
     }
+    LETTERS_BRAILLE_TO_ENGLISH = LETTERS_ENGLISH_TO_BRAILLE.invert
+    NUMBERS_BRAILLE_TO_ENGLISH = NUMBERS_ENGLISH_TO_BRAILLE.invert
     BRAILLE_CAPITAL_FOLLOWS = ".....O"
     BRAILLE_NUMBER_FOLLOWS  = ".O.OOO"
     BRAILLE_SPACE           = "......"
@@ -54,30 +63,30 @@ class Translator
         capital_follows = false
         number_follows  = false
 
-        # Iterate over the string 's', processing it in chunks of 6 characters (representing a braille character)
+        # Process chunks of 6 characters in braille_string (representing one braille character)
         braille_string.chars.each_slice(6) do |slice|
             braille_char = slice.join
 
-            case
-            when braille_char == BRAILLE_CAPITAL_FOLLOWS
+            case braille_char
+            when BRAILLE_CAPITAL_FOLLOWS
                 capital_follows = true
-            when braille_char == BRAILLE_NUMBER_FOLLOWS
+            when BRAILLE_NUMBER_FOLLOWS
                 number_follows = true
-            when braille_char == BRAILLE_SPACE
-                translation += " "
+            when BRAILLE_SPACE
+                translation << " "
                 number_follows = false # When a Braille `number follows` symbol is read, assume all following symbols are numbers until the next `space` symbol
             else
                 # Convert braille to English using braille_to_english hash
-                english_char = BRAILLE_TO_ENGLISH[braille_char]
+                english_char = LETTERS_BRAILLE_TO_ENGLISH[braille_char]
 
                 # Capitalize English letter if capital_follows = true
                 english_char = capital_follows ? english_char.capitalize : english_char
                 capital_follows = false
 
                 # Convert English letter to its digit equivalent if number_follows = true
-                english_char = number_follows ? BRAILLE_LETTER_TO_DIGIT[english_char] : english_char
+                english_char = number_follows ? NUMBERS_BRAILLE_TO_ENGLISH[braille_char] : english_char
 
-                translation += english_char
+                translation << english_char
             end
         end
 
@@ -86,17 +95,50 @@ class Translator
     end
 
     def translate_to_braille(english_string)
+        def is_capitalized?(char)
+            char.ord.between?(65, 90)
+        end
+
+        def is_digit?(char)
+            char.ord.between?(48, 57)
+        end
+
+        # Initialize an empty string to hold the translation
         translation = ""
+
+        number_follows_active = false
+
+        english_string.each_char do |english_char|
+            case
+            when is_capitalized?(english_char)
+                translation << BRAILLE_CAPITAL_FOLLOWS + LETTERS_ENGLISH_TO_BRAILLE[english_char]
+            when is_digit?(english_char)
+                unless number_follows_active
+                    translation << BRAILLE_NUMBER_FOLLOWS
+                    number_follows_active = true
+                end
+                translation << NUMBERS_ENGLISH_TO_BRAILLE[english_char]
+            when english_char == " "
+                translation << BRAILLE_SPACE
+                number_follows_active = false
+            else
+                translation << LETTERS_ENGLISH_TO_BRAILLE[english_char]
+            end
+        end
+
         puts translation
     end
 end
 
-translator= Translator.new
-ARGV.each do|arg|
-    if translator.is_braille?(arg)
-        translator.translate_to_english(arg)
+if __FILE__ == $0
+    translator= Translator.new
+    string_to_translate = ARGV.join(" ")
+
+    if translator.is_braille?(string_to_translate)
+        translator.translate_to_english(string_to_translate)
     else
-        translator.translate_to_braille(arg)
+        translator.translate_to_braille(string_to_translate)
     end
 end
+
 
