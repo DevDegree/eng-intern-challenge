6475947898fe3bbd89debdbeab0d3d407faa7d65
guranshvir <guranshvir@gmail.com>
2024-09-06 21:35:47 -0700

    Guranshvir

--- python/translator.test.py
+++ python/translator.test.py
@@ -5,16 +5,13 @@ import subprocess
 class TestTranslator(unittest.TestCase):
     def test_output(self):
         # Command to run translator.py script
-        command = ["python3", "translator.py",
-                   "Abc", "123", "xYz", "42", "Hello world"]
-
-        # I added more test to check, well I am leaving them here so as if you want to check.
+        command = ["python3", "translator.py", "Abc", "123", "xYz"]
 
         # Run the command and capture output
         result = subprocess.run(command, capture_output=True, text=True)
 
         # Expected output without the newline at the end
-        expected_output = ".....OO.....O.O...OO...........O.OOOO.....O.O...OO..........OO..OO.....OOO.OOOO..OOO.......O.OOOOO.O..O.O..............OO.OO..O..O..O.O.O.O.O.O.O..OO........OOO.OO..OO.O.OOO.O.O.O.OO.O.."
+        expected_output = ".....OO.....O.O...OO...........O.OOOO.....O.O...OO..........OO..OO.....OOO.OOOO..OOO"
 
         # Strip any leading/trailing whitespace from the output and compare
         self.assertEqual(result.stdout.strip(), expected_output)
