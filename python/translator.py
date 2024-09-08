b562cec1f70f2c243fe7d33ee2fe1a199d7abff1
Bogdan Feher <bogdan.feher@gmail.com>
2024-09-08 09:36:40 -0400

    refactored code, much cleaner, also added comments for important steps

--- python/translator.py
+++ python/translator.py
@@ -1,6 +1,5 @@
 
 import sys
-import re
 
 '''
 first solution that comes to mind:
@@ -29,172 +28,181 @@ return result
 def translate_input(args):
     result = ""
 
-    # create dict for english to braille
-    english_to_braille = {
-        'a': "O.....",
-        'b': "O.O...",
-        'c': "OO....",
-        'd': "OO.O..",
-        'e': "O..O..",
-        'f': "OOO...",
-        'g': "OOOO..",
-        'h': "O.OO..",
-        'i': ".OO...",
-        'j': ".OOO..",
-        'k': "O...O.",
-        'l': "O.O.O.",
-        'm': "OO..O.",
-        'n': "OO.OO.",
-        'o': "O..OO.",
-        'p': "OOO.O.",
-        'q': "OOOOO.",
-        'r': "O.OOO.",
-        's': ".OO.O.",
-        't': ".OOOO.",
-        'u': "O...OO",
-        'v': "O.O.OO",
-        'w': ".OOO.O",
-        'x': "OO..OO",
-        'y': "OO.OOO",
-        'z': "O..OOO",
-        
-        '1': "O.....",
-        '2': "O.O...",
-        '3': "OO....",
-        '4': "OO.O..",
-        '5': "O..O..",
-        '6': "OOO...",
-        '7': "OOOO..",
-        '8': "O.OO..",
-        '9': ".OO...",
-        '0': ".OOO..",
-        
-        ',': "..O...",
-        ';': "..O.O.",
-        ':': "..OO..",
-        '.': "..OO.O",
-        '!': "..OOO.",
-        '?': "..O.OO",
-        '(': ".OO.OO",
-        ')': ".OO.OO",
-        '-': "..O..O",
-        '/': ".O.OO.",
-        '<': ".O.OO.",
-        '>': ".O.OO.",
-        
-        'capital': ".....O",
-        'number': ".O.OOO",
-        ' ': "......"
-    }
+    # assume that we are either translating all arguments from english to braille or from braille to english but not both 
+    # (ex: translator.py '......' 'Hello' -> wont work).
+    
+    if set(args[0]).issubset({'O', '.'}):
+        # is braille, braille only 1st argument since the space is '......'
+        braille_text = args[0]
 
-    #create dict for braille to english
-    braille_to_english = {
-        "O.....": 'a',
-        "O.O...": 'b',
-        "OO....": 'c',
-        "OO.O..": 'd',
-        "O..O..": 'e',
-        "OOO...": 'f',
-        "OOOO..": 'g',
-        "O.OO..": 'h',
-        ".OO...": 'i',
-        ".OOO..": 'j',
-        "O...O.": 'k',
-        "O.O.O.": 'l',
-        "OO..O.": 'm',
-        "OO.OO.": 'n',
-        "O..OO.": 'o',
-        "OOO.O.": 'p',
-        "OOOOO.": 'q',
-        "O.OOO.": 'r',
-        ".OO.O.": 's',
-        ".OOOO.": 't',
-        "O...OO": 'u',
-        "O.O.OO": 'v',
-        ".OOO.O": 'w',
-        "OO..OO": 'x',
-        "OO.OOO": 'y',
-        "O..OOO": 'z',
-        
-        "..O...": ',',
-        "..O.O.": ';',
-        "..OO..": ':',
-        "..OO.O": '.',
-        "..OOO.": '!',
-        "..O.OO": '?',
-        ".OO.OO": '(',
-        ".OO.OO": ')',
-        "..O..O": '-',
-        ".O.OO.": '/',
-        ".O.OO.": '<',
-        ".O.OO.": '>',
-        
-        ".....O": 'capital',
-        ".O.OOO": 'number',
-        "......": ' '
-    }
+        #create dict for braille to english
+        braille_to_english = {
+            "O.....": 'a',
+            "O.O...": 'b',
+            "OO....": 'c',
+            "OO.O..": 'd',
+            "O..O..": 'e',
+            "OOO...": 'f',
+            "OOOO..": 'g',
+            "O.OO..": 'h',
+            ".OO...": 'i',
+            ".OOO..": 'j',
+            "O...O.": 'k',
+            "O.O.O.": 'l',
+            "OO..O.": 'm',
+            "OO.OO.": 'n',
+            "O..OO.": 'o',
+            "OOO.O.": 'p',
+            "OOOOO.": 'q',
+            "O.OOO.": 'r',
+            ".OO.O.": 's',
+            ".OOOO.": 't',
+            "O...OO": 'u',
+            "O.O.OO": 'v',
+            ".OOO.O": 'w',
+            "OO..OO": 'x',
+            "OO.OOO": 'y',
+            "O..OOO": 'z',
+            
+            "..O...": ',',
+            "..O.O.": ';',
+            "..OO..": ':',
+            "..OO.O": '.',
+            "..OOO.": '!',
+            "..O.OO": '?',
+            ".OO.OO": '(',
+            ".OO.OO": ')',
+            "..O..O": '-',
+            ".O.OO.": '/',
+            ".O.OO.": '<',
+            ".O.OO.": '>',
+            
+            ".....O": 'capital',
+            ".O.OOO": 'number',
+            "......": ' '
+        }
 
-    braille_to_number = {
-        "O.....": '1',
-        "O.O...": '2',
-        "OO....": '3',
-        "OO.O..": '4',
-        "O..O..": '5',
-        "OOO...": '6',
-        "OOOO..": '7',
-        "O.OO..": '8',
-        ".OO...": '9',
-        ".OOO..": '0',
-    }
+        braille_to_number = {
+            "O.....": '1',
+            "O.O...": '2',
+            "OO....": '3',
+            "OO.O..": '4',
+            "O..O..": '5',
+            "OOO...": '6',
+            "OOOO..": '7',
+            "O.OO..": '8',
+            ".OO...": '9',
+            ".OOO..": '0',
+        }
 
+        isNumber = False
+        isCapital = False
+        for i in range(0, len(braille_text), 6):
+            braille_char = braille_text[i:i+6]
+            char = braille_to_english[braille_char]
 
-    for i in range(len(args)):
-        # check if input is braille or english
-        if set(args[i]).issubset({'O', '.', ' '}):
-            # is braille
+            # handle capital
+            if isCapital:
+                char = char.upper()
+                isCapital = False
+            
+            elif char == 'capital':
+                isCapital = True
+                continue
+            
+            elif isNumber and char == ' ': 
+                isNumber = False
+
+            # handle number
+            elif isNumber: 
+                char = braille_to_number[braille_char]
+
+            elif char == 'number':
+                isNumber = True
+                continue
+            
+            result += char
+        isNumber = False
+        isCapital = False
+
+    else:
+        # is english
+        english_txt = ' '.join(args)
+
+        # create dict for english to braille
+        english_to_braille = {
+            'a': "O.....",
+            'b': "O.O...",
+            'c': "OO....",
+            'd': "OO.O..",
+            'e': "O..O..",
+            'f': "OOO...",
+            'g': "OOOO..",
+            'h': "O.OO..",
+            'i': ".OO...",
+            'j': ".OOO..",
+            'k': "O...O.",
+            'l': "O.O.O.",
+            'm': "OO..O.",
+            'n': "OO.OO.",
+            'o': "O..OO.",
+            'p': "OOO.O.",
+            'q': "OOOOO.",
+            'r': "O.OOO.",
+            's': ".OO.O.",
+            't': ".OOOO.",
+            'u': "O...OO",
+            'v': "O.O.OO",
+            'w': ".OOO.O",
+            'x': "OO..OO",
+            'y': "OO.OOO",
+            'z': "O..OOO",
+            
+            '1': "O.....",
+            '2': "O.O...",
+            '3': "OO....",
+            '4': "OO.O..",
+            '5': "O..O..",
+            '6': "OOO...",
+            '7': "OOOO..",
+            '8': "O.OO..",
+            '9': ".OO...",
+            '0': ".OOO..",
+            
+            ',': "..O...",
+            ';': "..O.O.",
+            ':': "..OO..",
+            '.': "..OO.O",
+            '!': "..OOO.",
+            '?': "..O.OO",
+            '(': ".OO.OO",
+            ')': ".OO.OO",
+            '-': "..O..O",
+            '/': ".O.OO.",
+            '<': ".O.OO.",
+            '>': ".O.OO.",
+            
+            'capital': ".....O",
+            'number': ".O.OOO",
+            ' ': "......"
+        }
+
+        for word in english_txt:
             isNumber = False
-            isCapital = False
-            for j in range(0, len(args[i]), 6):
-                braille_char = args[i][j:j+6]
-                char = braille_to_english[braille_char]
-                # handle capital
-                if isCapital:
-                    char = char.upper()
-                    isCapital = False
-                
-                elif char == 'capital':
-                    isCapital = True
-                    continue
-                
-                elif isNumber and char == ' ':
-                    isNumber = False
+            for letter in word:
 
-                elif isNumber:
-                    char = braille_to_number[braille_char]
-
-                elif char == 'number':
-                    isNumber = True
-                    continue
-                
-                result += char
-            isNumber = False
-            isCapital = False
-        else:
-            # is english
-            isNumber = False
-            for letter in args[i]:
                 # handle capital case
                 if letter.isupper():
                     result += english_to_braille['capital']
                     letter = letter.lower()
+
                 # handle numbers
                 elif letter.isnumeric() and not isNumber:
                     isNumber = True
                     result += english_to_braille['number']
+                
+                # add string to result
                 result += english_to_braille[letter]
-            # add space after element if its not the last one
-            if i != len(args)-1:
-                result += english_to_braille[' ']
 
     return result
 
