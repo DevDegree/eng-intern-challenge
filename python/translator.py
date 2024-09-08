bc50b3f9f37b8a9de6eb415bba98c56185cbd18b
MDerazNasr <85366477+MDerazNasr@users.noreply.github.com>
2024-09-08 07:47:02 -0400

    feat: create mapping between English and Braille

--- python/translator.py
+++ python/translator.py
@@ -1,5 +1,41 @@
 import sys
 
+
+# Mapping from English letters and numbers to Braille symbols
+english_to_braille_map = {
+    'a': "O.....", 'b': "O.O...", 'c': "OO....", 'd': "OO.O..", 'e': "O..O..",
+    'f': "OOO...", 'g': "OOOO..", 'h': "O.OO..", 'i': ".OO...", 'j': ".OOO..",
+    'k': "O...O.", 'l': "O.O.O.", 'm': "OO..O.", 'n': "OO.OO.", 'o': "O..OO.",
+    'p': "OOO.O.", 'q': "OOOOO.", 'r': "O.OOO.", 's': ".OO.O.", 't': ".OOOO.",
+    'u': "O...OO", 'v': "O.O.OO", 'w': ".OOO.O", 'x': "OO..OO", 'y': "OO.OOO",
+    'z': "O..OOO",
+    '1': "O.....", '2': "O.O...", '3': "OO....", '4': "OO.O..", '5': "O..O..",
+    '6': "OOO...", '7': "OOOO..", '8': "O.OO..", '9': ".OO...", '0': ".OOO..",
+    ' ': "......",  # Space character
+}
+
+# Mapping from Braille symbols to English letters and numbers
+braille_to_english_map = {
+    "O.....": 'a', "O.O...": 'b', "OO....": 'c', "OO.O..": 'd', "O..O..": 'e',
+    "OOO...": 'f', "OOOO..": 'g', "O.OO..": 'h', ".OO...": 'i', ".OOO..": 'j',
+    "O...O.": 'k', "O.O.O.": 'l', "OO..O.": 'm', "OO.OO.": 'n', "O..OO.": 'o',
+    "OOO.O.": 'p', "OOOOO.": 'q', "O.OOO.": 'r', ".OO.O.": 's', ".OOOO.": 't',
+    "O...OO": 'u', "O.O.OO": 'v', ".OOO.O": 'w', "OO..OO": 'x', "OO.OOO": 'y',
+    "O..OOO": 'z', "......": ' ',  # Braille for space
+}
+
+# Mapping for numbers (similar to letters 'a' to 'j', but in number mode)
+braille_to_number_map = {
+    "O.....": '1', "O.O...": '2', "OO....": '3', "OO.O..": '4', "O..O..": '5',
+    "OOO...": '6', "OOOO..": '7', "O.OO..": '8', ".OO...": '9', ".OOO..": '0',
+}
+
+# Special Braille characters for flags like capitalization and numbers
+special_braille_chars = {
+    ".....O": "capital flag",  # Indicates the next letter is uppercase
+    ".O.OOO": "number flag",   # Indicates the following characters are numbers
+}
+
 def is_braille(text):
     """
     Determines if a given text is valid Braille by checking its length and characters.
