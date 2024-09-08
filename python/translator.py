7f72c5d53104a67fca8437b14401ec6cdf42c549
Xuan Pham <hpham024@uottawa.ca>
2024-09-07 22:29:59 -0400

    completed translate methods -- fails on translating letters and numbers attached with no space

--- python/translator.py
+++ python/translator.py
@@ -8,8 +8,10 @@ The translator console application can be used by running this file or externall
 import sys
 
 # translation constants
+BRAILLE_CHARACTER_LENGTH = 6
 CAPITAL_FOLLOWS = '.....O'
 NUMBER_FOLLOWS = '.O.OOO'
+SPACE = '......'
 
 # a dictionary of braille symbols
 BRAILLE_ALPHABET = {
@@ -28,9 +30,9 @@ BRAILLE_NUMBER_EQUIVALENTS = {
 ENGLISH_ALPHABET = {}
 
 i = 1
-for english_letter, braille_letter in BRAILLE_ALPHABET:
+for english_letter, braille_letter in BRAILLE_ALPHABET.items():
     if i is not None:
-        ENGLISH_ALPHABET[braille_letter] = (english_letter, i)
+        ENGLISH_ALPHABET[braille_letter] = (english_letter, str(i))
         i = (i + 1) % 10
 
         if i == 1:
@@ -40,15 +42,51 @@ for english_letter, braille_letter in BRAILLE_ALPHABET:
 
 
 def translate_braille_to_english(braille: str) -> str:
-    pass
+    number_or_letter_selector = 0
+    capital_follows_flag = False
+    text = []
+
+    for i in range(0, len(braille), BRAILLE_CHARACTER_LENGTH):
+        letter = braille[i: i + BRAILLE_CHARACTER_LENGTH]
+
+        if letter == NUMBER_FOLLOWS:
+            number_or_letter_selector = 1
+
+        elif letter == SPACE:
+            text.append(' ')
+            number_or_letter_selector = 0
+
+        elif letter == CAPITAL_FOLLOWS:
+            capital_follows_flag = True
+
+        else:
+            english_letter = ENGLISH_ALPHABET[letter][number_or_letter_selector]
+            text.append(english_letter.upper() if capital_follows_flag else english_letter)
+            capital_follows_flag = False
+
+    return ''.join(text)
 
 
 def translate_english_to_braille(text: str) -> str:
-    pass
+    return SPACE.join(map(_get_braille_word, text.split()))
 
 
-def _get_braille(char: str) -> str:
-    pass
+def _get_braille_word(word: str) -> str:
+    braille = []
+
+    if numeric := word.isnumeric():
+        braille.append(NUMBER_FOLLOWS)
+
+    for char in word:
+        if numeric:
+            char = BRAILLE_NUMBER_EQUIVALENTS[char]
+        elif char.isupper():
+            braille.append(CAPITAL_FOLLOWS)
+            char = char.lower()
+
+        braille.append(BRAILLE_ALPHABET[char])
+
+    return ''.join(braille)
 
 
 def main():
