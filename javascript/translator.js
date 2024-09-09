5aecb2ff75d9b79122a335ba0d87c29823a8d828
Doyinsola Bode-Fakunle <doyinfakunle@yahoo.com>
2024-09-09 09:59:57 -0400

    added function to check if input is Braille

--- javascript/translator.js
+++ javascript/translator.js
@@ -1,2 +1,49 @@
 const inputVal = process.argv[2];
 console.log(inputVal);
+
+function isBraille(inputVal) {
+    if (inputVal.length % 6 !== 0) {
+        return false;
+    }
+    for (let i = 0; i < inputVal.length; i += 6) {
+        const splitInput = inputVal.slice(i, i + 6);
+        if (splitInput.length !== 6) {
+            return false;
+        }
+        for (const char of splitInput) {
+            if (char !== 'O' && char !== '.') {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+console.log(isBraille(inputVal));
+
+const brailleAlphabet = {
+
+    'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..',
+    'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..',
+    'k': 'O...O.', 'l': 'O.O.O.', 'm': 'OO..O.', 'n': 'OO.OO.', 'o': 'O..OO.',
+    'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.',
+    'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO', 'y': 'OO.OOO',
+    'z': 'O..OOO',
+
+    'A': 'O.....', 'B': 'O.O...', 'C': 'OO....', 'D': 'OO.O..', 'E': 'O..O..',
+    'F': 'OOO...', 'G': 'OOOO..', 'H': 'O.OO..', 'I': '.OO...', 'J': '.OOO..',
+    'K': 'O...O.', 'L': 'O.O.O.', 'M': 'OO..O.', 'N': 'OO.OO.', 'O': 'O..OO.',
+    'P': 'OOO.O.', 'Q': 'OOOOO.', 'R': 'O.OOO.', 'S': '.OO.O.', 'T': '.OOOO.',
+    'U': 'O...OO', 'V': 'O.O.OO', 'W': '.OOO.O', 'X': 'OO..OO', 'Y': 'OO.OOO',
+    'Z': 'O..OOO',
+
+    // Space
+    ' ': '......',
+
+    // Numbers (0-9)
+    '1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..', '5': 'O..O..',
+    '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...', '0': '.OOO..',
+
+    'CAPITAL': '.....O',
+    'NUMBER': '.O.O..'
+}
