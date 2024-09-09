5d4683791a171937115d9aab4dd97f68962ac528
Kelvin Oo <Kelvin.Oo@umanitoba.ca>
2024-09-09 14:43:30 -0500

    cleanup: removed comments

--- python/translator.py
+++ python/translator.py
@@ -1,15 +1,3 @@
-# Translator
-# Given arguments passed into the program at runtime, determine if the given string should be translated to English or Braille.
-# For Braille, each character is stored as a series of O (the letter O) or . (a period).
-# Store Braille symbols as a 6 character string reading left to right, line by line, starting at the top left. See examples below.
-# When a Braille capital follows symbol is read, assume only the next symbol should be capitalized.
-# When a Braille number follows symbol is read, assume all following symbols are numbers until the next space symbol.
-# Braille Alphabet
-# Letters a through z
-# The ability to capitalize letters
-# Numbers 0 through 9
-# The ability to include spaces ie: multiple words
-
 import sys
 
 
