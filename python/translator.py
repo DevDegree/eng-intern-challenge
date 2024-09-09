9b005840fa508adbbffb2ad8625699bc502b0cc2
callumrg <callum.r.gillies@gmail.com>
2024-09-09 13:30:25 -0400

    first start

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,18 @@
+import sys
 
+def braille2text(braille_str):
+    return "hello"
+
+def text2braille(word_list):
+    return "......"
+
+def main():
+    if len(sys.argv) > 1:
+        text2braille(sys.argv)
+    elif len(sys.argv[0]) % 6 == 0 and sys.argv[0].count(".") + sys.argv[0].count("O") == len(sys.argv[0]):
+        braille2text(sys.argv[0])
+    else: 
+        text2braille(sys.argv)
+
+if __name__ == '__main__':
+    main()
