62c02ff041733e89c33eeaea7e6e16207fd23560
notzree <notzree@gmail.com>
2024-09-09 15:02:01 -0400

    Parse and combine cli arguments

--- go/translator.go
+++ go/translator.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	var input string
+	if len(os.Args) < 2 {
+		fmt.Println("Please provide a string to translate")
+		return
+	}
+	// Join with a space to preservce spaces in input.
+	input = strings.Join(os.Args[1:], " ")
+}
