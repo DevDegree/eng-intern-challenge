4488164108823b5e58b50aa5cd2527a9e04e6962
Nahiyan Ishtiaque <nahiyan.ishtiaque@gmail.com>
2024-09-10 10:44:00 -0400

    added dictionaries required for translation

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,26 @@
+#dictionary that holds English characters as key and the braille equivalent as the value
+englishToBraille = {
+    'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..',
+    'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..',
+    'k': 'O...O.', 'l': 'O.O.O.', 'm': 'OO..O.', 'n': 'OO.OO.', 'o': 'O..OO.',
+    'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.',
+    'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO', 'y': 'OO.OOO',
+    'z': 'O..OOO', ' ': '......', '0': '.OOO..', '1': 'O.....', '2': 'O.O...', 
+    '3': 'OO....', '4': 'OO.O..', '5': 'O..O..', '6': 'OOO...', '7': 'OOOO..', 
+    '8': 'O.OO..', '9': '.OO...', "number follows": ".O.OOO","uppercase follows": ".....O"
+}
 
+#vice-versa of the above dictionary
+brailleToEnglish = {
+    "O.....": "a", "O.O...": "b", "OO....": "c", "OO.O..": "d", "O..O..": "e",
+    "OOO...": "f", "OOOO..": "g", "O.OO..": "h", ".OO...": "i", ".OOO..": "j",
+    "O...O.": "k", "O.O.O.": "l", "OO..O.": "m", "OO.OO.": "n", "O..OO.": "o",
+    "OOO.O.": "p", "OOOOO.": "q", "O.OOO.": "r", ".OO.O.": "s", ".OOOO.": "t",
+    "O...OO": "u", "O.O.OO": "v", ".OOO.O": "w", "OO..OO": "x", "OO.OOO": "y",
+    "O..OOO": "z", "......": " ", ".O.OOO": "number follows", ".....O": "uppercase follows"
+}
+
+brailleToEnglishNumbers = {
+    ".OOO..": "0", "O.....": "1", "..OO..": "2", "OO....": "3", "OO.O..": "4", 
+    "O..O..": "5", "OOO...": "6", "..OO.O": "7", "OOOO..": "8", ".OO...": "9"
+}
