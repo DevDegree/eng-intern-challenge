5eab1b3f3bdf4ae3ecb543dd142862dd91b3d14d
BrianLi23 <b77li@uwaterloo.ca>
2024-09-09 22:46:43 -0400

    feat: added braille checker for string

--- python/translator.py
+++ python/translator.py
@@ -11,6 +11,35 @@ number_Braille = {
     "1": "O.....", "2": "O.O...", "3": "OO....", "4": "OO.O..", "5": "O..O..", "6": "OOO...", "7": "OOOO..",
     "8": "O.OO..", "9": ".OO...", "0": ".OOO.."
 }
+
 number_Follows = ".O.OOO"
 capital_Follows = ".....O"
 space = "......"
+
+"""
+Checks if input string is a valid Braille representation.
+
+    Args:
+    string (str): The input string to be checked.
+
+    Returns:
+    bool: True if the string is valid Braille, False otherwise indicating String is English.
+"""
+def brailleChecker(string str) -> bool:
+    # Checks if length of string is a multiple of 6.
+    if len(str) % 6 != 0:
+        return False
+    
+    # Checks if string contains only '.' (flat dot) and 'O' (raised dot) characters.
+    if not all(char in '.O' for char in str):
+        return False
+
+    # Set of all valid braille values
+    valid_braille = set(letter_Braille.values()) and set(number_Braille.values())
+    valid_braille.add(number_Follows)
+    valid_braille.add(capital_Follows)
+    valid_braille.add(space)
+
+    # Check for every 6 characters they're a valid braille
+    return all(string[i:i+6] in valid_braille for i in range(0, len(string), 6))
+    
