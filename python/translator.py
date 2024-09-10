1e38678ac5805467c4330aa263fcf26e7c2262d1
Lucy Lin <lucylin6699@gmail.com>
2024-09-09 22:27:00 -0400

    added a english to braille translation in the form of a dictionary

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,26 @@
+# translations in dictionary
+english_braille = {
+    ('a', '1'): 'O.....', ('b', '2'): 'O.O...',
+    ('c', '3'): 'OO....', ('d', '4'): 'OO.O..',
+    ('e', '5'): 'O..O..', ('f', '6'): 'OOO...',
+    ('g', '7'): 'OOOO..', ('h', '8'): 'O.OO..', 
+    ('i', '9'): '.OO...', ('j', '0'): '.OOO..',
+    'k': 'O...O.', 'l': 'O.O.O.',
+    'm': 'OO..O.', 'n': 'OO.OO.', 
+    'o': 'O..OO.', 'p': 'OOO.O.',
+    'q': 'OOOO.', 'r': 'O.OOO.',
+    's': '.OO.O.', 't': '.OOOO.',
+    'u': 'O...OO', 'v': 'O.O.OO',
+    'w': '.OOO.O', 'x': 'OO..OO',
+    'y': 'OO.OOO', 'z': 'O..OOO',
+    'capital follows': '.....O', 'decimal follows': '.O...O',
+    'number follows': '.O.OOO', ' ': '......',
+    '.': '..OO.O', ',': '..O...',
+    '?': '..O.OO', '!': '..OOO.',
+    ':': '..OO..', ';': '..O.O.',
+    '-': '....OO', '/': '.O..O.',
+    '<': '.OO..O', '>': 'O..OO.',
+    '(': 'O.O..O', ')': '.O.OO.'
+}
 
+def translator(string s):
