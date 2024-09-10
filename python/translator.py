04c3a61b8c438303b7c639d66355d91075a31fae
BrianLi23 <b77li@uwaterloo.ca>
2024-09-09 22:11:16 -0400

    feat: initialize letter/braille dictionaries and const values

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,16 @@
+import sys
 
+letter_Braille = {
+    "a": "O.....", "b": "O.O...", "c": "OO....", "d": "OO.O..", "e": "O..O..", "f": "OOO...","g": "OOOO..", 
+    "h": "O.OO..", "i": ".OO...", "j": ".OOO..", "k": "O...O.", "l": "O.O.O.","m": "OO..O.", "n": "OO.OO.", 
+    "o": "O..OO.", "p": "OOO.O.", "q": "OOOOO.", "r": "O.OOO.", "s": ".OO.O.", "t": ".OOOO.", "u": "O...OO", 
+    "v": "O.O.OO", "w": ".OOO.O", "x": "OO..OO", "y": "OO.OOO", "z": "O..OOO"
+}
+
+number_Braille = {
+    "1": "O.....", "2": "O.O...", "3": "OO....", "4": "OO.O..", "5": "O..O..", "6": "OOO...", "7": "OOOO..",
+    "8": "O.OO..", "9": ".OO...", "0": ".OOO.."
+}
+number_Follows = ".O.OOO"
+capital_Follows = ".....O"
+space = "......"
