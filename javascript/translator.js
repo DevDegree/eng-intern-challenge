df9238a0b9ba6a02c4b8c96cb5c67bcc37889d3a
Viviane Viana Bresolin <vivianebresolin@gmail.com>
2024-09-10 12:39:25 -0400

    feat: Add mappings for English letters, numbers, and Braille symbols

--- javascript/translator.js
+++ javascript/translator.js
@@ -1,5 +1,37 @@
 const process = require('process');
 
+const englishToBraille = {
+  'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..',
+  'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..',
+  'k': 'O...O.', 'l': 'O.O.O.', 'm': 'OO..O.', 'n': 'OO.OO.', 'o': 'O..OO.',
+  'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.',
+  'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO', 'y': 'OO.OOO', 'z': 'O..OOO',
+  ' ': '......',  // Space
+  'capital': '.....O',  // Capital indicator
+  'number': '.O.OOO'    // Number indicator
+};
+
+const brailleToEnglish = {
+  'O.....': 'a', 'O.O...': 'b', 'OO....': 'c', 'OO.O..': 'd', 'O..O..': 'e',
+  'OOO...': 'f', 'OOOO..': 'g', 'O.OO..': 'h', '.OO...': 'i', '.OOO..': 'j',
+  'O...O.': 'k', 'O.O.O.': 'l', 'OO..O.': 'm', 'OO.OO.': 'n', 'O..OO.': 'o',
+  'OOO.O.': 'p', 'OOOOO.': 'q', 'O.OOO.': 'r', '.OO.O.': 's', '.OOOO.': 't',
+  'O...OO': 'u', 'O.O.OO': 'v', '.OOO.O': 'w', 'OO..OO': 'x', 'OO.OOO': 'y', 'O..OOO': 'z',
+  '......': ' ',  // Space
+  '.....O': 'capital',  // Capital indicator
+  '.O.OOO': 'number'    // Number indicator
+};
+
+const numberToBraille = {
+  '1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..', '5': 'O..O..',
+  '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...', '0': '.OOO..'
+};
+
+const brailleToNumber = {
+  'O.....': '1', 'O.O...': '2', 'OO....': '3', 'OO.O..': '4', 'O..O..': '5',
+  'OOO...': '6', 'OOOO..': '7', 'O.OO..': '8', '.OO...': '9', '.OOO..': '0'
+};
+
 // Function to check if the input is Braille or English
 function isBraille(input) {
   const braillePattern = /^[O.]+$/; // Braille will only contain 'O' and '.'
