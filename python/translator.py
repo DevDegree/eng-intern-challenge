f9f98580563a45cb120f405e80437d894920386a
Simon <SimonVutov1@gmail.com>
2024-09-10 11:13:28 -0400

    Fixed extra space insertion in English to Braille translation

--- python/translator.py
+++ python/translator.py
@@ -1,93 +1,84 @@
+import sys
+
 # Braille encoding
 BRAILLE_ENCODING = {
-    'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..',
-    'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..',
-    'k': 'O...O.', 'l': 'O.O.O.', 'm': 'OO..O.', 'n': 'OO.OO.', 'o': 'O..OO.',
-    'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.',
-    'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO', 'y': 'OO.OOO',
-    'z': 'O..OOO', ' ': '......', 'cap': '.....O', 'num': '.O.OOO', '.': '.O...O'
+    'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..', 'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..', 
+    'i': '.OO...', 'j': '.OOO..', 'k': 'O...O.', 'l': 'O.O.O.', 'm': 'OO..O.', 'n': 'OO.OO.', 'o': 'O..OO.', 'p': 'OOO.O.', 
+    'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.', 'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO', 
+    'y': 'OO.OOO', 'z': 'O..OOO', 
+    '1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..', '5': 'O..O..', '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', 
+    '9': '.OO...', '0': '.OOO..', 
+    ' ': '......', 'capital': '.....O', 'number': '.O.OOO'
 }
 
-# Numbers encoding remains the same
-NUMBER_ENCODING = {
-    '1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..', '5': 'O..O..',
-    '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...', '0': '.OOO..'
+BRAILLE_TO_ENGLISH_LETTERS = {
+    'O.....': 'a',  'O.O...': 'b',  'OO....': 'c',  'OO.O..': 'd',  'O..O..': 'e',  'OOO...': 'f',  'OOOO..': 'g',  'O.OO..': 'h', 
+    '.OO...': 'i',  '.OOO..': 'j',  'O...O.': 'k',  'O.O.O.': 'l',  'OO..O.': 'm',  'OO.OO.': 'n',  'O..OO.': 'o',  'OOO.O.': 'p', 
+    'OOOOO.': 'q',  'O.OOO.': 'r',  '.OO.O.': 's',  '.OOOO.': 't',  'O...OO': 'u',  'O.O.OO': 'v',  '.OOO.O': 'w',  'OO..OO': 'x', 
+    'OO.OOO': 'y',  'O..OOO': 'z',  
+    '......': ' ',  '.....O': 'capital',  '.O.OOO': 'number'
 }
-
-# Reverse dictionary for faster lookups
-ENGLISH_FROM_BRAILLE = {v: k for k, v in BRAILLE_ENCODING.items()}
-NUMBER_FROM_BRAILLE = {v: k for k, v in NUMBER_ENCODING.items()}
-
-def braille_to_english(braille_string):
-    # Split the input string into segments of 6 characters each
-    braille_list = [braille_string[i:i+6] for i in range(0, len(braille_string), 6)]
-    english_output = []
-    is_capital = False
-    is_number = False
-
-    for br in braille_list:
-        if br == BRAILLE_ENCODING['cap']:
-            is_capital = True
-            continue
-        elif br == BRAILLE_ENCODING['num']:
-            is_number = True
-            continue
-
-        # Handling number decoding
-        if is_number:
-            if br in NUMBER_FROM_BRAILLE:
-                char = NUMBER_FROM_BRAILLE[br]
-            else:
-                is_number = False  # Reset number mode if the pattern isn't a number
-                char = ENGLISH_FROM_BRAILLE.get(br, '?')  # Check if it's a letter or punctuation
-        else:
-            char = ENGLISH_FROM_BRAILLE.get(br, '?')
-
-        if char == '?':
-            print(f"Warning: Unrecognized Braille pattern '{br}'")
-        else:
-            if is_capital and char.isalpha():
-                char = char.upper()
-                is_capital = False  # Reset capital flag after use
-
-        english_output.append(char)
-
-    return ''.join(english_output)
+BRAILLE_TO_ENGLISH_NUMBERS = {
+    'O.....': '1', 'O.O...': '2', 'OO....': '3', 'OO.O..': '4', 'O..O..': '5', 
+    'OOO...': '6','OOOO..': '7', 'O.OO..': '8', '.OO...': '9', '.OOO..': '0'
+}
 
 def english_to_braille(english_string):
-    braille_output = []
-    last_was_number = False  # To track if the last character was a number
+    result = []
+    number_mode = False
 
     for char in english_string:
         if char.isdigit():
-            if not last_was_number:
-                braille_output.append(BRAILLE_ENCODING['num'])  # Append num only if the last character was not a number
-            braille_output.append(NUMBER_ENCODING[char])
-            last_was_number = True
-        else:
-            if last_was_number and char != ' ':
-                last_was_number = False  # Reset if next character is not a number and not a space
+            if not number_mode:
+                result.append(BRAILLE_ENCODING['number'])
+                number_mode = True
+            result.append(BRAILLE_ENCODING[char])
+        elif char.isalpha():
+            if number_mode:
+                number_mode = False
             if char.isupper():
-                braille_output.append(BRAILLE_ENCODING['cap'])
-                braille_output.append(BRAILLE_ENCODING[char.lower()])
+                result.append(BRAILLE_ENCODING['capital'])
+            result.append(BRAILLE_ENCODING[char.lower()])
+        elif char == ' ':
+            result.append(BRAILLE_ENCODING[' '])
+            number_mode = False
+
+    return ''.join(result)
+
+def braille_to_english(braille_string):
+    result = []
+    number_mode = False
+    i = 0
+
+    while i < len(braille_string):
+        braille_char = braille_string[i:i+6]
+        if braille_char == BRAILLE_ENCODING['capital']:
+            i += 6
+            braille_char = braille_string[i:i+6]
+            result.append(BRAILLE_TO_ENGLISH_LETTERS[braille_char].upper())
+        elif braille_char == BRAILLE_ENCODING['number']:
+            number_mode = True
+        elif braille_char == BRAILLE_ENCODING[' ']:
+            result.append(' ')
+            number_mode = False  # Turn off number mode when a space is encountered
+        else:
+            if number_mode:
+                num = BRAILLE_TO_ENGLISH_NUMBERS[braille_char]
+                result.append(num)
             else:
-                braille_output.append(BRAILLE_ENCODING[char])
+                letter = BRAILLE_TO_ENGLISH_LETTERS[braille_char]
+                result.append(letter)
+                number_mode = False  # Only turn off number_mode when processing a non-number
 
-    return ''.join(braille_output)
+        i += 6
 
-# Be sure to adjust the dictionaries and other variables as necessary to match your actual encoding and application structure.
+    return ''.join(result)
 
-def main(input_string):
-    # Determine if the input is Braille or English
+if __name__ == '__main__':
+    input_string = " ".join(sys.argv[1:])
+    
+    # Check if input is Braille (contains only 'O' and '.')
     if all(c in 'O.' for c in input_string):
-        return braille_to_english(input_string)
+        print(braille_to_english(input_string))
     else:
-        return english_to_braille(input_string)
-
-if __name__ == "__main__":
-    import sys
-    if len(sys.argv) > 1:
-        input_string = ''.join(sys.argv[1:])  # Join without spaces
-        print(main(input_string))
-    else:
-        print("Usage: python braille_translator.py <string>")
+        print(english_to_braille(input_string))
