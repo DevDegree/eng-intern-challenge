728bc5cf8924c6b89eeeffad502d0a46a2fa3c91
Zhengliang Wang <puck19970418@gmail.com>
2024-09-10 16:42:30 -0400

    added solution

--- javascript/translator.test.js
+++ javascript/translator.test.js
@@ -9,4 +9,40 @@ describe('translator.js script', () => {
             done();
         });
     });
+
+    it('should output correct answer to the console', (done) => {
+        exec("node translator.js Hello world", (error, stdout, stderr) => {
+            expect(error).toBeNull();
+            expect(stderr).toBe("");
+            expect(stdout.trim()).toBe(".....OO.OO..O..O..O.O.O.O.O.O.O..OO........OOO.OO..OO.O.OOO.O.O.O.OO.O..");
+            done();
+        });
+    });
+
+    it('should output correct answer to the console', (done) => {
+        exec("node translator.js 42", (error, stdout, stderr) => {
+            expect(error).toBeNull();
+            expect(stderr).toBe("");
+            expect(stdout.trim()).toBe(".O.OOOOO.O..O.O...");
+            done();
+        });
+    });
+
+    it('should output correct answer to the console', (done) => {
+        exec("node translator.js .....OO.....O.O...OO...........O.OOOO.....O.O...OO....", (error, stdout, stderr) => {
+            expect(error).toBeNull();
+            expect(stderr).toBe("");
+            expect(stdout.trim()).toBe("Abc 123");
+            done();
+        });
+    });
+
+    it('should output correct answer to the console', (done) => {
+        exec("node translator.js 1 1 ", (error, stdout, stderr) => {
+            expect(error).toBeNull();
+            expect(stderr).toBe("");
+            expect(stdout.trim()).toBe(".O.OOOO............O.OOOO.....");
+            done();
+        });
+    });
 });
