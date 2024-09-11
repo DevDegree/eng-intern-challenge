50f48ed1daa757a0cc0d182181cd0f3bf4439202
Bita Fotovvat <bita.ftvt@gmail.com>
2024-09-10 21:01:52 -0400

    defined the reverse braille map

--- javascript/translator.js
+++ javascript/translator.js
@@ -4,6 +4,10 @@ const brailMap = {
     "q":"ooooo.", "r":"o.ooo.", "s":".oo.o.", "t":".oooo.", "u":"o...oo", "v":"o.o.oo", "w":".ooo.o", "x":"oo..oo", 
     "y":"oo.ooo", "z":"o..ooo", "cap":".....o", "num":".o.ooo", "decimal":".o...o", "1":"o.....",
     "2":"o.o...", "3":"oo....", "4":"oo.o..", "5":"o..o..", "6":"ooo...", "7":"oooo..", "8":"o.oo..", "9":".oo...",
-    "0":".000..", ".":"..oo.o", ",":"..o...", "?":"..o.oo", "!":"..ooo.", ":":"..oo..", ";":"..o.o.", "-":"....oo",
+    "0":".ooo..", ".":"..oo.o", ",":"..o...", "?":"..o.oo", "!":"..ooo.", ":":"..oo..", ";":"..o.o.", "-":"....oo",
     "/":".o..o.", "<":".oo..o", ">":"o..oo.", "(":"o.o..o", ")":".o.oo.", " ":"......"
+}
+const reverseBrailleMap = {};
+for (const [key, value] of Object.entries(brailleMap)) {
+    reverseBrailleMap[value] = key;
 }
