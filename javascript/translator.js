109ad6db5ba5756eec4bd463aa53b6965c78f825
Bita Fotovvat <bita.ftvt@gmail.com>
2024-09-10 21:33:52 -0400

    addressed the space issue in the code

--- javascript/translator.js
+++ javascript/translator.js
@@ -1,12 +1,12 @@
 const brailleMap = {
-    "a":"o.....", "b":"o.o...", "c":"oo....", "d":"oo.o..", "e":"o..o..", "f":"ooo...", "g":"oooo..", "h":"o.oo..",
-    "i":".oo...", "j":".ooo..", "k":"o...o.", "l":"o.o.o.", "m":"oo..o.", "n":"oo.oo.", "o":"o..oo.", "p":"ooo.o.", 
-    "q":"ooooo.", "r":"o.ooo.", "s":".oo.o.", "t":".oooo.", "u":"o...oo", "v":"o.o.oo", "w":".ooo.o", "x":"oo..oo", 
-    "y":"oo.ooo", "z":"o..ooo", "cap":".....o", "num":".o.ooo", "decimal":".o...o", "1":"o.....",
-    "2":"o.o...", "3":"oo....", "4":"oo.o..", "5":"o..o..", "6":"ooo...", "7":"oooo..", "8":"o.oo..", "9":".oo...",
-    "0":".ooo..", ".":"..oo.o", ",":"..o...", "?":"..o.oo", "!":"..ooo.", ":":"..oo..", ";":"..o.o.", "-":"....oo",
-    "/":".o..o.", "<":".oo..o", ">":"o..oo.", "(":"o.o..o", ")":".o.oo.", " ":"......"
-}
+    "a": "o.....", "b": "o.o...", "c": "oo....", "d": "oo.o..", "e": "o..o..", "f": "ooo...", "g": "oooo..", "h": "o.oo..",
+    "i": ".oo...", "j": ".ooo..", "k": "o...o.", "l": "o.o.o.", "m": "oo..o.", "n": "oo.oo.", "o": "o..oo.", "p": "ooo.o.", 
+    "q": "ooooo.", "r": "o.ooo.", "s": ".oo.o.", "t": ".oooo.", "u": "o...oo", "v": "o.o.oo", "w": ".ooo.o", "x": "oo..oo", 
+    "y": "oo.ooo", "z": "o..ooo", "cap": ".....o", "num": ".o.ooo", "decimal": ".o...o", "1": "o.....",
+    "2": "o.o...", "3": "oo....", "4": "oo.o..", "5": "o..o..", "6": "ooo...", "7": "oooo..", "8": "o.oo..", "9": ".oo...",
+    "0": ".ooo..", ".": "..oo.o", ",": "..o...", "?": "..o.oo", "!": "..ooo.", ":": "..oo..", ";": "..o.o.", "-": "....oo",
+    "/": ".o..o.", "<": ".oo..o", ">": "o..oo.", "(": "o.o..o", ")": ".o.oo.", " ": "......"
+};
 const reverseBrailleMap = {};
 for (const [key, value] of Object.entries(brailleMap)) {
     reverseBrailleMap[value] = key;
@@ -17,10 +17,8 @@ function isBraille(input) {
 function translateToBraille(text) {
     let braille = "";
     let numberMode = false;
-
     for (let i = 0; i < text.length; i++) {
         let char = text[i].toLowerCase();
-
         if (char >= "0" && char <= "9") {
             if (!numberMode) {
                 braille += brailleMap["num"];
@@ -29,7 +27,6 @@ function translateToBraille(text) {
             braille += brailleMap[char];
         } else if (char >= "a" && char <= "z") {
             if (numberMode) {
-                braille += " ";
                 numberMode = false;
             }
             if (text[i] !== char) {
@@ -40,14 +37,13 @@ function translateToBraille(text) {
             braille += brailleMap[char] || "";
         }
     }
-    return braille;
+    return braille.replace(/o/g, "O").trim();
 }
 function translateToEnglish(braille) {
     let english = "";
     let numberMode = false;
     let capMode = false;
     const brailleChars = braille.match(/.{1,6}/g) || [];
-
     for (let i = 0; i < brailleChars.length; i++) {
         const char = brailleChars[i].replace(/O/g, "o");
         if (char === brailleMap["cap"]) {
