ed1fa52bfa52d01581d343176f84fbc8cfb6d045
DomTorres <neildominictorres@gmail.com>
2024-09-11 11:40:02 -0700

    added scan from standard input

--- javascript/translator.js
+++ javascript/translator.js
@@ -3,4 +3,15 @@
  * neildominictorres@gmail.com
  * Shopify eng-intern-challenge
  */
+const readline = require('readline-sync');
+
+function main() {
+    const input = readline.question('Input: ');
+    console.log(`Output: ${parseInput(input)}`);
+}
+main();
+
+function parseInput(input) {
+    return input;
+}
 
