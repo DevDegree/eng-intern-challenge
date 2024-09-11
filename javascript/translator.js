ffc18e719333f2922982524ea778e6f869f3c0bc
Praise Yu <praiseyu@gmail.com>
2024-09-10 21:16:51 -0600

    Write pseudocode

---
+++ javascript/translator.js
@@ -0,0 +1,23 @@
+// VARIABLES
+
+// "dictionary" object with key/value pairs to translate
+// string to hold the result
+
+// create translator reference as an OBJECT
+
+
+// create a FUNCTION to translate input string
+
+// determine if string is english or braille 
+// if string contains ONLY . & Os, then it is braille
+
+// if braille, split string into substrings of 6 characters (or arrays?)
+// each string of 6 characters will be translated into English/symbols/numbers
+// look up in object to find corresponding character and add to result string
+// return result string
+
+// else it is english
+// split string into characters
+// for each character, find the corresponding braille string in the translator object
+// add braille strings to result string 
+// return result string
