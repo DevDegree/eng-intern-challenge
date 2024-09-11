46a751bbcd095b4ddc1ef89bab7acbb94f0abcc7
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2024-09-11 03:21:39 -0400

    finish Braille to English translation

--- javascript/translator.js
+++ javascript/translator.js
@@ -1,23 +1,23 @@
-const capitalFollows = '.....0';
-const decimalFollows = '.0...0';
-const numberFollows = '.0.000';
+const capitalFollows = '.....O';
+const decimalFollows = '.O...O';
+const numberFollows = '.O.OOO';
 const space = '......';
 
 const brailleAlpha = {
-    '0.....': 'a', '0.0...': 'b', '00....': 'c', '00.0..': 'd', '0..0..': 'e', '000...': 'f', '0000..': 'g',
-    '0.00..': 'h', '.00...': 'i', '.000..': 'j', '0...0.': 'k', '0.0.0.': 'l', '00..0.': 'm', '00.00.': 'n',
-    '0..00.': 'o', '000.0.': 'p', '00000.': 'q', '0.000.': 'r', '.00.0.': 's', '.0000.': 't', '0...00': 'u',
-    '0.0.00': 'v', '.000.0': 'w', '00..00': 'x', '00.000': 'y', '0..000': 'z'
+    'O.....': 'a', 'O.O...': 'b', 'OO....': 'c', 'OO.O..': 'd', 'O..O..': 'e', 'OOO...': 'f', 'OOOO..': 'g',
+    'O.OO..': 'h', '.OO...': 'i', '.OOO..': 'j', 'O...O.': 'k', 'O.O.O.': 'l', 'OO..O.': 'm', 'OO.OO.': 'n',
+    'O..OO.': 'o', 'OOO.O.': 'p', 'OOOOO.': 'q', 'O.OOO.': 'r', '.OO.O.': 's', '.OOOO.': 't', 'O...OO': 'u',
+    'O.O.OO': 'v', '.OOO.O': 'w', 'OO..OO': 'x', 'OO.OOO': 'y', 'O..OOO': 'z'
 };
 
 const brailleNumeric = {
-    '0.....': 1, '0.0...': 2, '00....': 3, '00.0..': 4, '0..0..': 5,
-    '000...': 6, '0000..': 7, '0.00..': 8, '.00...': 9, '.000..': 0
+    'O.....': '1', 'O.O...': '2', 'OO....': '3', 'OO.O..': '4', 'O..O..': '5',
+    'OOO...': '6', 'OOOO..': '7', 'O.OO..': '8', '.OO...': '9', '.OOO..': '0'
 };
 
 const brailleCharacters = {
-    '..00.0': '.', '..0...': ',', '..0.00': '?', '..00.0': '!', '..00..': ':', '..0.0.': ';',
-     '....00': '-', '.0..0.': '/', '.00..0': '<', '0..00.': '>', '0.0..0': '(', '.0.00.': ')'
+    '..OO.O': '.', '..O...': ',', '..O.OO': '?', '..OO.O': '!', '..OO..': ':', '..O.O.': ';',
+     '....OO': '-', '.O..O.': '/', '.OO..O': '<', 'O..OO.': '>', 'O.O..O': '(', '.O.OO.': ')'
 };
 
 (() => {
@@ -26,11 +26,11 @@ const brailleCharacters = {
         return;
     }
     const toElements = process.argv.toSpliced(0, 2).join(' ').split('');
-    const isBraille = (toElements.length % 6 === 0) && toElements.every(element => element === '.' || element === "0");
+    const isBraille = (toElements.length % 6 === 0) && toElements.every(element => element === '.' || element === "O");
     if(isBraille){
         const toString = toElements.join('');
         const toSplit = toString.match(/.{6}/g);
-        const translationObject = toSplit.reduce((accumulator, currentValue, index) => {
+        const translationObject = toSplit.reduce((accumulator, currentValue) => {
             const temp = {...accumulator};
             if(currentValue === space){
                 temp.isNumber = false;
@@ -71,7 +71,41 @@ const brailleCharacters = {
             isCapital: false,
             isDecimal: false
         });
-
         console.log(translationObject.output);
+    }
+    else{
+        const toEnglish = toElements.reduce((accumulator, currentValue, index) => {
+            if(currentValue.charCodeAt(0) >= 65 && currentValue.charCodeAt(0) <= 90){
+                accumulator.output += `${capitalFollows}${Object.keys(brailleAlpha).find(key => brailleAlpha[key] === currentValue.toLowerCase())}`;
+            }
+            else if(currentValue.charCodeAt(0) >= 48 && currentValue.charCodeAt(0) <= 57){
+                if(accumulator.isNumber){
+                    accumulator.output += `${Object.keys(brailleNumeric).find(key => brailleNumeric[key] === currentValue) 
+                        || Object.keys(brailleAlpha).find(key => brailleAlpha[key] === currentValue)
+                        || Object.keys(brailleCharacters).find(key => brailleCharacters[key] === currentValue) 
+                        || `(${currentValue}  has no Braille equivalent)`}`;
+                }
+                else{
+                    accumulator.isNumber = true;
+                    accumulator.output += `${numberFollows}${Object.keys(brailleNumeric).find(key => brailleNumeric[key] === currentValue) 
+                        || Object.keys(brailleAlpha).find(key => brailleAlpha[key] === currentValue)
+                        || Object.keys(brailleCharacters).find(key => brailleCharacters[key] === currentValue) 
+                        || `(${currentValue}  has no Braille equivalent)`}`;
+                }
+            }
+            else if(currentValue === ' '){
+                accumulator.output += space;
+                accumulator.isNumber = false
+
+            }
+            else{
+                accumulator.output += Object.keys(brailleAlpha).find(key => brailleAlpha[key] === currentValue);
+            }
+            return accumulator;
+        }, {
+            output: '',
+            isNumber: false
+        });
+        console.log(toEnglish.output);
     }
 })();
