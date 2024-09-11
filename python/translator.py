33b0f06b925ab7ce3d7cffc6a1d6b5510dd798e9
Omar Ziad <O.Ziad@outlook.com>
2024-09-10 22:18:33 -0400

    Create skeleton main function

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,13 @@
+import sys
 
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python translator.py <text>")
+        return
+    args = sys.argv[1:]
+    input_text = " ".join(args)
+
+
+if __name__ == "__main__":
+    main()
