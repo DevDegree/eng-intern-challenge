e154122432c8b39f5900e78962a889e5a4cf7893
Jenna Knechtel <jmknec@gmail.com>
2024-09-12 13:02:02 -0400

    add alphabet array

--- javascript/translator.js
+++ javascript/translator.js
@@ -1,3 +1,170 @@
 #!/usr/bin/env node
 
-console.log("test print to console");
+const alphabet = [
+  {
+    br: "O.....",
+    en: "a",
+    num: 1,
+  },
+  {
+    br: "O.O...",
+    en: "b",
+    num: 2,
+  },
+  {
+    br: "OO....",
+    en: "c",
+    num: 3,
+  },
+  {
+    br: "OO.O..",
+    en: "d",
+    num: 4,
+  },
+  {
+    br: "O..O..",
+    en: "e",
+    num: 5,
+  },
+  {
+    br: "OOO...",
+    en: "f",
+    num: 6,
+  },
+  {
+    br: "OOOO..",
+    en: "g",
+    num: 7,
+  },
+  {
+    br: "O.OO..",
+    en: "h",
+    num: 8,
+  },
+  {
+    br: ".OO...",
+    en: "i",
+    num: 9,
+  },
+  {
+    br: ".OOO..",
+    en: "j",
+    num: 0,
+  },
+  {
+    br: "O...O.",
+    en: "k",
+  },
+  {
+    br: "O.O.O.",
+    en: "l",
+  },
+  {
+    br: "OO..O.",
+    en: "m",
+  },
+  {
+    br: "OO.OO.",
+    en: "n",
+  },
+  {
+    br: "O..OO.",
+    en: "o",
+  },
+  {
+    br: "OOO.O.",
+    en: "p",
+  },
+  {
+    br: "OOOOO.",
+    en: "q",
+  },
+  {
+    br: "O.OOO.",
+    en: "r",
+  },
+  {
+    br: ".OO.O.",
+    en: "s",
+  },
+  {
+    br: ".OOOO.",
+    en: "t",
+  },
+  {
+    br: "O...OO",
+    en: "u",
+  },
+  {
+    br: "O.O.OO",
+    en: "v",
+  },
+  {
+    br: ".OOO.O",
+    en: "w",
+  },
+  {
+    br: "OO..OO",
+    en: "x",
+  },
+  {
+    br: "OO.OOO",
+    en: "y",
+  },
+  {
+    br: "O..OOO",
+    en: "z",
+  },
+  {
+    br: "..OO.O",
+    en: ".",
+  },
+  {
+    br: "..O...",
+    en: ",",
+  },
+  {
+    br: "..O.OO",
+    en: "?",
+  },
+  {
+    br: "..OOO.",
+    en: "!",
+  },
+  {
+    br: "..OO..",
+    en: ":",
+  },
+  {
+    br: "..O.O.",
+    en: ";",
+  },
+  {
+    br: "....OO",
+    en: "-",
+  },
+  {
+    br: ".O..O.",
+    en: "/",
+  },
+  {
+    br: ".OO..O",
+    en: "<",
+  },
+  {
+    br: "O..OO.",
+    en: ">",
+  },
+  {
+    br: "O.O..O",
+    en: "(",
+  },
+  {
+    br: ".O.OO.",
+    en: ")",
+  },
+  {
+    br: "......",
+    en: " ",
+  },
+];
