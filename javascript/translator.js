f01b1466334223336de00d76d76652eb7fb0cbe3
Eds-Dbug <edsDbug@gmail.com>
2024-09-12 13:13:46 -0400

    Do the checks for converting english to braille.

--- javascript/translator.js
+++ javascript/translator.js
@@ -49,16 +49,56 @@ const isBraille = (str) => {
 //Translate English to Braille
 //reversing the mappings should have been done first but doesnt make a difference in the end
 const engToBrl = (str) => {
+    let result = '';
+    let upperCase = /[A-Z]/;
+    let numbers = /[0-9]/;
+    let isNumber = false; //boolean to see if its a number
 
+    //Loop through each char of 'str' and perform a check to see which mappings are appropriate.
+    for(let char of str) {
+        
+        // If the character is a space append a 'braille' space to result.
+        if(char === ' ') {
+            result += brailleChars.space;
+        } else if (upperCase.test(char)) { //check if it is an uppercase;
+            // So check if the characters 
+            result += brailleChars.capital + brailleChars[char.toLowerCase()];
+        } else if(numbers.test(char)) {
+            if(!isNumber) {
+                result += brailleChars.number; //prefix
+                isNumber = true;
+            }
+            result += brailleChars[char]
+        } else {
+            result += brailleChars[char];
+        }
+    }
+
+    return result;
 }
 
 
 // Test isBraille
-console.log(isBraille("O....."));      // should be true 'a' 
-console.log(isBraille("OO.O.."));      // true 'd'
-console.log(isBraille("O.OO.. "));     // f 'space 
-console.log(isBraille("O.....1"));     // f there is a 1
-console.log(isBraille("abc.OOO"));     // f 'abc'
-console.log(isBraille("O.....X"));     // f 'X'
-console.log(isBraille(".....O.O...OOO.OOO"));  // t
-console.log(isBraille("O..... O...."));        // f 'space in middle'
+// console.log(isBraille("O....."));      // should be true 'a' 
+// console.log(isBraille("OO.O.."));      // true 'd'
+// console.log(isBraille("O.OO.. "));     // f 'space 
+// console.log(isBraille("O.....1"));     // f there is a 1
+// console.log(isBraille("abc.OOO"));     // f 'abc'
+// console.log(isBraille("O.....X"));     // f 'X'
+// console.log(isBraille(".....O.O...OOO.OOO"));  // t
+// console.log(isBraille("O..... O...."));        // f 'space in middle'
+
+// Test engToBrl function
+console.log(engToBrl("hello")); 
+console.log(engToBrl("HELLO")); 
+console.log(engToBrl("Hello World")); 
+console.log(engToBrl("abc 123"));
+console.log(engToBrl("Good Morning!"));
+// O.OO.O..O..O.O.O.O.O.O.O.OO.
+// .....OO.OO......OO..O.......OO.O.O......OO.O.O......OO.OO.
+// .....OO.OO.O..O..O.O.O.O.O.O.O.OO............O.OOO.OO.OO.O.OOO.O.O.O.OO.O..
+// O.....O.O...OO...........O.OOOO.....O.O...OO....
+// .....OOOOO..O.OO.O.OO.OO.O.............OOO..O.O.OO.O.OOO.OO.OO..OO...OO.OO.OOOO..undefined
+
+
+
