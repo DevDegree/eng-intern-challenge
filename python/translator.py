735e48dd3de9d0ec018a12d306f9de19096890af
Leonardo Atalla <latal094@uottawa.ca>
2024-09-12 15:13:08 -0400

    isBraille function

--- python/translator.py
+++ python/translator.py
@@ -1,2 +1,19 @@
-for i in range(3):
-    print(1)
+
+#converts braille to english
+def brailleToEnglish(string):
+    return 0
+
+#converts english to braille
+def englishToBraille(string):
+    return 0
+
+#checks if inputted string is already braille or not
+def isBraille(string):
+    for i in range(len(string)):
+        if string[i] != "." and string[i] != "O":
+            return 0
+    return 1
+
+text = input("String: ")
+print(text)
+x = isBraille(text[0:9])
