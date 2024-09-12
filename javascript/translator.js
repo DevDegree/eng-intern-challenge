01805a408a71c9f68d17c646e50888e4c06740b8
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-09-12 12:27:58 -0400

    Create brailleCharacters object

--- javascript/translator.js
+++ javascript/translator.js
@@ -1,6 +1,69 @@
 // Take CLI arguments
 const userInput = process.argv.slice(2);
 
+const brailleCharacters = {
+  // Letters
+  "a": "O.....",
+  "b": "O.O...",
+  "c": "OO....",
+  "d": "OO.O..",
+  "e": "O..O..",
+  "f": "OOO...",
+  "g": "OOOO..",
+  "h": "O.OO..",
+  "i": ".OO...",
+  "j": ".OOO..",
+  "k": "O...O.",
+  "l": "O.O.O.",
+  "m": "OO..O.",
+  "n": "OO.OO.",
+  "o": "O..OO.",
+  "p": "OOO.O.",
+  "q": "OOOOO.",
+  "r": "O.OOO.",
+  "s": ".OO.O.",
+  "t": ".OOOO.",
+  "u": "O...OO",
+  "v": "O.O.OO",
+  "w": ".OOO.O",
+  "x": "OO..OO",
+  "y": "OO.OOO",
+  "z": "O..OOO",
+
+  // Numbers (same as letters a-j with number sign prefix)
+  "1": "O.....",
+  "2": "O.O...",
+  "3": "OO....",
+  "4": "OO.O..",
+  "5": "O..O..",
+  "6": "OOO...",
+  "7": "OOOO..",
+  "8": "O.OO..",
+  "9": ".OO...",
+  "0": ".OOO..",
+
+  // Symbols
+  ",": "O.....",
+  ";": "O.O...",
+  ":": "OO....",
+  ".": "OO.O..",
+  "!": "O..O..",
+  "?": "OOO...",
+  "(": "OOOO..",
+  ")": "O.OO..",
+  "'": ".OO...",
+  "-": "O....O",
+  "/": ".O...O",
+
+  // Space
+  " ": "......",
+
+  // Prefixes
+  "#": ".O.OOO",  // Number follows
+  "CAP": ".....O",  // Capital follows
+  "DEC": "..O.O.",  // Decimal follows
+};
+
 // determine if given arguments are English or Braille
 const languageType = (language) => {
   // Braille will ALWAYS include a period .
