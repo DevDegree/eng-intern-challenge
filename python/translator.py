2328eb089be047cc0283383f366c6f411d56e8bd
yhchung12 <yhchung@uwaterloo.ca>
2024-09-12 17:52:35 -0400

    added basic structure of main translator file and implemented get_str_from_args

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,29 @@
+import sys 
+from translator_help import *
 
+def get_str_from_args() -> str: 
+    list_of_arguments = sys.argv[1:]
+    if len(list_of_arguments) == 0: 
+        return ""
+    else: 
+        return " ".join(list_of_arguments)
+    
+def check_if_english(text: str) -> bool: 
+    return True 
+
+def convert_to_braille(text: str) -> str: 
+    return text
+
+def convert_to_english(text: str) -> str: 
+    return text 
+
+def main():
+    text = get_str_from_args()
+    is_english = check_if_english(text)
+    if is_english: 
+        print(convert_to_braille(text))
+    else: 
+        print(convert_to_english(text))
+
+if __name__ == "__main__": 
+    main()
