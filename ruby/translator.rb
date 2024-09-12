494722d68b0004a92d76b9bac728d97f4afe4f78
Abror Kamalov <abrkamalov@gmail.com>
2024-09-11 20:55:05 -0400

    Simple implementation of Braille to English Translator

---
+++ ruby/translator.rb
@@ -0,0 +1,91 @@
+#!/usr/bin/env ruby
+
+# Braille Translator
+# Author: [Your Name]
+# Date: September 11, 2024
+
+class BrailleTranslator
+    BRAILLE_TO_CHAR = {
+      'O.....' => 'a', 'O.O...' => 'b', 'OO....' => 'c', 'OO.O..' => 'd', 'O..O..' => 'e',
+      'OOO...' => 'f', 'OOOO..' => 'g', 'O.OO..' => 'h', '.OO...' => 'i', '.OOO..' => 'j',
+      'O...O.' => 'k', 'O.O.O.' => 'l', 'OO..O.' => 'm', 'OO.OO.' => 'n', 'O..OO.' => 'o',
+      'OOO.O.' => 'p', 'OOOOO.' => 'q', 'O.OOO.' => 'r', '.OO.O.' => 's', '.OOOO.' => 't',
+      'O...OO' => 'u', 'O.O.OO' => 'v', '.OOO.O' => 'w', 'OO..OO' => 'x', 'OO.OOO' => 'y',
+      'O..OOO' => 'z', '..OOOO' => 'number', '.....O' => 'capital'
+    }.freeze
+  
+    CHAR_TO_BRAILLE = BRAILLE_TO_CHAR.invert.freeze
+  
+    NUMBER_MAP = {
+      'a' => '1', 'b' => '2', 'c' => '3', 'd' => '4', 'e' => '5',
+      'f' => '6', 'g' => '7', 'h' => '8', 'i' => '9', 'j' => '0'
+    }.freeze
+  
+    def initialize(input)
+      @input = input
+    end
+  
+    def translate
+      is_braille? ? braille_to_text : text_to_braille
+    end
+  
+    private
+  
+    def is_braille?
+      @input.match?(/\A[O.]+\z/)
+    end
+  
+    def braille_to_text
+      chars = @input.scan(/.{6}/)
+      result = []
+      capitalize_next = false
+      number_mode = false
+  
+      chars.each do |char|
+        case char
+        when CHAR_TO_BRAILLE['capital']
+          capitalize_next = true
+        when CHAR_TO_BRAILLE['number']
+          number_mode = true
+        when ' ' * 6
+          result << ' '
+          number_mode = false
+        else
+          letter = BRAILLE_TO_CHAR[char]
+          letter = NUMBER_MAP[letter] if number_mode && NUMBER_MAP.key?(letter)
+          letter.upcase! if capitalize_next
+          result << letter
+          capitalize_next = false
+        end
+      end
+  
+      result.join
+    end
+  
+    def text_to_braille
+      result = []
+      @input.each_char do |char|
+        if char.match?(/[A-Z]/)
+          result << CHAR_TO_BRAILLE['capital']
+          char.downcase!
+        elsif char.match?(/\d/)
+          result << CHAR_TO_BRAILLE['number'] unless result.last == CHAR_TO_BRAILLE['number']
+          char = NUMBER_MAP.key(char)
+        end
+  
+        result << (char == ' ' ? ' ' * 6 : CHAR_TO_BRAILLE[char])
+      end
+      result.join
+    end
+  end
+  
+  if __FILE__ == $PROGRAM_NAME
+    if ARGV.empty?
+      puts "Usage: ./translator.rb <text_to_translate>"
+      exit 1
+    end
+  
+    input = ARGV.join(' ')
+    translator = BrailleTranslator.new(input)
+    puts translator.translate
+  end
