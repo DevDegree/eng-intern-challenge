51110f12ef9b8757c59241cccd84889a696ae250
mkudrenecky <mkudrene@student.ubc.ca>
2024-09-13 08:14:34 -0600

    add translation hash

---
+++ ruby/translator.rb
@@ -0,0 +1,42 @@
+BRAILLE_TO_ENGLISH = { 'O.....' => 'a',
+                       'O.O...' => 'b',
+                       'OO....' => 'c',
+                       'OO.O..' => 'd',
+                       'O..O..' => 'e',
+                       'OOO...' => 'f',
+                       'OOOO..' => 'g',
+                       'O.OO..' => 'h',
+                       '.O.O..' => 'i',
+                       '.OOO..' => 'j',
+                       'O...O.' => 'k',
+                       'O.O.O.' => 'l',
+                       'OO..O.' => 'm',
+                       'OO.OO.' => 'n',
+                       'O..OO.' => 'o',
+                       'OOO.O.' => 'p',
+                       'OOOO.O' => 'q',
+                       'O.OOO.' => 'r',
+                       '.OO.O.' => 's',
+                       '.OOOO.' => 't',
+                       'O...OO' => 'u',
+                       'O.O.OO' => 'v',
+                       '.OOO.O' => 'w',
+                       'OO..OO' => 'x',
+                       'OO.OOO' => 'y',
+                       'O..OOO' => 'z',
+                       '......' => ' ' }.freeze
+ENGLISH_TO_BRAILLE = BRAILLE_TO_ENGLISH.invert
+BRAILLE_TO_NUMBERS = { 'O.....' => '1',
+                       'O.O...' => '2',
+                       'OO....' => '3',
+                       'OO.O..' => '4',
+                       'O..O..' => '5',
+                       'OOO...' => '6',
+                       'OOOO..' => '7',
+                       'O.OO..' => '8',
+                       '.O.O..' => '9',
+                       '.OOO..' => '0' }.freeze
+NUMBERS_TO_BRAILLE = BRAILLE_TO_NUMBERS.invert
+NUMBER_FOLLOWS = '.O.OOO'.freeze
+CAPITAL_FOLLOWS = '.....O'.freeze
+
