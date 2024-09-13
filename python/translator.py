bf56660a3b92cac4fbd75dac50abb3fef67b5a92
Palak <palak.desai4501@gmail.com>
2024-09-12 22:10:07 -0400

    Completed Braille Translator

--- python/translator.py
+++ python/translator.py
@@ -1,51 +1,73 @@
 import sys
 
-# Braille dictionary for English to Braille
-braille_alphabet = {
-    'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..', 'f': 'OOO...',
-    'g': 'OOOO..', 'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..', 'k': 'O...O.', 'l': 'O.O.O.',
-    'm': 'OO..O.', 'n': 'OO.OO.', 'o': 'O..OO.', 'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.',
-    's': '.OO.O.', 't': '.OOOO.', 'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO',
-    'y': 'OO.OOO', 'z': 'O..OOO',
-    '1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..', '5': 'O..O..', '6': 'OOO...',
-    '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...', '0': '.OOO..',
-    ' ': '......', ',': '..O...', ';': '..OO..', ':': 'O..O..', '.': '..OOO.', '?': '..O.OO',
-    '!': '..OO.O', '-': '..O..O', '/': 'O.O...', '(': 'O..OO.', ')': '.OOOOO',
-    'capital': '.....O', 'number': '.O.OOO'
+# Braille character mappings
+braille_map = {
+    'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..', 'f': 'OOO...', 'g': 'OOOO..',
+    'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..', 'k': 'O...O.', 'l': 'O.O.O.', 'm': 'OO..O.', 'n': 'OO.OO.',
+    'o': 'O..OO.', 'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.', 'u': 'O...OO',
+    'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO', 'y': 'OO.OOO', 'z': 'O..OOO', '1': 'O.....', '2': 'O.O...',
+    '3': 'OO....', '4': 'OO.O..', '5': 'O..O..', '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...',
+    '0': '.OOO..', ' ': '......', 'capital': '.....O', 'number': '.O.OOO'
 }
 
-# Function to translate English to Braille
-def english_to_braille(text):
-    braille_output = []
-    number_mode = False  # Used to track if we are in number mode
+reverse_braille_map = {v: k for k, v in braille_map.items()}
+
+def is_braille(input_str):
+    return all(c in ['O', '.'] for c in input_str)
+
+def translate_to_braille(text):
+    result = []
+    number_mode = False
     for char in text:
-        if char.isdigit():  # Handle numbers
+        if char.isdigit():
             if not number_mode:
-                braille_output.append(braille_alphabet['number'])  # Prefix number mode
+                result.append(braille_map['number'])
                 number_mode = True
-            braille_output.append(braille_alphabet[char])
-        elif char.isalpha():  # Handle letters
+            result.append(braille_map[char])
+        elif char.isalpha():
+            if number_mode:
+                number_mode = False  
             if char.isupper():
-                braille_output.append(braille_alphabet['capital'])  # Prefix capital mode
-            braille_output.append(braille_alphabet[char.lower()])  # Add lowercase version
-            number_mode = False  # Exit number mode
+                result.append(braille_map['capital'])
+            result.append(braille_map[char.lower()])
+        elif char == ' ':
+            result.append(braille_map[' '])
+            number_mode = False  
+    return ''.join(result)
+
+def translate_to_english(braille):
+    result = []
+    number_mode = False
+    i = 0
+    while i < len(braille):
+        symbol = braille[i:i + 6]
+        if symbol == braille_map['number']:
+            number_mode = True
+            i += 6
+            continue
+        if symbol == braille_map['capital']:
+            i += 6
+            symbol = braille[i:i + 6]
+            char = reverse_braille_map.get(symbol, '?')
+            result.append(char.upper())
+            i += 6
         else:
-            # Handle unsupported characters (like spaces, punctuation)
-            braille_output.append(braille_alphabet.get(char, '......'))  # Defaults to space
-            number_mode = False  # Exit number mode for spaces or unsupported chars
-    return ''.join(braille_output)
+            char = reverse_braille_map.get(symbol, '?')
+            if number_mode and char in 'abcdefghijklmnopqrstuvwxyz':
+                result.append(char)
+            else:
+                result.append(char)
+            number_mode = False  
+            i += 6
+    return ''.join(result)
 
-# Main function to handle multiple arguments and convert them
-if __name__ == "_main_":
-    if len(sys.argv) < 2:
-        print("Usage: python translator.py <text_to_translate>")
-        sys.exit(1)
+def main():
+    input_str = ' '.join(sys.argv[1:])
 
-    # Combine all arguments into one string
-    input_text = ' '.join(sys.argv[1:])
-    
-    # Translate the combined input to Braille
-    translated_text = english_to_braille(input_text)
-    
-    # Output the translated Braille text
-    print(translated_text, end="")  # No newline at the end
+    if is_braille(input_str):
+        print(translate_to_english(input_str))
+    else:
+        print(translate_to_braille(input_str))
+
+if _name_ == "_main_":
+    main()
