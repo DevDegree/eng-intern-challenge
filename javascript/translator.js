b7c633e30dd497690ee3ad5f0bf47bd57c49c7f0
Edina Hollo <edina.rostas8@gmail.com>
2024-09-12 23:03:24 -0700

    Clean up code, finish english to braille

--- javascript/translator.js
+++ javascript/translator.js
@@ -1,53 +1,123 @@
-let brailleAlphabet = [
-    ['A', "O....."],
-    ['B', "O.O..."],
-    ['C', "OO...."],
-    ['D', "OO.O.."],
-    ['E', "O..O.."],
-    ['F', "OOO..."],
-    ['G', "OOOO.."],
-    ['H', "O.OO.."],
-    ['I', ".OO..."],
-    ['J', ".OOO.."],
-    ['K', "O...O."],
-    ['L', "O.O.O."],
-    ['M', "OO..O."],
-    ['N', "OO.OO."],
-    ['O', "O..OO."],
-    ['P', "OOO.O."],
-    ['Q', "OOOOO."],
-    ['R', "O.OOO."],
-    ['S', ".OO.O."],
-    ['T', ".OOOO."],
-    ['U', "O...OO"],
-    ['V', "O.O.OO"],
-    ['W', ".OOO.O"],
-    ['X', "OO..OO"],
-    ['Y', "OO.OOO"],
-    ['Z', "O..OOO"]
+const ENGLISH_TO_BRAILLE = [
+    ['a', "O....."],
+    ['b', "O.O..."],
+    ['c', "OO...."],
+    ['d', "OO.O.."],
+    ['e', "O..O.."],
+    ['f', "OOO..."],
+    ['g', "OOOO.."],
+    ['h', "O.OO.."],
+    ['i', ".OO..."],
+    ['j', ".OOO.."],
+    ['k', "O...O."],
+    ['l', "O.O.O."],
+    ['m', "OO..O."],
+    ['n', "OO.OO."],
+    ['o', "O..OO."],
+    ['p', "OOO.O."],
+    ['q', "OOOOO."],
+    ['r', "O.OOO."],
+    ['s', ".OO.O."],
+    ['t', ".OOOO."],
+    ['u', "O...OO"],
+    ['v', "O.O.OO"],
+    ['w', ".OOO.O"],
+    ['x', "OO..OO"],
+    ['y', "OO.OOO"],
+    ['z', "O..OOO"],
+    ['.', "..OO.O"],
+    [',', "..O..."],
+    ['?', "..O.OO"],
+    ['!', "..OOO."],
+    [':', "..OO.."],
+    [';', "..O.O."],
+    ['-', "....OO"],
+    ['/', ".O..O."],
+    ['<', ".OO..O"],
+    ['>', "O..OO."],
+    ['(', "O.O..O"],
+    [')', ".O.OO."],
+    [' ', "......"],
+    ['1', "O....."],
+    ['2', "O.O..."],
+    ['3', "OO...."],
+    ['4', "OO.O.."],
+    ['5', "O..O.."],
+    ['6', "OOO..."],
+    ['7', "OOOO.."],
+    ['8', "O.OO.."],
+    ['9', ".OO..."],
+    ['0', ".OOO.."],
 ]
 
-function translate(text) {
-    text = text.toUpperCase();
-    let result = '';
+const CAPITAL_MODIFIER = '.....O';
+const DECIMAL_MODIFIER = '.O...O';
+const NUMBER_MODIFIER = '.O.OOO';
+const BRAILLE_REGEX = /^[\.O]+$/;
+const CAPITAL_REGEX = /[A-Z]/;
+const NUNBER_REGEX = /[0-9]/;
+const TEXT_STATE = 'TEXT_STATE';
+const NUMBER_STATE = 'NUMBER_STATE';
+const LOWERCASE_CHARACTER = 'LOWERCASE_CHARACTER';
+const UPPERCASE_CHARACTER = 'UPPERCASE_CHARACTER';
+const NUMBER_CHARACTER = 'NUMBER_CHARACTER';
 
-    for (let character of text) {
-        let braille = brailleAlphabet.find(([letter]) => letter === character);
-        if (braille) {
-            result += braille[1];
-        } else {
-            result += 'not in the alphabet yet';
+function translate(text) {
+    let result = '';
+    if (BRAILLE_REGEX.test(text)) {
+        return 'Braille';
+    } else {
+        let state = TEXT_STATE;
+        for (let character of text) {
+            switch (testEnglishCharacter(character)) {
+                case LOWERCASE_CHARACTER: {
+                    state = TEXT_STATE;
+                    result += englishToBraille(character);
+                    break;
+                }
+                case UPPERCASE_CHARACTER: {
+                    state = TEXT_STATE;
+                    result += CAPITAL_MODIFIER;
+                    result += englishToBraille(character.toLowerCase());
+                    break;
+                }
+                case NUMBER_CHARACTER: {
+                    if (state !== NUMBER_STATE) {
+                        result += NUMBER_MODIFIER;
+                        state = NUMBER_STATE;
+                    }
+                    result += englishToBraille(character);
+                    break;
+                }
+            }
         }
     }
+    return result;
+}
 
-    return result.trim();
+function testEnglishCharacter(character) {
+    if (CAPITAL_REGEX.test(character)) return UPPERCASE_CHARACTER;
+    else if (NUNBER_REGEX.test(character)) return NUMBER_CHARACTER;
+    else return LOWERCASE_CHARACTER;
+}
+
+function englishToBraille(characterToFind) {
+    return ENGLISH_TO_BRAILLE.find(([character]) => character === characterToFind)[1];
+}
+
+function brailleToEnglish(characterToFind) {
+    return ENGLISH_TO_BRAILLE.find(([_, character]) => character === characterToFind)[0];
 }
 
 let input = '';
 
 process.argv.forEach(function (val, index, array) {
     if (index > 1) {
-        input += val;
+        if (index > 2) {
+            input += ' ' + val;
+        } else {
+            input += val
+        }
     }
 });
 
