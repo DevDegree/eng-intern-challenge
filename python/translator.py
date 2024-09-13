2c453ab85c18b208968ac73eed4b3cb2b27272b5
DannyFabs <aderemifawoyed@gmail.com>
2024-09-13 08:17:38 -0500

    Added function to check text type

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,22 @@
 
+def isBraille(inp_text: str):
+    braille_char_set = {"O", "."}
+
+    is_valid_braille = True
+
+    # validate length
+    if len(inp_text) % 6 != 0:
+        is_valid_braille = False
+
+    if is_valid_braille:
+        # validate characters
+        for char in inp_text:
+            if char not in braille_char_set:
+                is_valid_braille = False
+    return is_valid_braille
+
+
+
+# main guard
+if __name__ == "__main__":
+    print(isBraille(""))
