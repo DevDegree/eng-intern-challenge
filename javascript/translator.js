d47d01b435665f5644ac13837b592673d26be5b9
TNealy <66037350+JunNealy@users.noreply.github.com>
2024-09-13 11:56:12 -0400

    initial local commit

---
+++ javascript/translator.js
@@ -0,0 +1,6 @@
+//create map for standard char to braille representation
+//Parse if input string is braille or char string
+
+//based on this either
+//A) Loop through the char string, determining if each letter is uppercase, lowercase, number, decimal, appending the correct leading braille representation to translation. push the appropriate braille representation to array. join array and log/return in console
+//B) As above but inverted. Create method for detecting capital preceeding braille rep and modify char accordingly before pushing to array
