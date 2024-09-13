ab29b34527ae3604fa2890842308e75c82376173
Yiwei Liu <e-well@outlook.com>
2024-09-13 17:54:56 -0400

    translator built by Python

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,178 @@
 
+from dataclasses import dataclass
+from abc import ABC, abstractmethod
+import sys
+
+@dataclass
+class BraileSymbol:
+    '''
+    BraileSymbol symbol representation
+    '''
+    letter: str = None 
+    number: str = None
+    special: str = None
+    rule: str = None # capital follows, number follows, decimal follows, default
+
+class Translator(ABC):
+    '''
+    Translator abstract class
+    '''
+    # one-to-one mapping is easy for further translation
+    eng_letters_to_braile = { 'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..',
+        'e': 'O..O..', 'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..',
+        'i': '.OO...', 'j': '.OOO..', 'k': 'O...O.', 'l': 'O.O.O.',
+        'm': 'OO..O.', 'n': 'OO.OO.', 'o': 'O..OO.', 'p': 'OOO.O.',
+        'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.',
+        'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO',
+        'y': 'OO.OOO', 'z': 'O..OOO'}
+    
+    eng_numbers_to_braile = {'1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..', '5': 'O..O..',
+        '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...', '0': '.OOO..'}
+    
+    eng_special_to_braile = {' ': '......', '.': '..OO.O',',': '..O...', '?': '..O.OO',
+        '!': '..OOO.', ':': '..OO..', ';': '..O.O.', '-': '....OO',
+        '/': '.O..O.', '<': '.OO...O', '>': 'O..OO.', '(': 'O.O..O',
+        ')': '.O.OO.'}
+    
+    CAPITAL_FOLLOWS = '.....O'
+
+    NUMBER_FOLLOWS = '.O.OOO'
+
+    DECIMAL_FOLLOWS = '.O...O'
+
+    @abstractmethod
+    def run(self, input_str: str) -> str:
+        pass
+
+    @abstractmethod
+    def translate_by_rules(self, symbol) -> str:
+        pass
+
+
+class BraileTranslator(Translator):
+    def __init__(self) -> None:
+        self.braile_to_eng : dict[str, BraileSymbol] = {}
+
+        for letter, braile in self.eng_letters_to_braile.items():
+            if braile not in self.braile_to_eng:
+                self.braile_to_eng[braile] = BraileSymbol(letter=letter) 
+            else:
+                self.braile_to_eng[braile].letter = letter
+        
+        for number, braile in self.eng_numbers_to_braile.items():
+            if braile not in self.braile_to_eng:
+                self.braile_to_eng[braile] = BraileSymbol(number=number)
+            else:
+                self.braile_to_eng[braile].number = number
+
+        for special, braile in self.eng_special_to_braile.items():
+            if braile not in self.braile_to_eng:
+                self.braile_to_eng[braile] = BraileSymbol(special=special)
+            else:
+                self.braile_to_eng[braile].special = special
+
+        self.braile_to_eng[self.CAPITAL_FOLLOWS] = BraileSymbol(rule='capital follows')
+        self.braile_to_eng[self.NUMBER_FOLLOWS] = BraileSymbol(rule = 'number follows')
+        self.braile_to_eng[self.DECIMAL_FOLLOWS] = BraileSymbol(rule = 'decimal follows')
+
+        self.mark = ''
+
+    def reset_mark(self) -> None:
+        self.mark = 'default'
+    
+    def update_mark(self, mark: str) -> None:
+        self.mark = mark
+        
+    def translate_by_rules(self, symbol: BraileSymbol) -> str:
+        output = 'Not found'
+        if symbol.rule:
+            self.update_mark(symbol.rule)
+            if self.mark == 'decimal follows':
+                output = '.'
+            else:
+                output = ''
+        else:
+            if self.mark == 'default':  
+                output = symbol.letter if symbol.letter else symbol.special if symbol.special else symbol.number
+            elif self.mark == 'capital follows':
+                output = symbol.letter.upper()
+                self.reset_mark()
+            else: # number or decimal follows as marked
+                if symbol.special == ' ':
+                    output = symbol.special
+                    self.reset_mark()
+                else:
+                    output = symbol.number
+        return output
+
+    def run(self, input_str: str) -> str:
+        output = ''
+        for i in range(0, len(input_str), 6):
+            chunk = input_str[i:i+6]
+            if chunk in self.braile_to_eng:
+                symbol = self.braile_to_eng[chunk]
+                output += self.translate_by_rules(symbol)
+            else:
+                raise ValueError(f"Invalid Braille chunk: {chunk}")
+            
+        # group by 6 characters
+        # check for rule marks
+        # translate to english
+        return output
+    
+class EnglishTranslator(Translator):
+    def __init__(self) -> None:
+        self.eng_to_braile = {**self.eng_letters_to_braile, **self.eng_numbers_to_braile, **self.eng_special_to_braile}
+        self.is_number = False
+
+    def run(self, input_str: str) -> str:
+        output = ''
+        for char in input_str:
+            output += self.translate_by_rules(char)
+        return output
+
+    def translate_by_rules(self, symbol: str) -> str:
+        if symbol.isupper():
+            return self.CAPITAL_FOLLOWS + self.eng_to_braile[symbol.lower()]
+        elif symbol.isdigit():
+            digit_braile = self.eng_to_braile[symbol]
+            if self.is_number:
+                return digit_braile
+            else:
+                self.is_number = True
+                return self.NUMBER_FOLLOWS + digit_braile
+        elif symbol == '.':
+            if self.is_number:
+                return self.DECIMAL_FOLLOWS
+            else:    
+                return self.eng_to_braile[symbol]
+        elif symbol == ' ' and self.is_number:
+            self.is_number = False
+
+        return self.eng_to_braile[symbol]
+        
+
+class TranslatorFactory:
+    @staticmethod
+    def get_translator(inputs: str) -> Translator:
+        lang = 'braile'
+        for c in inputs:
+            if c not in ['O', '.']:
+                lang = 'english'
+                break
+        if lang == 'braile':
+            return BraileTranslator()
+        elif lang == 'english':
+            return EnglishTranslator()
+        
+def main():
+    input_str = ' '.join(sys.argv[1:])
+    if not input_str:
+        print("Please provide an input string to translate.")
+        sys.exit(1)
+
+    translator = TranslatorFactory.get_translator(input_str)
+    print(translator.run(input_str), end='')
+
+if __name__ == '__main__':
+    main()
