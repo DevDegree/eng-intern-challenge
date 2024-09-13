3f042c1d0636ed0e617b2922bcc75afc8c31559a
kareem717 <kareem.717@icloud.com>
2024-09-13 17:17:48 -0400

    feat: refactor

--- go/cmd/translate.go
+++ go/cmd/translate.go
@@ -25,44 +25,20 @@ func (d *Dictionary) Reverse() Dictionary {
 	}
 }
 
-var dictionary = Dictionary{
-	Alphabet: map[string]string{
-		"a": "O.....", "b": "O.O...", "c": "OO....", "d": "OO.O..", "e": "O..O..",
-		"f": "OOO...", "g": "OOOO..", "h": "O.OO..", "i": ".OO...", "j": ".OOO..",
-		"k": "O...O.", "l": "O.O.O.", "m": "OO..O.", "n": "OO.OO.", "o": "O..OO.",
-		"p": "OOO.O.", "q": "OOOOO.", "r": "O.OOO.", "s": ".OO.O.", "t": ".OOOO.",
-		"u": "O...OO", "v": "O.O.OO", "w": ".OOO.O", "x": "OO..OO", "y": "OO.OOO",
-		"z": "O..OOO",
-	},
-	Numbers: map[string]string{
-		"0": ".OOO..", "1": "O.....", "2": "O.O...", "3": "OO....", "4": "OO.O..",
-		"5": "O..O..", "6": "OOO...", "7": "OOOO..", "8": "O.OO..", "9": ".OO...",
-	},
-	Symbols: map[string]string{
-		" ": "......",
-	},
-	Modifiers: map[string]string{
-		"capital": ".....O", "decimal": ".O...O", "number": ".O.OOO",
-	},
-}
-
 var rootCmd = &cobra.Command{
 	Use:   "translator",
 	Short: "Translator is a tool to translate between text and braille",
 	Run: func(cmd *cobra.Command, args []string) {
-		output := []string{}
+		isBraille := len(args) == 1 && isBraille(args[0])
+		translator := NewTranslator(isBraille)
 
-		for _, arg := range args {
-			translated, err := translate(arg, len(args) == 1 && isBraille(args[0]))
-			if err != nil {
-				fmt.Println("Error:", err)
-				continue
-			}
-
-			output = append(output, translated)
+		output, err := translator.Translate(args)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
 		}
 
-		fmt.Println(strings.Join(output, dictionary.Symbols[" "]))
+		fmt.Println(output)
 	},
 }
 
@@ -73,94 +49,191 @@ func Execute() {
 	}
 }
 
-func translate(text string, isBraille bool) (string, error) {
-	var splitText, modifier string
-	increment := 1
-	if isBraille {
-		increment = 6
-		dictionary = dictionary.Reverse()
-	}
+type Modifier string
 
-	for i := 0; i < len(text); i += increment {
-		chunk := text[i : i+increment]
+const (
+	Capital Modifier = "capital"
+	Decimal Modifier = "decimal"
+	Number  Modifier = "number"
+)
 
-		if modifier != "" {
-			splitText += handleModifier(chunk, modifier, isBraille)
-			modifier = ""
-			continue
-		}
-
-		val, found := handleChunk(chunk, isBraille, &modifier)
-		if !found {
-			return "", fmt.Errorf("character %s not found in dictionary", chunk)
-		}
-		splitText += val
-	}
-
-	return splitText, nil
+type Translator struct {
+	dictionary Dictionary
+	modifier   *Modifier
+	handlers   map[string]func(string) (string, bool)
+	isBraille  bool
+	chunkSize  int
 }
 
-func handleModifier(chunk, modifier string, isBraille bool) string {
-	switch modifier {
-	case "capital":
-		if isBraille {
-			return strings.ToUpper(dictionary.Alphabet[chunk])
-		}
-		return dictionary.Modifiers["capital"] + dictionary.Alphabet[chunk]
-	case "decimal":
-		if val, ok := dictionary.Numbers[chunk]; ok {
-			if isBraille {
-				return "." + val
-			}
-			return dictionary.Modifiers["decimal"] + val
-		}
-	case "number":
-		if val, ok := dictionary.Numbers[chunk]; ok {
-			return val
-		}
-	}
-	return dictionary.Symbols[chunk]
-}
-
-func handleChunk(chunk string, isBraille bool, modifier *string) (string, bool) {
-	handlers := map[string]func(string) (string, bool){
-		"Alphabet": func(chunk string) (string, bool) { val, ok := dictionary.Alphabet[chunk]; return val, ok },
-		"Symbols":  func(chunk string) (string, bool) { val, ok := dictionary.Symbols[chunk]; return val, ok },
-		"Modifiers": func(chunk string) (string, bool) {
-			val, ok := dictionary.Modifiers[chunk]
-			if ok {
-				*modifier = val
-			}
-			return "", ok
+func NewTranslator(isBraille bool) *Translator {
+	dict := Dictionary{
+		Alphabet: map[string]string{
+			"a": "O.....", "b": "O.O...", "c": "OO....", "d": "OO.O..", "e": "O..O..",
+			"f": "OOO...", "g": "OOOO..", "h": "O.OO..", "i": ".OO...", "j": ".OOO..",
+			"k": "O...O.", "l": "O.O.O.", "m": "OO..O.", "n": "OO.OO.", "o": "O..OO.",
+			"p": "OOO.O.", "q": "OOOOO.", "r": "O.OOO.", "s": ".OO.O.", "t": ".OOOO.",
+			"u": "O...OO", "v": "O.O.OO", "w": ".OOO.O", "x": "OO..OO", "y": "OO.OOO",
+			"z": "O..OOO",
+		},
+		Numbers: map[string]string{
+			"0": ".OOO..", "1": "O.....", "2": "O.O...", "3": "OO....", "4": "OO.O..",
+			"5": "O..O..", "6": "OOO...", "7": "OOOO..", "8": "O.OO..", "9": ".OO...",
+		},
+		Symbols: map[string]string{
+			" ": "......",
+		},
+		Modifiers: map[string]string{
+			"capital": ".....O", "decimal": ".O...O", "number": ".O.OOO",
 		},
 	}
 
-	if !isBraille {
-		handlers["Lowercase"] = func(chunk string) (string, bool) {
-			val, ok := dictionary.Alphabet[strings.ToLower(chunk)]
-			if ok {
-				*modifier = "capital"
-				return dictionary.Modifiers["capital"] + val, true
+	translator := Translator{dictionary: dict, isBraille: isBraille, chunkSize: 1}
+
+	if isBraille {
+		translator.brailleMode()
+	}
+
+	return &translator
+}
+
+func (t *Translator) brailleMode() {
+	t.isBraille = true
+	t.dictionary = t.dictionary.Reverse()
+	t.chunkSize = 6
+}
+
+func (t *Translator) Translate(input []string) (string, error) {
+	output := []string{}
+
+	// We must make sure that the order of the handlers is consistent,
+	// so that we don't confuse numbers with letters.
+	handlerOrder := []string{"Alphabet", "Symbols", "Numbers", "Modifiers"}
+
+	for _, text := range input {
+		var splitText string
+
+		for i := 0; i < len(text); i += t.chunkSize {
+			chunk := text[i : i+t.chunkSize]
+
+			if t.modifier != nil {
+				splitText = t.HandleModifier(chunk, splitText)
+				continue
 			}
-			return "", false
+
+			handlers := t.GetHandlers()
+
+			found := false
+			for _, handlerKey := range handlerOrder {
+				handler := handlers[handlerKey]
+				if val, ok := handler(chunk); ok {
+					splitText += val
+					found = true
+					break
+				}
+			}
+
+			if !found {
+				return "", fmt.Errorf("character %s not found in dictionary", chunk)
+			}
 		}
 
+		output = append(output, splitText)
+	}
+
+	var delimiter string
+	if t.isBraille {
+		delimiter = " "
+	} else {
+		delimiter = t.dictionary.Symbols[" "]
+	}
+
+	return strings.Join(output, delimiter), nil
+}
+
+func (t *Translator) HandleModifier(chunk, splitText string) string {
+	switch *t.modifier {
+	case Capital:
+		if t.isBraille {
+			splitText += strings.ToUpper(t.dictionary.Alphabet[chunk])
+		} else {
+			splitText += t.dictionary.Modifiers[string(Capital)] + t.dictionary.Alphabet[chunk]
+		}
+	case Decimal:
+		if val, ok := t.dictionary.Numbers[chunk]; ok {
+			if t.isBraille {
+				splitText += "." + val
+			} else {
+				splitText += t.dictionary.Modifiers[string(Decimal)] + val
+			}
+		}
+	case Number:
+		if val, ok := t.dictionary.Numbers[chunk]; ok {
+			splitText += val
+		} else if " " == chunk || " " == t.dictionary.Symbols[chunk] {
+			splitText += t.dictionary.Symbols[chunk]
+			t.modifier = nil
+		} else {
+			// If it's not a number or space, treat it as regular text
+			if val, ok := t.dictionary.Alphabet[chunk]; ok {
+				splitText += val
+			}
+			t.modifier = nil
+		}
+		return splitText
+	}
+	t.modifier = nil
+
+	return splitText
+}
+
+func (t *Translator) GetHandlers() map[string]func(string) (string, bool) {
+	handlers := map[string]func(string) (string, bool){
+		"Alphabet": func(chunk string) (string, bool) {
+			val, ok := t.dictionary.Alphabet[chunk]
+
+			if val, isUpper := t.dictionary.Alphabet[strings.ToLower(chunk)]; !ok && isUpper {
+				return t.dictionary.Modifiers[string(Capital)] + val, true
+			}
+
+			return val, ok
+		},
+		"Symbols": func(chunk string) (string, bool) { val, ok := t.dictionary.Symbols[chunk]; return val, ok },
+	}
+
+	if t.isBraille {
 		handlers["Numbers"] = func(chunk string) (string, bool) {
-			val, ok := dictionary.Numbers[chunk]
+			val, ok := t.dictionary.Numbers[chunk]
+
 			if ok {
-				*modifier = "number"
-				return dictionary.Modifiers["number"] + val, true
+				modifier := Number
+				t.modifier = &modifier
 			}
+
+			return val, ok
+		}
+
+		handlers["Modifiers"] = func(chunk string) (string, bool) {
+			val, ok := t.dictionary.Modifiers[chunk]
+			if ok {
+				modifier := Modifier(val)
+				t.modifier = &modifier
+			}
+			return "", ok
+		}
+	} else {
+		handlers["Numbers"] = func(chunk string) (string, bool) {
+			if val, ok := t.dictionary.Numbers[chunk]; ok {
+				modifier := Number
+				t.modifier = &modifier
+
+				return t.dictionary.Modifiers[string(Number)] + val, true
+			}
+
 			return "", false
 		}
 	}
 
-	for _, handler := range handlers {
-		if val, ok := handler(chunk); ok {
-			return val, true
-		}
-	}
-	return "", false
+	return handlers
 }
 
 func reverseMap(m map[string]string) map[string]string {
--- go/translator_test.go
+++ go/translator_test.go
@@ -22,6 +22,22 @@ func TestSolutionOutput(t *testing.T) {
 	}
 }
 
+func TestSolutionOutputTwo(t *testing.T) {
+	cmd := exec.Command("go", "run", "translator.go", "Abc", "123", "xYz")
+	outputBytes, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatalf("Failed to run the command: %v", err)
+	}
+
+	// Trim space from output and expected value
+	output := strings.TrimSpace(string(outputBytes))
+	expected := strings.TrimSpace(".....OO.....O.O...OO...........O.OOOO.....O.O...OO..........OO..OO.....OOO.OOOO..OOO")
+
+	if output != expected {
+		t.Errorf("Unexpected output, got: %q, want: %q", output, expected)
+	}
+}
+
 func TestTranslateToBrailleAlphabet(t *testing.T) {
 	cmd := exec.Command("go", "run", "translator.go", "Hello world")
 	outputBytes, err := cmd.CombinedOutput()
@@ -48,6 +64,38 @@ func TestTranslateToEnglish(t *testing.T) {
 	// Trim space from output and expected value
 	output := strings.TrimSpace(string(outputBytes))
 	expected := strings.TrimSpace("Abc 123 xYz")
+
+	if output != expected {
+		t.Errorf("Unexpected output, got: %q, want: %q", output, expected)
+	}
+}
+
+func TestTranslateToBrailleNumber(t *testing.T) {
+	cmd := exec.Command("go", "run", "translator.go", "42")
+	outputBytes, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatalf("Failed to run the command: %v", err)
+	}
+
+	// Trim space from output and expected value
+	output := strings.TrimSpace(string(outputBytes))
+	expected := strings.TrimSpace(".O.OOOOO.O..O.O...")
+
+	if output != expected {
+		t.Errorf("Unexpected output, got: %q, want: %q", output, expected)
+	}
+}
+
+func TestTranslateToEnglishTwo(t *testing.T) {
+	cmd := exec.Command("go", "run", "translator.go", ".....OO.....O.O...OO...........O.OOOO.....O.O...OO....")
+	outputBytes, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatalf("Failed to run the command: %v", err)
+	}
+
+	// Trim space from output and expected value
+	output := strings.TrimSpace(string(outputBytes))
+	expected := strings.TrimSpace("Abc 123")
 
 	if output != expected {
 		t.Errorf("Unexpected output, got: %q, want: %q", output, expected)
