b64d5c3feb6a1b93c8ddfe120781d437556005a2
Kevan Hollbach <khollbach@gmail.com>
2024-09-13 13:14:51 -0400

    Add hello-world rust program
    Gets a command-line argument and prints it to stdout.
    
    Next up: translate from English to Braille.

---
+++ rust/src/main.rs
@@ -0,0 +1,15 @@
+use std::env;
+
+use anyhow::{Context, Result};
+use itertools::Itertools;
+
+fn main() -> Result<()> {
+    let (_prog_name, input) = env::args()
+        .collect_tuple()
+        .context("expected 1 argument: the string to translate")?;
+
+    let output = format!("TODO: translate {input}");
+    println!("{}", output);
+
+    Ok(())
+}
