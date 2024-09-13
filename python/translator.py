70fb330fff678b231c45e8d97b965085a378a48d
Ali Imad Hasan <aliimadhasan97@gmail.com>
2024-09-12 21:47:45 -0700

    Working encode + decode

--- python/translator.py
+++ python/translator.py
@@ -1,12 +1,105 @@
 # Description: This script will take a message and convert it to/from braille into plaintext.
 
+# Braille mapping. LUT is the most efficient runtime (O(1)) for this problem.
+BRAILLE_TO_CHAR = {
+    'O.....': 'a',
+    'O.O...': 'b',
+    'OO....': 'c',
+    'OO.O..': 'd',
+    'O..O..': 'e',
+    'OOO...': 'f',
+    'OOOO..': 'g',
+    'O.OO..': 'h',
+    '.OO...': 'i',
+    '.OOO..': 'j',
+    'O...O.': 'k',
+    'O.O.O.': 'l',
+    'OO..O.': 'm',
+    'OO.OO.': 'n',
+    'O..OO.': 'o',
+    'OOO.O.': 'p',
+    'OOOOO.': 'q',
+    'O.OOO.': 'r',
+    '.OO.O.': 's',
+    '.OOOO.': 't',
+    'O...OO': 'u',
+    'O.O.OO': 'v',
+    '.OOO.O': 'w',
+    'OO..OO': 'x',
+    'OO.OOO': 'y',
+    'O..OOO': 'z',
+    '.....O': 'CAPITAL',  # special chars, "follows"
+    '.O.OOO': 'NUMBER',  # map to A->J respectively
+    '......': ' ',  # ' '
+    # not required
+    # '.O...O': 'DECIMAL',
+    # '.OO..O': '>',
+    # 'O..OO.': '<',  # collides
+    # '..O...': ',',
+    # '..O.OO': '?',
+    # '..OOO.': '!',
+    # '..OO.O': '.',
+    # '..OO..': ':',
+    # '..O.O.': ';',
+    # '....OO': '-',
+    # '.O..O.': '/',
+    # 'O.O..O': '(',
+    # '.O.OO.': ')'
+}
+CHAR_TO_BRAILLE = {v: k for k, v in BRAILLE_TO_CHAR.items()}
+BRAILLE_LEN = len(next(iter(BRAILLE_TO_CHAR.keys())))
+
+
+# Name:         chunks
+# Purpose:      Yield n number of striped chunks from l
+# Parameters:   l - a list
+#               n - an integer
+# Returns:      a generator object of n number of striped chunks from l
+def chunks(l, n):
+    """Yield n number of striped chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i+n]
+
+
 # Name:         decode
 # Purpose:      Decodes a message from braille to plaintext
 # Parameters:   i - a string containing the braille message
 # Returns:      a string containing the decoded message
 def decode(i: str):
     decoded_msg = str()
+    chars = chunks(i, BRAILLE_LEN)
+    # print(BRAILLE_LEN)
+    # print(list(chars))
+    # exit(1)
+    isCapital = False
+    isNumber = False
+    for c in chars:
+        decoded_c = BRAILLE_TO_CHAR[c]
+        if decoded_c == 'CAPITAL':
+            isCapital = True
+            continue
+        if decoded_c == 'NUMBER':
+            isNumber = True
+            continue
+        if decoded_c == 'DECIMAL':
+            continue
+
+        if isCapital:
+            decoded_c = decoded_c.upper()
+            isCapital = False
+        if isNumber:  # iterate thru chars until space is found
+            isNumber = False
+            while BRAILLE_TO_CHAR[c] != ' ':
+                decoded_c = BRAILLE_TO_CHAR[c]
+                # map a->j to 1-0
+                decoded_msg += str(ord(decoded_c) - ord('a'))
+                c = next(chars)
+            if BRAILLE_TO_CHAR[c] == ' ':
+                decoded_msg += ' '
+                continue
+        decoded_msg += decoded_c
     return decoded_msg
+
 
 # Name:         encode
 # Purpose:      Encodes a message from plaintext to braille
@@ -14,7 +107,24 @@ def decode(i: str):
 # Returns:      a string containing the encoded message
 def encode(i: str):
     encoded_msg = str()
-    return encoded_msg
+    # break the input into words
+    words = i.split(' ')
+    for idx, word in enumerate(words):
+        if word.isdigit():
+            encoded_msg += CHAR_TO_BRAILLE['NUMBER']
+            for c in word:
+                encoded_msg += CHAR_TO_BRAILLE[chr(ord('a') + int(c))]
+            encoded_msg += CHAR_TO_BRAILLE[' ']
+            continue
+        for c in word:
+            if c.isupper():
+                encoded_msg += CHAR_TO_BRAILLE['CAPITAL']
+                c = c.lower()
+            encoded_msg += CHAR_TO_BRAILLE[c]
+        if idx < len(words) - 1:
+            encoded_msg += CHAR_TO_BRAILLE[' ']
+    return encoded_msg.strip()
+
 
 if __name__ == '__main__':
     import sys
@@ -24,11 +134,11 @@ if __name__ == '__main__':
         print('Please provide a message to convert to/from braille into plaintext. Usage: python translator.py <msg>')
         exit(1)
     if len(sys.argv) > 2:
-        msg: str = sys.argv[1:].join(' ')
+        msg: str = ' '.join(sys.argv[1:])
     else:
         msg: str = sys.argv[1]
-    # Check if the message contains only 0 or o
-    if not all(c in '0o' for c in msg):
+    # Check if the message contains only O and/or .
+    if not all(c in 'O.' for c in msg):
         # message is a plaintext message
         print(encode(msg))
     else:
