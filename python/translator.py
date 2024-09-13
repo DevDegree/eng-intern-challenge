fcfd90f1e1d3c0d87a8242074e8438371a6d8644
Aditya Shahi <ashahi1@ualberta.ca>
2024-09-13 04:45:46 -0600

    dict defined method to identify english or braille as input

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,36 @@
+import sys
 
+
+#bRAILLE MAPPINGS
+BRAILLE_ALPHABET = {
+    'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..', 'f': 'OOO...',
+    'g': 'OOOO..', 'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..', 'k': 'O...O.', 'l': 'O.O.O.',
+    'm': 'OO..O.', 'n': 'OO.OO.', 'o': 'O..OO.', 'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.',
+    's': '.OO.O.', 't': '.OOOO.', 'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO',
+    'y': 'OO.OOO', 'z': 'O..OOO',
+    ' ': '......',  # Braille space
+}
+
+REVERSE_BRAILLE_ALPHABET = {
+    'O.....': 'a', 'O.O...': 'b', 'OO....': 'c', 'OO.O..': 'd', 'O..O..': 'e',
+    'OOO...': 'f', 'OOOO..': 'g', 'O.OO..': 'h', '.OO...': 'i', '.OOO..': 'j',
+    'O...O.': 'k', 'O.O.O.': 'l', 'OO..O.': 'm', 'OO.OO.': 'n', 'O..OO.': 'o',
+    'OOO.O.': 'p', 'OOOOO.': 'q', 'O.OOO.': 'r', '.OO.O.': 's', '.OOOO.': 't',
+    'O...OO': 'u', 'O.O.OO': 'v', '.OOO.O': 'w', 'OO..OO': 'x', 'OO.OOO': 'y', 'O..OOO': 'z',
+    '......': ' '  # Space mapping
+}
+REVERSE_BRAILLE_NUMBERS = {
+    'O.....': '1', 'O.O...': '2', 'OO....': '3', 'OO.O..': '4', 'O..O..': '5',
+    'OOO...': '6', 'OOOO..': '7', 'O.OO..': '8', '.OO...': '9', '.OOO..': '0'
+}
+
+BRAILLE_NUMBERS = {
+    '1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..', '5': 'O..O..', '6': 'OOO...',
+    '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...', '0': '.OOO..',
+}
+CAPITAL_PREFIX = '.....O'  # Marks that the next letter is capitalized
+NUMBER_PREFIX = '.O.OOO'   # Marks that the following characters are numbers
+
+#to dtermine if input is english or braille
+def is_braille(input_str):
+    return all(c in 'O.' for c in input_str)
