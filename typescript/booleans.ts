d02edcff97b72994a0d37310e26e06989199d218
Jovan <jovanmedford@gmail.com>
2024-09-13 20:17:09 -0400

    Feat: add boolean functions to help with checking certain facts

---
+++ typescript/booleans.ts
@@ -0,0 +1,28 @@
+import { Braille } from "./types";
+import { utilityMap } from "./objects";
+
+export function isUtility(str: Braille) {
+  return (
+    str === utilityMap.capitalize ||
+    str === utilityMap.number ||
+    str === utilityMap.space
+  );
+}
+
+export function isNumber(str: string) {
+  return /[0-9]/.test(str);
+}
+
+export function isBraille(str: string): boolean {
+  if (str.length !== 6) {
+    return false;
+  }
+
+  for (let char of str) {
+    if (!(char == "o" || char == ".")) {
+      return false;
+    }
+  }
+
+  return true;
+}
