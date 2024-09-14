9d92b45d2f8a72ba27a1e4f693201c183bc106e5
Harmin Patel <hr373866@dal.ca>
2024-09-14 01:01:31 -0300

    util: added braille and alphabets list

--- javascript/translator.js
+++ javascript/translator.js
@@ -1,3 +1,44 @@
+const BRAILLE_TO_ENGLISH_LOWER = {
+    "O.....": "a","O.O...": "b","OO....": "c","OO.O..": "d","O..O..": "e","OOO...": "f","OOOO..": "g",
+    "O.OO..": "h",".OO...": "i",".OOO..": "j","O...O.": "k","O.O.O.": "l","OO..O.": "m","OO.OO.": "n",
+    "O..OO.": "o","OOO.O.": "p","OOOOO.": "q","O.OOO.": "r",".OO.O.": "s",".OOOO.": "t","O...OO": "u",
+    "O.O.OO": "v",".OOO.O": "w","OO..OO": "x", "OO.OOO": "y","O..OOO": "z","......": " ",
+  };
+  
+  const BRAILLE_TO_ENGLISH_NUMS = {
+    "O.....": "1","O.O...": "2",
+    "OO....": "3","OO.O..": "4",
+    "O..O..": "5","OOO...": "6",
+    "OOOO..": "7","O.OO..": "8",
+    ".OO...": "9",".OOO..": "0",
+  };
+  
+  const BRAILLE_TO_ENGLISH_SYMBOLS = {
+    "..OO.O": ".", "..O...": ",","..O.OO": "?", "..OO..": "!", "..O.O.": ":","..O..O": ";",
+    "...O..": "-","..O..O": "/","...O.O": "<","...OO.": ">","...OOO": "(","..O..O": ")",
+    "......": " ",".....O": "CAP", ".O.OOO": "NUM",  "..OOO.": "DEC", 
+  };
+  
+const ENGLISH_TO_BRAILLE_LOWER = Object.fromEntries(
+    Object.entries(BRAILLE_TO_ENGLISH_LOWER).map(([k, v]) => [v, k])
+  );
+  
+  const ENGLISH_TO_BRAILLE_NUMS = {
+    1: "O.....",2: "O.O...",
+    3: "OO....",4: "OO.O..",
+    5: "O..O..",6: "OOO...",
+    7: "OOOO..",8: "O.OO..",
+    9: ".OO...",0: ".OOO..",
+  };
+  
+  const ENGLISH_TO_BRAILLE_SYMBOLS = {
+    ".": "..OO.O",",": "..O...","?": "..O.OO","!": "..OO..",
+    ":": "..O.O.",";": "..O..O","-": "...O..","/": "..O..O",
+    "<": "...O.O",">": "...OO.","(": "...OOO",")": "..O..O",
+    CAP: ".....O",NUM: ".O.OOO",DEC: "..OOO."," ": "......",
+  }
+  
+    
     //basic function
     function translateToEnglish(inputText)
     {
