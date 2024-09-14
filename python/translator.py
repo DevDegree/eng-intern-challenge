d37b59335c372ce13ac743c7d9e802156bb93763
Tommy Wang <tommywang1015@hotmail.com>
2024-09-14 17:47:10 -0400

    translator.py

--- python/translator.py
+++ python/translator.py
@@ -1,15 +1,19 @@
 braille_dict = {
-    'a': '0.....', 'b': '0.0...', 'c': '00....', 'd': '00.0..', 'e': '0..0..', 'f': '000...', 'g': '0000..',
-    'h': '0.00..', 'i': '.00...', 'j': '.000..', 'k': '0...0.', 'l': '0.0.0.', 'm': '00..0.', 'n': '00.00.',
-    'o': '0..00.', 'p': '000.0.', 'q': '00000.', 'r': '0.000.', 's': '.00.0.', 't': '.0000.', 'u': '0...00',
-    'v': '0.0.00', 'w': '.000.0', 'x': '00..00', 'y': '00.000', 'z': '0..000', '0': '0.....', '2': '0.0...',
-    '3': '00....', '4': '00.0..', '5': '0..0..', '6': '000...', '7': '0000..', '8': '0.00..', '9': '.00...',
-    '.': '.0000.', ' ': '......'  
+    'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..', 'f': 'OOO...', 'g': 'OOOO..',
+    'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..', 'k': 'O...O.', 'l': 'O.O.O.', 'm': 'OO..O.', 'n': 'OO.OO.',
+    'o': 'O..OO.', 'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.', 'u': 'O...OO',
+    'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO', 'y': 'OO.OOO', 'z': 'O..OOO', '.': '.OOOO.', ' ': '......', 
+    ',': '..O...', '?': '..O.OO', '!': '..OOO.', ':': '..OO..', ';': '..O.O.', '-': '....OO', '/': '.O..O.', 
+    '<': '.OO..O', '>': 'O..OO.', '(': 'O.O..O', ')': '.O.OO.'
 }
+number_dict = { 
+    '1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..', '5': 'O..O..', '6': 'OOO...', '7': 'OOOO..',
+    '8': 'O.OO..', '9': '.OO...', '0':'.OOO..'}
 reverse_braille_dict = {x:y for y, x in braille_dict.items()}
-uppercase_indicator = '.....0'
-decimal_indicator = '.0...0'
-number_indicator = '.0.000'
+reverse_number_dict = {x:y for y, x in number_dict.items()}
+uppercase_indicator = '.....O'
+decimal_indicator = '.O...O'
+number_indicator = '.O.OOO'
 
 def text_to_braille(text):
     braille_result = []
@@ -29,8 +33,8 @@ def text_to_braille(text):
             if not is_number_mode:#start of number insert num_indicator
                 braille_result.append(number_indicator)
                 is_number_mode = True
-            braille_result.append(braille_dict.get(char))
-        else: #no cap letter
+            braille_result.append(number_dict.get(char))
+        elif char in braille_dict:
             is_number_mode = False
             braille_result.append(braille_dict.get(char))
 
@@ -42,25 +46,35 @@ def braille_to_text(braille_list):
     i = 0
     while i < len(braille_list):
         braille = braille_list[i]
-        if braille == uppercase_indicator: #uppercase the next letter
+        if braille == uppercase_indicator: #case 1: Uppercase
             i += 1
             braille = braille_list[i]
             text_result.append(reverse_braille_dict.get(braille).upper())
         elif braille == number_indicator:
             is_number_mode = True
-        else: #normal letter
-            text_result.append(reverse_braille_dict.get(braille))
-        if braille == '......':  # Handle space in Braille
+        elif braille == decimal_indicator:
+            i += 1
+            braille = braille_list[i]
+            if is_number_mode:
+                text_result.append(reverse_number_dict.get(braille))
+            else:
+                text_result.append(reverse_braille_dict.get(braille))
+        elif braille == '......':
             is_number_mode = False
+            text_result.append(' ')
+        else:
+            if is_number_mode:
+                text_result.append(reverse_number_dict.get(braille))
+            else:
+                text_result.append(reverse_braille_dict.get(braille))
         i += 1
-
     return ''.join(text_result)
 
 def main():
     import sys
-    input_text = sys.argv[1]
+    input_text = ' '.join(sys.argv[1:])
 
-    if all(char in '0.' for char in input_text):#braille
+    if all(char in 'O.' for char in input_text):#braille
         braille_list = []
         for i in range(0, len(input_text), 6):
             braille_list.append(input_text[i:i+6]) #split into array consisting of 6 char each
@@ -72,3 +86,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
