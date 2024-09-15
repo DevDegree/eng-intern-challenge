1e602ad6517be631c4661f4b281c9418f4654c86
Oluchukwu Obi-Njoku <oobi-njoku@myseneca.ca>
2024-09-15 19:54:27 +0000

    bidict install

--- python/translator.py
+++ python/translator.py
@@ -2,7 +2,12 @@
 
 import character #python file that contains all character mappings. Braille to English, and vice versa
 import sys
+import subprocess
 
+def install_bidict()->None:
+
+    command = ["python3", "-m", "pip", "install", "bidict"]
+    subprocess.run(command, capture_output=True, text=True)
 
 def check_is_braille(value: str) -> bool:
 
@@ -93,6 +98,7 @@ def translate_statement_to_braille(value:str) -> str:
 def main():
    
     if len(sys.argv) > 1:
+        install_bidict()
         translated_parts = []
         is_braille = False
 
