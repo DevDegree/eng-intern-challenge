08ff979d2e7cde26f58498af745ff6b156bec162
aryankumar2828 <aryan.nair.personal@gmail.com>
2024-09-15 13:12:51 -0600

    Added English to Braille Function
    Implemented function and Tested.
    Fixed the capital letter bug.

--- python/translator.py
+++ python/translator.py
@@ -17,3 +17,34 @@ numbers = {
     '4': 'OO.O..',    '5': 'O..O..',    '6': 'OOO...',    '7': 'OOOO..',
     '8': 'O.OO..',    '9': '.OO...'
 }
+
+capital_symbol = '.....O'
+number_symbol = '..OO.O'
+
+def english_to_braille(text):
+    result = []
+    is_number_mode = False
+
+    for char in text:
+        if char.isupper():
+            result.append(capital_symbol)
+            char = char.lower()
+
+        if char.isdigit():
+            if not is_number_mode:
+                result.append(number_symbol)
+                is_number_mode = True
+            result.append(numbers[char])
+        else:
+            is_number_mode = False
+            result.append(letters[char])
+
+    return ' '.join(result)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Invalid String")
+        sys.exit(1)
+
+    text = ' '.join(sys.argv[1:])
+    print(english_to_braille(text))
