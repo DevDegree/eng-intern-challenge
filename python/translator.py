f85493b401bbff423632bcb9c04225792525e557
Alex <alex.yimo.liu@gmail.com>
2024-09-14 20:38:27 -0400

    Separate chunk generation into chunk function

--- python/translator.py
+++ python/translator.py
@@ -60,15 +60,23 @@ braille_letter_map = { b : l for l, b in letter_braille_map.items()}
 braille_number_map = { b : n for n, b in number_braille_map.items()}
 
 '''
-checks if a given input string is in braille or not.
-assuming all input is correct and something like "OOOOOO" is not given as input
+chunks a string into sections of 6 (with remainder)
 
-TODO: check for multiple of 6, validate each chunk of 6 to see if it exists in braille set
+returns a list
+'''
+def chunk_string(string):
+    return list(string[0+i : 6 + i] for i in range(0, len(string), 6))
+
+'''
+checks if a given input string is in braille or not.
+check for multiple of 6, validate each chunk of 6 to see if it exists in braille set
+TODO: if the input includes '.', it is braille. "English" specification does not include '.' so can safely
+assume all input containing '.' is valid braille input
 '''
 def is_braille(string):
     if len(string) % 6 != 0:
         return False
-    chunks = (string[0+i : 6 + i] for i in range(0, len(string), 6))
+    chunks = chunk_string(string)
     for chunk in chunks:
         # 6 character chunk does not exist in the braille set
         if not (chunk in letter_braille_map.values()):
