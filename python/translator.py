ffc088819caf8b7960114131a5af02097ac56c05
Peloboi <vincent.peloquin@hotmail.com>
2024-09-16 14:25:55 -0400

    Separated dictionaries from the main file.

--- python/translator.py
+++ python/translator.py
@@ -1,4 +1,5 @@
 import sys
+import translation_dictionaries as td
 
 ## Potential problems that should be addressed in a future iteration:
 
@@ -10,26 +11,6 @@ import sys
 
 # This script translates between English text and Braille.
 # It includes functions to convert Braille to English and English to Braille.
-
-english_to_braille_main_dict = {
-    'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..', 'f': 'OOO...',
-    'g': 'OOOO..', 'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..', 'k': 'O...O.', 'l': 'O.O.O.',
-    'm': 'OO..O.', 'n': 'OO.OO.', 'o': 'O..OO.', 'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.',
-    's': '.OO.O.', 't': '.OOOO.', 'u': 'O..O.O', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO',
-    'y': 'OO.OOO', 'z': 'O..OOO', ' ': '......', 'capital': '.....O', 'number': '.O.OOO'
-}
-
-# Separate dictionary because of the redundancy with letters.
-english_to_braille_numbers_dict = {
-    '0': '.OOO..','1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..', '5': 'O..O..', '6': 'OOO...',
-    '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...'
-}
-
-# Create dicts specifically for the reverse translation. Takes a bit more memory, but keeps searches O(1).  
-braille_to_english_main_dict = {v: k for k, v in english_to_braille_main_dict.items()}
-braille_to_english_numbers_dict = {v: k for k, v in english_to_braille_numbers_dict.items()}
-
-braille_alphabet = ['O','.']
 
 def translator(*args):
     input = ' '.join(args)
@@ -58,7 +39,7 @@ def braille_to_english(braille):
             english_translation.append(' ')
             previous_was_number = False
 
-        elif braille[i:i+6] == english_to_braille_main_dict['number']:
+        elif braille[i:i+6] == td.english_to_braille_main_dict['number']:
             i += 6
             english_translation.append(braille_to_number(braille[i:i+6]))
             previous_was_number = True
@@ -66,7 +47,7 @@ def braille_to_english(braille):
         elif previous_was_number:
             english_translation.append(braille_to_number(braille[i:i+6]))
 
-        elif braille[i:i+6] == english_to_braille_main_dict['capital']:
+        elif braille[i:i+6] == td.english_to_braille_main_dict['capital']:
             i += 6
             english_translation.append(braille_to_letter(braille[i:i+6]).upper())
             previous_was_number = False
@@ -83,10 +64,10 @@ def braille_to_english(braille):
     return ''.join(english_translation)
 
 def braille_to_number(braille_segment):
-    return braille_to_english_numbers_dict.get(braille_segment, '')
+    return td.braille_to_english_numbers_dict.get(braille_segment, '')
 
 def braille_to_letter(braille_segment):
-    return braille_to_english_main_dict.get(braille_segment, '')
+        return td.braille_to_english_main_dict.get(braille_segment, '')
 
 def english_to_braille(english):
     braille_translation = []
@@ -96,19 +77,19 @@ def english_to_braille(english):
 
         if char.isdigit():
             if not (previous_char_was == "number"):
-                braille_translation.append(english_to_braille_main_dict['number'])
-            braille_translation.append(english_to_braille_numbers_dict[char])
+                braille_translation.append(td.english_to_braille_main_dict['number'])
+            braille_translation.append(td.english_to_braille_numbers_dict[char])
             previous_char_was = "number"
 
         elif char.isalpha():
             if previous_char_was == "number":
                 raise Exception("Invalid input. Cannot have a letter directly after a number under current rules.")
             if char.isupper():
-                braille_translation.append(english_to_braille_main_dict['capital'])
-                braille_translation.append(english_to_braille_main_dict[char.lower()])
+                braille_translation.append(td.english_to_braille_main_dict['capital'])
+                braille_translation.append(td.english_to_braille_main_dict[char.lower()])
 
             else:
-                braille_translation.append(english_to_braille_main_dict[char])
+                braille_translation.append(td.english_to_braille_main_dict[char])
             
             previous_char_was = "letter"
 
@@ -121,8 +102,6 @@ def english_to_braille(english):
 
 
     return ''.join(braille_translation)
-
-
 
 if __name__ == "__main__":
     translator(*sys.argv[1:])
