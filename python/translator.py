889d8bdcbf2915c65673c0b7156719d8f189d4d7
PrincessLiz16 <LizWong616@gmail.com>
2024-09-16 14:27:38 -0400

    challenge setup

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,23 @@
+import sys
 
+def isBraille(line):
+    for n in range(len(line)):
+        if line[n] != "0" and line[n] != ".":
+            return False
+    return True
+
+
+translation = {"CAPITAL": ".....O",
+               "DECIMAL": ".0...0",
+               "SPACE": "......",
+               "a": "aa"
+}
+
+translationNumbers = {}
+
+# braile is in segments of 6, so any braille code would be in segments of 6
+# unsure if the sys.argv = 2 is just bc of how im testing my logic here
+if (len(sys.argv[1]) % 6) == 0 and isBraille(sys.argv[1]):
+
+
+    # print(".....OO.....O.O...OO...........O.OOOO.....O.O...OO..........OO..OO.....OOO.OOOO..OOO")
