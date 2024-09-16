c352f3590b590605e4526469e1cd4aef5654a8f6
Kevan Hollbach <khollbach@gmail.com>
2024-09-16 09:53:48 -0400

    Add Python wrapper script to call Rust code

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,12 @@
+import os
+import subprocess
+import sys
 
+if __name__ == '__main__':
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    executable = f'{script_dir}/../rust/target/release/rust'
+
+    command = [executable]
+    command += sys.argv[1:]
+
+    subprocess.run(command)
