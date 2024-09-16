04a43a356114093da368a6a5b56a698bb4fb8e64
ExoPrisma <SMiHuang02084@gmail.com>
2024-09-15 23:07:24 -0400

    Implementation of Braille to English along with some tests

--- javascript/translator.js
+++ javascript/translator.js
@@ -44,13 +44,13 @@ function englishToBraille(text) {
     const lowerChar = char.toLowerCase();
 
     if(lowerChar in ENGLISH_LETTER_TO_BRAILLE | lowerChar in NUMBER_TO_BRAILLE) {
-      // Close number follow 
+      // Close `number follow` 
       if(isNum & !isNumber(char)) {
         result += ".O.OOO";
         isNum = false;
       }
 
-      // Handle number follow
+      // Handle `number follow`
       if(isNumber(char)) {
         if(!isNum) {
           result += ".O.OOO";
@@ -58,7 +58,7 @@ function englishToBraille(text) {
         isNum = true;
         result += NUMBER_TO_BRAILLE[char];
       }      
-      // Handle capital follow
+      // Handle `capital follow`
       else if(isCapitalized(char)) {
         result += ".....O";
         result += ENGLISH_LETTER_TO_BRAILLE[lowerChar];
@@ -69,14 +69,54 @@ function englishToBraille(text) {
     }
     else {
       console.log(`ERROR: ${char} is not an alphanumerical character!`);
-      return(`ERROR: ${char} is not an alphanumerical character!`);
+      return `ERROR: ${char} is not an alphanumerical character!`;
     }
   }
   console.log(result);
   return result;
 }
 
+function brailledToEnglish(text) {
+  const brailleChunk = text.match(/.{1,6}/g);
+  let result = "";
+  let isNum = false;
+  let isCapital = false;
 
-console.log(englishToBraille("Hello world") === ".....OO.OO..O..O..O.O.O.O.O.O.O..OO........OOO.OO..OO.O.OOO.O.O.O.OO.O..");
-console.log(englishToBraille("42") === ".O.OOOOO.O..O.O...");
-console.log(englishToBraille("Abc 123") === ".....OO.....O.O...OO...........O.OOOO.....O.O...OO....");
+  for(const chunk of brailleChunk) {
+    // Handle `capitale follow`
+    if(chunk === ".....O") {
+      isCapital = true;
+      continue;
+    }
+    // Handle `numbers follow`
+    else if(chunk === ".O.OOO") {
+      isNum = !isNum;
+      continue;
+    }
+    else {
+      if(!chunk in BRAILLE_TO_ENGLISH_LETTER | !chunk in BRAILLE_TO_NUMBER){
+        console.log(`ERROR: ${chunk} is not a valid Braille character`);
+        return `ERROR: ${chunk} is not a valid Braille character`
+      }
+      else{
+        if(isCapital) {
+          result += BRAILLE_TO_ENGLISH_LETTER[chunk].toUpperCase();
+          isCapital = false;
+        }
+        else if(isNum) {
+          result += BRAILLE_TO_NUMBER[chunk];
+        }
+        else {
+          result += BRAILLE_TO_ENGLISH_LETTER[chunk];
+        }
+      }    
+    }
+  }
+
+  console.log(result);
+  return result;
+}
+
+console.log(brailledToEnglish(".....OO.OO..O..O..O.O.O.O.O.O.O..OO........OOO.OO..OO.O.OOO.O.O.O.OO.O..") === "Hello world");
+console.log(brailledToEnglish(".O.OOOOO.O..O.O...") === "42");
+console.log(brailledToEnglish(".....OO.....O.O...OO...........O.OOOO.....O.O...OO....") === "Abc 123");
