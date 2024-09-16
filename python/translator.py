415d8182d1b40f1ed5335ded7209bedcb6389323
Sanmiareoye <sanmiareoye1@gmail.com>
2024-09-16 15:33:23 +0100

    added some comments and changed variabled for easier understanding

--- python/translator.py
+++ python/translator.py
@@ -1,7 +1,7 @@
 
 import sys
 
-alphabet = {
+english_to_braille = {
     'a': 'O.....',
     'b': 'O.O...',
     'c': 'OO....',
@@ -54,7 +54,7 @@ alphabet = {
     ' ': '......'
 }
 
-braille_alphabet = {
+braille_to_english = {
     'O.....': 'a',
     'O.O...': 'b',
     'OO....': 'c',
@@ -96,7 +96,7 @@ braille_alphabet = {
     'O.O..O': '(',
     '......': ' '
 }
-braille_alphabet_numbers = {
+braille_to_englsh_numbers = {
     'O.....': '1',
     'O.O...': '2', 
     'OO....': '3',
@@ -108,68 +108,81 @@ braille_alphabet_numbers = {
     '.OO...': '9', 
     '.OOO..': '0'}
 
-def braille_translator(braille):
+def braille_to_english_translator(braille):
+  # segments is used to store the 6-charachter braille segments.
   segments = []
-  string = ''
+  english = ''
+
+  # Loop to break braille into 6-charachter segments.
   for i in range(0, len(braille), 6):
     segment = braille[i:i + 6]
     segments.append(segment)
   
   index = 0
-  numbermode = False
+  # Number mode is used to track if numbers are being translated.
+  number_mode = False
+
+  # Loop through segments to translate each one.
   while index < len(segments):
-    if braille_alphabet[segments[index]] == 'cf':
+
+    # To handle capiital letters.
+    if braille_to_english[segments[index]] == 'cf':
       if index + 1 < len(segments):
-          string += braille_alphabet[segments[index + 1]].upper()
+          english += braille_to_english[segments[index + 1]].upper()
           index += 2 
 
-    elif braille_alphabet[segments[index]] == 'nf':
-      numbermode = True
+    # To enter number mode.
+    elif braille_to_english[segments[index]] == 'nf':
+      number_mode = True
       index += 1
       
-    elif numbermode:
-      if braille_alphabet[segments[index]] == ' ':
+    # To exit number mode if a space is encountered and add charachters accordingly.
+    elif number_mode:
+      if braille_to_english[segments[index]] == ' ':
         numbermode = False
-        string += braille_alphabet[segments[index]]
+        english += braille_to_english[segments[index]]
       else:
-        string += braille_alphabet_numbers[segments[index]]
+        english += braille_to_english_numbers[segments[index]]
       index += 1
+    
+    # Normal translation.
     else:
-      string += braille_alphabet[segments[index]]
+      english += braille_to_english[segments[index]]
       index += 1
 
-  return string
+  return english
 
-def string_translator(string):
+def english_to_braille_translator(english):
   braille = ''
-  first_number = False
-  for char in string:
+  number_mode = False
+  for char in english:
 
     if char.isupper():
-      braille += alphabet['cf']
-      braille += alphabet[char.lower()]
+      braille += english_to_braille['cf']
+      braille += english_to_braille[char.lower()]
 
     elif char.isdigit():
-      if not first_number:
-        braille += alphabet['nf']
-        first_number = True
-      braille += alphabet[char]
+      if not number_mode:
+        braille += english_to_braille['nf']
+        number_mode = True
+      braille += english_to_braille[char]
 
     elif char == ' ':
-      braille += alphabet[' ']
-      first_number = False
+      braille += english_to_braille[' ']
+      number_mode = False
 
     else:
-      braille += alphabet[char]
+      braille += english_to_braille[char]
   
   return braille
     
 def translator(argument):
     braille_char = ['.', 'O']
+    # To check whether the input(argument) is braille or english and run functions accordingly.
     if all(char in braille_char for char in argument) and len(argument) >= 6:
-        return braille_translator(argument) 
+        return braille_to_english_translator(argument) 
     else:
-        return string_translator(argument)
+        return english_to_braille_translator(argument)
     
 if __name__ == '__main__':
   argument = ' '.join(sys.argv[1:])
