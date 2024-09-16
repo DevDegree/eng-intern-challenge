f0c87c9b692604d4fcc2f37e294042cc8be41fd4
Kent Leng <Luxaus@users.noreply.github.com>
2024-09-16 11:36:37 -0400

    Checks if arguments are in braille or not

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,19 @@
+import sys
+import re
 
+def is_braille(word):
+    """ Checks if word is in Braille
+
+    Valid braille words only contain O or . and are a length multiple of 6
+    Keyword arguments:
+    word - the string to check
+    """
+    
+    return (bool(re.match('^[O\.]+$', word)) and len(word) % 6 == 0)
+
+def braille_translator():
+    return ""
+
+
+if __name__ == "__main__":
+    is_braille(sys.argv[1])
