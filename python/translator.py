a2d1947919034cee762ae65089a7bcd646552747
Asma <asma71612@gmail.com>
2024-09-17 19:33:17 -0400

    Initiating braille letters and numbers mapping

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,55 @@
+import sys
 
+# Braille codes for letters
+braille_letters = {
+    'a': 'O.....',
+    'b': 'O.O...',
+    'c': 'OO....',
+    'd': 'OO.O..',
+    'e': 'O..O..',
+    'f': 'OOO...',
+    'g': 'OOOO..',
+    'h': 'O.OO..',
+    'i': '.OO...',
+    'j': '.OOO..',
+    'k': 'O...O.',
+    'l': 'O.O.O.',
+    'm': 'OO..O.',
+    'n': 'OO.OO.',
+    'o': 'O..OO.',
+    'p': 'OOO.O.',
+    'q': 'OOOOO.',
+    'r': 'O.OOO.',
+    's': '.OO.O.',
+    't': '.OOOO.',
+    'u': 'O...OO',
+    'v': 'O.O.OO',
+    'w': '.OOO.O',
+    'x': 'OO..OO',
+    'y': 'OO.OOO',
+    'z': 'O..OOO',
+}
+
+braille_nums = {
+    '1': 'O.....',  # a
+    '2': 'O.O...',  # b
+    '3': 'OO....',  # c
+    '4': 'OO.O..',  # d
+    '5': 'O..O..',  # e
+    '6': 'OOO...',  # f
+    '7': 'OOOO..',  # g
+    '8': 'O.OO..',  # h
+    '9': '.OO...',  # i
+    '0': '.OOO..',  # j
+}
+
+# Braille special symbols
+capital_sign = '.....O'
+number_sign = '.O.OOO'
+space_sign = '......'
+
+def main():
+    print("main")
+
+if __name__ == '__main__':
+    main()
