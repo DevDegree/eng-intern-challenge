a510ae004455583a18b19be38839cbc4670dbacc
Xsylum <fleuryxander@gmail.com>
2024-09-17 14:28:41 -0400

    Complete Braille-to-English translation

--- python/translator.py
+++ python/translator.py
@@ -1,3 +1,29 @@
+brailleToNumber = {
+	"O....." : "1",
+	"O.O..." : "2",
+	"OO...." : "3",
+	"OO.O.." : "4",
+	"O..O.." : "5",
+	"OOO..." : "6",
+	"OOOO.." : "7",
+	"O.OO.." : "8",
+	".OO..." : "9",
+	".OOO.." : "0",
+}
+
+numberToBraille = {
+	"1" : "O.....",
+	"2" : "O.O...",
+	"3" : "OO....",
+	"4" : "OO.O..",
+	"5" : "O..O..",
+	"6" : "OOO...",
+	"7" : "OOOO..",
+	"8" : "O.OO..",
+	"9" : ".OO...",
+	"0" : ".OOO..",
+}
+
 brailleToChar = {
 	"O....." : "a",
 	"O.O..." : "b",
@@ -25,17 +51,6 @@ brailleToChar = {
 	"OO..OO" : "x",
 	"OO.OOO" : "y",
 	"O..OOO" : "z",
-
-	"O....." : "1",
-	"O.O..." : "2",
-	"OO...." : "3",
-	"OO.O.." : "4",
-	"O..O.." : "5",
-	"OOO..." : "6",
-	"OOOO.." : "7",
-	"O.OO.." : "8",
-	".OO..." : "9",
-	".OOO.." : "0",
 
 	".....O" : "capital",
 	".O...O" : "decimal",
@@ -84,17 +99,6 @@ charToBraille = {
 	"y" : "OO.OOO",
 	"z" : "O..OOO",
 
-	"1" : "O.....",
-	"2" : "O.O...",
-	"3" : "OO....",
-	"4" : "OO.O..",
-	"5" : "O..O..",
-	"6" : "OOO...",
-	"7" : "OOOO..",
-	"8" : "O.OO..",
-	"9" : ".OO...",
-	"0" : ".OOO..",
-
 	"capital" : ".....O",
 	"decimal" : ".O...O",
 	"number" : ".O.OOO",
@@ -118,6 +122,11 @@ def main(input):
 	print(input)
 	if isBraille(input):
 		brailleStrings = splitBraille(input)
+		return translateBrailleStringsToEnglish(brailleStrings)
+	else:
+		englishChars = list(input)
+		return(englishChars)
+			
 
 def isBraille(input):
 	for c in input:
@@ -135,7 +144,44 @@ def splitBrailleTail(input, output):
 	else:
 		output.append(input[0:6])
 		return splitBrailleTail(input[6:], output)
+	
+def translateBrailleStringsToEnglish(brailleStrings):
+	translatedSentence = ""
+	specialFollowingCharacter = 0 # 0 for nothing, 1 for capital, 2 for decimal, 3 for number
+	
+	for string in brailleStrings:
+		englishGlyph = brailleToChar[string]
+		# space will mark the end of a potential number
+		if (englishGlyph == " "):
+			specialFollowingCharacter = 0
 
-main(".....OO.....O.O...OO...........O.OOOO.....O.O...OO..........OO..OO.....OOO.OOOO..OOO")
+		# translating a capital
+		if (specialFollowingCharacter == 1):
+			englishGlyph = englishGlyph.capitalize()
+		# translating a number
+		elif (specialFollowingCharacter == 3):
+			numberGlyph = brailleToNumber[string]
+			translatedSentence += numberGlyph
+			continue
+			
+		# special characters which stipulate the translation of following character(s)
+		if (englishGlyph == "capital"):
+			specialFollowingCharacter = 1
+			continue
+		elif (englishGlyph == "decimal"):
+			specialFollowingCharacter = 2
+			continue
+		elif (englishGlyph == "number"):
+			specialFollowingCharacter = 3
+			continue
+	
+		translatedSentence += englishGlyph
+		specialFollowingCharacter = 0
+
+	return translatedSentence
+
+
+
+print(main(".....OO.....O.O...OO...........O.OOOO.....O.O...OO..........OO..OO.....OOO.OOOO..OOO"))
 
 
