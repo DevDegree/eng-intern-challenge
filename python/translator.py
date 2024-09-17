00954028cb9a4a4652069ba9a3823a2d9d342d50
Nolelle <eyu1@ualberta.ca>
2024-09-17 15:29:49 -0600

    add braille to eng mappings and number mappings, and add isbraille function

--- python/translator.py
+++ python/translator.py
@@ -1,5 +1,24 @@
 import sys
 
+# Braille mappings
+BRAILLE_TO_ENG = {
+    'O.....': 'a', 'O.O...': 'b', 'OO....': 'c', 'OO.O..': 'd', 'O..O..': 'e',
+    'OOO...': 'f', 'OOOO..': 'g', 'O.OO..': 'h', '.OO...': 'i', '.OOO..': 'j',
+    'O...O.': 'k', 'O.O.O.': 'l', 'OO..O.': 'm', 'OO.OO.': 'n', 'O..OO.': 'o',
+    'OOO.O.': 'p', 'OOOOO.': 'q', 'O.OOO.': 'r', '.OO.O.': 's', '.OOOO.': 't',
+    'O...OO': 'u', 'O.O.OO': 'v', '.OOO.O': 'w', 'OO..OO': 'x', 'OO.OOO': 'y',
+    'O..OOO': 'z', '......': ' ',
+    '.....O': 'capital', '.O.OOO': 'number'
+}
+
+# Number mappings
+NUMBER_MAP = {
+    'a': '1', 'b': '2', 'c': '3', 'd': '4', 'e': '5',
+    'f': '6', 'g': '7', 'h': '8', 'i': '9', 'j': '0'
+}
+
+
+
 
 if len(sys.argv) != 2:
     print("Usage: python translator.py <input-string>")
@@ -7,6 +26,16 @@ if len(sys.argv) != 2:
 
 
 input_string = sys.argv[1]
+
+
+def is_braille(s: str) -> bool:
+    valid_chars = {'O', ',', ' '}
+    return all(letter in valid_chars for letter in s)
+
+    
+
+
+
 
 
 
