8b986b5f76da310379c540a7f13b030cdbf38ab1
Gumesh Teklay <gteklay1@nait.ca>
2024-09-18 11:35:49 +0300

    Added English dictionary and Braille dictionary

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,41 @@
 
+# English dictionary
+english_dictionary  = { 
+    # Numbers
+    '0': '..O.OO', '1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..',
+    '5': 'O..O..', '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...',
+    # Lowercase letters
+    'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..',
+    'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..',
+    'k': 'O...O.', 'l': 'O.O.O.', 'm': 'OO..O.', 'n': 'OO.OO.', 'o': 'O..OO.',
+    'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.',
+    'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO', 'y': 'OO.OOO',
+    'z': 'O..OOO',
+    # Capitalization symbol
+    'capital': '.....O',
+    # Space in Braille
+    'space': '......',
+    # Number indicator in Braille
+    'number': '.O.OOO'
+}
+
+# Braille dictionary 
+braille_dictionary  = { 
+    # Numbers
+    '..O.OO': '0', 'O.....': '1', 'O.O...': '2', 'OO....': '3', 'OO.O..': '4',
+    'O..O..': '5', 'OOO...': '6', 'OOOO..': '7', 'O.OO..': '8', '.OO...': '9',
+    # Lowercase letters
+    'O.....': 'a', 'O.O...': 'b', 'OO....': 'c', 'OO.O..': 'd', 'O..O..': 'e',
+    'OOO...': 'f', 'OOOO..': 'g', 'O.OO..': 'h', '.OO...': 'i', '.OOO..': 'j',
+    'O...O.': 'k', 'O.O.O.': 'l', 'OO..O.': 'm', 'OO.OO.': 'n', 'O..OO.': 'o',
+    'OOO.O.': 'p', 'OOOOO.': 'q', 'O.OOO.': 'r', '.OO.O.': 's', '.OOOO.': 't',
+    'O...OO': 'u', 'O.O.OO': 'v', '.OOO.O': 'w', 'OO..OO': 'x', 'OO.OOO': 'y',
+    'O..OOO': 'z',
+    # Capitalization symbol
+     '.....O': 'capital',
+    # Space in Braille
+    '......': 'space' ,
+    # Number indicator in Braille
+    '.O.OOO': 'number'
+}
+
