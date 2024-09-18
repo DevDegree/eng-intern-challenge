cf858ee9177c9a2ba5502a490c3bf03006633331
sina-uOttawa <snega084@uOttawa.ca>
2024-09-18 21:11:17 +0000

    Add allowed characters set for Braille input

--- python/translator.py
+++ python/translator.py
@@ -4,6 +4,8 @@ ENGLISH_LETTERS = set('abcdefghijklmnopqrstuvwxyz')
 ENGLISH_NUMBERS = set('0123456789')
 ENGLISH_CHARACTERS = ENGLISH_LETTERS.union(ENGLISH_NUMBERS)
 
+BRAILLE_CHARACTERS = set('O.')
+
 def translate(text):
     # TODO: if English then translate to Braille
     # TODO: if Braille then translate to English
