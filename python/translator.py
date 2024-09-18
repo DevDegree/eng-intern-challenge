7d46ff341586d236ab782d585592ba5c6dfba76f
sina-uOttawa <snega084@uOttawa.ca>
2024-09-18 21:07:38 +0000

    Add translate function

--- python/translator.py
+++ python/translator.py
@@ -1,7 +1,13 @@
 import sys
 
+def translate(text):
+    # TODO: if English then translate to Braille
+    # TODO: if Braille then translate to English
+    pass
+
 if __name__ == "__main__":
     argvs = sys.argv
     text = argvs[1:]
 
     print(text)
+    translate(text)
