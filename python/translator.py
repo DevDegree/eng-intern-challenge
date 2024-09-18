e02fdae9caaf9a799b9b506e1894586c7fd6ef5d
Nolelle <eyu1@ualberta.ca>
2024-09-18 12:16:26 -0600

    refactor braille to english function, bug fix

--- python/translator.py
+++ python/translator.py
@@ -3,30 +3,60 @@ import sys
 #Mappings
 CAPITAL_PREFIX = '.....O'
 NUMBER_PREFIX = '.O.OOO'
-DECIMAL_PREFIX = '.O..OO'
 
 BRAILLE_TO_ENG = {
-    'O.....': 'a', 'O.O...': 'b', 'OO....': 'c', 'OO.O..': 'd', 'O..O..': 'e',
-    'OOO...': 'f', 'OOOO..': 'g', 'O.OO..': 'h', '.OO...': 'i', '.OOO..': 'j',
-    'O...O.': 'k', 'O.O.O.': 'l', 'OO..O.': 'm', 'OO.OO.': 'n', 'O..OO.': 'o',
-    'OOO.O.': 'p', 'OOOOO.': 'q', 'O.OOO.': 'r', '.OO.O.': 's', '.OOOO.': 't',
-    'O...OO': 'u', 'O.O.OO': 'v', '.OOO.O': 'w', 'OO..OO': 'x', 'OO.OOO': 'y',
-    'O..OOO': 'z', '......': ' '
+    'O.....': 'a',
+    'O.O...': 'b',
+    'OO....': 'c',
+    'OO.O..': 'd',
+    'O..O..': 'e',
+    'OOO...': 'f',
+    'OOOO..': 'g',
+    'O.OO..': 'h',
+    '.OO...': 'i',
+    '.OOO..': 'j',
+    'O...O.': 'k',
+    'O.O.O.': 'l',
+    'OO..O.': 'm',
+    'OO.OO.': 'n',
+    'O..OO.': 'o',
+    'OOO.O.': 'p',
+    'OOOOO.': 'q',
+    'O.OOO.': 'r',
+    '.OO.O.': 's',
+    '.OOOO.': 't',
+    'O...OO': 'u',
+    'O.O.OO': 'v',
+    '.OOO.O': 'w',
+    'OO..OO': 'x',
+    'OO.OOO': 'y',
+    'O..OOO': 'z',
+    '......': ' '
 }
 
+# Braille uses a-j to map numbers 1-0
 NUMBER_TO_BRAILLE = {
-    '0': 'OOOOO.', '1': 'O....O', '2': 'O.OOO.', '3': 'OOO...', '4': 'OOO.O.',
-    '5': 'O..OO.', '6': 'OOOO..', '7': 'OOOO.O', '8': 'OO.O..', '9': '.OOO..'
+    '1': 'O.....',  
+    '2': 'O.O...', 
+    '3': 'OO....',  
+    '4': 'OO.O..',  
+    '5': 'O..O..',  
+    '6': 'OOO...',  
+    '7': 'OOOO..',  
+    '8': 'O.OO..',  
+    '9': '.OO...',  
+    '0': '.OOO..'   
 }
 
 ENG_TO_BRAILLE = {value: key for key, value in BRAILLE_TO_ENG.items()}
 BRAILLE_TO_NUMBER = {value: key for key, value in NUMBER_TO_BRAILLE.items()}
 
 
+
 # Functions
-def is_braille(s: str) -> bool:
-    valid_chars = {'O', ',', ' '}
-    return all(letter in valid_chars for letter in s)
+def is_braille(string: str) -> bool:
+    valid_chars = {'O', '.', ' '}
+    return all(letter in valid_chars for letter in string)
 
 def english_to_braille(english_text: str)->str :
     braille_output =''
@@ -46,10 +76,7 @@ def english_to_braille(english_text: str)->str :
         else:
             if is_number_mode:
                 is_number_mode = False
-            if char == ' ':
-                braille_output += ' '  
-            else:
-                braille_output  += ENG_TO_BRAILLE.get[char, '']
+            braille_output  += ENG_TO_BRAILLE.get(char, '')
         
     return braille_output 
 
@@ -59,40 +86,41 @@ def braille_to_english(braille_text: str) -> str:
     i = 0
     length = len(braille_text)
     is_number_mode = False
+    is_capital = False
 
     
     while i < length:
-        # handle space
-        if braille_text[i] == ' ':
-            english_output += ' '
-            i += 1
-            continue
+        braille_char = braille_text[i:i+6]
 
-        braille_char = braille_text[i: i + 6]
-
-        # handle capitals
         if braille_char == CAPITAL_PREFIX:
-            i+= 6
-            braille_char = braille_text[i: i + 6]
-            english_char = braille_to_english.get(braille_char,'')
-            english_output += english_char.upper()
+            is_capital = True 
+            i += 6
+            continue
         elif braille_char == NUMBER_PREFIX:
             is_number_mode = True
             i += 6
+            continue
+        elif braille_char == '......':
+            english_output += ' '
+            is_number_mode = False
+            is_capital = False
         else:
             if is_number_mode:
-                char = BRAILLE_TO_ENG.get(braille_char,'')
-
-                is_number_mode = False
+                char = BRAILLE_TO_NUMBER.get(braille_char,'')
             else:
                 char = BRAILLE_TO_ENG.get(braille_char,'')
+            
+            if is_capital:
+                char = char.upper()
+                is_capital = False
             english_output += char
+
         i += 6
+    
     return english_output
-
-
-
-
+    
+            
+            
 
 
 if __name__ == '__main__':
@@ -111,18 +139,6 @@ if __name__ == '__main__':
     print(result)
 
 
-
-
-                
-            
-
-
-
-
-
-
-
-            
     
         
 
