226a1dced259e5bbb4bd762f999c0d831535f749
cpt.waffle <vasily.klimkin@gmail.com>
2024-09-19 17:47:51 -0400

    ✅ implement isBraille to pass tests

--- javascript/isBraille/index.js
+++ javascript/isBraille/index.js
@@ -1,6 +1,13 @@
 
+// Function that checks if a string passed, is a braille string or a regular one.
 const isBraille = string => {
+  // if string is not divisble by 6, then it cannot be a true brailie string.
+  if (string.length % 6 > 0) return false;
+  
+  // if string does not contain combinations of . symbol and O symbol, then its not a brailie.
+  if (!(/^[.O]+$/.test(string))) return false;
 
+  return true;
 }
 
 module.exports = isBraille;
