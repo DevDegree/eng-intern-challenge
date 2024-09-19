c577aeda8ed45410c0c2130227172f7634c61729
yashdave727 <yashdave727@gmail.com>
2024-09-19 02:30:33 -0400

    Implemented English to Braille Translator

--- python/translator.py
+++ python/translator.py
@@ -1,5 +1,5 @@
 import sys
-from typing import Dict
+import re
 
 
 class Translator:
@@ -9,6 +9,14 @@ class Translator:
         self.decimal_follows = ".O...O"
         self.numbers_follows = ".O.OOO"
         letters = "qwertyuiopasdfghjklzxcvbnm"
+
+        self.letter_to_braille_offset = {
+            'a': 0x01, 'b': 0x03, 'c': 0x09, 'd': 0x19, 'e': 0x11,
+            'f': 0x0B, 'g': 0x1B, 'h': 0x13, 'i': 0x0A, 'j': 0x1A,
+            'k': 0x05, 'l': 0x07, 'm': 0x0D, 'n': 0x1D, 'o': 0x15,
+            'p': 0x0F, 'q': 0x1F, 'r': 0x17, 's': 0x0E, 't': 0x1E,
+            'u': 0x25, 'v': 0x27, 'w': 0x3A, 'x': 0x2D, 'y': 0x3D, 'z': 0x35
+        }
 
         self.english_to_braille_mapping = {
             "capital": self.capital_follows,
@@ -39,17 +47,18 @@ class Translator:
 
     def _english_mapping(self, letters: str) -> None:
         # Starting braille code representing "a" in braille
-        base_braille_code = 0x2801
+        base_braille_code = 0x2800
 
         for letter in letters:
             # Convert the letter into a braille letter
-            offset = ord(letter) - ord("a")
-            braille_letter = chr(base_braille_code + offset)
+            offset = ord(letter) - ord("a") + 1
+            braille_letter = self._letter_to_braille(letter)
 
             # Convert the braille letter into a binary string representation
             unicode_code_point = ord(braille_letter)
-            dot_pattern = unicode_code_point - base_braille_code - 1
-            binary_representation = f"{dot_pattern:06b}"
+            dot_pattern = unicode_code_point - base_braille_code
+            block = f"{dot_pattern:06b}"
+            binary_representation = block[:4:3] + block[1:5:3] + block[2:6:3]
 
             # Convert binary string representation to desired ./O representation
             binary_representation = binary_representation.replace("1", "O")
@@ -64,12 +73,17 @@ class Translator:
 
             # Letters a to i and numbers 1 to 9 are the same in braille
             if "a" <= letter <= "i":
-                number = f"{offset + 1}"
+                number = f"{offset}"
                 self.english_to_braille_mapping[number] = string_representation
 
             elif letter == "j":
                 number = f"{0}"
                 self.english_to_braille_mapping[number] = string_representation
+
+    def _letter_to_braille(self, character: str) -> str:
+
+        braille_base = 0x2800  # Starting point for Braille Unicode characters
+        return chr(braille_base + self.letter_to_braille_offset[character])
 
     def english_to_braille(self, english_text: str) -> str:
         """Converts given string in English english_text and returns
@@ -78,7 +92,28 @@ class Translator:
         :param english_text:
         :return:
         """
-        pass
+        braille_string = ""
+        numbers = False
+        for character in english_text:
+            if character.isnumeric() and not numbers:
+                print("character.isnumeric() and not numbers")
+                braille_string += self.numbers_follows
+                numbers = True
+
+            elif character == "." and numbers:
+                print("character == \".\" and numbers")
+                braille_string += self.decimal_follows
+
+            elif not character.isnumeric() and numbers:
+                print("not character.isnumeric() and numbers")
+                braille_string += self.english_to_braille_mapping[character]
+                numbers = False
+
+            else:
+                print("else")
+                braille_string += self.english_to_braille_mapping[character]
+
+        return braille_string
 
     def braille_to_english(self, braille_text: str) -> str:
         """Converts given string in Braille braille_text and returns
@@ -102,14 +137,16 @@ if __name__ == "__main__":
     raised_count = string_to_translate.count("O")
     lowered_count = string_to_translate.count(".")
 
+    # Conditions for text to be braille
     is_braille_text = raised_count + lowered_count == len(string_to_translate)
     length_divides_six = len(string_to_translate) % 6 == 0
 
+    translator = Translator()
+
+    # Assumption is text will be english
     if is_braille_text and length_divides_six:
         current_language = "BRAILLE"
     else:
-        current_language = "ENGLISH"
-
-    translator = Translator()
-    print(translator.english_to_braille_mapping)
+        translated_string = translator.english_to_braille(string_to_translate)
+        print(translated_string)
 
