bc7b439d104c8b8d2c1aa415b3155b3ffe448470
Divya <divya.vithi@gmail.com>
2024-09-18 21:24:31 -0400

    took care of edge cases, added comments

--- python/translator.py
+++ python/translator.py
@@ -1,48 +1,53 @@
+# SHOPIFY ENG INTERN CHALLENGE
+# Divya Vithiyatharan
+
+# follows flags
 CAPITAL_FOLLOWS_FLAG = '.....O'
 DECIMAL_FOLLOWS_FLAG = '.O...O'
 NUMBER_FOLLOWS_FLAG = '.O.OOO'
 
+# english to braille dicts
 LETTER_BRAILLE_DICT = {
     'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..',
     'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..',
     'k': 'O...O.', 'l': 'O.O.O.', 'm': 'OO..O.', 'n': 'OO.OO.', 'o': 'O..OO.',
     'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.',
-    'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO', 'y': 'OO.OOO',
+    'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOOO.', 'x': 'OO..OO', 'y': 'OO.OOO',
     'z': 'O..OOO'
 }
-letter_key_list = list(LETTER_BRAILLE_DICT.keys())
-letter_value_list = list(LETTER_BRAILLE_DICT.values())
-
 NUMBER_BRAILLE_DICT = {
     '1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..', '5': 'O..O..',
     '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...', '0': '.OOO..'
 }
-number_key_list = list(NUMBER_BRAILLE_DICT.keys())
-number_value_list = list(NUMBER_BRAILLE_DICT.values())
-
 PUNCTUATION_BRAILLE_DICT = {
     '.': '..OO.O',
     ',': '..O...',
     '?': '..O.OO',
-    '!': '..OO.O',
+    '!': '..OOO.',
     ':': '..OO..',
     ';': '..O.O.',
-    '-': '....O.',
-    '/': '..O..O',
-    '>': '.OO..O',
-    '<': '.O..OO',
-    '(': '.O.O.O',
-    ')': '.O.OOO',
+    '-': '....OO',
+    '/': '.O..O.',
+    '<': '.OO..O',
+    '>': 'O..OO.',
+    '(': 'O.O..O',
+    ')': '.O.OO.',
     ' ': '......'
 }
-punct_key_list = list(PUNCTUATION_BRAILLE_DICT.keys())
-punct_value_list = list(PUNCTUATION_BRAILLE_DICT.values())
+
+#lists of keys and values for each dict for braille_to_eng() to get key using value
+LETTER_KEY_LIST = list(LETTER_BRAILLE_DICT.keys())
+letter_VALUE_LIST = list(LETTER_BRAILLE_DICT.values())
+NUMBER_KEY_LIST = list(NUMBER_BRAILLE_DICT.keys())
+NUMBER_VALUE_LIST = list(NUMBER_BRAILLE_DICT.values())
+PUNCT_KEY_LIST = list(PUNCTUATION_BRAILLE_DICT.keys())
+PUNCT_VALUE_LIST = list(PUNCTUATION_BRAILLE_DICT.values())
 
 def braille_to_eng(braille_input_str: str) -> str:
     split_braille_str = []
     eng_output = ''
-    captial_flag = False
-    number_flag = False
+    captial_flag = False # tracks whether following char is a capitalized letter
+    number_flag = False # tracks whether the current char are/should be numbers
 
     split_braille_str = [braille_input_str[i:i+6] for i in range(0, len(braille_input_str), 6)]
 
@@ -52,26 +57,30 @@ def braille_to_eng(braille_input_str: str) -> str:
             continue
         elif char == CAPITAL_FOLLOWS_FLAG:
             captial_flag = True
-            number_flag = False
+            number_flag = False # disable number if capital is encountered
             continue 
 
         if char in NUMBER_BRAILLE_DICT.values() and number_flag:
-            number = number_key_list[number_value_list.index(char)]
+            number = NUMBER_KEY_LIST[NUMBER_VALUE_LIST.index(char)]
             eng_output += number
         
         elif char in LETTER_BRAILLE_DICT.values():
-            letter = letter_key_list[letter_value_list.index(char)]
+            letter = LETTER_KEY_LIST[letter_VALUE_LIST.index(char)]
+            # assume only the next symbol should be capitalized
             if captial_flag:
                 letter = letter.upper()
                 captial_flag = False
             eng_output += letter
         
         elif char in PUNCTUATION_BRAILLE_DICT.values():
-            punct = punct_key_list[punct_value_list.index(char)]
+            punct = PUNCT_KEY_LIST[PUNCT_VALUE_LIST.index(char)]
+            # assume all following symbols are numbers until the next space symbol
+            if number_flag and punct == ' ':
+                number_flag = False
             eng_output += punct
 
         else:
-            return "error: input contains invalid character(s)"
+            return "error: input contains invalid character(s) " + char
         
     return eng_output
 
@@ -81,18 +90,18 @@ def eng_to_braille(eng_input_str: str) -> str:
     for char in eng_input_str:
         if char in NUMBER_BRAILLE_DICT:
             if char.isupper():
-                braille_output += CAPITAL_FOLLOWS_FLAG
+                braille_output += CAPITAL_FOLLOWS_FLAG # add capital flag before capitalized letter
             braille_output += LETTER_BRAILLE_DICT.get(char.lower())
 
         elif char in LETTER_BRAILLE_DICT:
-            braille_output += NUMBER_FOLLOWS_FLAG
+            braille_output += NUMBER_FOLLOWS_FLAG # add number flag before numbers
             braille_output += NUMBER_BRAILLE_DICT[char]
         
         elif char in PUNCTUATION_BRAILLE_DICT:
             braille_output += PUNCTUATION_BRAILLE_DICT[char]
         
         else:
-            return "error: input contains invalid character(s)"
+            return "error: input contains invalid character(s) " + char
     
     return braille_output
 
@@ -100,7 +109,7 @@ def main():
     input_str = input("enter value: ")
     if set(input_str).issubset({'O', '.'}) and len(input_str) % 6 == 0:
         print(braille_to_eng(input_str))
-    if not(set(input_str).issubset({'O', '.'}) and len(input_str) % 6 == 0):
+    else:
         print(eng_to_braille(input_str))
 
 if __name__ == '__main__':
