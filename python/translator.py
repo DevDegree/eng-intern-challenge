9abfbc6049b6a6f22d6210a7f99b15388608de7f
yashdave727 <yashdave727@gmail.com>
2024-09-18 23:49:32 -0400

    Parsed command-line arguments to form initial text and checked if text is Braille or English

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,23 @@
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        print("Missing Argument. Need Braille text or English text")
+
+    # Parse the command line arguments and store full text
+    system_arguments = sys.argv[1:]
+    string_to_translate = " ".join(system_arguments)
+
+    # Check if string is braille or english
+    raised_count = string_to_translate.count("O")
+    lowered_count = string_to_translate.count(".")
+
+    is_braille_text = raised_count + lowered_count == len(string_to_translate)
+    length_divides_six = len(string_to_translate) % 6 == 0
+
+    if is_braille_text and length_divides_six:
+        braille_to_english = True
+    else:
+        braille_to_english = False
 
