18edebcc439c11018ba91a23e741c2e068df6a59
Arnav <65367548+arnavgupta03@users.noreply.github.com>
2024-09-20 19:06:27 -0400

    add other direction and bug fixes

--- go/translator.go
+++ go/translator.go
@@ -9,7 +9,7 @@ import (
 var uppercaseMode bool
 var numberMode bool
 
-func allBraileChars(testString string) bool {
+func allBrailleChars(testString string) bool {
 	for _, c := range testString {
 		if c != 'O' && c != '.' {
 			return false
@@ -17,6 +17,16 @@ func allBraileChars(testString string) bool {
 	}
 
 	return true
+}
+
+func isNum(char string) bool {
+	return char == "1" || char == "2" || char == "3" || char == "4" ||
+		char == "5" || char == "6" || char == "7" || char == "8" ||
+		char == "9" || char == "0"
+}
+
+func isUpper(char string) bool {
+	return strings.ToUpper(char) == char
 }
 
 func brailleToEnglish(charSet string) string {
@@ -129,14 +139,94 @@ func brailleToEnglish(charSet string) string {
 	return c
 }
 
-func main() {
-	fmt.Println(len(os.Args), os.Args)
+func englishToBraille(char string) string {
+	var brailleSet strings.Builder
 
+	if isNum(char) && !numberMode {
+		brailleSet.WriteString(".O.OOO")
+
+		numberMode = true
+	} else if !isNum(char) && isUpper(char) {
+		brailleSet.WriteString(".....O")
+	}
+
+	switch char {
+	case "1", "a", "A":
+		brailleSet.WriteString("O.....")
+	case "2", "b", "B":
+		brailleSet.WriteString("O.O...")
+	case "3", "c", "C":
+		brailleSet.WriteString("OO....")
+	case "4", "d", "D":
+		brailleSet.WriteString("OO.O..")
+	case "5", "e", "E":
+		brailleSet.WriteString("O..O..")
+	case "6", "f", "F":
+		brailleSet.WriteString("OOO...")
+	case "7", "g", "G":
+		brailleSet.WriteString("OOOO..")
+	case "8", "h", "H":
+		brailleSet.WriteString("O.OO..")
+	case "9", "i", "I":
+		brailleSet.WriteString(".OO...")
+	case "10", "j", "J":
+		brailleSet.WriteString(".OOO..")
+	case "k", "K":
+		brailleSet.WriteString("O...O.")
+	case "l", "L":
+		brailleSet.WriteString("O.O.O.")
+	case "m", "M":
+		brailleSet.WriteString("OO..O.")
+	case "n", "N":
+		brailleSet.WriteString("OO.OO.")
+	case "o", "O":
+		brailleSet.WriteString("O..OO.")
+	case "p", "P":
+		brailleSet.WriteString("OOO.O.")
+	case "q", "Q":
+		brailleSet.WriteString("OOOOO.")
+	case "r", "R":
+		brailleSet.WriteString("O.OOO.")
+	case "s", "S":
+		brailleSet.WriteString(".OO.O.")
+	case "t", "T":
+		brailleSet.WriteString(".OOOO.")
+	case "u", "U":
+		brailleSet.WriteString("O...OO")
+	case "v", "V":
+		brailleSet.WriteString("O.O.OO")
+	case "w", "W":
+		brailleSet.WriteString(".OOO.O")
+	case "x", "X":
+		brailleSet.WriteString("OO..OO")
+	case "y", "Y":
+		brailleSet.WriteString("OO.OOO")
+	case "z", "Z":
+		brailleSet.WriteString("O..OOO")
+	case " ":
+		brailleSet.WriteString("......")
+		numberMode = false
+	}
+
+	return brailleSet.String()
+}
+
+func main() {
 	var translatedString strings.Builder
 
 	// check for English case (if spaces or non-Braille chars present, must be English)
-	if len(os.Args) > 2 || !allBraileChars(os.Args[1]) {
+	if len(os.Args) > 2 || !allBrailleChars(os.Args[1]) {
 		// parse English
+		for i, arg := range os.Args[1:] {
+			for _, c := range arg {
+				translatedString.WriteString(englishToBraille(string(c)))
+			}
+
+			if i < len(os.Args)-2 {
+				translatedString.WriteString("......")
+				numberMode = false
+			}
+		}
 	} else {
 		// parse Braile
 		var curBraille strings.Builder
@@ -150,4 +240,6 @@ func main() {
 			}
 		}
 	}
+
+	fmt.Println(translatedString.String())
 }
