29d4669ae976c597a26ffaf752c05beddde33784
Kieran Dunch <dunch.kieran@gmail.com>
2024-09-20 16:25:53 -0600

    basic style fixes using rubocop

--- ruby/translator.rb
+++ ruby/translator.rb
@@ -1,57 +1,58 @@
+# frozen_string_literal: true
 
 # create the hash of braille to english
 BRAILLE_HASH = {
-  "a" => 'O.....',
-  "b" => 'O.O...',
-  "c" => 'OO....',
-  "d" => 'OO.O..',
-  "e" => 'O..O..',
-  "f" => 'OOO...',
-  "g" => 'OOOO..',
-  "h" => 'O.OO..',
-  "i" => '.OO...',
-  "j" => '.OOO..',
-  "k" => 'O...O.',
-  "l" => 'O.O.O.',
-  "m" => 'OO..O.',
-  "n" => 'OO.OO.',
-  "o" => 'O..OO.',
-  "p" => 'OOO.O.',
-  "q" => 'OOOOO.',
-  "r" => 'O.OOO.',
-  "s" => '.OO.O.',
-  "t" => '.OOOO.',
-  "u" => 'O...OO',
-  "v" => 'O.O.OO',
-  "w" => '.OOO.O',
-  "x" => 'OO..OO',
-  "y" => 'OO.OOO',
-  "z" => 'O..OOO',
-  " " => '......',
-}
+  'a' => 'O.....',
+  'b' => 'O.O...',
+  'c' => 'OO....',
+  'd' => 'OO.O..',
+  'e' => 'O..O..',
+  'f' => 'OOO...',
+  'g' => 'OOOO..',
+  'h' => 'O.OO..',
+  'i' => '.OO...',
+  'j' => '.OOO..',
+  'k' => 'O...O.',
+  'l' => 'O.O.O.',
+  'm' => 'OO..O.',
+  'n' => 'OO.OO.',
+  'o' => 'O..OO.',
+  'p' => 'OOO.O.',
+  'q' => 'OOOOO.',
+  'r' => 'O.OOO.',
+  's' => '.OO.O.',
+  't' => '.OOOO.',
+  'u' => 'O...OO',
+  'v' => 'O.O.OO',
+  'w' => '.OOO.O',
+  'x' => 'OO..OO',
+  'y' => 'OO.OOO',
+  'z' => 'O..OOO',
+  ' ' => '......'
+}.freeze
 
 # create the hash of english to braille numbers
 BRAILLE_NUMBERS_HASH = {
-  "1" => 'O.....',
-  "2" => 'O.O...',
-  "3" => 'OO....',
-  "4" => 'OO.O..',
-  "5" => 'O..O..',
-  "6" => 'OOO...',
-  "7" => 'OOOO..',
-  "8" => 'O.OO..',
-  "9" => '.OO...',
-  "0" => '.OOO..',
-}
+  '1' => 'O.....',
+  '2' => 'O.O...',
+  '3' => 'OO....',
+  '4' => 'OO.O..',
+  '5' => 'O..O..',
+  '6' => 'OOO...',
+  '7' => 'OOOO..',
+  '8' => 'O.OO..',
+  '9' => '.OO...',
+  '0' => '.OOO..'
+}.freeze
 
 # create the hash of braille to english numbers
-ENGLISH_NUMBERS_HASH = BRAILLE_NUMBERS_HASH.invert
+ENGLISH_NUMBERS_HASH = BRAILLE_NUMBERS_HASH.invert.freeze
 # create the hash of english to braille
-ENGLISH_HASH = BRAILLE_HASH.invert
+ENGLISH_HASH = BRAILLE_HASH.invert.freeze
 
 # Create an evaluator to determine if english or braille
 def braille_checker?(input_string)
-  input_string.include?(".")
+  input_string.include?('.')
 end
 
 # If english, translate to braille
@@ -60,8 +61,7 @@ def english_translator(input_string)
   capital_follows = false
 
   braille_translation = input_string.chars.map do |character|
-
-    number_follows = false if character == " "
+    number_follows = false if character == ' '
 
     if character == character.upcase && character.match(/[a-zA-Z]/)
       capital_follows = true
@@ -84,7 +84,6 @@ def english_translator(input_string)
   braille_translation.join
 end
 
-
 # If braille, translate to english
 def braille_translator(input_string)
   # MAKE SURE TO REMEMBER to account for number follows and capital letter follows
@@ -93,17 +92,17 @@ def braille_translator(input_string)
   braille_characters = input_string.scan(/.{6}/)
 
   english_translation = braille_characters.map do |braille_character|
-    if braille_character == ".....O"
+    if braille_character == '.....O'
       capital_follows = true
       next
     end
 
-    if braille_character == ".O.OOO"
+    if braille_character == '.O.OOO'
       number_follows = true
       next
     end
 
-    number_follows = false if braille_character == "......"
+    number_follows = false if braille_character == '......'
 
     if capital_follows
       capital_follows = false
@@ -118,10 +117,9 @@ def braille_translator(input_string)
   english_translation.join
 end
 
-
 # Program must work at runtime
 def run(input_string)
   puts braille_checker?(input_string) ? braille_translator(input_string) : english_translator(input_string)
 end
 
-run(ARGV.join(" "))
+run(ARGV.join(' '))
