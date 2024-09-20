747cd9153bc4bb94a10fd5ff6992ef48e46c35a6
Kieran Dunch <dunch.kieran@gmail.com>
2024-09-20 14:55:30 -0600

    pseudocode the problem

---
+++ ruby/translator.rb
@@ -0,0 +1,24 @@
+
+# create the hash of braille to english
+# create the hash of english to braille
+
+# Create an evaluator to determine if english or braille
+
+# If english, translate to braille
+# If braille, translate to english
+
+# Output the translation
+
+# MAKE SURE TO REMEMBER to account for number follows and capital letter follows
+
+
+# Program must work at runtime, a single time or constantly?
+def run
+  puts "Hello!"
+
+  input = gets.chomp!
+
+  puts input
+end
+
+run
