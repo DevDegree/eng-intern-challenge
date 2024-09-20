1482de8c9bea88d45463e583cab388cd143e69cc
Kunal Bhatnagar <kunalbhatnagar@Kunals-MacBook-Air.local>
2024-09-20 16:48:42 -0400

    Implemented Braille to English translator in JavaScript

--- javascript/translator.js
+++ javascript/translator.js
@@ -1,31 +1,10 @@
 // Braille mappings (O for raised, . for unraised)
 const BRAILLE_ALPHABET = {
-    "a": "O.....",
-    "b": "O.O...",
-    "c": "OO....",
-    "d": "OO.O..",
-    "e": "O..O..",
-    "f": "OOO...",
-    "g": "OOOO..",
-    "h": "O.OO..",
-    "i": ".OO...",
-    "j": ".OOO..",
-    "k": "O...O.",
-    "l": "O.O.O.",
-    "m": "OO..O.",
-    "n": "OO.OO.",
-    "o": "O..OO.",
-    "p": "OOO.O.",
-    "q": "OOOOO.",
-    "r": "O.OOO.",
-    "s": ".OO.O.",
-    "t": ".OOOO.",
-    "u": "O...OO",
-    "v": "O.O.OO",
-    "w": ".OOO.O",
-    "x": "OO..OO",
-    "y": "OO.OOO",
-    "z": "O..OOO",
+    "a": "O.....", "b": "O.O...", "c": "OO....", "d": "OO.O..", "e": "O..O..", 
+    "f": "OOO...", "g": "OOOO..", "h": "O.OO..", "i": ".OO...", "j": ".OOO..",
+    "k": "O...O.", "l": "O.O.O.", "m": "OO..O.", "n": "OO.OO.", "o": "O..OO.",
+    "p": "OOO.O.", "q": "OOOOO.", "r": "O.OOO.", "s": ".OO.O.", "t": ".OOOO.",
+    "u": "O...OO", "v": "O.O.OO", "w": ".OOO.O", "x": "OO..OO", "y": "OO.OOO", "z": "O..OOO",
     " ": "......",  // Space character
     "1": "O.....", "2": "O.O...", "3": "OO....", "4": "OO.O..", "5": "O..O..", 
     "6": "OOO...", "7": "OOOO..", "8": "O.OO..", "9": ".OO...", "0": ".OOO.."
@@ -49,15 +28,26 @@ function detectInputType(input) {
 // Translate English to Braille
 function englishToBraille(text) {
     let result = '';
+    let isNumber = false;
+    
     for (const char of text) {
         if (/[A-Z]/.test(char)) {
             result += CAPITAL_PREFIX + BRAILLE_ALPHABET[char.toLowerCase()];
         } else if (/\d/.test(char)) {
-            result += NUMBER_PREFIX + BRAILLE_ALPHABET[char];
+            if (!isNumber) {
+                result += NUMBER_PREFIX;
+                isNumber = true;
+            }
+            result += BRAILLE_ALPHABET[char];
+        } else if (char === " ") {
+            result += BRAILLE_ALPHABET[char];
+            isNumber = false; // Reset number mode after a space
         } else {
             result += BRAILLE_ALPHABET[char] || '';
+            isNumber = false; // Reset number mode for non-numeric characters
         }
     }
+    
     return result;
 }
 
@@ -73,17 +63,24 @@ function brailleToEnglish(braille) {
         if (brailleChar === CAPITAL_PREFIX) {
             i += 6;
             const nextBrailleChar = braille.slice(i, i + 6);
-            result += ENGLISH_ALPHABET[nextBrailleChar].toUpperCase();
+            const capitalLetter = ENGLISH_ALPHABET[nextBrailleChar];
+            if (capitalLetter) {
+                result += capitalLetter.toUpperCase();
+            } else {
+                throw new Error("Invalid Braille sequence.");
+            }
         } else if (brailleChar === NUMBER_PREFIX) {
             isNumberMode = true;
             i += 6;
         } else {
             const englishChar = ENGLISH_ALPHABET[brailleChar];
-            if (isNumberMode) {
-                result += englishChar;
-                isNumberMode = false;
+            if (englishChar) {
+                result += isNumberMode && /\d/.test(englishChar) ? englishChar : englishChar;
             } else {
-                result += englishChar || '';
+                throw new Error("Invalid Braille sequence.");
+            }
+            if (isNumberMode && englishChar === " ") {
+                isNumberMode = false;
             }
         }
         i += 6;
