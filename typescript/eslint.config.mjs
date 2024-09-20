0bd8b1331e498fae02a683a891932c91c4a005f3
Samuel Gomez <samuelgomez4010@gmail.com>
2024-09-20 09:56:57 -0400

    Add additional config files

---
+++ typescript/eslint.config.mjs
@@ -0,0 +1,13 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import shopifyEslintPlugin from '@shopify/eslint-plugin';
+import tseslint from 'typescript-eslint';
+
+
+export default [
+  {files: ['**/*.{js,mjs,cjs,ts}']},
+  {languageOptions: {globals: globals.browser}},
+  pluginJs.configs.recommended,
+  ...shopifyEslintPlugin.configs.es5,
+  ...tseslint.configs.recommended,
+];
