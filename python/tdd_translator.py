9803000135fb9e3551a2c86a99a8d5bfb23f52c5
Evan Smedley <evan.smedley22@gmail.com>
2024-09-20 00:52:32 -0400

    Translator structure and tdd setup

---
+++ python/tdd_translator.py
@@ -0,0 +1,48 @@
+import unittest
+
+from translator import Braille_Translator
+
+class BrailleTest(unittest.TestCase):
+
+    def test_is_braille(self):
+        """
+        Test the 'is_braille' method.
+        """
+        braille = Braille_Translator()
+
+        self.fail()
+
+
+
+    def test_eng_to_braille(self):
+        """
+        Test the 'eng_to_braille' method.
+        """
+        braille = Braille_Translator()
+
+        self.fail()
+
+    
+
+    def test_braille_to_eng(self):
+        """
+        Test the 'braille_to_eng' method.
+        """
+        braille = Braille_Translator()
+
+        self.fail()
+
+
+    
+    def test_translate(self):
+        """
+        Test the 'translate' method.
+        """
+        braille = Braille_Translator()
+
+        self.fail()
+
+
+
+if __name__ == "__main__":
+    unittest.main()
--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,21 @@
 
+class Braille_Translator():
+
+    def __init__(self):
+        pass
+
+
+    def is_braille(self, input: str) -> bool:
+        pass
+
+
+    def eng_to_braille(self, english: str) -> str:
+        pass
+
+
+    def braille_to_eng(self, braille: str) -> str:
+        pass
+
+
+    def translate(self, input: str) -> str:
+        pass
