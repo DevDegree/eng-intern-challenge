045b54d101ce48f0e042c2d414ed48d7217d1e8e
anvu2002 <anvu6028@gmail.com>
2024-09-20 19:48:31 -0700

    initialize hashmap and taskmap

--- go/translator.go
+++ go/translator.go
@@ -13,3 +13,63 @@ type Translator struct {
 	numberMode  bool
 	capitalMode bool
 }
+
+// Enable bi-directional hashmap for English <--> Braille
+func CreateReverseMapping(mapping map[rune]string) map[string]rune {
+	reverseMapping := make(map[string]rune)
+	for k, v := range mapping {
+		reverseMapping[v] = k
+	}
+	return reverseMapping
+}
+
+// Initialize a new Translator with mappings
+func NewTranslator() *Translator {
+	AlphaE2B := map[rune]string{
+		'a': "O.....", 'b': "O.O...", 'c': "OO....", 'd': "OO.O..", 'e': "O..O..",
+		'f': "OOO...", 'g': "OOOO..", 'h': "O.OO..", 'i': ".OO...", 'j': ".OOO..",
+		'k': "O...O.", 'l': "O.O.O.", 'm': "OO..O.", 'n': "OO.OO.", 'o': "O..OO.",
+		'p': "OOO.O.", 'q': "OOOOO.", 'r': "O.OOO.", 's': ".OO.O.", 't': ".OOOO.",
+		'u': "O...OO", 'v': "O.O.OO", 'w': ".OOO.O", 'x': "OO..OO", 'y': "OO.OOO", 'z': "O..OOO",
+		' ': "......",
+	}
+
+	NumberE2B := map[rune]string{
+		'1': "O.....", '2': "O.O...", '3': "OO....", '4': "OO.O..", '5': "O..O..",
+		'6': "OOO...", '7': "OOOO..", '8': "O.OO..", '9': ".OO...", '0': ".OOO..",
+	}
+
+	PunctE2B := map[rune]string{
+		'.': "..OO.O", '?': "..O.OO", '!': "..OOO.", ':': "..OO..", ';': "..O.O.",
+		'-': "....OO", '/': ".O..O.", '(': "O.O..O", ')': ".O.OO.", ' ': "......",
+	}
+
+	Symbols := map[rune]string{
+		'C': ".....O", // Capital Follows
+		'N': ".O.OOO", // Number Follows
+		'S': "......", // Space or end symbol
+	}
+
+	t := &Translator{
+		AlphaE2B:    AlphaE2B,
+		NumberE2B:   NumberE2B,
+		PunctE2B:    PunctE2B,
+		Symbols:     Symbols,
+		AlphaB2E:    CreateReverseMapping(AlphaE2B),
+		NumberB2E:   CreateReverseMapping(NumberE2B),
+		PunctB2E:    CreateReverseMapping(PunctE2B),
+		numberMode:  false,
+		capitalMode: false,
+	}
+
+	// Initialize the task map with functions per scenarios
+	t.taskMap = map[string]func(rune) string{
+		"space":       t.handleSpace,
+		"number":      t.handleNumber,
+		"uppercase":   t.handleUppercase,
+		"lowercase":   t.handleLowercase,
+		"punctuation": t.handlePunctuation,
+	}
+
+	return t
+}
