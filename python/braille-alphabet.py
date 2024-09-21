5a70e0af04a1adc76534ea48a6bcbdda9b59b3e6
Alexis Sabourin <155196582+alexissabourin@users.noreply.github.com>
2024-09-20 22:30:49 -0400

    feat: add english and braille alphabets and their respective required mappings

---
+++ python/braille-alphabet.py
@@ -0,0 +1,63 @@
+braille_to_letters = {
+    "O.....": "a",
+    "O.O...": "b",
+    "OO....": "c",
+    "OO.O..": "d",
+    "O..O..": "e",
+    "OOO...": "f",
+    "OOOO..": "g",
+    "O.OO..": "h",
+    ".OO...": "i",
+    ".OOO..": "j",
+    "O...O.": "k",
+    "O.O.O.": "l",
+    "OO..O.": "m",
+    "OO.OO.": "n",
+    "O..OO.": "o",
+    "OOO.O.": "p",
+    "OOOOO.": "q",
+    "O.OOO.": "r",
+    ".OO.O.": "s",
+    ".OOOO.": "t",
+    "O...OO": "u",
+    "O.O.OO": "v",
+    ".OOO.O": "w",
+    "OO..OO": "x",
+    "OO.OOO": "y",
+    "O..OOO": "z",
+}
+
+braille_to_numbers = {
+    ".OOO..": 0,
+    "O.....": 1,
+    "O.O...": 2,
+    "OO....": 3,
+    "OO.O..": 4,
+    "O..O..": 5,
+    "OOO...": 6,
+    "OOOO..": 7,
+    "O.OO..": 8,
+    ".OO...": 9,
+}
+
+braille_to_special_characters = {
+    "..OO.O": ".",
+    "..O...": ",",
+    "..O.OO": "?",
+    "..OOO.": "!",
+    "..OO..": ":",
+    "..O.O.": ";",
+    "....OO": "-",
+    ".O..O.": "/",
+    ".OO..O": "<",
+    "O..OO.": ">",
+    "O.O..O": "(",
+    ".O.OO.": ")",
+    "......": " ",
+}
+
+braille_to_rules = {
+    ".....O": "capital_follows",
+    ".O...O": "number_follows",
+    ".O.OOO": "decimal_follows",
+}
---
+++ python/english-alphabet.py
@@ -0,0 +1,63 @@
+letters_to_braille = {
+    "a": "O.....",
+    "b": "O.O...",
+    "c": "OO....",
+    "d": "OO.O..",
+    "e": "O..O..",
+    "f": "OOO...",
+    "g": "OOOO..",
+    "h": "O.OO..",
+    "i": ".OO...",
+    "j": ".OOO..",
+    "k": "O...O.",
+    "l": "O.O.O.",
+    "m": "OO..O.",
+    "n": "OO.OO.",
+    "o": "O..OO.",
+    "p": "OOO.O.",
+    "q": "OOOOO.",
+    "r": "O.OOO.",
+    "s": ".OO.O.",
+    "t": ".OOOO.",
+    "u": "O...OO",
+    "v": "O.O.OO",
+    "w": ".OOO.O",
+    "x": "OO..OO",
+    "y": "OO.OOO",
+    "z": "O..OOO",
+}
+
+numbers_to_braille = {
+    0: ".OOO..",
+    1: "O.....",
+    2: "O.O...",
+    3: "OO....",
+    4: "OO.O..",
+    5: "O..O..",
+    6: "OOO...",
+    7: "OOOO..",
+    8: "O.OO..",
+    9: ".OO...",
+}
+
+special_characters_to_braille = {
+    ".": "..OO.O",
+    ",": "..O...",
+    "?": "..O.OO",
+    "!": "..OOO.",
+    ":": "..OO..",
+    ";": "..O.O.",
+    "-": "....OO",
+    "/": ".O..O.",
+    "<": ".OO..O",
+    ">": "O..OO.",
+    "(": "O.O..O",
+    ")": ".O.OO.",
+    " ": "......",
+}
+
+rules_to_braille = {
+    "capital_follows": ".....O",
+    "decimal_follows": ".O...O",
+    "number_follows": ".O.OOO",
+}
