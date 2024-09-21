ae265bad63fc384094263e644ac3a71f9f85e56d
anvu2002 <anvu6028@gmail.com>
2024-09-20 19:45:50 -0700

    create Translator struct

--- go/translator.go
+++ go/translator.go
@@ -1,1 +1,15 @@
 package main
+
+// Struct to represent translation state
+type Translator struct {
+	AlphaE2B    map[rune]string
+	NumberE2B   map[rune]string
+	PunctE2B    map[rune]string
+	AlphaB2E    map[string]rune
+	NumberB2E   map[string]rune
+	PunctB2E    map[string]rune
+	Symbols     map[rune]string
+	taskMap     map[string]func(rune) string // Task map for different scenarios
+	numberMode  bool
+	capitalMode bool
+}
