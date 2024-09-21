d79aed55364f6240e50cc480dcf0ab9a65f0355c
whr129 <howardwang0315@gmail.com>
2024-09-21 17:29:07 -0400

    fix: first commit

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,144 @@
+import sys
 
+English_dict = {
+    "a": "O.....",
+    "b": "O.O...",
+    "c": "OO....",
+    "d": "OO.O..",
+    "e": "O..O..",
+    "f": "OOO...",
+    "g": "OOOO..",
+    "h": "O.OO..",
+    "i": ".OO...",
+    "j": ".OOO..",
+    "k": "O...O.",
+    "l": "O.O.O.",
+    "m": "OO..O.",
+    "n": "OO.OO.",
+    "o": "O..OO.",
+    "p": "OOO.O.",
+    "q": "OOOOO.",
+    "r": "O.OOO.",
+    "s": ".OO.O.",
+    "t": ".OOOO.",
+    "u": "O...OO",
+    "v": "O.O.OO",
+    "w": ".OOO.O",
+    "x": "OO..OO",
+    "y": "OO.OOO",
+    "z": "O..OOO",
+    "capital": ".....O",
+    "decimal": ".O...O",
+    "number": ".O.OOO",
+}
+
+Braille_dict = {
+    "O.....": "a",
+    "O.O...": "b",
+    "OO....": "c",
+    "OO.O..": "d",
+    "O..O..": "e",
+    "OOO...": "f",
+    "OOOO..": "g",
+    "O.OO..": "h",
+    ".OO...": "i",
+    ".OOO..": "j",
+    "O...O.": "k",
+    "O.O.O.": "l",
+    "OO..O.": "m",
+    "OO.OO.": "n",
+    "O..OO.": "o",
+    "OOO.O.": "p",
+    "OOOOO.": "q",
+    "O.OOO.": "r",
+    ".OO.O.": "s",
+    ".OOOO.": "t",
+    "O...OO": "u",
+    "O.O.OO": "v",
+    ".OOO.O": "w",
+    "OO..OO": "x",
+    "OO.OOO": "y",
+    "O..OOO": "z",
+    ".....O": "capital",
+    "......": "space",
+    ".O.OOO": "number",
+}
+
+
+
+def is_content_English(content):
+    if '.' in content[1]:
+        return False
+    else:
+        return True
+
+
+def translate_to_Braille(English):
+    i = 0
+    ans = ""
+    content_len = len(English) - 1
+    for word in English:
+        is_number = False
+        if i == 0:
+            i += 1
+            continue
+        else:
+            i += 1
+            for letter in word:
+                if ord(letter) >= ord('0') and ord(letter) <= ord('9') and not is_number:
+                    is_number = True
+                    ans += English_dict["number"]
+                if not is_number:
+                    if letter.isupper():
+                        ans += English_dict['capital']
+                    ans += English_dict[letter.lower()]
+                elif letter == '0':
+                    ans += English_dict['j']
+                else:
+                    ans += English_dict[chr(ord('a') + ord(letter) - ord('1'))]
+            if i < content_len + 1:
+                ans += "......"
+    return ans
+
+
+def translate_to_English(Braille):
+    ans = ""
+    is_number = False
+    is_capital = False
+    word_len = int(len(Braille) / 6)
+    for i in range(word_len):
+        left = i * 6
+        right = (i + 1) * 6
+        word = Braille[left: right]
+        english_word = Braille_dict[word]
+        if english_word == "number":
+            is_number = True
+        elif english_word == "capital":
+            is_capital = True
+        elif english_word == "space":
+            is_number = False
+            ans += " "
+        elif is_number == True and english_word != 'j':
+            ans += chr(ord('1') + ord(english_word) - ord('a'))
+        elif is_number == True and english_word == 'j':
+            ans += "0"
+        elif is_capital == True:
+            ans += english_word.upper()
+            is_capital = False
+        else:
+            ans += english_word
+    return ans
+
+
+def translate(content):
+    is_English = is_content_English(content)
+    ans = ""
+    if not is_English:
+        ans = translate_to_English(content[1])
+    else:
+        ans = translate_to_Braille(content)
+    print(ans)
+
+
+if __name__ == '__main__':
+    translate(sys.argv)
--- python/translator.test.py
+++ python/translator.test.py
@@ -2,18 +2,55 @@ import unittest
 import subprocess
 
 class TestTranslator(unittest.TestCase):
-    def test_output(self):
+    def test_output_english(self):
         # Command to run translator.py script
         command = ["python3", "translator.py", "Abc", "123", "xYz"]
+        command_1 = ["python3", "translator.py", "Hello", "world"]
+        command_2 = ["python3", "translator.py", "42"]
+        command_3 = ["python3", "translator.py", "Abc", "123"]
         
         # Run the command and capture output
         result = subprocess.run(command, capture_output=True, text=True)
+        result_1 = subprocess.run(command_1, capture_output=True, text=True)
+        result_2 = subprocess.run(command_2, capture_output=True, text=True)
+        result_3 = subprocess.run(command_3, capture_output=True, text=True)
         
         # Expected output without the newline at the end
         expected_output = ".....OO.....O.O...OO...........O.OOOO.....O.O...OO..........OO..OO.....OOO.OOOO..OOO"
-        
+        expected_output_1 = ".....OO.OO..O..O..O.O.O.O.O.O.O..OO........OOO.OO..OO.O.OOO.O.O.O.OO.O.."
+        expected_output_2 = ".O.OOOOO.O..O.O..."
+        expected_output_3 = ".....OO.....O.O...OO...........O.OOOO.....O.O...OO...."
+
         # Strip any leading/trailing whitespace from the output and compare
         self.assertEqual(result.stdout.strip(), expected_output)
+        self.assertEqual(result_1.stdout.strip(), expected_output_1)
+        self.assertEqual(result_2.stdout.strip(), expected_output_2)
+        self.assertEqual(result_3.stdout.strip(), expected_output_3)
+
+    def test_output_braille(self):
+        # Command to run translator.py script
+        command = ["python3", "translator.py", ".....OO.....O.O...OO...........O.OOOO.....O.O...OO..........OO..OO.....OOO.OOOO..OOO"]
+        command_1 = ["python3", "translator.py", ".....OO.OO..O..O..O.O.O.O.O.O.O..OO........OOO.OO..OO.O.OOO.O.O.O.OO.O.."]
+        command_2 = ["python3", "translator.py", ".O.OOOOO.O..O.O..."]
+        command_3 = ["python3", "translator.py", ".....OO.....O.O...OO...........O.OOOO.....O.O...OO...."]
+
+        # Run the command and capture output
+        result = subprocess.run(command, capture_output=True, text=True)
+        result_1 = subprocess.run(command_1, capture_output=True, text=True)
+        result_2 = subprocess.run(command_2, capture_output=True, text=True)
+        result_3 = subprocess.run(command_3, capture_output=True, text=True)
+
+        # Expected output without the newline at the end
+        expected_output = "Abc 123 xYz"
+        expected_output_1 = "Hello world"
+        expected_output_2 = "42"
+        expected_output_3 = "Abc 123"
+
+        # Strip any leading/trailing whitespace from the output and compare
+        self.assertEqual(result.stdout.strip(), expected_output)
+        self.assertEqual(result_1.stdout.strip(), expected_output_1)
+        self.assertEqual(result_2.stdout.strip(), expected_output_2)
+        self.assertEqual(result_3.stdout.strip(), expected_output_3)
 
 if __name__ == '__main__':
     unittest.main()
