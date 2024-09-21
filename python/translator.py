e32c0bfeff637d40cccd4a678cfe1305ec617e56
Ahmad Asha <59888372+aiasha97@users.noreply.github.com>
2024-09-21 10:07:26 -0400

    created code skeleton

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,37 @@
+import sys
 
+# define dictionaries to store the mappings
+
+english_to_braille = {}
+
+braille_to_english = {}
+
+# function to determine input
+
+
+def is_english(input):
+    return True
+
+
+def is_braille(input):
+    return True
+
+
+def translate(text):
+    return
+
+
+def main():
+    if len(sys.args) != 2:
+        print("Usage: python translator.py <input>")
+        return
+
+    if is_english(sys.args[1]):
+        print(translate(sys.args[1]))
+
+    if is_braille(sys.args[1]):
+        print(translate(sys.args[1]))
+
+
+if __name__ == "__main__":
+    main()
