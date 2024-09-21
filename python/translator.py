9af52f2d2f0034e82817b297524279fa20931812
Akshen Jasikumar <akshen.jasikumar@gmail.com>
2024-09-20 20:06:00 -0700

    [CHORE] Created the english and braille dictionaries.
    - Created three dictionaries where the first three represents english to braille, digits to braille, and special characters to braille.
    - Created another three dictionaries that represent the reverse of the first three.
    
    Low risk: Only made dictionaries and did not made any working code yet.

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,30 @@
+import sys
 
+CAPITAL_LETTER = "capital"
+DIGIT = "digit"
+SPACE = " "
+
+ENG_TO_BRAILLE = {
+    "a": "O.....", "b": "O.O...", "c": "OO....", "d": "OO.O..", "e": "O..O..", "f": "OOO...", "g": "OOOO..", 
+    "h": "O.OO..", "i": ".OO...", "j": ".OOO..", "k": "O...O.", "l": "O.O.O.", "m": "OO..O.", "n": "OO.OO.", 
+    "o": "O..OO.", "p": "OOO.O.", "q": "OOOOO.", "r": "O.OOO.", "s": ".OO.O.", "t": ".OOOO.", "u": "O...OO", 
+    "v": "O.O.OO", "w": ".OOO.O", "x": "OO..OO", "y": "OO.OOO", "z": "O..OOO"
+}
+DIGITS_TO_BRAILLE = {
+    "0": ".OOO..", "1": "O.....", "2": "O.O...", "3": "OO....", "4": "OO.O..", "5": "O..O..", "6": "OOO...", 
+    "7": "OOOO..", "8": "O.OO..", "9": ".OO...", 
+}
+SPECIAL_CHARS = {
+    CAPITAL_LETTER: ".....O", DIGIT: ".O.OOO", SPACE: "......",
+}
+
+BRAILLE_TO_DIGITS = {v: k for k, v in DIGITS_TO_BRAILLE.items()}
+BRAILLE_TO_ENG = {v: k for k, v in ENG_TO_BRAILLE.items()}
+REVERSE_SPECIAL_CHARS = {v: k for k, v in SPECIAL_CHARS.items()}
+
+
+def main():
+    return 0
+
+if __name__ == "__main__":
+    main()
