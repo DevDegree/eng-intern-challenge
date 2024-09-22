bd8e2875227ef8dd1358ed35ebab52749b78ab6f
Ally MacLeod <155204136+amacleod1417@users.noreply.github.com>
2024-09-22 10:28:56 -0400

    fix CLI outputt

--- ruby/translator.rb
+++ ruby/translator.rb
@@ -14,23 +14,20 @@ class BrailleTranslator
     }
   
     BRAILLE_SPECIAL = {
-      'capital' => '.....O',  
-      'number'  => '.O.OOO'  
+      'capital' => '.....O',
+      'number' => '.O.OOO'
     }
   
-    # braille to english
     ENGLISH_ALPHABET = BRAILLE_ALPHABET.invert
     ENGLISH_NUMBERS = BRAILLE_NUMBERS.invert
     SPECIAL_BRAILLE = BRAILLE_SPECIAL.invert
   
-    # new BrailleTranslator instance
     def initialize(input_string)
       @input_string = input_string
       @number_mode = false
       @capital_mode = false
     end
   
-    #determine translation direction
     def translate
       if is_braille?
         braille_to_english
@@ -41,109 +38,89 @@ class BrailleTranslator
   
     private
   
-    # Detect if the input is braille
     def is_braille?
       @input_string.chars.all? { |c| c == 'O' || c == '.' }
     end
   
-    #English to Braille
     def english_to_braille
       result = []
       @number_mode = false
   
       @input_string.each_char do |char|
-        result << handle_capital(char) if char =~ /[A-Z]/
-        result << handle_number(char) if char =~ /\d/
-  
-        if BRAILLE_ALPHABET[char.downcase]
-          result << BRAILLE_ALPHABET[char.downcase]
-        elsif BRAILLE_NUMBERS[char]
-          result << BRAILLE_NUMBERS[char]
+        # Handle spaces
+        if char == ' '
+          result << BRAILLE_ALPHABET[char]
+          next
         end
   
-        result << reset_number_mode(char)
+        # Handle capital letters
+        if char =~ /[A-Z]/
+          result << BRAILLE_SPECIAL['capital']  # Add Braille capital prefix
+          char = char.downcase  # Continue with lowercase equivalent
+        end
+  
+        # Handle numbers
+        if char =~ /\d/
+          unless @number_mode
+            result << BRAILLE_SPECIAL['number']  # Add Braille number prefix
+            @number_mode = true
+          end
+          result << BRAILLE_NUMBERS[char]
+          next
+        end
+  
+        # Reset number mode after a non-number character
+        @number_mode = false if char !~ /\d/
+  
+        # Translate letters
+        result << BRAILLE_ALPHABET[char] if BRAILLE_ALPHABET[char]
       end
   
-      result.compact.join
+      result.join  # Join without spaces to avoid adding extra spaces between Braille characters
     end
   
-
-    # Handles capital letters
-    def handle_capital(char)
-      return unless char =~ /[A-Z]/
-      BRAILLE_SPECIAL['capital']
-    end
-  
-    # Handles numbers 
-    def handle_number(char)
-      if !@number_mode
-        @number_mode = true
-        BRAILLE_SPECIAL['number']
-      end
-    end
-  
-    # Reset number mode after spaces
-    def reset_number_mode(char)
-      @number_mode = false if char == ' '
-      nil
-    end
-  
-    #Braille to English
     def braille_to_english
       result = []
       @number_mode = false
       @capital_mode = false
+      chars = @input_string.scan(/.{6}/)  # Braille characters are 6-dots long
   
-      # Split into 6-character chunks
-      chars = @input_string.scan(/.{6}/)
-  
-      # Process each chunk
       chars.each do |braille_char|
         if braille_char == '......'  # Handle spaces
           result << ' '
+          next
+        end
+  
+        # Handle special characters (capital and number)
+        if braille_char == BRAILLE_SPECIAL['capital']
+          @capital_mode = true
+          next
+        elsif braille_char == BRAILLE_SPECIAL['number']
+          @number_mode = true
+          next
+        end
+  
+        if @number_mode
+          result << ENGLISH_NUMBERS[braille_char]
+          @number_mode = false
         else
-          # Handle special characters (capital, number)
-          special_char = handle_braille_special(braille_char)
-          if special_char.nil?  # If special mode , skip this chunk
-            next
-          else
-            result << special_char 
+          letter = ENGLISH_ALPHABET[braille_char]
+          if @capital_mode
+            letter = letter.upcase
+            @capital_mode = false
           end
+          result << letter
         end
       end
   
       result.join
     end
-  
-    # Handles special characters
-    def handle_braille_special(braille_char)
-      if braille_char == BRAILLE_SPECIAL['capital']
-        @capital_mode = true
-        return nil
-      elsif braille_char == BRAILLE_SPECIAL['number']
-        @number_mode = true
-        return nil
-      end
-  
-      # Translate based on the current mode (number/capital/regular)
-      if @number_mode
-        char = ENGLISH_NUMBERS[braille_char]
-        @number_mode = false if braille_char == '......'  # Space resets number mode
-      else
-        char = ENGLISH_ALPHABET[braille_char]
-        if @capital_mode
-          char = char.upcase  
-          @capital_mode = false 
-        end
-      end
-  
-      char  #
-    end
   end
   
   # Command-line interface
   def main
-    input_string = ARGV[0]
+
+    input_string = ARGV.join(" ")
     translator = BrailleTranslator.new(input_string)
     puts translator.translate
   end
