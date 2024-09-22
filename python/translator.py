08028cc69032eb3521015df68fc5916a71543f56
Frank Cho <frankcho@franks-air.home>
2024-09-22 17:31:55 -0400

    define plan of action

--- python/translator.py
+++ python/translator.py
@@ -1,3 +1,28 @@
 # Braille Translator Submission by Frank Siyung Cho
 # 20fsc@queensu.ca
 
+'''
+Plan of Action:
+1. Determine logic for classifying if input string is braille or english
+    - if the input string only contains the characters O and . then it is braille
+2. For a Braille to English conversion we can map the set of braille combinations into 6 bit binary values using this matrix:
+
+[1][4]
+[2][5]
+[3][6]
+
+Therefore, if the Braille Letter is O..... Then the 6 bit combination is 0b000001
+From the provided braille to letter combinations, we can see that the letters A-J only use bit positions 1,2,4 and 5.
+We can also see that the letters K-T add the bit position 3 to the same bit patterns as A-J.
+The same occurs for letters U-Z however adding bit position 6 as well.
+This means we can partition the alphabet into 3 sections and by distinguishing if bit positions 3 and 6 are used we can identify which corresponding letter should be added.
+We can also use ASCII character encodings to transform the binary values of the 6 bit combination into characters.
+
+3. Implement logic which identifies if 'capital follows', 'decimal follows' or 'number follows' 6 bit combinations are present
+4. Implement numerical logic so that if 'decimal follows' or 'number follows' 6 bit combination is found then instead of letter combinations, number combinations are used.
+5. Implement special characters logic to identify any ',', '.', '?', '!', ':', ';', '-', '/', '<', '>', '(', ')', ' '
+'''
+
+import sys
+
+args = sys.argv[1:] # Ingest all the inputted text ie, Braille or English into a list where each element is a word
