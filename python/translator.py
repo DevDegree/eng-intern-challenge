a5177a69b25cccb2d548effb58986e658d7ac76b
devanshu.singhvi <dev.singhvi@mail.utoronto.ca>
2024-09-22 21:50:48 -0400

    adding conversion dictionaries

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,15 @@
 
+
+braille_to_english = {
+    'O.....': 'a', 'O.O...': 'b', 'OO....': 'c', 'OO.O..': 'd', 'O..O..': 'e', 'OOO...': 'f', 'OOOO..': 'g',
+    'O.OO..': 'h', '.OO...': 'i', '.OOO..': 'j', 'O...O.': 'k', 'O.O.O.': 'l', 'OO..O.': 'm', 'OO.OO.': 'n',
+    'O..OO.': 'o', 'OOO.O.': 'p', 'OOOOO.': 'q', 'O.OOO.': 'r', '.OO.O.': 's', '.OOOO.': 't', 'O...OO': 'u',
+    'O.O.OO': 'v', '.OOO.O': 'w', 'OO..OO': 'x', 'OO.OOO': 'y', 'O..OOO': 'z',
+    'O.....': '1', 'O.O...': '2', 'OO....': '3', 'OO.O..': '4', 'O..O..': '5', 'OOO...': '6', 'OOOO..': '7',
+    'O.OO..': '8', '.OO...': '9', '.OOO..': '0',
+    '......': ' ', '.....O': 'capital', '.O...O': 'decimal', '.O.OOO': 'number'
+}
+
+english_to_braille = {
+    char:braille for braille, char in braille_to_english.items()
+}
