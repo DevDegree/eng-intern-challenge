628376e33a8bc001c45bcd5980894b6d9f7f90a1
devanshu.singhvi <dev.singhvi@mail.utoronto.ca>
2024-09-22 22:27:50 -0400

    translating braille to english

--- python/translator.py
+++ python/translator.py
@@ -1,5 +1,5 @@
 
-
+# conversions
 english_to_braille = {
     'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..', 'f': 'OOO...', 'g': 'OOOO..',
     'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..', 'k': 'O...O.', 'l': 'O.O.O.', 'm': 'OO..O.', 'n': 'OO.OO.',
@@ -17,3 +17,36 @@ braille_to_english = {
 braille_to_numbers = {
     braille:num for num, braille in english_to_braille.items() if num.isdigit()
 }
+
+
+def translate_to_english(string):
+    translation = ''
+
+    # flags to check if next character is in caps or a num
+    caps_flag = False
+    number_flag = False
+
+    # splitting into 6 character blocks
+    braille_chars = [string[i: i+6] for i in range(0, len(string), 6)]
+
+    for braille in braille_chars:
+        # invalid character
+        if braille not in braille_to_english:
+            continue
+        # checking if next character will be in caps, a num or a space
+        if braille_to_english[braille] == 'capital':
+            caps_flag = True
+        elif braille_to_english[braille] == 'number':
+            number_flag = True
+        elif braille_to_english[braille] == ' ':
+            translation += ' '
+            number_flag = False
+        else:
+            # adding translated value accordingly
+            if caps_flag: translation += braille_to_english[braille].upper()
+            elif number_flag: translation += braille_to_numbers[braille]
+            else: translation += braille_to_english[braille]
+            # reset flag
+            caps_flag = False
+
+    return translation
