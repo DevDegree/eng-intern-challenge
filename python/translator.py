522f418c1b179ac27aef410fd544a1251973d0e6
mika.urakawa <awakaru0122@gmail.com>
2024-09-25 14:06:32 -0700

    feat: add character to all tulples

--- python/translator.py
+++ python/translator.py
@@ -3,68 +3,76 @@ from enum import Enum
 # List up all Braille
 class BrailleDot(Enum):
     RAISED = '0' # which represents the raised dot
-    NOT_RAISED = '1' # which represents the not raised dot 
+    NOT_RAISED = '1' # which represents the not raised dot
 
 class Braille(Enum):
     # Alphabet
-    A = (0, 1, 1, 1, 1, 1)
-    B = (0, 1, 0, 1, 1, 1)
-    C = (0, 0, 1, 1, 1, 1)
-    D = (0, 0, 1, 0, 1, 1)
-    E = (0, 1, 1, 0, 1, 1)
-    F = (0, 0, 0, 1, 1, 1)
-    G = (0, 0, 0, 0, 1, 1)
-    H = (0, 1, 0, 0, 1, 1)
-    I = (1, 0, 0, 1, 1, 1)
-    J = (1, 0, 0, 0, 1, 1)
-    K = (0, 1, 1, 1, 0, 1)
-    L = (0, 1, 0, 1, 0, 1)
-    M = (0, 0, 1, 1, 0, 1)
-    N = (0, 0, 1, 0, 0, 1)
-    O = (0, 1, 1, 0, 0, 1)
-    P = (0, 0, 0, 1, 0, 1)
-    Q = (0, 0, 0, 0, 0, 1)
-    R = (0, 1, 0, 0, 0, 1)
-    S = (1, 0, 0, 1, 0, 1)
-    T = (1, 0, 0, 0, 0, 1)
-    U = (0, 1, 1, 1, 0, 0)
-    V = (0, 1, 0, 1, 0, 0)
-    W = (1, 0, 0, 0, 1, 0)
-    X = (0, 0, 1, 1, 0, 0)
-    Y = (0, 0, 1, 0, 0, 0)
-    Z = (0, 1, 1, 0, 0, 0)
+    A = ((0, 1, 1, 1, 1, 1), "a")
+    B = ((0, 1, 0, 1, 1, 1), "b")
+    C = ((0, 0, 1, 1, 1, 1), "c")
+    D = ((0, 0, 1, 0, 1, 1), "d")
+    E = ((0, 1, 1, 0, 1, 1), "e")
+    F = ((0, 0, 0, 1, 1, 1), "f")
+    G = ((0, 0, 0, 0, 1, 1), "g")
+    H = ((0, 1, 0, 0, 1, 1), "h")
+    I = ((1, 0, 0, 1, 1, 1), "i")
+    J = ((1, 0, 0, 0, 1, 1), "j")
+    K = ((0, 1, 1, 1, 0, 1), "k")
+    L = ((0, 1, 0, 1, 0, 1), "l")
+    M = ((0, 0, 1, 1, 0, 1), "m")
+    N = ((0, 0, 1, 0, 0, 1), "n")
+    O = ((0, 1, 1, 0, 0, 1), "o")
+    P = ((0, 0, 0, 1, 0, 1), "p")
+    Q = ((0, 0, 0, 0, 0, 1), "q")
+    R = ((0, 1, 0, 0, 0, 1), "r")
+    S = ((1, 0, 0, 1, 0, 1), "s")
+    T = ((1, 0, 0, 0, 0, 1), "t")
+    U = ((0, 1, 1, 1, 0, 0), "u")
+    V = ((0, 1, 0, 1, 0, 0), "v")
+    W = ((1, 0, 0, 0, 1, 0), "w")
+    X = ((0, 0, 1, 1, 0, 0), "x")
+    Y = ((0, 0, 1, 0, 0, 0), "y")
+    Z = ((0, 1, 1, 0, 0, 0), "z")
     
     # Numbers
-    ONE   = (0, 1, 1, 1, 1, 1)
-    TWO   = (0, 1, 0, 1, 1, 1)
-    THREE = (0, 0, 1, 1, 1, 1)
-    FOUR  = (0, 0, 1, 0, 1, 1)
-    FIVE  = (0, 1, 1, 0, 1, 1)
-    SIX   = (0, 0, 0, 1, 1, 1)
-    SEVEN = (0, 0, 0, 0, 1, 1)
-    EIGHT = (0, 1, 0, 0, 1, 1)
-    NINE  = (1, 0, 0, 1, 1, 1)
-    ZERO  = (1, 0, 0, 0, 1, 1)
+    ONE   = ((0, 1, 1, 1, 1, 1), "1")
+    TWO   = ((0, 1, 0, 1, 1, 1), "2")
+    THREE = ((0, 0, 1, 1, 1, 1), "3")
+    FOUR  = ((0, 0, 1, 0, 1, 1), "4")
+    FIVE  = ((0, 1, 1, 0, 1, 1), "5")
+    SIX   = ((0, 0, 0, 1, 1, 1), "6")
+    SEVEN = ((0, 0, 0, 0, 1, 1), "7")
+    EIGHT = ((0, 1, 0, 0, 1, 1), "8")
+    NINE  = ((1, 0, 0, 1, 1, 1), "9")
+    ZERO  = ((1, 0, 0, 0, 1, 1), "0")
     
     # FOLLOWS
-    CAPITAL_FOLLOWS = (1, 1, 1, 1, 1, 0)
-    DECIMAL_FOLLOWS = (1, 0, 1, 1, 1, 0)
-    NUMBER_FOLLOWS  = (1, 0, 1, 0, 0, 0)
+    CAPITAL_FOLLOWS = ((1, 1, 1, 1, 1, 0), None)
+    DECIMAL_FOLLOWS = ((1, 0, 1, 1, 1, 0), None)
+    NUMBER_FOLLOWS  = ((1, 0, 1, 0, 0, 0), None)
     
     # Special Characters
-    PERIOD        = (1, 1, 0, 0, 1, 0)
-    COMMA         = (1, 1, 0, 1, 1, 1)
-    QUESTION      = (1, 1, 0, 1, 0, 0)
-    EXCLAMATION   = (1, 1, 0, 0, 0, 1)
-    COLON         = (1, 1, 0, 0, 1, 1)
-    SEMICOLON     = (1, 1, 0, 1, 0, 1)
-    HYPHEN        = (1, 1, 1, 1, 0, 0)
-    SLASH         = (1, 0, 1, 1, 0, 1)
-    LESS_THAN     = (1, 0, 0, 1, 1, 0)
-    GREATER_THAN  = (0, 1, 1, 0, 0, 1)
-    BRACKET_OPEN  = (0, 1, 0, 1, 1, 0)
-    BRACKET_CLOSE = (1, 0, 1, 0, 0, 1)
-    SPACE         = (1, 1, 1, 1, 1, 1)
+    PERIOD        = ((1, 1, 0, 0, 1, 0), ".")
+    COMMA         = ((1, 1, 0, 1, 1, 1), ",")
+    QUESTION      = ((1, 1, 0, 1, 0, 0), "?")
+    EXCLAMATION   = ((1, 1, 0, 0, 0, 1), "!")
+    COLON         = ((1, 1, 0, 0, 1, 1), ":")
+    SEMICOLON     = ((1, 1, 0, 1, 0, 1), ";")
+    HYPHEN        = ((1, 1, 1, 1, 0, 0), "-")
+    SLASH         = ((1, 0, 1, 1, 0, 1), "/")
+    LESS_THAN     = ((1, 0, 0, 1, 1, 0), "<")
+    GREATER_THAN  = ((0, 1, 1, 0, 0, 1), ">")
+    BRACKET_OPEN  = ((0, 1, 0, 1, 1, 0), "(")
+    BRACKET_CLOSE = ((1, 0, 1, 0, 0, 1), ")")
+    SPACE         = ((1, 1, 1, 1, 1, 1), " ")
+    
+    def __init__(self, braille, symbol):
+        self._braille = braille
+        self._symbol = symbol
+
+    @property
+    def symbol(self):
+        return self._symbol
 
 # Check if the string given to it is either Braille or English
 def isBraille(string):
@@ -87,5 +95,10 @@ def isBraille(string):
     return True
 
 # Convert the string from English to Braille
+def convertFromEnglishToBraille(string):
+    result = ""
+    
+    for char in string:
+        print(char)
 
 # Convert the string from Braille to English
