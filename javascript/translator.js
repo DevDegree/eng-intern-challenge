8b2210910a32ff7219912d145d3a4515ebf17f50
reganbrown <regan_brown@icloud.com>
2024-09-26 08:48:23 -0300

    create English dictionary object

--- javascript/translator.js
+++ javascript/translator.js
@@ -1,5 +1,3 @@
-// construct dictionary objects of key value pairs for Braille and English
-
 // create a function to take in a string and determine if it is English or Braille
 // and run a function based on that
 
@@ -48,4 +46,50 @@ const brailleDictionary = {
   capital: ".....0",
   number: ".0.000",
   space: "......",
+};
+
+// declare the english letters dictionary keys and values
+const englishLetterDictionary = {
+  "0.....": "a",
+  "0.0...": "b",
+  "00....": "c",
+  "00.0..": "d",
+  "0..0..": "e",
+  "000...": "f",
+  "0000..": "g",
+  "0.00..": "h",
+  ".00...": "i",
+  ".000..": "j",
+  "0...0.": "k",
+  "0.0.0.": "l",
+  "00..0.": "m",
+  "00.00.": "n",
+  "0..00.": "o",
+  "000.0.": "p",
+  "00000.": "q",
+  "0.000.": "r",
+  ".00.0.": "s",
+  ".0000.": "t",
+  "0...00": "u",
+  "0.0.00": "v",
+  ".000.0": "w",
+  "00..00": "x",
+  "00.000": "y",
+  "0..000": "z",
+  ".....0": "capital",
+  "......": " ",
+};
+
+// declare the numbers dictionary
+const numberDictionary = {
+  "0.....": "1",
+  "0.0...": "2",
+  "00....": "3",
+  "00.0..": "4",
+  "0..0..": "5",
+  "000...": "6",
+  "0000..": "7",
+  "0.00..": "8",
+  ".00...": "9",
+  ".000..": "0",
 };
