fa9b21e6e1da11b3e46d1b422128cd753d89a8ae
reganbrown <regan_brown@icloud.com>
2024-09-26 08:08:36 -0300

    initialize project

---
+++ javascript/translator.js
@@ -0,0 +1,8 @@
+// construct dictionary objects of key value pairs for Braille and English
+
+// create a function to take in a string and determine if it is English or Braille
+// and run a function based on that
+
+// create a function that takes an English string and converts to Braille
+
+// create a function that takes a Braille string and converts to English
