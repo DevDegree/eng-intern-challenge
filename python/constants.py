0a9eb281558d4b60894b25785e87ebd6efcf808b
sam <117124009+haenlonns@users.noreply.github.com>
2024-09-27 02:39:05 -0400

    Add translation constants

---
+++ python/constants.py
@@ -0,0 +1,50 @@
+CHAR_TO_BRAILLE = {
+    'a': 'O.....',
+    'b': 'O.O...',
+    'c': 'OO....',
+    'd': 'OO.O..',
+    'e': 'O..O..',
+    'f': 'OOO...',
+    'g': 'OOOO..',
+    'h': 'O.OO..',
+    'i': '.OO...',
+    'j': '.OOO..',
+    'k': 'O...O.',
+    'l': 'O.O.O.',
+    'm': 'OO..O.',
+    'n': 'OO.OO.',
+    'o': 'O..OO.',
+    'p': 'OOO.O.',
+    'q': 'OOOOO.',
+    'r': 'O.OOO.',
+    's': '.OO.O.',
+    't': '.OOOO.',
+    'u': 'O...OO',
+    'v': 'O.O.OO',
+    'w': '.OOO.O',
+    'x': 'OO..OO',
+    'y': 'OO.OOO', 
+    'z': 'O..OOO',
+    ' ': '......'
+}
+
+BRAILLE_TO_CHAR = { braille: char for char, braille in CHAR_TO_BRAILLE.items() }
+
+NUM_TO_BRAILLE = {
+    '1': 'O.....',
+    '2': 'O.O...',
+    '3': 'OO....',
+    '4': 'OO.O..',
+    '5': 'O..O..',
+    '6': 'OOO...',
+    '7': 'OOOO..',
+    '8': 'O.OO..',
+    '9': '.OO...',
+    '0': '.OOO..'
+}
+
+BRAILLE_TO_NUM = { braille: num for num, braille in NUM_TO_BRAILLE.items() }
+
+
+CAPITAL_FOLLOWS = '.....O'
+NUMBER_FOLLOWS = '.O.OOO'
