10325afe43c8032dbd0068694c9e505ea0be19ee
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-09-28 15:29:25 -0400

    Add Braille and English dictionaries

--- javascript/translator.js
+++ javascript/translator.js
@@ -1,13 +1,72 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 28 Sep 2024, 2:49:59 PM
- *  Last update: 28 Sep 2024, 3:11:51 PM
+ *  Last update: 28 Sep 2024, 3:25:08 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 
 // English-to-Braille object
+// TODO: figure out if it's better to index on ASCII number or character
 const englishToBraille = {
+    // letters
     a: "O.....",
+    b: "O.O...",
+    c: "OO....",
+    d: "OO.O..",
+    e: "O..O..",
+    f: "OOO...",
+    g: "OOOO..",
+    h: "O.OO..",
+    i: ".OO...",
+    j: ".OOO..",
+    k: "O...O.",
+    l: "O.O.O.",
+    m: "OO..O.",
+    n: "OO.OO.",
+    o: "O..OO.",
+    p: "OOO.O.",
+    q: "OOOOO.",
+    r: "O.OOO.",
+    s: ".OO.O.",
+    t: ".OOOO.",
+    u: "O...OO",
+    v: "O.O.OO",
+    w: ".OOO.O",
+    x: "OO..OO",
+    y: "OO.OOO",
+    z: "O..OOO",
+
+    // numbers
+    1: "O.....",
+    2: "O.O...",
+    3: "OO....",
+    4: "OO.O..",
+    5: "O..O..",
+    6: "OOO...",
+    7: "OOOO..",
+    8: "O.OO..",
+    9: ".OO...",
+    0: ".OOO..",
+
+    // special indicators
+    capital: ".....O",
+    decimal: ".O...O",
+    number: ".O.OOO",
+
+    // special characters
+    ".": "..OO.O",
+    ",": "..O...",
+    "?": "..O.OO",
+    "!": "..OOO.",
+    ":": "..OO..",
+    ";": "..O.O.",
+    "-": "....OO",
+    "/": ".O..O.",
+    "<": ".OO..O",
+    ">": "O..OO.",
+    "(": "O.O..O",
+    ")": ".O.OO.",
+    " ": "......",
 };
 console.log(englishToBraille);
 
