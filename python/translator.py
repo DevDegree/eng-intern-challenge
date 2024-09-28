f7cb0f7514050686680b5c43410eb9323e8a3312
AugustRoad <kevin.ma.work11@gmail.com>
2024-09-27 23:42:59 -0400

    Set up for Translate class

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,36 @@
 
+ENGLISH_TO_BRAILLE = {
+    'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..',
+    'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..',
+    'k': 'O...O.', 'l': 'O.O.O.', 'm': 'OO..O.', 'n': 'OO.OO.', 'o': 'O..OO.',
+    'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.',
+    'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO', 'y': 'OO.OOO', 'z': 'O..OOO',
+    '1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..', '5': 'O..O..',
+    '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...', '0': '.OOO..',
+    'capital': '.....O', 'number': '.O.OOO', ' ': '......',
+}
+
+
+class Translator:
+    def __init__(self, text: str):
+        self.text: str = text
+        self.language = self.detectLang()
+    
+    def detectLang(self)-> str:
+        
+        return "eng"
+    
+    def translate(self):
+        
+        return ""
+    
+    def eng_to_bra(self):
+        
+        return ""
+    
+    def bra_to_eng(self):
+        
+        return ""
+
+if __name__ == '__main__':
+    print()
