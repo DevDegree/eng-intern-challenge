24bf26cc28f389af09672220cd765a6c04e24310
kvijayan@uwaterloo.ca <kvijayan@uwaterloo.ca>
2024-09-27 21:16:03 -0400

    Updated

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,81 @@
+braille_to_english = {
+    'O.....': 'a', 'O.O...': 'b', 'OO....': 'c', 'OO.O..': 'd', 'O..O..': 'e',
+    'OOO...': 'f', 'OOOO..': 'g', 'O.OO..': 'h', '.OO...': 'i', '.OOO..': 'j',
+    'O...O.': 'k', 'O.O.O.': 'l', 'OO..O.': 'm', 'OO.OO.': 'n', 'O..OO.': 'o',
+    'OOO.O.': 'p', 'OOOOO.': 'q', 'O.OOO.': 'r', '.OO.O.': 's', '.OOOO.': 't',
+    'O...OO': 'u', 'O.O.OO': 'v', '.OOO.O': 'w', 'OO..OO': 'x', 'OO.OOO': 'y', 'O..OOO': 'z',
+    '.O....': '1', '.O.O..': '2', '..OO..': '3', '..OOO.': '4', '.OOO..': '5', '..OO.O': '6',
+    '..OOOO': '7', '.OOOO.': '8', '.OO.O.': '9', '.OO.OO': '0', '......': ' '
+}
 
+CAPITAL_PREFIX = '.....O'
+NUMBER_PREFIX = '.O.OOO'
+
+english_to_braille = {v: k for k, v in braille_to_english.items()}
+english_to_braille.update({
+    ' ': '......'
+})
+
+def convertToBraille(englishString):
+    brailleOutput = []
+    number_mode = False
+    for char in englishString:
+        if char.isupper():
+            brailleOutput.append(CAPITAL_PREFIX)
+            char = char.lower()
+        if char.isdigit():
+            if not number_mode:
+                brailleOutput.append(NUMBER_PREFIX)
+                number_mode = True
+        else:
+            number_mode = False
+        brailleChar = english_to_braille.get(char, '......')
+        brailleOutput.append(brailleChar)
+    braille = ''.join(brailleOutput)
+    print(braille)
+
+def convertToEnglish(brailleString):
+    englishOutput = []
+    brailleChars = []
+    isNumber = False
+    isCapital = False
+
+    for i in range(0, len(brailleString), 6):
+        brailleChars.append(brailleString[i:i+6])
+    
+    for brailleChar in brailleChars:
+        if brailleChar == CAPITAL_PREFIX:
+            isCapital = True
+            continue
+        if brailleChar == NUMBER_PREFIX:
+            isNumber = True
+            continue
+        
+        char = braille_to_english.get(brailleChar, '?')
+        
+        if char != '?':
+            if isNumber:
+                if char in 'abcdefghijklmnopqrstuvwxyz':
+                    englishOutput.append(char)
+                    isNumber = False
+                else:
+                    englishOutput.append(char)
+            else:
+                if isCapital:
+                    char = char.upper()
+                    isCapital = False
+                englishOutput.append(char)
+        else:
+            englishOutput.append('?')
+
+    english = ''.join(englishOutput).replace('?', '')
+    print(english)
+
+userInput = input("Enter text: ")
+
+isBraille = all(char in 'O.' for char in userInput)
+
+if isBraille:
+    convertToEnglish(userInput)
+else:
+    convertToBraille(userInput)
