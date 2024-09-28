6997a05899a1240c2a9998d42d8ebdc8b4519c0c
JunYYZ <junhengzhang@cmail.carleton.ca>
2024-09-28 15:16:14 -0400

    minor style change
    added comments and removed error print
    
    Update translator.go
    
    Realized possible redundancy
    
    minor changes
    
    commented out one test case, removed empty line
    
    Update translator.go
    
    Added a descriptive comments, slightly changed if statement structure of brailleToEnglish function to be more streamlined.
    
    Cleaned up main by moving input type check to helper function.
    
    squashed multiple not very useful commits

--- go/translator.go
+++ go/translator.go
@@ -6,7 +6,12 @@ import (
 	"strings"
 )
 
-// braille mappings
+/*  braille mappings.
+	note that we need to store them seperately as 1 and a are the same symbols
+	this is important when we translate from braille to english
+	as then we have two keys that map to the same value: O..... = a but O..... = 1
+	this saves an extra check at the cost of two seperate map variable
+*/
 var alphaToBraile = map[rune]string{
 	'a': "O.....", 'b': "O.O...", 'c': "OO....", 'd': "OO.O..", 'e': "O..O..", 'f': "OOO...", 'g': "OOOO..", 'h': "O.OO..", 'i': ".OO...", 'j': ".OOO..",
 	'k': "O...O.", 'l': "O.O.O.", 'm': "OO..O.", 'n': "OO.OO.", 'o': "O..OO.", 'p': "OOO.O.", 'q': "OOOOO.", 'r': "O.OOO.", 's': ".OO.O.", 't': ".OOOO.",
@@ -17,11 +22,17 @@ var numToBraile = map[rune]string{
 	'1': "O.....", '2': "O.O...", '3': "OO....", '4': "OO.O..", '5': "O..O..", '6': "OOO...", '7': "OOOO..", '8': "O.OO..", '9': ".OO...", '0': ".OOO..",
 }
 
+// specific symbols
 var capitalFollow = ".....O"
 var numFollow = ".O.OOO"
 
+// function to translate braille to english characters
 func brailleToEnglish(input string) string {
 
+	/* 
+	init the opposite maps only if the input is braille
+	that way we can save time and space if input is only text
+	*/
 	var brailleToAlpha = map[string]rune{}
 	var brailleToNum = map[string]rune{}
 
@@ -36,35 +47,35 @@ func brailleToEnglish(input string) string {
 	output := ""
 	isCapital := false
 	isNumber := false
-
+	
+	// iterate over the braille input string 6 characters at a time
 	for i := 0; i < len(input); i += 6 {
-		brailleChar := input[i : i+6]
+		brailleChar := input[i : i+6] // since each braille charcter is 6 characters long
 
-		//special symbol checks
+		//special symbol checks, a symbol can't be two different things at once
 		if brailleChar == capitalFollow {
 			isCapital = true
-			continue
-		}
-
-		if brailleChar == numFollow {
+		} else if brailleChar == numFollow {
 			isNumber = true
-			continue
-		}
-
-		if brailleChar == alphaToBraile[' '] {
+		} else if brailleChar == alphaToBraile[' '] {
 			isNumber = false
 			output += " "
-			continue
 		}
 
 		if isNumber {
 			if translatedNum, exists := brailleToNum[brailleChar]; exists {
 				output += string(translatedNum)
-			} else {
-				output += "?"
+			} else { 
+				output += "?" 
+				/*
+				added error character output
+				since we are not inputting or outputing the "?" symbol we can use this as unknown symbol
+				we could also output just nothing but then user would be unaware of an error input
+				*/
 			}
 		} else {
 			if translatedAlpha, exists := brailleToAlpha[brailleChar]; exists {
+				// since each capital only capitalizes the next letter, set to false to prevent next letter being capitalized
 				if isCapital {
 					output += strings.ToUpper(string(translatedAlpha))
 					isCapital = false
@@ -80,29 +91,55 @@ func brailleToEnglish(input string) string {
 	return strings.TrimSpace(output)
 }
 
+// function to translate english to braille
 func englishToBraille(input string) string {
 	output := ""
 	isNumber := false
 
+	// iterate over the input string
 	for _, char := range input {
+		// since there is a capital symbol ahead of each capital letter, output cap symbol each time
 		if char >= 'A' && char <= 'Z' {
 			output += capitalFollow
 			char = char + 32 // convert to lowercase
 			isNumber = false
 		}
+
+		// since we only output the number symbol ahead of the first number in a chain,
+		// only output when we hit the first number in a row
 		if char >= '0' && char <= '9' {
 			if !isNumber {
 				output += numFollow
 				isNumber = true
 			}
 			output += numToBraile[char]
-		} else {
+		} else { //if not number than its a character
 			output += alphaToBraile[char]
 			isNumber = false
 		}
 
 	}
+
 	return strings.TrimSpace(output)
+}
+
+/*
+	function to check if the input is braille or .
+	the logic is that is that if the input string is not a multiple of 6, it must be text
+	if it is a muliple of 6, we check if the charcters are braille characters or not
+*/
+bool isBraille(input String) {
+	if len(input) % 6 != 0 {
+		retrun false
+	}
+
+	for _, char := range input {
+		if char != 'O' && char != '.' {
+			return false
+		}
+	}
+
+	return true
 }
 
 func main() {
@@ -110,32 +147,19 @@ func main() {
 	var input string
 
 	if len(os.Args) < 2 {
+		// testing, also prevent crashes
 		// input = strings.TrimSpace(".O.OOOOOOOOOO.....O.O....O...OO.OO.......O......OO..OO")
-		// input = strings.TrimSpace(".O.OOOOOOOOO")
+		// input = strings.TrimSpace(".O.OOOaaaaaaOOOOOO")
 		// input = strings.TrimSpace("")
-		fmt.Println("Please provide string to translate.")
 	} else {
 		input = strings.Join(os.Args[1:], " ")
 	}
 
-	isBraille := true
-
-	//check if input is braille or not
-	if len(input)%6 != 0 {
-		fmt.Println(englishToBraille(input))
-		isBraille = false
-	} else {
-		for _, char := range input {
-			if char != 'O' && char != '.' {
-				isBraille = false
-				fmt.Println(englishToBraille(input))
-			}
-		}
-
-	}
-
-	if isBraille {
+	//print statements
+	if isBraille(input) {
 		fmt.Println(brailleToEnglish(input))
+	} else {
+		fmt.Println(englishToBraille(input))
 	}
 
 }
