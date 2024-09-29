00f127efccf7380defa055f512918e7948b1ba1e
lavindude <lavindude@gmail.com>
2024-09-28 17:02:47 -0700

    translation between English and Braille implemented

---
+++ python/constants.py
@@ -0,0 +1,21 @@
+ENGLISH_TO_BRAILLE_MISC = {
+    'capital_follows': '.....O', 'number_follows': '.O.OOO',
+}
+
+ENGLISH_TO_BRAILLE_LETTERS = {
+    'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..',
+    'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..',
+    'k': 'O...O.', 'l': 'O.O.O.', 'm': 'OO..O.', 'n': 'OO.OO.', 'o': 'O..OO.',
+    'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.',
+    'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO', 'y': 'OO.OOO',
+    'z': 'O..OOO', ' ': '......',
+}
+
+ENGLISH_TO_BRAILLE_NUMBERS = {
+    '1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..', '5': 'O..O..',
+    '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...', '0': '.OOO..',
+}
+
+BRAILLE_TO_ENGLISH_MISC = {v: k for k, v in ENGLISH_TO_BRAILLE_MISC.items()}
+BRAILLE_TO_ENGLISH_LETTERS = {v: k for k, v in ENGLISH_TO_BRAILLE_LETTERS.items()}
+BRAILLE_TO_ENGLISH_NUMBERS = {v: k for k, v in ENGLISH_TO_BRAILLE_NUMBERS.items()}
