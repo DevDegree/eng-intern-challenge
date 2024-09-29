7db3f97a42582e86d34cf1bc42e084cb3a25debd
Amine-chisquare <aminesahraouics@outlook.com>
2024-09-29 15:34:41 -0400

    Turning into a cdl app

--- python/translator.py
+++ python/translator.py
@@ -1,8 +1,11 @@
+import sys
+
 # For simplicity, we will consider each braille character to be a 6-dimensional row vector
 # We start by building an English - Braille lookup table.
 
 # We note that the letter A to J form the "base set"
-table = {
+def buildLookupTable():
+    table = {
     "a": "O.....",
     "b": "O.O...",
     "c": "OO....",
@@ -26,53 +29,56 @@ table = {
     "(": "O.O..O",
     ")": ".O.OO.",
     " ": "......",
-}
+    }
 
-# In the range of letters ["k", ..., "t"], the ith letter can be related to the ith letter in the range ["a", ..., "j"]:
-rangeTenTwenty = [chr(i) for i in range(ord('k'), ord('t') + 1)] # the ten next letters after j (after the base set)
-for letter in rangeTenTwenty:
-    base_braille = list(table[chr(ord(letter) - 10)]) # get the braille rep of the corresponding letter in the base set
-    base_braille[4] = 'O' # the rule relating the letters in the base set to those in rangeTenTwenty is that the '.' at index 4 is replaced by a 'O'
-    table[letter] = "".join(base_braille)
+    # In the range of letters ["k", ..., "t"], the ith letter can be related to the ith letter in the range ["a", ..., "j"]:
+    rangeTenTwenty = [chr(i) for i in range(ord('k'), ord('t') + 1)] # the ten next letters after j (after the base set)
+    for letter in rangeTenTwenty:
+        base_braille = list(table[chr(ord(letter) - 10)]) # get the braille rep of the corresponding letter in the base set
+        base_braille[4] = 'O' # the rule relating the letters in the base set to those in rangeTenTwenty is that the '.' at index 4 is replaced by a 'O'
+        table[letter] = "".join(base_braille)
 
-# Same concept for the letters ["u", ..., "v"] 
-rangeTwentyTwentySix = [chr(i) for i in range(ord('u'), ord('z') + 1)] # the two next letters after t 
-offset = 10
+    # Same concept for the letters ["u", ..., "v"] 
+    rangeTwentyTwentySix = [chr(i) for i in range(ord('u'), ord('z') + 1)] # the two next letters after t 
+    offset = 10
 
-for letter in rangeTwentyTwentySix:
-    if letter == 'w':
-        table[letter] = ".OOO.O" # w is an exception to the rule we came up with
-        offset = 11 # the bases of the letters after w are then offset by 1 more 
-        continue
+    for letter in rangeTwentyTwentySix:
+        if letter == 'w':
+            table[letter] = ".OOO.O" # w is an exception to the rule we came up with
+            offset = 11 # the bases of the letters after w are then offset by 1 more 
+            continue
 
-    base_braille = list(table[chr(ord(letter) - offset)]) # get the braille rep of the corresponding letter in the base set
-    base_braille[5] = 'O' # '.' at index 5 is replaced by a 'O'
-    table[letter] = "".join(base_braille)
+        base_braille = list(table[chr(ord(letter) - offset)]) # get the braille rep of the corresponding letter in the base set
+        base_braille[5] = 'O' # '.' at index 5 is replaced by a 'O'
+        table[letter] = "".join(base_braille)
 
 
-# add the digits
-numberFlag = ".O.OOO" # when met, everything that follows is a number UNTIL space pattern
-decimalFlag = ".O...O" # signifies a fractional part coming up; continues until space is met. 
-for digit in range(10):
-    if digit == 0:
-        table[str(digit)] = "n" + table["j"]
-        continue
+    # add the digits
+    numberFlag = ".O.OOO" # when met, everything that follows is a number UNTIL space pattern
+    decimalFlag = ".O...O" # signifies a fractional part coming up; continues until space is met. 
+    for digit in range(10):
+        if digit == 0:
+            table[str(digit)] = "n" + table["j"]
+            continue
 
-    table[str(digit)] = "n" + table[chr(ord('a') + digit - 1)] 
+        table[str(digit)] = "n" + table[chr(ord('a') + digit - 1)] 
 
-# add capital letters
-capitalFlag = ".....O"
-# when met, only next letter is capital
-for ascii in range(ord('a'), ord('z') + 1):
-    table[chr(ascii).upper()] = capitalFlag + table[chr(ascii)]
+    # add capital letters
+    capitalFlag = ".....O"
+    # when met, only next letter is capital
+    for ascii in range(ord('a'), ord('z') + 1):
+        table[chr(ascii).upper()] = capitalFlag + table[chr(ascii)]
 
-# Reverse table into a Braille to English table
-braille_table = {}
-for key, braille in table.items():
-    braille_table[braille] = key
+    # Reverse table into a Braille to English table
+    braille_table = {}
+    for key, braille in table.items():
+        braille_table[braille] = key
+
+    return table, braille_table
+
 
 # a function to translate English input to Braille
-def englishToBraille(inputString:str):
+def englishToBraille(table:dict, inputString:str):
     capitalFlag = ".....O"
     numberFlag = ".O.OOO"
     decimalFlag = ".O...O"
@@ -114,7 +120,7 @@ def englishToBraille(inputString:str):
     return translation
 
 
-def brailleToEnglish(inputString:str):
+def brailleToEnglish(braille_table:dict, inputString:str):
     pass
     # parse 6 characters at a time
     inputArray = list(inputString)
@@ -141,7 +147,7 @@ def brailleToEnglish(inputString:str):
             # If number flag is found
             index += 1
             while index < len(groups):
-                translation += braille_table.get("n" + groups[index], "?")  # Use "?" for unrecognized Braille
+                translation += braille_table["n" + groups[index]]  # Use "?" for unrecognized Braille
                 index += 1
                 if index < len(groups) and groups[index - 1] == " ":
                     break  # Stop if we hit a space
@@ -150,22 +156,46 @@ def brailleToEnglish(inputString:str):
             # If decimal flag is found, treat next group as a decimal
             index += 1
             if index < len(groups):
-                translation += braille_table.get(groups[index], "?")
+                translation += braille_table[groups[index]]
                 index += 1  # Move past the number after the flag
 
         else:
             # Normal character translation
-            translation += braille_table.get(groups[index], "?")
+            translation += braille_table[groups[index]]
 
         index += 1  # Move to the next group
 
 
     return translation
 
+def main():
+    # Build the translation tables
+    english_braille_table, braille_english_table = buildLookupTable()
 
+    # Command-line interface
+    print("Welcome to the Braille Translator!")
+    print("Choose an option:")
+    print("1. Translate English to Braille")
+    print("2. Translate Braille to English")
+    print("3. Exit")
+
+    while True:
+        choice = input("Enter your choice (1/2/3): ")
         
+        if choice == "1":
+            english_input = input("Enter the English text to translate to Braille: ")
+            braille_output = englishToBraille(english_braille_table, english_input)
+            print(f"Braille output: {braille_output}\n")
+        elif choice == "2":
+            braille_input = input("Enter the Braille text to translate to English (use dots and spaces): ")
+            english_output = brailleToEnglish(braille_english_table, braille_input)
+            print(f"English output: {english_output}\n")
+        elif choice == "3":
+            print("Exiting the Braille Translator. Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
 
-
-
-        # check if special char 
+if __name__ == "__main__":
+    main()
          
