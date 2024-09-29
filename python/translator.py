ca9ec260473c401d2784db08cf79d9c3f66bdd78
Maggie Chen <97855985+maggieyc@users.noreply.github.com>
2024-09-29 18:15:57 +0200

    reformat local variables

--- python/translator.py
+++ python/translator.py
@@ -4,7 +4,53 @@ import re
 BRAILLE_CHAR_LENGTH = 6
 
 
-def translate_braille(text, braille_dict, braille_numbers_dict):
+def translate_braille(text):
+
+  braille_dict = {
+    'O.....': 'A',
+    'O.O...': 'B',
+    'OO....': 'C',
+    'OO.O..': 'D',
+    'O..O..': 'E',
+    'OOO...': 'F',
+    'OOOO..': 'G',
+    'O.OO..': 'H',
+    '.OO...': 'I',
+    '.OOO..': 'J',
+    'O...O.': 'K',
+    'O.O.O.': 'L',
+    'OO..O.': 'M',
+    'OO.OO.': 'N',
+    'O..OO.': 'O',
+    'OOO.O.': 'P',
+    'OOOOO.': 'Q',
+    'O.OOO.': 'R',
+    '.OO.O.': 'S',
+    '.OOOO.': 'T',
+    'O...OO': 'U',
+    'O.O.OO': 'V',
+    '.OOO.O': 'W',
+    'OO..OO': 'X',
+    'OO.OOO': 'Y',
+    'O..OOO': 'Z',
+    '.....O': 'capital_follow',
+    '.O.OOO': 'number_follow',
+    '......': ' '
+  }
+
+  braille_numbers_dict = {
+    'O.....': '1',
+    'O.O...': '2',
+    'OO....': '3',
+    'OO.O..': '4',
+    'O..O..': '5',
+    'OOO...': '6',
+    'OOOO..': '7',
+    'O.OO..': '8',
+    '.OO...': '9',
+    '.OOO..': '0'
+  }
+
   result = ""
   is_number = False
   is_capital = False
@@ -33,7 +79,50 @@ def translate_braille(text, braille_dict, braille_numbers_dict):
 
   return result
 
-def translate_english(text, english_dict):
+def translate_english(text):
+
+  english_dict = {
+    'A': 'O.....',
+    'B': 'O.O...',
+    'C': 'OO....',
+    'D': 'OO.O..',
+    'E': 'O..O..',
+    'F': 'OOO...',
+    'G': 'OOOO..',
+    'H': 'O.OO..',
+    'I': '.OO...',
+    'J': '.OOO..',
+    'K': 'O...O.',
+    'L': 'O.O.O.',
+    'M': 'OO..O.',
+    'N': 'OO.OO.',
+    'O': 'O..OO.',
+    'P': 'OOO.O.',
+    'Q': 'OOOOO.',
+    'R': 'O.OOO.',
+    'S': '.OO.O.',
+    'T': '.OOOO.',
+    'U': 'O...OO',
+    'V': 'O.O.OO',
+    'W': '.OOO.O',
+    'X': 'OO..OO',
+    'Y': 'OO.OOO',
+    'Z': 'O..OOO',
+    '1': 'O.....',
+    '2': 'O.O...',
+    '3': 'OO....',
+    '4': 'OO.O..',
+    '5': 'O..O..',
+    '6': 'OOO...',
+    '7': 'OOOO..',
+    '8': 'O.OO..',
+    '9': '.OO...',
+    '0': '.OOO..',
+    'capital_follow': '.....O',
+    'number_follow': '.O.OOO',
+    ' ': '......'
+  }
+  
   result = ""
   is_number = False
 
@@ -56,96 +145,8 @@ def is_braille(l):
 
 if __name__ == "__main__":
 
-  english_dict = {
-  'A': 'O.....',
-  'B': 'O.O...',
-  'C': 'OO....',
-  'D': 'OO.O..',
-  'E': 'O..O..',
-  'F': 'OOO...',
-  'G': 'OOOO..',
-  'H': 'O.OO..',
-  'I': '.OO...',
-  'J': '.OOO..',
-  'K': 'O...O.',
-  'L': 'O.O.O.',
-  'M': 'OO..O.',
-  'N': 'OO.OO.',
-  'O': 'O..OO.',
-  'P': 'OOO.O.',
-  'Q': 'OOOOO.',
-  'R': 'O.OOO.',
-  'S': '.OO.O.',
-  'T': '.OOOO.',
-  'U': 'O...OO',
-  'V': 'O.O.OO',
-  'W': '.OOO.O',
-  'X': 'OO..OO',
-  'Y': 'OO.OOO',
-  'Z': 'O..OOO',
-  '1': 'O.....',
-  '2': 'O.O...',
-  '3': 'OO....',
-  '4': 'OO.O..',
-  '5': 'O..O..',
-  '6': 'OOO...',
-  '7': 'OOOO..',
-  '8': 'O.OO..',
-  '9': '.OO...',
-  '0': '.OOO..',
-  'capital_follow': '.....O',
-  'number_follow': '.O.OOO',
-  ' ': '......'
-}
-
-braille_dict = {
-  'O.....': 'A',
-  'O.O...': 'B',
-  'OO....': 'C',
-  'OO.O..': 'D',
-  'O..O..': 'E',
-  'OOO...': 'F',
-  'OOOO..': 'G',
-  'O.OO..': 'H',
-  '.OO...': 'I',
-  '.OOO..': 'J',
-  'O...O.': 'K',
-  'O.O.O.': 'L',
-  'OO..O.': 'M',
-  'OO.OO.': 'N',
-  'O..OO.': 'O',
-  'OOO.O.': 'P',
-  'OOOOO.': 'Q',
-  'O.OOO.': 'R',
-  '.OO.O.': 'S',
-  '.OOOO.': 'T',
-  'O...OO': 'U',
-  'O.O.OO': 'V',
-  '.OOO.O': 'W',
-  'OO..OO': 'X',
-  'OO.OOO': 'Y',
-  'O..OOO': 'Z',
-  '.....O': 'capital_follow',
-  '.O.OOO': 'number_follow',
-  '......': ' '
-}
-
-braille_numbers_dict = {
-  'O.....': '1',
-  'O.O...': '2',
-  'OO....': '3',
-  'OO.O..': '4',
-  'O..O..': '5',
-  'OOO...': '6',
-  'OOOO..': '7',
-  'O.OO..': '8',
-  '.OO...': '9',
-  '.OOO..': '0'
-}
-
-input_args = sys.argv[1:]
-
-if is_braille(input_args):
-  print(translate_braille(input_args, braille_dict, braille_numbers_dict))
-else:
-  print(translate_english(input_args, english_dict))
+  input_args = sys.argv[1:]
+  if is_braille(input_args):
+    print(translate_braille(input_args))
+  else:
+    print(translate_english(input_args))
