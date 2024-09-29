7d98cec011af87e12f9da9b423487c2304b3946b
lei23lei <lei23lei91@gmail.com>
2024-09-29 19:23:22 -0400

    adjust braileMAP

--- javascript/translator.js
+++ javascript/translator.js
@@ -1,70 +1,77 @@
-// Braille Alphabet Mapping
-const brailleAlphabets = {
-    'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..',
-    'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..',
-    'k': 'O...O.', 'l': 'O.O.O.', 'm': 'OO..O.', 'n': 'OO.OO.', 'o': 'O..OO.',
-    'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.',
-    'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO', 'y': 'OO.OOO', 'z': 'O..OOO',
-    ' ': '......', 
-    '1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..', '5': 'O..O..',
-    '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...', '0': '.OOO..'
+const brailleMap = {
+    "a": "O.....", "b": "O.O...", "c": "OO....", "d": "OO.O..", "e": "O..O..",
+    "f": "OOO...", "g": "OOOO..", "h": "O.OO..", "i": ".OO...", "j": ".OOO..",
+    "k": "O...O.", "l": "O.O.O.", "m": "OO..O.", "n": "OO.OO.", "o": "O..OO.",
+    "p": "OOO.O.", "q": "OOOOO.", "r": "O.OOO.", "s": ".OO.O.", "t": ".OOOO.",
+    "u": "O...OO", "v": "O.O.OO", "w": ".OOO.O", "x": "OO..OO", "y": "OO.OOO",
+    "z": "O..OOO",
+    "1": "O.....", "2": "O.O...", "3": "OO....", "4": "OO.O..", "5": "O..O..",
+    "6": "OOO...", "7": "OOOO..", "8": "O.OO..", "9": ".OO...", "0": ".OOO..",
+    " ": "......", 'capital': '.....O', 'number': '.O.OOO'
 };
 
-// Reverse Mapping
-const brailleToEnglish = Object.keys(brailleAlphabets).reduce((obj, key) => {
-    obj[brailleAlphabets[key]] = key;
-    return obj;
-}, {});
+const reverseBrailleMap = Object.fromEntries(Object.entries(brailleMap).map(([k, v]) => [v, k]));
 
-// Function to Determine if String is Braille
-function isBraille(str) {
-    return /^[O.]+$/.test(str);
+function isBraille(text) {
+    return /^[O.]{6,}$/.test(text);
 }
 
-// Function to Translate Braille to English
-function translateToEnglish(brailleText) {
-    let english = '';
-    let isCapital = false;
+function translateToBraille(text) {
+    let brailleTranslation = '';
+    let isNumber = false;
+    for (let char of text) {
+        if (char >= 'A' && char <= 'Z') {
+            brailleTranslation += brailleMap['capital'];
+            char = char.toLowerCase();
+        }
+        if (char >= '0' && char <= '9' && !isNumber) {
+            brailleTranslation += brailleMap['number'];
+            isNumber = true;
+        } else if (char === ' ') {
+            isNumber = false;
+        }
+        brailleTranslation += brailleMap[char] || '';
+    }
+    return brailleTranslation;
+}
 
+function translateToEnglish(brailleText) {
+    let englishTranslation = '';
+    let isCapital = false;
+    let isNumber = false;
     for (let i = 0; i < brailleText.length; i += 6) {
         const brailleChar = brailleText.slice(i, i + 6);
-        if (brailleChar === '.....O') { // Capital letter flag
+        if (brailleChar === brailleMap['capital']) {
             isCapital = true;
             continue;
         }
-
-        const translatedChar = brailleToEnglish[brailleChar];
-        if (translatedChar !== undefined) {
-            english += isCapital ? translatedChar.toUpperCase() : translatedChar;
-            isCapital = false; // Reset capital flag
-        } 
-    }
-    return english;
-}
-
-// Function to Translate English to Braille
-function translateToBraille(text) {
-    let braille = '';
-    
-    for (const char of text) {
-        if (/[A-Z]/.test(char)) {
-            braille += '.....O' + brailleAlphabets[char.toLowerCase()]; // Capital letter
-        } else if (brailleAlphabets[char]) {
-            braille += brailleAlphabets[char];
+        if (brailleChar === brailleMap['number']) {
+            isNumber = true;
+            continue;
+        }
+        let char = reverseBrailleMap[brailleChar] || '';
+        if (isCapital) {
+            char = char.toUpperCase();
+            isCapital = false;
+        }
+        if (isNumber && char >= 'a' && char <= 'j') {
+            char = (char.charCodeAt(0) - 'a'.charCodeAt(0) + 1).toString();
+        }
+        englishTranslation += char;
+        if (char === ' ') {
+            isNumber = false;
         }
     }
-    return braille;
+    return englishTranslation;
 }
 
-// Main Function to Handle Translation
-function brailleTranslator(input) {
+function main() {
+    const input = process.argv.slice(2).join(' ');
     if (isBraille(input)) {
-        return translateToEnglish(input);
+        console.log(translateToEnglish(input));
     } else {
-        return translateToBraille(input);
+        console.log(translateToBraille(input));
     }
 }
 
-// Get input from command line arguments
-const input = process.argv.slice(2).join(' ');
-console.log(brailleTranslator(input));
+main();
