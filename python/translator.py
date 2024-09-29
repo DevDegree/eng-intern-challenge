69f0fee821e75e67e357a3d1fe663020380a2ed4
Amine-chisquare <aminesahraouics@outlook.com>
2024-09-29 13:07:21 -0400

    Fixing the table

--- python/translator.py
+++ python/translator.py
@@ -16,19 +16,30 @@ table = {
 }
 
 # In the range of letters ["k", ..., "t"], the ith letter can be related to the ith letter in the range ["a", ..., "j"]:
-rangeTenTwenty = [chr(i) for i in range(97 + 10, 97+20)] # the ten next letters after j (after the base set)
+rangeTenTwenty = [chr(i) for i in range(ord('k'), ord('t') + 1)] # the ten next letters after j (after the base set)
 for letter in rangeTenTwenty:
     base_braille = list(table[chr(ord(letter) - 10)]) # get the braille rep of the corresponding letter in the base set
     base_braille[4] = 'O' # the rule relating the letters in the base set to those in rangeTenTwenty is that the '.' at index 4 is replaced by a 'O'
     table[letter] = "".join(base_braille)
 
-# Same concept for the letters ["u", ..., "z"] 
-rangeTwentyTwentySix = [chr(i) for i in range(97 + 20, 97+26)] # the six next letters after t 
-for letter in rangeTwentyTwentySix:
-    base_braille = list(table[chr(ord(letter) - 10)]) # get the braille rep of the corresponding letter in the base set
+# Same concept for the letters ["u", ..., "v"] 
+rangeTwentyTwentyTwo = [chr(i) for i in range(ord('u'), ord('z') + 1)] # the two next letters after t 
+offset = 10
+
+for letter in rangeTwentyTwentyTwo:
+    index = chr(ord(letter) - 10) 
+
+    if chr(ord(letter) - 10) == 'w':
+        table['w'] = ".OOO.O" # w is an exception to the rule we came up with
+        offset = 11 # the bases of the letters after w are then offset by 1 more 
+        continue
+
+    base_braille = list(table[chr(ord(letter) - offset)]) # get the braille rep of the corresponding letter in the base set
     base_braille[5] = 'O' # '.' at index 5 is replaced by a 'O'
     table[letter] = "".join(base_braille)
 
-table['w'] = ".OOO.O" # an exception to the rule we came up with
+
+
+# and finally ["x", "y", "z"] follow the same rule
 
 print(table)
