4c1d8be0b20caf1a5b5d57aa6ad7b5abf64fa7f9
ryan wong <ryanpersonal18@gmail.com>
2024-09-29 06:14:36 -0600

    Braille map fix

--- python/translator.py
+++ python/translator.py
@@ -7,7 +7,7 @@ braille_letters = {
     "k": "O...O.", "l": "O.O.O.", "m": "OO..O.", "n": "OO.OO.", "o": "O..OO.",
     "p": "OOO.O.", "q": "OOOOO.", "r": "O.OOO.", "s": ".OO.O.", "t": ".OOOO.",
     "u": "O...OO", "v": "O.O.OO", "w": ".OOO.O", "x": "OO..OO", "y": "OO.OOO",
-    "z": "O..OO)", " ": "......",  # Space
+    "z": "O..OOO", " ": "......",  # Space
 }
 
 braille_numbers = {
