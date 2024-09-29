84e100e53b5692b3f382181da28692f1a5d8d8e1
Emmanuel Alofe <oluwatopealofe@gmail.com>
2024-09-29 02:39:36 -0400

    fix english to braille translation function to work properly and add reading input from command line

--- python/translator.py
+++ python/translator.py
@@ -2,48 +2,49 @@
 English to Braille and Braille to English translator
 """
 
+import sys
 
 def translate_english_to_braille(phrase):
     english_chars_to_braille = {
-        'a': '0.....',
-        'b': '0.0...',
-        'c': '00....',
-        'd': '00.0..',
-        'e': '0..0..',
-        'f': '000...',
-        'g': '0000..',
-        'h': '0.00..',
-        'i': '.00...',
-        'j': '.000..',
-        'k': '0....0',
-        'l': '0.0.0.',
-        'm': '00..0.',
-        'n': '00.00.',
-        'o': '0..00.',
-        'p': '000.0.',
-        'q': '00000.',
-        'r': '0.000.',
-        's': '.00.0.',
-        't': '.0000.',
-        'u': '0...00',
-        'v': '0.0.00',
-        'w': '.000.0',
-        'x': '00..00',
-        'y': '00.000',
-        'z': '0..000',
+        'a': 'O.....',
+        'b': 'O.O...',
+        'c': 'OO....',
+        'd': 'OO.O..',
+        'e': 'O..O..',
+        'f': 'OOO...',
+        'g': 'OOOO..',
+        'h': 'O.OO..',
+        'i': '.OO...',
+        'j': '.OOO..',
+        'k': 'O....O',
+        'l': 'O.O.O.',
+        'm': 'OO..O.',
+        'n': 'OO.OO.',
+        'o': 'O..OO.',
+        'p': 'OOO.O.',
+        'q': 'OOOOO.',
+        'r': 'O.OOO.',
+        's': '.OO.O.',
+        't': '.OOOO.',
+        'u': 'O...OO',
+        'v': 'O.O.OO',
+        'w': '.OOO.O',
+        'x': 'OO..OO',
+        'y': 'OO.OOO',
+        'z': 'O..OOO',
         ' ': '......',
-        '1': '0.....',
-        '2': '0.0...',
-        '3': '00....',
-        '4': '00.0..',
-        '5': '0..0..',
-        '6': '000...',
-        '7': '0000..',
-        '8': '0.00..',
-        '9': '.00...',
-        '0': '.000..',
-        'CAP': '.....0',
-        'NUM': '.0.000',
+        '1': 'O.....',
+        '2': 'O.O...',
+        '3': 'OO....',
+        '4': 'OO.O..',
+        '5': 'O..O..',
+        '6': 'OOO...',
+        '7': 'OOOO..',
+        '8': 'O.OO..',
+        '9': '.OO...',
+        'O': '.OOO..',
+        'CAP': '.....O',
+        'NUM': '.O.OOO',
         }
 
     braille_translation = []
@@ -60,8 +61,6 @@ def translate_english_to_braille(phrase):
         else:
             if num_mode:
                 num_mode = False
-                braille_translation.append(english_chars_to_braille[' '
-                        ])
 
             if char.isupper():
                 braille_translation.extend([english_chars_to_braille['CAP'
@@ -74,48 +73,48 @@ def translate_english_to_braille(phrase):
 
 def translate_braille_to_english(phrase):
     braille_to_english_letters_mods = {
-        '0.....': 'a',
-        '0.0...': 'b',
-        '00....': 'c',
-        '00.0..': 'd',
-        '0..0..': 'e',
-        '000...': 'f',
-        '0000..': 'g',
-        '0.00..': 'h',
-        '.00...': 'i',
-        '.000..': 'j',
-        '0....0': 'k',
-        '0.0.0.': 'l',
-        '00..0.': 'm',
-        '00.00.': 'n',
-        '0..00.': 'o',
-        '000.0.': 'p',
-        '00000.': 'q',
-        '0.000.': 'r',
-        '.00.0.': 's',
-        '.0000.': 't',
-        '0...00': 'u',
-        '0.0.00': 'v',
-        '.000.0': 'w',
-        '00..00': 'x',
-        '00.000': 'y',
-        '0..000': 'z',
+        'O.....': 'a',
+        'O.O...': 'b',
+        'OO....': 'c',
+        'OO.O..': 'd',
+        'O..O..': 'e',
+        'OOO...': 'f',
+        'OOOO..': 'g',
+        'O.OO..': 'h',
+        '.OO...': 'i',
+        '.OOO..': 'j',
+        'O....O': 'k',
+        'O.O.O.': 'l',
+        'OO..O.': 'm',
+        'OO.OO.': 'n',
+        'O..OO.': 'o',
+        'OOO.O.': 'p',
+        'OOOOO.': 'q',
+        'O.OOO.': 'r',
+        '.OO.O.': 's',
+        '.OOOO.': 't',
+        'O...OO': 'u',
+        'O.O.OO': 'v',
+        '.OOO.O': 'w',
+        'OO..OO': 'x',
+        'OO.OOO': 'y',
+        'O..OOO': 'z',
         '......': ' ',
-        '.....0': 'CAP',
-        '.0.000': 'NUM',
+        '.....O': 'CAP',
+        '.O.OOO': 'NUM',
         }
 
     braille_to_english_numbers = {
-        '0.....': '1',
-        '0.0...': '2',
-        '00....': '3',
-        '00.0..': '4',
-        '0..0..': '5',
-        '000...': '6',
-        '0000..': '7',
-        '0.00..': '8',
-        '.00...': '9',
-        '.000..': '0',
+        'O.....': '1',
+        'O.O...': '2',
+        'OO....': '3',
+        'OO.O..': '4',
+        'O..O..': '5',
+        'OOO...': '6',
+        'OOOO..': '7',
+        'O.OO..': '8',
+        '.OO...': '9',
+        '.OOO..': 'O',
         }
 
     phrase_length = len(phrase)
@@ -155,19 +154,16 @@ def translate_braille_to_english(phrase):
 
 def non_braille_chars_present(phrase):
     for char in phrase:
-        if char != '0' and char != '.':
+        if char != 'O' and char != '.':
             return True
     return False
 
 
 def main():
-    phrase = input()
+    if len(sys.argv) >= 2:
+        user_input = " ".join(sys.argv[1:])
+        if non_braille_chars_present(user_input):
+            return translate_english_to_braille(user_input)
+        return translate_braille_to_english(user_input)
 
-    if phrase:
-        if non_braille_chars_present(phrase):
-            return translate_english_to_braille(phrase)
-        return translate_braille_to_english(phrase)
-    return ''
-
-
-print(main())
+sys.stdout.write(main())
