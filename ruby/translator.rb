e18cf434d7163aed579cb42289b879fd85e8819d
Jiani Cao <jianicakes@gmail.com>
2024-09-28 18:07:12 -0700

    more refactoring and getting rid of redundancy

--- ruby/translator.rb
+++ ruby/translator.rb
@@ -1,4 +1,4 @@
-alphabet_map = {
+ALPHABET_TO_BRAILLE = {
   'a' => "O.....",
   'b' => "O.O...",
   'c' => "OO....",
@@ -25,21 +25,10 @@ alphabet_map = {
   'x' => "OO..OO",
   'y' => "OO.OOO",
   'z' => "O..OOO",
-  '.' => "..OO.O",
-  ',' => "..O...",
-  '?' => "..O.OO",
-  '!' => "..OOO.",
-  ':' => "..OO..",
-  ';' => "..O.O.",
-  '-' => "....OO",
-  '/' => ".O..O.",
-  '<' => ".OO..O",
-  '>' => "O..OO.",
-  '(' => "O.O..O",
-  ')' => ".O.OO.",
-}
+  ' ' => "......"
+}.freeze
 
-numbers_map = {
+NUMBER_TO_BRAILLE = {
   '1' => "O.....",
   '2' => "O.O...",
   '3' => "OO....",
@@ -50,64 +39,71 @@ numbers_map = {
   '8' => "O.OO..",
   '9' => ".OO...",
   'O' => ".OOO.."
-}
+}.freeze
 
-SPACE = '......'
+BRAILLE_TO_ALPHABET = ALPHABET_TO_BRAILLE.invert.freeze
+BRAILLE_TO_NUMBER = NUMBER_TO_BRAILLE.invert.freeze
+
 CAPITAL_FOLLOWS = '.....O'
 NUMBER_FOLLOWS = '.O.OOO'
 
 input_string = ARGV.join(' ')
+is_braille = input_string.count('^.O').zero? && (input_string.length % 6).zero?
 
-capslock_on = false
-number_mode = false
+def convert_to_braille(input_string)
+  number_mode = false
+  output = ''
 
-##### if braille #####
-if input_string.count('^.O').zero? && (input_string.length % 6).zero?
-## split input_string into chunks of 6
+  input_string.each_char do |char|
+    if char =~ /[0-9]/ # current char is number
+      unless number_mode # add number follows symbol if first number
+        output += NUMBER_FOLLOWS
+        number_mode = true
+      end
+      output += NUMBER_TO_BRAILLE[char]
+    else
+      # add capital follows symbol if char is in uppercase
+      output += CAPITAL_FOLLOWS if char =~ /[A-Z]/
+      output += ALPHABET_TO_BRAILLE[char.downcase]
+      number_mode = false if number_mode
+    end
+  end
+
+  output
+end
+
+def convert_to_english(input_string)
+  capslock_on = false
+  number_mode = false
+
+  # split into chunks of braille characters
   chunks = input_string.scan(/.{1,6}/)
+
   output = chunks.map do |chunk|
     if chunk == CAPITAL_FOLLOWS
       capslock_on = true
-      next
+      next # skip to next iteration
     elsif chunk == NUMBER_FOLLOWS
       number_mode = true
-      next
+      next # skip to next iteration
     elsif capslock_on
       capslock_on = false
-      alphabet_map.key(chunk).upcase
+      BRAILLE_TO_ALPHABET[chunk].upcase
     elsif number_mode
-      if chunk == SPACE
+      if chunk == ALPHABET_TO_BRAILLE[' '] # exit number mode if chunk is space char
         number_mode = false
         ' '
-      else # else convert to number using numbers map
-        numbers_map.key(chunk)
-      end
-    else # convert alphabet
-      alphabet_map.key(chunk)
-    end
-  end
-  output = output.join
-
-else  ##### assume string #####
-  output = ''
-  input_string.each_char do |char|
-    
-    if char.to_i.to_s == char # if current char is number
-      if number_mode
-        output += numbers_map[char]
       else
-        number_mode = true
-        output += NUMBER_FOLLOWS
-        output += numbers_map[char]
+        BRAILLE_TO_NUMBER[chunk]
       end
-    elsif char == ' '
-      number_mode = false
-      output += SPACE
-    else # assuming current char is alphabet
-      output += char == char.upcase ? CAPITAL_FOLLOWS : ''
-      output += alphabet_map[char.downcase]
+    else
+      BRAILLE_TO_ALPHABET[chunk]
     end
   end
+
+  output.join
 end
+
+output = is_braille ? convert_to_english(input_string) : convert_to_braille(input_string)
 
 puts output
