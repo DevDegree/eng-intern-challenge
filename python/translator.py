1460f903576af282f3ba6677e6b15c5ab3eaf5de
Sriram <sriramjj5@gmail.com>
2024-09-29 15:12:24 -0700

    Complete all character maps

--- python/translator.py
+++ python/translator.py
@@ -1,5 +1,26 @@
-letters = {'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..', 
+# could definitely merge into one map, but feel like this is easier to read/maintain
+# especially when reversing key:values for reverse translations -- there could be duplicates if it was all one map
+
+letters = {
+        'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..', 
         'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..', 
         'k': 'O...O.', 'l': 'O.O.O.', 'm': 'OO..O.', 'n': 'OO.OO.', 'o': 'O..OO.', 
         'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.', 
-        'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO', 'y': 'OO.OOO', 'z': 'O..OOO'}
+        'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO', 'y': 'OO.OOO', 
+        'z': 'O..OOO'
+}
+
+numbers = {
+        '1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..', '5': 'O..O..',
+        '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...', '0': '.OOO..'
+}
+
+symbols = {
+        '.': '..OO.O', ',': '..O...', '?': '..O.OO', '!': '..OOO.', ':': '..OO..', 
+        ';': '..O.O.', '-': '....OO', '/': '.O..O.', '<': '.OO..O', '>': 'O..OO.',
+        '(': 'O.O..O', ')': '.O.OO.', ' ': '......'
+}
+
+flags = {
+        'capital': '.....O', 'decimal': '.O...O', 'number': '.O.OOO'
+}
