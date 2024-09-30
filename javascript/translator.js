606a4b1ea4b413f2493dd560aa1e0d97a3d7b138
Ammar Ahmed <ammar.ahmed1@uwaterloo.ca>
2024-09-30 00:51:10 -0400

    braille or english input

--- javascript/translator.js
+++ javascript/translator.js
@@ -1,1 +1,12 @@
 const input = process.argv.slice(2).join(" ");
+
+function isBraille(input) {
+  for (let char of input) {
+    if (char !== "." && char !== "O") {
+      return false;
+    }
+  }
+
+  return true;
+}
+
