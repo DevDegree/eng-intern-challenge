82a12b4401e6e07c112654fb1fac936c472a2e0e
usaid <usaid.malik@nyu.edu>
2024-09-29 22:40:34 -0400

    added documentation for all functions and methods

--- python/translator.py
+++ python/translator.py
@@ -89,85 +89,152 @@ class Translator:
         letters the following loop looks for that . to determine if it is Braille 
         """
         i = 0
-        while i < len(self.sourceText) and i < 6 and self.sourceText[i] != ".":
+        while i < len(sourceText) and i < 6 and sourceText[i] != ".":
             i += 1
         
-
         """
-        In this condition of the iterator is unable to get
+        In this condition if the iterator is unable to get
         past the 6th character it means that a . was encountered
-        and the chosen text is braille otherwise the chosen text is in english 
+        and the chosen text is Braille otherwise the chosen text is in English 
         """
         if i < 6:
-            return self._Braille2English()
-        return self._English2Braille() 
+            return self._Braille2English(sourceText)
+        return self._English2Braille(sourceText) 
          
 
-    def _English2Braille(self, sourceText):
-        
+    def _English2Braille(self, englishText: str) -> str:
+        """
+        Converts English text into its Braille representation.
+
+        Handles capitalization using a special Braille sequence for capital letters. If numbers
+        are encountered, a number indicator is inserted.
+        params: 
+            sourceText (str): The input text to translate to braille
+        returns:
+            str: The Braille translation of the English input.
+        """
         def convert():
+            """
+            Generator function that translates English text to Braille. It handles capital letters, digits, 
+            and other characters by converting them to the corresponding Braille representation.
+            
+            Yields:
+                str: Braille representation of the next character or sequence from the English text.
+            """
+
             i = 0 
-            while i < len(self.sourceText):
-                char = self.sourceText[i]
+            while i < len(englishText):
+                char = englishText[i] 
+                
                 if char.isupper():
-                    yield english2BrailleDictionary["CAP"]
-                    yield english2BrailleDictionary[char.lower()]
+                    yield english2BrailleDictionary["CAP"]  # inserting capitalization marker and getting the character
+                    yield english2BrailleDictionary[char.lower()] 
+                
                 elif char.isdigit():
-                    yield english2BrailleDictionary["NUMFOLLOWS"]
-                    """
-                    Variable change from char to instead self.sourceText[i]
-                    this was done to make the code shorter and have the loop 
-                    update the variable in the condition easier
-                    additionallly the checks are to ensure the next char is also a digit
-                    however if it does not terminate with a space the braille2english will not 
-                    work properly as in the case of 12A 
-                    """
-                    while i < len(self.sourceText) and self.sourceText[i].isdigit(): 
-                        yield english2BrailleNumerical[self.sourceText[i]]
-                        i += 1
+                    yield english2BrailleDictionary["NUMFOLLOWS"]  # Yield the Braille number indicator
 
-                    if i < len(self.sourceText):
-                        yield english2BrailleDictionary[self.sourceText[i]]
+                    """
+                    Handling consecutive digits by continuing the loop until a non-digit character is found.
+                    Instead of using the char variable again, directly accessing sourceText[i] to
+                    simplify the code and ensure that the loop updates correctly.
 
+                    Additionally, the loop goes until a non digit is found.
+                    The loop will break if the text ends or a non-digit character is encountered. However, 
+                    if the number sequence doesn't end with a space, the Braille translation will fail when translating back 
+                    from Braille to English. For instance, a sequence like 12A would not be handled in this case and 
+                    is assumed by the technical specifications to not be encounted.
+                    """
+                    while i < len(englishText) and englishText[i].isdigit():
+                        yield english2BrailleNumerical[englishText[i]]  
+                        i += 1 
+
+                    """Only add the next character that ended the sequence
+                     (presumably a space for proper execution)
+                     if i am not at the end of the string"""
+                    if i < len(englishText):
+                        yield english2BrailleDictionary[englishText[i]]
+                
                 else:
-                    yield english2BrailleDictionary[char]
-                i += 1
-        return ''.join(convert()) 
+                    yield english2BrailleDictionary[char] 
+                
+                i += 1  # Move to the next character in the sourceText
 
-    def _Braille2English(self, sourceText):
+        # After the generator has processed all characters, join the yielded Braille characters into a string
+        return ''.join(convert())
+
+
+    def _Braille2English(self, brailleText):
+        
+        """
+        Converts Braille text into its English representation following the specified
+        pattern from the technical specifications.
+   
+        params: 
+            brailleText (str): The braille text to translate to english
+        returns:
+            str: The English translation of the Braille input.
+        """
 
         def convert():
             i = 0
-            while i < len(self.sourceText):
-                char = self.sourceText[i:i+6] # getting the six braille characters
+            while i < len(brailleText):
+                char = brailleText[i:i+6] # incrementing by 6 since each Braille cell is 6 characters
+
+                # Handle Braille uppercase marker
                 if char == ".....O":
-                    i += 6 # incrementing i to get to the next character
-                    nextChar = self.sourceText[i: i + 6] # this assumes the braille is valid
-                    yield braille2EnglishDictionary[nextChar].upper() # the capitalized character that was found
+                    i += 6 
+                    # this assumes the braille is valid and the next character is a letter
+                    char = brailleText[i: i + 6] 
+                    yield braille2EnglishDictionary[char].upper()
+
+                # Handle Braille numeric marker
                 elif char == ".O.OOO":
-                    # this means evertyhign after is a num until i hit a space character
-                    i += 6 # incrementing to see the next braille numbers
-                    while i < len(self.sourceText) and self.sourceText[i:i + 6] != "......": # i keep going until my braille is a space or i reach the end of the string
-                        yield Braille2EnglishNumerical[self.sourceText[i:i + 6]] 
+                    
+                    i += 6 # incrementing to move past the numeric marker
+
+                    """
+                    Handling the consecutive digits by going through them with a skip 
+                    of 6, this assumes that all the next digits are numbers and are
+                    valid braille until a space braille character is encountered
+                    """                    
+                    while i < len(brailleText) and brailleText[i:i + 6] != "......": 
+                        yield Braille2EnglishNumerical[brailleText[i:i + 6]] 
                         i += 6
 
-                    if i < len(self.sourceText):
-                        yield " " # yielding a space since my increment stopped at one and the next increment will move past it
+                    if i < len(brailleText):
+                        yield " " # yielding a space since my increment stopped at a space assuming valid braille, otherwise would be skipped
                 else:
-                    yield braille2EnglishDictionary[char] # yielding the normal character
+                    yield braille2EnglishDictionary[char] 
                 
-                i += 6 # incrementing to the next character once the loop ends
+                i += 6
+                
         return ''.join(convert())
+
+
 def main(argc: int, argv: List[int]):
+    """
+    Main function to handle command line arguments and initiate translation.
+
+    Args:
+        argc (int): The number of command line arguments passed.
+        argv (List[int]): List of command line arguments.
+    """
+
+    # If less than two arguments are passed then the translation cannot proceed
     if argc < 2:
         print("IMPROPER USAGE! Correct Usage: python translator.py [source_text]")
         return
-    sourceText = " ".join(argv[1:]) # getting the source text passed in an joining via a space
-    translator = Translator(sourceText)
-    translatedText = translator.translate()
+
+    # Join the passed arguments (excluding the script name) to form the source text
+    sourceText: str = " ".join(argv[1:])
+
+    translator = Translator()
+    translatedText: str = translator.translate(sourceText)
+
     print(translatedText)
+
+
 
 if __name__ == "__main__":
     argv = sys.argv # this is in a variable so i dont call sys.argv twice
     main(len(argv), argv)
-    # passing in the source text from the CLI
