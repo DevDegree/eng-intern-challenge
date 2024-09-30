b3988d0bfb4a950b8320f3524fb225c10278f231
Ammar Ahmed <ammar.ahmed1@uwaterloo.ca>
2024-09-30 01:07:54 -0400

    util funcs + braille maps

---
+++ javascript/alphabet.js
@@ -0,0 +1,36 @@
+
+const alphabet = {
+  'A': "O.....",
+  'B': "O.O...",
+  'C': "OO....",
+  'D': "OO.O..",
+  'E': "O..O..",
+  'F': "OOO...",
+  'G': "OOOO..",
+  'H': "O.OO..",
+  'I': ".OO...",
+  'J': ".OOO..",
+  'K': "O...O.",
+  'L': "O.O.O.",
+  'M': "OO..O.",
+  'N': "OO.OO.",
+  'O': "O..OO.",
+  'P': "OOO.O.",
+  'Q': "OOOOO.",
+  'R': "O.OOO.",
+  'S': ".OO..O",
+  'T': ".OOO.O",
+  'U': "O...OO",
+  'V': "O.O.OO",
+  'W': ".OOO.O",
+  'X': "OO..OO",
+  'Y': "OO.OOO",
+  'Z': "O..OOO",
+}
+
+const numbers = Object.values(alphabet).slice(0, 10);
+
+module.exports = {
+  alphabet,
+  numbers
+}
--- javascript/translator.js
+++ javascript/translator.js
@@ -1,3 +1,5 @@
+const { alphabet, numbers } = require("./alphabet");
+
 const input = process.argv.slice(2).join(" ");
 
 function isBraille(input) {
@@ -10,3 +12,33 @@ function isBraille(input) {
   return true;
 }
 
+function isChar(str) {
+  if (str.length !== 1) {
+    throw new Error("'char' must be a single character string!");
+  }
+  return true;
+}
+
+function isNumber(char) {
+  if (isChar(char)) {
+    return !isNaN(parseFloat(char))
+  }
+}
+
+function isSpace(char) {
+  if (isChar(char)) {
+    return char === " ";
+  }
+}
+
+function isCapital(char) {
+  if (isChar(char)) {
+    return char.toUpperCase() === char;
+  }
+}
+
+if (isBraille(input)) {
+  // convert to english
+} else {
+  // convert to braille
+}
