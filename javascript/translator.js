6b98df2379b944bd3b8c469e65957b21c6c1c2b4
Karsten <karstenkleung@gmail.com>
2024-09-30 01:06:43 -0400

    Created braille map

---
+++ javascript/translator.js
@@ -0,0 +1,49 @@
+// Braille map object
+const brailleMap = {
+    // Alphabet
+    "alphabet": {
+        "A": "O.....",
+        "B": "O.O...",
+        "C": "OO....",
+        "D": "OO.O..",
+        "E": "O..O..",
+        "F": "OOO...",
+        "G": "OOOO..",
+        "H": "O.OO..",
+        "I": ".OO...",
+        "J": ".OOO..",
+        "K": "O...O.",
+        "L": "O.O.O.",
+        "M": "OO..O.",
+        "N": "OO.OO.",
+        "O": "O..OO.",
+        "P": "OOO.O.",
+        "Q": "OOOOO.",
+        "R": "O.OOO.",
+        "S": ".OO.O.",
+        "T": ".OOOO.",
+        "U": "O...OO",
+        "V": "O.O.OO",
+        "W": ".OOO.O",
+        "X": "OO..OO",
+        "Y": "OO.OOO",
+        "Z": "O..OOO",
+    },
+
+    // Numbers
+    "1": "O.....",
+    "2": "O.O...",
+    "3": "OO....",
+    "4": "OO.O..",
+    "5": "O..O..",
+    "6": "OOO...",
+    "7": "OOOO..",
+    "8": "O.OO..",
+    "9": ".OO...",
+    "0": ".OOO..",
+
+    // Following Elements and Others
+    "capital_follows": ".....O",
+    "number_follows": ".O.OOO",
+    "space": "......"
+};
