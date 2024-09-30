d7d0cbe1021fbafebc42bbcb286421abd1643b0e
Sriram <sriramjj5@gmail.com>
2024-09-29 18:33:32 -0700

    Add english to braille conversion

--- python/translator.py
+++ python/translator.py
@@ -1,7 +1,8 @@
 import sys
 
 # could definitely merge into one map, but feel like this is easier to read/maintain
-# especially when reversing key:values for reverse translations -- there could be duplicates if it was all one map
+# especially when reversing key:values for braille to english translations
+	# there could be duplicates if it was all one map
 
 letters = {
 	'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..', 
@@ -29,8 +30,7 @@ flags = {
 
 # if braille -- 
 	# (string length mod 6) == 0
-	# string will only contain "." or "O" 
-	# could also check to see if it converts to valid english, but i think that's overkill
+	# string will only contain "." or "O"
 def is_braille(txt):
 	if ((len(txt) % 6) != 0):
 		return False
@@ -39,11 +39,56 @@ def is_braille(txt):
 			return False
 	return True
 
+def english_to_braille(txt):
+	txt_in_braille = ""
+	numeric_flag = False
+
+	# 'decimal follows' flag?
+	# ambiguous in the spec
+
+	# according to actual braille language -- 
+	# if something like 0.50 -- just add the decimal point in between
+	# if .50 -- numeric mode flag -> decimal point -> numbers
+
+	# according to spec -- 
+	# 'When a Braille number follows symbol is read, assume all following symbols are numbers until the next space symbol.'
+		# ^ probably means not to worry about it
+		# if we assume only true for braille to english, converting the same thing back-and-forth would be messed up
+		# i'll leave it unimplemented for now 
+
+	for char in txt:
+		if (char.isdigit()):
+			if (numeric_flag == False):
+				numeric_flag = True
+				txt_in_braille += flags['number'] + numbers[char]
+			else:
+				txt_in_braille += numbers[char]
+			continue
+
+		if (char.islower()):
+			txt_in_braille += letters[char]
+		elif (char.isupper()):
+			txt_in_braille += flags['capital'] + letters[char.lower()]
+		elif (char in list(symbols.keys())):
+			txt_in_braille += symbols[char]
+		else:
+			sys.exit('Invalid character in input')
+
+	return txt_in_braille
+		
+
+		
+		
+
 def main():
 	input_arr = sys.argv[1:]
 	if not input_arr:
 		sys.exit('Invalid input')
 	input_str = ' '.join(input_arr)
+	if (is_braille(input_str)):
+		print("Not implemented yet")
+	else:
+		print(english_to_braille(input_str))
 
 if __name__ == "__main__":
 	main()
