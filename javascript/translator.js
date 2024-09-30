70f2ab82f00a174707cba169672d2a4f55cb5626
ConnorBP <connor.postma@gmail.com>
2024-09-30 03:24:08 -0400

    mostly completed braille to eng

--- javascript/translator.js
+++ javascript/translator.js
@@ -1,33 +1,37 @@
 
-// lookup table
+// Lookup Table Based Braille Translator
+// Author: Connor "ConnorBP" Postma
+// 2024-09-30
+
+// Missing: decimals handling
 
 const braille_lut = {
-    'a': 'o.....',
-    'b': 'o.o...',
-    'c': 'oo....',
-    'd': 'oo.o..',
-    'e': 'o..o..',
-    'f': 'ooo...',
-    'g': 'oooo..',
-    'h': 'o.oo..',
-    'i': '.oo...',
-    'j': '.ooo..',
-    'k': 'o...o.',
-    'l': 'o.o.o.',
-    'm': 'oo..o.',
-    'n': 'oo.oo.',
-    'o': 'o..oo.',
-    'p': 'ooo.o.',
-    'q': 'ooooo.',
-    'r': 'o.ooo.',
-    's': '.oo.o.',
-    't': '.oooo.',
-    'u': 'o...oo',
-    'v': 'o.o.oo',
-    'w': '.ooo.o',
-    'x': 'oo..oo',
-    'y': 'oo.ooo',
-    'z': 'o..ooo',
+    'a': 'O.....',
+    'b': 'O.O...',
+    'c': 'OO....',
+    'd': 'OO.O..',
+    'e': 'O..O..',
+    'f': 'OOO...',
+    'g': 'OOOO..',
+    'h': 'O.OO..',
+    'i': '.OO...',
+    'j': '.OOO..',
+    'k': 'O...O.',
+    'l': 'O.O.O.',
+    'm': 'OO..O.',
+    'n': 'OO.OO.',
+    'o': 'O..OO.',
+    'p': 'OOO.O.',
+    'q': 'OOOOO.',
+    'r': 'O.OOO.',
+    's': '.OO.O.',
+    't': '.OOOO.',
+    'u': 'O...OO',
+    'v': 'O.O.OO',
+    'w': '.OOO.O',
+    'x': 'OO..OO',
+    'y': 'OO.OOO',
+    'z': 'O..OOO',
     '1': '0.....',
     '2': '0.0...',
     '3': '00....',
@@ -39,64 +43,150 @@ const braille_lut = {
     '9': '.00...',
     '0': '.000..',
     // special commands
-    'CAPITAL': '.....o',
-    'DECIMAL': '.o...o',
-    'NUMBER': '.o.ooo',
-    '.': '..oo.o',
-    ',': '..o...',
-    '?': '..o.oo',
-    '!': '..ooo.',
-    ':': '..oo..',
-    ';': '..o.o.',
-    '-': '....oo',
-    '/': '.o..o.',
-    '<': '.oo..o',
-    '>': 'o..oo.',
-    '(': 'o.o..o',
-    ')': '.o.oo.',
+    'CAPITAL': '.....O',
+    'DECIMAL': '.O...O',
+    'NUMBER': '.O.OOO',
+    '.': '..OO.O',
+    ',': '..O...',
+    '?': '..O.OO',
+    '!': '..OOO.',
+    ':': '..OO..',
+    ';': '..O.O.',
+    '-': '....OO',
+    '/': '.O..O.',
+    '<': '.OO..O',
+    '>': 'O..OO.',
+    '(': 'O.O..O',
+    ')': '.O.OO.',
     ' ': '......',
 };
+
+// Create a reverse lookup table of the brail
+const english_lut = {};
+const numbers_lut = {};
+for (const [key, value] of Object.entries(braille_lut)) {
+    if(isDigit(key)) {
+        numbers_lut[value] = key;
+    } else {
+        english_lut[value] = key;
+    }
+}
+
+// console.log(numbers_lut);
 
 // a couple helpers
 
 // Fetch the braille value from lut with a fallback to empty str
 const get_braille = (char) => braille_lut[char] || '';
 
+// Fetch the eng char value from lut with a fallback to empty str
+const get_char = (char) => english_lut[char] || '';
+const get_num = (char) => numbers_lut[char] || '';
+
 // check if char is am ascii digit
 function isDigit(char) {
     return char >= '0' && char <= '9'; // Checks if the character is within the ASCII range for digits
+}
+
+// iterate all characters and check for non o or .
+// to determine if it is a non braille string
+function isBraille(input) {
+    for (const char of input) {
+        if (char!== ' ' && char !== 'O' && char !== '.') {
+            return false; // Found an invalid character
+        }
+    }
+    return true;
 }
 
 // get the args
 const args = process.argv.slice(2);
 // console.log('Arguments:', args);
 
-let str_result = '';
+function convert_args_to_braille() {
+    let str_result = '';
+    for (arg of args) {
+        // keep track of number runs
+        let wasLastADigit = false;
+        for(c of arg) {
+            let lowerchar = c.toLowerCase();
+            // console.log(lowerchar);
 
-for (arg of args) {
-    // keep track of number runs
-    let wasLastADigit = false;
-    for(c of arg) {
-        let lowerchar = c.toLowerCase();
-        // console.log(lowerchar);
+            if(isDigit(c) && !wasLastADigit) {
+                wasLastADigit = true;
+                str_result += braille_lut['NUMBER'];
+            } else if(c === ' ') {
+                // end the digit run
+                // to allow another number cmd
+                wasLastADigit = false;
+            }
 
-        if(isDigit(c) && !wasLastADigit) {
-            wasLastADigit = true;
-            str_result += braille_lut['NUMBER'];
-        } else if(c === ' ') {
-            // end the digit run
-            // to allow another number cmd
-            wasLastADigit = false;
+            // if lowerchar and char are diff
+            // then char is uppercase
+            if(lowerchar!==c) {
+                str_result+=braille_lut['CAPITAL'];
+            }
+
+            str_result += get_braille(lowerchar)
         }
-
-        // if lowerchar and char are diff
-        // then char is uppercase
-        if(lowerchar!==c) {
-            str_result+=braille_lut['CAPITAL'];
-        }
-
-        str_result += get_braille(lowerchar)
+    
     }
-   
+    return str_result;
 }
-console.log(str_result);
+
+function convert_args_to_english() {
+    let str_result = '';
+    for (arg of args) {
+        let nextIsNumber = false;
+        let nextIsCapital = false;
+        // iterate over the braille string in chunks
+        for (let i = 0; i < arg.length; i += 6) {
+            const section = arg.slice(i, i + 6);
+            
+            let c = get_char(section);
+            
+            // console.log(c, ' ', section);
+
+            // parse number command
+            if(c==='NUMBER') {
+                console.log('number')
+                nextIsNumber = true;
+                continue;
+            } else if(nextIsNumber && c !== ' ') {
+                let num = get_num(section);
+
+                console.log('num ', section);
+
+                str_result += num;
+                continue;
+                
+            } else {
+                nextIsNumber = false;
+            }
+
+            if(c == 'CAPITAL') {
+                nextIsCapital = true;
+                continue;
+            }
+
+            if(nextIsCapital) {
+                c = c.toUpperCase();
+                nextIsCapital = false;
+            }
+
+            str_result += c;
+        }
+    }
+    return str_result;
+}
+
+// check if the input is in english or braille
+// does not handle trailing or prefixed space characters
+// or other characters dirtying the input
+let is_braille = isBraille(args.join(''));
+
+
+let translated = is_braille ? convert_args_to_english() : convert_args_to_braille();
+
+//output answer
+console.log(translated);
