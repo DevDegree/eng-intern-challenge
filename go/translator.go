601e483e1ea6f765c75a5720c9212abe5e2ed121
Bokang Mogomotsi <bokangkeith.mogomotsi@gmail.com>
2024-10-01 11:28:29 -0400

    Main translation section
    Chomp through the input and translate it to braille or latin, depending on what the string is. Tests work!

--- go/translator.go
+++ go/translator.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"solution/helpers"
+	"solution/lib"
+	"solution/mappers"
+	"strings"
+)
+
+func main() {
+	// assuming an argument will always be provided
+	argsWithoutProg := os.Args[1:]
+	var translatedWords []string
+	for _, word := range argsWithoutProg {
+		translation, err := lib.Translate(word)
+		if err != nil {
+			log.Printf("[ERROR] Could not translate %s, got %v", word, err)
+		}
+		translatedWords = append(translatedWords, translation)
+	}
+	space := " "
+	if helpers.IsBraille(translatedWords[0]) {
+		space = mappers.BRAILLE_SPACE
+	}
+	finalTranslation := strings.Join(translatedWords, space)
+	fmt.Printf("%s", finalTranslation)
+
+}
