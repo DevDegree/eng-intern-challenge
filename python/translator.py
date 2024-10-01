d6de2b6438476eca9c3a0c4124b3fc715384f23a
Negar Sadr <negar.sadr@ubc.ca>
2024-10-01 14:07:47 -0700

    added the braille to english function

--- python/translator.py
+++ python/translator.py
@@ -1,7 +1,7 @@
 import sys
 
-# Dictionary (alphabet, numbers, special chars)
-alphabet_dict = {
+# Define mappings
+alphabet_to_braille = {
     'a': 'O.....',
     'b': 'O.O...',
     'c': 'OO....',
@@ -36,7 +36,7 @@ special_dict = {
     'capital' : '.....O'
 }
 
-numbers_dict = {
+number_to_braille = {
     "0": ".OOO..",
     "1": "O.....",
     "2": "O.O...",
@@ -49,9 +49,8 @@ numbers_dict = {
     "9": ".OO...",
 }
 
-#
-braille_to_alphabet = {v: k for k, v in alphabet_dict.items()}
-braille_to_numbers = {v: k for k, v in numbers_dict.items()}
+braille_to_alphabet = {v: k for k, v in alphabet_to_braille.items()}
+braille_to_number = {v: k for k, v in number_to_braille.items()}
 
 def is_braille(str):
     """
@@ -72,8 +71,61 @@ def is_braille(str):
     return all(c in {'O', '.'} for c in str)
 
 
-# Function to translate Braille to English
+
+def braille_to_english(str):
+    """
+    Convert a Braille string to English text.
+
+    :param str: A string representing the Braille input, where each character is represented by a 6-digit Braille code.
+    :return: A string containing the translated English text.
+    """
+
+    length = len(str)
+    english_out = ""
+    is_number = False
+    for i in range(0, len(str), 6):
+        braille_char = str[i:i + 6]
+        if braille_char == str['space']:
+            english_out += ' '
+            is_number = False
+        elif braille_char == str['capital']:
+            i += 6
+            braille_char = str[i:i + 6]
+            english_out += braille_to_alphabet.get(braille_char, '?').upper()
+        elif braille_char == special_dict['number']:
+            is_number = True
+        else:
+            if is_number:
+                english_out += braille_to_number.get(braille_char, '?')
+            else:
+                english_out += braille_to_alphabet.get(braille_char, '?')
+    return english_out
 
 
 
-# Function to translate English to Braille
+def english_to_braille(str):
+    """
+    Convert a Braille string to English text.
+
+    :param str: A string representing the Braille input, where each character is represented by a 6-digit Braille code.
+    :return: A string containing the translated English text.
+    """
+
+
+def main():
+    # Raise error if lenght of input less than 2
+    if len(sys.argv) < 2:
+        print('Error: Not sufficient argument to translate')
+        sys.exit(1)
+
+    input_str = sys.argv[1]
+
+    if is_braille(input_str):
+        print(braille_to_english(input_str))
+    else:
+        print(english_to_braille(input_str))
+
+
+if __name__ == "__main__":
+    main()
+
