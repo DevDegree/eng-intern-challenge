4d0225fc766ac18df169e46c8c6618c8d17c6986
Pazz0 <massimoscanga@hotmail.com>
2024-10-02 14:19:37 -0400

    Eng to braille function

--- python/translator.py
+++ python/translator.py
@@ -16,8 +16,28 @@ braille_alphabet = {
 english_alphabet = {v: k for k, v in braille_alphabet.items()}
 
 # Function for Eng to Braille
-def
+def english_to_braille(text):
+    """Translate English to Braille."""
+    braille = []
+    number_mode = False
+
+    for char in text:
+        if char.isupper():  # Handle uppercase letters
+            braille.extend([braille_alphabet['cap'], braille_alphabet[char.lower()]])
+        elif char.isdigit():  # Handle numbers with number indicator
+            braille.append(braille_alphabet['num'] if not number_mode else braille_alphabet[char])
+            number_mode = True
+        else:  # Handle letters and spaces
+            braille.append(braille_alphabet.get(char, '......'))
+            number_mode = False
+
+    return ''.join(braille)
 
 # Function for Braille to Eng
 
 # Detect Language input and choose correct translation function
+def main():
+
+# Run main function
+if __name__ == "__main__":
+    main() 
