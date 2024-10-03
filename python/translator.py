20776004a4bd4778852a7aaa4983466bce0839eb
Kelly Morilla-Ugaz <morilla-ugaz.kelly@courrier.uqam.ca>
2024-10-03 16:34:35 -0400

    Add string outputs

--- python/translator.py
+++ python/translator.py
@@ -7,27 +7,6 @@ DIGIT_INDICATOR = ".O.OOO"
 BRAILLE_LETTERS = ["O.....", "O.O...", "OO....", "OO.O..", "O..O..", "OOO...", "OOOO..", "O.OO..", ".OO...", ".OOO..", "O...O.", "O.O.O.", "OO..O.", "OO.OO.", "O..OO.", "OOO.O.", "OOOOO.", "O.OOO.", ".OO.O.", ".OOOO.", "O...OO", "O.O.OO", ".OOO.O", "OO..OO", "OO.OOO", "O..OOO"] # a-z
 BRAILLE_NUMBERS = [".OOO..", "O.....", "O.O...", "OO....", "OO.O..", "O..O..", "OOO...", "OOOO..", "O.OO..", ".OO..."] # 0-9
 
-def convert_to_braille(english_text):
-    """
-    Convert english text to braille
-    """
-    number_mode = False
-    for c in english_text:
-        if c.isspace():
-            print(SPACE, end = "")
-            number_mode = False
-        elif c.isalpha():
-            if c.isupper():
-                print(CAPITAL_INDICATOR, end = "")
-                c = c.lower()
-            print(BRAILLE_LETTERS[st.ascii_lowercase.index(c)], end = "")
-            number_mode = False
-        elif c.isdigit():
-            if not number_mode:
-                print(DIGIT_INDICATOR, end="")
-                number_mode = True
-            print(BRAILLE_NUMBERS[int(c)], end = "")
-
 def is_braille(s):
     """
     Check if the input string is a valid braille string
@@ -45,6 +24,7 @@ def convert_to_english(braille_text):
     """
     Convert braille text to english
     """
+    output = ""
     braille_chars = [braille_text[i:i+6] for i in range(0, len(braille_text), 6)]
     number_mode = False
     capital_mode = False
@@ -55,30 +35,54 @@ def convert_to_english(braille_text):
             number_mode = True
             capital_mode = False
         elif b == SPACE:
-            print(" ", end="")
+            output += " "
             number_mode = False
             capital_mode = False
         elif number_mode:
-            print(BRAILLE_NUMBERS.index(str(b)), end="")
+            output += str(BRAILLE_NUMBERS.index(str(b)))
             capital_mode = False
         elif capital_mode:
-            print(st.ascii_uppercase[BRAILLE_LETTERS.index(b)].upper(), end="")
+            output += st.ascii_uppercase[BRAILLE_LETTERS.index(b)].upper()
             capital_mode = False 
         else :
-            print(st.ascii_lowercase[BRAILLE_LETTERS.index(b)], end="")
+            output += st.ascii_lowercase[BRAILLE_LETTERS.index(b)]
             number_mode = False
             capital_mode = False
+    return output
+
+def convert_to_braille(english_text):
+    """
+    Convert english text to braille
+    """
+    output = ""
+    number_mode = False
+    for c in english_text:
+        if c.isspace():
+            output += SPACE
+            number_mode = False
+        elif c.isalpha():
+            if c.isupper():
+                output += CAPITAL_INDICATOR
+                c = c.lower()
+            output += BRAILLE_LETTERS[st.ascii_lowercase.index(c)]
+            number_mode = False
+        elif c.isdigit():
+            if not number_mode:
+                output += DIGIT_INDICATOR
+                number_mode = True
+            output += BRAILLE_NUMBERS[int(c)]
+    return output
 
 def main():
     if len(sys.argv) < 2:
         sys.exit(1)
     input = ' '.join(sys.argv[1:])
-   
+    output = ""
     if is_alphanumeric(input): 
-        convert_to_braille(input)
+        output = convert_to_braille(input)
     elif is_braille(input.replace(' ', '')):
-        convert_to_english(input.replace(' ', ''))
-    print()
+        output = convert_to_english(input.replace(' ', ''))
+    print(output)
     return
 
 if __name__ == '__main__':
