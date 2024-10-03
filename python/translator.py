3f588ab0390ea484d484e2b29215511531a41c89
Daniel Kamenetsky <20dk19@queensu.ca>
2024-10-03 18:54:22 -0400

    Initial commit: Add BrailleTranslator class and pseudocode for each required dictionaries, input checker, and translating functions.

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,89 @@
 
+#Approach:
+#Two functions to create the braille-english dictionaries
+#One function to determine if braille or english
+#Two functions to translate
+class BrailleTranslator:
+    def __init__(self):
+        """
+        Initialize the translator and create dictionaries
+        """
+
+
+    def braille_to_english_dict():
+        """
+        Create a dictionary matching braille to english letters, numbers, spaces and specific rules 
+        """
+        return
+
+    def english_to_braille_dict():
+        """
+        Create a dictionary matching english letters, numbers, spaces and specific rules to braille.
+        """
+        return
+
+
+    def string_to_translate():
+        """
+        If string contains symbol or space that is not 'O' or '.' then english, else it is braille.
+        """
+        return
+
+    def translate_braille():
+        """
+        Pattern match every 6 symbols. If capital or first number; prefix with relevant 6 symbols
+        otherwise 6 symbols for letter, number, space or punctuation.
+        """
+        return 
+
+
+    def translate_english():
+        """
+        pattern match every letter, number, space and other symbols to the braille configuration.
+        include prefix for capital letters and first number. 
+        """
+        return 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
