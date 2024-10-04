3797291fffb90dd907e2468e8f96415d4cf28514
Christina <chrisleebear@jadens-mbp-2.home>
2024-10-03 22:37:37 -0400

    init commit

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,161 @@
+# Author: Christina N
 
+braille_letter_dict = {
+
+'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..',
+'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..',
+'k':'O...O.', 'l': 'O.O.O.', 'm': 'O..O.', 'n': 'OO.OO.', 'o': 'O..OO.',
+'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.',
+'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO', 'y': 'OO.OOO',
+'z': 'O..OOO', '.': '..O...', '!': '..OOO.', ':': '..OO..', '-': '....OO',
+'/': '.OO..O.', '<': '.O..O.', '>': 'O.O..O', '(': 'O.O..O', ')': '.O.OO.',
+' ': '......',
+    'O.....': 'a',
+    'O.O...': 'b',
+    'OO....': 'c',
+    'OO.O..': 'd',
+    'O..O..': 'e',
+    'OOO...': 'f',
+    'OOOO..': 'g',
+    'O.OO..': 'h',
+    '.OO...': 'i',
+    '.OOO..': 'j',
+    'O...O.': 'k',
+    'O.O.O.': 'l',
+    'O..O.': 'm',
+    'OO.OO.': 'n',
+    'O..OO.': 'o',
+    'OOO.O.': 'p',
+    'OOOOO.': 'q',
+    'O.OOO.': 'r',
+    '.OO.O.': 's',
+    '.OOOO.': 't',
+    'O...OO': 'u',
+    'O.O.OO': 'v',
+    '.OOO.O': 'w',
+    'OO..OO': 'x',
+    'OO.OOO': 'y',
+    'O..OOO': 'z',
+    '..O...': '.',
+    '..OOO.': '!',
+    '..OO..': ':',
+    '....OO': '-',
+    '.OO..O.': '/',
+    '.O..O.': '<',
+    'O.O..O': '>',
+    'O.O..O': '(',
+    '.O.OO.': ')',
+    '......': ' ',
+}
+
+braille_number_dict = {
+
+'1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..', '5': 'O..O..',
+'6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...', '0': '.OOO..', 
+    'O.....': '1',
+    'O.O...': '2',
+    'OO....': '3',
+    'OO.O..': '4',
+    'O..O..': '5',
+    'OOO...': '6',
+    'OOOO..': '7',
+    'O.OO..': '8',
+    '.OO...': '9',
+    '.OOO..': '0'
+
+}
+
+# 
+def braille_tokenizer(string):
+    token_list = []
+    token = ''
+    counter = 0
+
+    for i in range(len(string)):
+        if counter == 6:
+            counter = 0
+            token_list.append(token)
+            token = ''
+        token += string[i]
+        counter += 1
+
+    if token:
+        token_list.append(token)
+    return token_list
+
+#'capital': '.....0', 'decimal': '.0...0', 'number': '.0.000', 
+
+def isBraille(string) :
+    for i in string:
+        if i != 'O' and i != '.':
+            return False
+    return True
+
+def translator(string):
+    translated = []
+    isCap = False
+    isNum = False
+
+    if isBraille(string): # braille
+        # read from list
+        token_list = braille_tokenizer(string)
+        # check if its a number, decimal or capital
+        for i in range(len(token_list)):
+            if token_list[i] == '.O.OOO': # then NUMBER
+                # check the trans
+                isNum = True
+                continue
+            elif token_list[i] == '.O...O': # then DECIMAL
+                continue
+            elif token_list[i] == '.....O': # then CAPITAL
+                isCap = True
+                continue
+            elif token_list[i] in braille_letter_dict and not isNum: # then LETTER
+                    translationToken = braille_letter_dict[token_list[i]]
+                    # check
+                    if isCap:
+                        translated.append(translationToken.upper())
+                        isCap = False
+                    else: 
+                        translated.append(translationToken)
+            elif isNum :
+                translationToken = braille_number_dict[token_list[i]] 
+                translated.append(translationToken)
+
+        # print translation
+        print("Translation:", ''.join(translated))
+    else: # english
+        for char in string:
+            # check if capital
+            if char.isupper():
+                translated.append('.....O') # capital
+                isCap = True
+            elif char.isdigit() :
+                if not isNum:
+                    translated.append('.O.OOO') 
+                    isNum = True
+
+            if char.lower() in braille_letter_dict: 
+                    translated.append(braille_letter_dict[char.lower()])
+            elif isNum and char in braille_number_dict:
+                translated.append(braille_number_dict[char])
+            else:
+                translated.append('') # if char not found then append empty string
+
+        print("Translation:", ''.join(translated))
+
+# translator('Hello world')  # Example English input
+# translator('.....OO.OO..O..O..O.O.O.O.O.O.O..OO........OOO.OO..OO.O.OOO.O.O.O.OO.O..')
+# translator('42')
+# translator('Abc 123')
+# translator('.....OO.....O.O...OO...........O.OOOO.....O.O...OO....')
+
+def main():
+    # Get input from command line arguments
+    input_strings = sys.argv[1:]  # Ignore the first argument which is the script name
+    for input_string in input_strings:
+        output = translator(input_string)  # Call the translator function
+        print(output)  # Print the output
+
+if __name__ == '__main__':
+    main()
